3cfb620b02f492f9f688e6b78fb05771697f1a0f
Lorenzo Viliani <lorenzo.viliani@cern.ch>
2015-11-10 10:32:19 +0100

    Top

---
+++ Configurations/Top/samples.py
@@ -0,0 +1,115 @@
+# samples
+
+#samples = {}
+    
+#                    
+#samples['DY']  = {    'name': ['latino_DYJetsToLL_M-50.root', 'latino_DYJetsToLL_M-10to50.root'],     #   file name    
+                      #'weight' : 'baseW*puW',              #   weight/cut 
+                      #'weights': ['GEN_weight_SM/abs(GEN_weight_SM) / 0.670032', 'GEN_weight_SM/abs(GEN_weight_SM) / 0.72760']                       #   additional cuts file dependent
+                      ##'weights': ['0.66998', '0.72760'] ,                      #   additional cuts file dependent
+                  #}
+
+#samples['DY']  = {    'name': ['latino_DYJetsToLL_M-10to50.root'],     #   file name    
+#                      'weight' : 'baseW*puW',              #   weight/cut 
+                      #'weights': ['GEN_weight_SM/abs(GEN_weight_SM) / 0.670032', 'GEN_weight_SM/abs(GEN_weight_SM) / 0.72760']                       #   additional cuts file dependent
+                      #'weights': ['0.66998', '0.72760'] ,                      #   additional cuts file dependent
+#                  }
+
+#samples['ttbar'] = {   'name': ['latino_TTJets.root'],         #   file name    
+                       #'weight' : 'baseW',                     #   weight/cut 
+                       ##'weights': ['0.33166']             #   additional cuts file dependent --> e.g. from +/- weights in MC
+                       #'weights': ['GEN_weight_SM/abs(GEN_weight_SM) * 0.33166']             #   additional cuts file dependent --> e.g. from +/- weights in MC
+                   #}
+
+
+
+#samples['Wjets']  = {    'name': ['latino_WJetsToLNu.root'],     #   file name    
+#                      'weight' : 'baseW*puW',              #   weight/cut 
+#                      'weights': ['GEN_weight_SM/abs(GEN_weight_SM) / 0.683927 * 61526.7 / 20508.9']                       #   additional cuts file dependent
+#                  }
+
+
+#samples['FakeQCD']  = {    'name': ['latino_QCD_Pt-30to50_EMEnriched.root', 'latino_QCD_Pt-30toInf_DoubleEMEnriched.root', 'latino_QCD_Pt-50to80_EMEnriched.root', 'latino_QCD_Pt-15to20_MuEnrichedPt5.root'],     #   file name    
+                      #'weight' : 'baseW*puW',              #   weight/cut 
+                      #'weights': ['-', '-', '-', '-']                       #   additional cuts file dependent
+                  #}
+
+
+#samples['ttbar'] = {   'name': ['latino_TT.root'],         #   file name    
+                       #'weight' : 'baseW',                 #   weight/cut 
+                       ##'weights': ['0.33166']             #   additional cuts file dependent --> e.g. from +/- weights in MC
+                       #'weights': ['1']                    #   additional cuts file dependent --> e.g. from +/- weights in MC
+                   #}
+
+samples['ttbar'] = {   'name': ['latino_TT_skim.root'],          
+                       'weight' : 'baseW*puW',                
+                       #'weights': ['GEN_weight_SM/abs(GEN_weight_SM) / 0.331907'],           
+                       'weights': ['1']                   
+                   }
+
+#samples['top'] = {   'name': ['latino_ST_tW_top.root', 'latino_ST_tW_antitop.root', 'latino_ST_t-channel.root'],   
+                       #'weight' : 'baseW*puW',                
+                       #'weights': ['1', '1', 'GEN_weight_SM/abs(GEN_weight_SM) / 0.215131' ]            
+                   #}
+
+
+#samples['top'] = {   'name': ['latino_ST_t-channel.root'],   
+#                       'weight' : 'baseW*puW',                
+#                       'weights': ['GEN_weight_SM/abs(GEN_weight_SM) / 0.215131' ]            
+#                   }
+
+
+#samples['WW']  = {    'name': ['latino_WWTo2L2Nu.root'],      
+                      #'weight' : 'baseW*puW',          
+                      #'weights': ['1']            
+                  #}
+
+#samples['WZ']  = {    'name': ['latino_WZ.root'],      
+#                      'weight' : '0.06630*puW',          
+#                      'weights': ['1']            
+#                  }
+
+#samples['ZZ']  = {    'name': ['latino_ZZ.root'],      
+                      #'weight' : '0.03184*puW',          
+                      #'weights': ['1']            
+                  #}
+
+
+
+samples['ggH']  = {    'name': ['latino_GluGluHToWWTo2L2Nu_M125_skim.root'],      
+                      'weight' : 'baseW*puW',          
+                      'weights': ['1']            
+                  }
+
+
+
+
+###########################################
+###########################################
+###########################################
+
+
+samples['DATA']  = {    'name': ['latino_Run2015D_05Oct2015_MuonEG_0.root'],
+                      'weight' : 'trigger',                              
+                      'weights': ['1']            
+                   }
+
+#samples['DATA']  = {   'name': ['../../data/latino_DoubleEG.root', 
+                                #'../../data/latino_DoubleMuon.root',
+                                #'../../data/latino_SingleElectron.root',
+                                #'../../data/latino_SingleMuon.root',
+                                #'../../data/latino_MuonEG.root'],      
+                       #'weight' : 'trigger',
+                  #}
+
+#samples['DATA']  = {   'name': ['dataB/latino_DoubleEG.root', 'dataB/latino_MuonEG.root', 'dataB/latino_SingleElectron.root', 'dataB/latino_SingleMu.root'],      
+                       #'weight' : 'trigger',          
+                  #}
+
+
+
+# --inputDir=/media/data/amassiro/LatinoTrees/50ns/
+
+
+
+
---
+++ Configurations/Top/variables.py
@@ -0,0 +1,145 @@
+# variables
+
+#variables = {}
+    
+   
+variables['events']  = {   'name': '1',      
+                        'range' : (1,0,2),  
+                        'xaxis' : 'events', 
+                         'fold' : 3
+                        }
+    
+variables['nvtx']  = {   'name': 'nvtx',      
+                        'range' : (40,0,40),  
+                        'xaxis' : 'nvtx', 
+                         'fold' : 3
+                        }
+                        
+variables['mll']  = {   'name': 'mll',            #   variable name    
+                        'range' : (100,0,200),    #   variable range
+                        'xaxis' : 'm_{ll} [GeV]',  #   x axis name
+                         'fold' : 3
+                        }
+
+#variables['dphill']  = {   'name': 'abs(dphill)',     
+                        #'range' : (20,0,3.14),   
+                        #'xaxis' : '#Delta#phi_{ll}',
+                         #'fold' : 3
+                        #}
+
+#variables['ptll']  = {   'name': 'ptll',     
+                        #'range' : (60,0,200),   
+                        #'xaxis' : 'p_{T}^{ll} [GeV]',
+                         #'fold' : 3
+                        #}
+
+#variables['pt1']  = {   'name': 'std_vector_lepton_pt[0]',     
+                        #'range' : (100,0,200),   
+                        #'xaxis' : 'p_{T} 1st lep',
+                        #'fold'  : 3                         
+                        #}
+
+#variables['pt2']  = {   'name': 'std_vector_lepton_pt[1]',     
+                        #'range' : (50,0,200),   
+                        #'xaxis' : 'p_{T} 2nd lep',
+                        #'fold'  : 3                         
+                        #}
+
+#variables['pt3']  = {   'name': 'std_vector_lepton_pt[2]',     
+                        #'range' : (10,0,50),   
+                        #'xaxis' : 'p_{T} 3rd lep',
+                        #'fold'  : 3                         
+                        #}
+
+#variables['eta1']  = {  'name': 'std_vector_lepton_eta[0]',     
+                        #'range' : (100,-3,3),   
+                        #'xaxis' : '#eta 1st lep',
+                        #'fold'  : 3                         
+                        #}
+
+#variables['eta2']  = {  'name': 'std_vector_lepton_eta[1]',     
+                        #'range' : (100,-3,3),   
+                        #'xaxis' : '#eta 2nd lep',
+                        #'fold'  : 3                         
+                        #}
+
+#variables['taupt1']  = {'name': 'std_vector_tau_pt[0]',     
+                        #'range' : (100,0,200),   
+                        #'xaxis' : 'p_{T} 1st tau',
+                        #'fold'  : 3                         
+                        #}
+
+
+
+
+variables['csvv2ivf_1']  = { 
+                        'name': 'std_vector_jet_csvv2ivf[0]',     
+                        'range' : (100,0,1),   
+                        'xaxis' : 'csvv2ivf jet 1st',
+                        'fold'  : 3                         
+                        }
+
+
+
+variables['csvv2ivf_2']  = { 
+                        'name': 'std_vector_jet_csvv2ivf[1]',     
+                        'range' : (100,0,1),   
+                        'xaxis' : 'csvv2ivf jet 2nd',
+                        'fold'  : 3                         
+                        }
+
+
+#variables['pfmet']  = { 
+                        #'name': 'pfType1Met',     
+                        #'range' : (100,0,100),   
+                        #'xaxis' : 'pfmet [GeV]',
+                        #'fold'  : 3                         
+                        #}
+
+
+#variables['muId1']  = { 
+                        #'name': 'std_vector_lepton_isTightMuon[0]',     
+                        #'range' : (2,0,2),   
+                        #'xaxis' : 'tight Muon Id 1st lep',
+                        #'fold'  : 0                         
+                        #}
+
+#variables['muId2']  = { 
+                        #'name': 'std_vector_lepton_isTightMuon[1]',     
+                        #'range' : (2,0,2),   
+                        #'xaxis' : 'tight Muon Id 2nd lep',
+                        #'fold'  : 0                         
+                        #}
+
+
+
+#variables['jetpt1']  = {
+                        #'name': 'std_vector_jet_pt[0]',     
+                        #'range' : (100,0,200),   
+                        #'xaxis' : 'p_{T} 1st jet'
+                        #}
+
+
+#variables['njet']  = {
+                        #'name': 'njet',     
+                        #'range' : (20,0,20),   
+                        #'xaxis' : 'Number of jets',
+                        #'fold' : 2   # 0 = not fold (default), 1 = fold underflowbin, 2 = fold overflow bin, 3 = fold underflow and overflow
+                        #}
+
+
+
+
+#variables['jetpt1']  = {
+                        #'name': 'std_vector_jet_pt[0]',     
+                        #'range' : (20,0,200),   
+                        #'xaxis' : 'p_{T} 1st jet',
+                        #'fold' : 2   # 0 = not fold (default), 1 = fold underflowbin, 2 = fold overflow bin, 3 = fold underflow and overflow
+                        #}
+
+#variables['jetpt2']  = {
+                        #'name': 'std_vector_jet_pt[1]',     
+                        #'range' : (20,0,200),   
+                        #'xaxis' : 'p_{T} 2nd jet',
+                        #'fold' : 2   # 0 = not fold (default), 1 = fold underflowbin, 2 = fold overflow bin, 3 = fold underflow and overflow
+                        #}
