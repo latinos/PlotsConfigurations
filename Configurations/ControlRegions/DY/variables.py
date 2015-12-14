64511f206e6d5454c4cfa49ab4c1afd6f9ce9e74
xjanssen <xavier.janssen@cern.ch>
2015-12-14 21:56:36 +0100

    DY plots

--- Configurations/ControlRegions/DY/variables.py
+++ Configurations/ControlRegions/DY/variables.py
@@ -15,15 +15,63 @@ variables['nvtx']  = {   'name': 'nvtx',
                         'xaxis' : 'nvtx', 
                          'fold' : 0
                         }
-                        
+
+variables['pt1']  = {   'name': 'std_vector_lepton_pt[0]',     
+                        'range' : (100,0,200),   
+                        'xaxis' : 'p_{T} 1st lep',
+                        'fold'  : 3                         
+                        }
+
+variables['pt2']  = {   'name': 'std_vector_lepton_pt[1]',     
+                        'range' : (50,0,200),   
+                        'xaxis' : 'p_{T} 2nd lep',
+                        'fold'  : 3                         
+                        }
+
+variables['eta1']  = {  'name': 'std_vector_lepton_eta[0]',     
+                        'range' : (100,-3.2,3.2),   
+                        'xaxis' : '#eta 1st lep',
+                        'fold'  : 3                         
+                        }
+
+variables['eta2']  = {  'name': 'std_vector_lepton_eta[1]',     
+                        'range' : (100,-3.2,3.2),   
+                        'xaxis' : '#eta 2nd lep',
+                        'fold'  : 3                         
+                        }
+                       
+variables['phi1']  = {  'name': 'std_vector_lepton_phi[0]',
+                        'range' : (100,-3.2,3.2),
+                        'xaxis' : '#phi 1st lep',
+                        'fold'  : 3
+                        }
+
+variables['phi2']  = {  'name': 'std_vector_lepton_phi[1]',
+                        'range' : (100,-3.2,3.2),
+                        'xaxis' : '#phi 2nd lep',
+                        'fold'  : 3
+                        }
+
+variables['dphill']  = {   'name': 'abs(dphill)',     
+                        'range' : (20,0,3.14),   
+                        'xaxis' : '#Delta#phi_{ll}',
+                         'fold' : 3
+                        }
+
+variables['ptll']  = {   'name': 'ptll',     
+                        'range' : (60,0,200),   
+                        'xaxis' : 'p_{T}^{ll} [GeV]',
+                         'fold' : 3
+                        }
+ 
 variables['mll']  = {   'name': 'mll',            #   variable name    
-                        'range' : (20,50,200),    #   variable range
+                        'range' : (50, 0,200),    #   variable range
                         'xaxis' : 'm_{ll} [GeV]',  #   x axis name
                          'fold' : 0
                         }
                         
 variables['mth']  = {   'name': 'mth',            #   variable name    
-                        'range' : (20,50,200),    #   variable range
+                        'range' : (50, 0,200),    #   variable range
                         'xaxis' : 'm_{T}^{H} [GeV]',  #   x axis name
                         'fold' : 0
                         }
@@ -59,4 +107,44 @@ variables['njet']  = {  'name': 'njet',
                         'fold' : 0
                         }
 
+variables['jetpt1']  = {
+                        'name': 'std_vector_jet_pt[0]',     
+                        'range' : (20,0,200),   
+                        'xaxis' : 'p_{T} 1st jet',
+                        'fold' : 2   # 0 = not fold (default), 1 = fold underflowbin, 2 = fold overflow bin, 3 = fold underflow and overflow
+                        }
 
+variables['jetpt2']  = {
+                        'name': 'std_vector_jet_pt[1]',     
+                        'range' : (20,0,200),   
+                        'xaxis' : 'p_{T} 2nd jet',
+                        'fold' : 2   # 0 = not fold (default), 1 = fold underflowbin, 2 = fold overflow bin, 3 = fold underflow and overflow
+                        }
+
+variables['jeteta1']  = {
+                        'name': 'std_vector_jet_eta[0]',
+                        'range' : (100,-5.2,5.2),
+                        'xaxis' : '#eta 1st jet',
+                        'fold' : 2   # 0 = not fold (default), 1 = fold underflowbin, 2 = fold overflow bin, 3 = fold underflow and overflow
+                        }
+
+variables['jeteta2']  = {
+                        'name': 'std_vector_jet_eta[1]',
+                        'range' : (100,-5.2,5.2),
+                        'xaxis' : '#eta 2nd jet',
+                        'fold' : 2   # 0 = not fold (default), 1 = fold underflowbin, 2 = fold overflow bin, 3 = fold underflow and overflow
+                        }
+
+variables['jetphi1']  = {
+                        'name': 'std_vector_jet_phi[0]',
+                        'range' : (100,-3.2,3.2),
+                        'xaxis' : '#phi 1st jet',
+                        'fold' : 2   # 0 = not fold (default), 1 = fold underflowbin, 2 = fold overflow bin, 3 = fold underflow and overflow
+                        }
+
+variables['jetphi2']  = {
+                        'name': 'std_vector_jet_phi[1]',
+                        'range' : (100,-3.2,3.2),
+                        'xaxis' : '#phi 2nd jet',
+                        'fold' : 2   # 0 = not fold (default), 1 = fold underflowbin, 2 = fold overflow bin, 3 = fold underflow and overflow
+                        }
