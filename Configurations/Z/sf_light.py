d632cebfab68d3bf9e7bf745a270780a64c53def
Piergiulio <piergiulio.lenzi@cern.ch>
2016-01-04 12:59:18 +0100

    added config files for Z

--- Configurations/Z/sf_light.py
+++ Configurations/Z/sf_light.py
@@ -5,31 +5,32 @@ import math
 
 filein = TFile("rootFile/plots_Z.root")
 
-plotData = filein.Get("Zjet/csvv2ivf_twobins_1/histo_DATA")
-plotData2 = filein.Get("Zjet/csvv2ivf__twobins_2/histo_DATA")
-plotData.Add(plotData2)
+def printSF(cutname):
+  plotData = filein.Get(cutname+"/csvv2ivf_twobins_1/histo_DATA")
+  plotData2 = filein.Get(cutname+"/csvv2ivf_twobins_2/histo_DATA")
+  plotData.Add(plotData2)
 
 
-plotDY = filein.Get("Zjet/csvv2ivf_twobins_1/histo_DY")
-plotDY2 = filein.Get("Zjet/csvv2ivf__twobins_2/histo_DY")
+  plotDY = filein.Get(cutname+"/csvv2ivf_twobins_1/histo_DY")
+  plotDY2 = filein.Get(cutname+"/csvv2ivf_twobins_2/histo_DY")
 
-plotDY.Add(plotDY2)
+  plotDY.Add(plotDY2)
 
 
-#plotData.Draw()
-#plotDY.Draw("sames")
+  effMC = plotDY.GetBinContent(2)/plotDY.Integral()
 
-effMC = plotDY.GetBinContent(2)/plotDY.Integral()
+  effData = plotData.GetBinContent(2)/plotData.Integral()
 
-effData = plotData.GetBinContent(2)/plotData.Integral()
+  errData = math.sqrt(effData*(1.-effData)*plotData.Integral()) / plotData.Integral()
 
-errData = math.sqrt(effData*(1.-effData)*plotData.Integral()) / plotData.Integral()
+  print cutname, "effMC   ", effMC
+  print cutname, "effData ", effData, "+/-", errData
 
-print "effMC   ", effMC
-print "effData ", effData, "+/-", errData
+  sf = effData/effMC
+  sferr = errData/effMC
 
-sf = effData/effMC
-sferr = errData/effMC
+  print cutname, "SF      ", sf, "+/-", sferr
 
-print "SF      ", sf, "+/-", sferr
-
+printSF("Zjets")
+print "*"*50
+printSF("Z0jet")
