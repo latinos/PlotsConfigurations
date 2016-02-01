435a34ef64920605c0c5496fa91d8135f97c682f
Lorenzo Viliani <lorenzo.viliani@cern.ch>
2016-02-01 17:07:14 +0100

    Update on top CR

--- Configurations/ControlRegions/Top1jet/mkAllPlots.sh
+++ Configurations/ControlRegions/Top1jet/mkAllPlots.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#mkPlot.py --pycfg=configuration_NoSF.py --inputFile=./rootFile/plots_TopNoSF.root
-#mkPlot.py --pycfg=configuration_BPogSF.py --inputFile=./rootFile/plots_TopSF.root
-#mkPlot.py --pycfg=configuration_BPogSFDown.py --inputFile=./rootFile/plots_TopSFDown.root
-#mkPlot.py --pycfg=configuration_BPogSFUp.py --inputFile=./rootFile/plots_TopSFUp.root
-mkPlot.py --pycfg=configuration_DD.py --inputFile=./rootFile/plots_TopDD.root
+mkPlot.py --pycfg=configuration_NoSF.py --inputFile=./rootFile/plots_TopNoSF.root
+mkPlot.py --pycfg=configuration_BPogSF.py --inputFile=./rootFile/plots_TopSF.root
+mkPlot.py --pycfg=configuration_BPogSFDown.py --inputFile=./rootFile/plots_TopSFDown.root
+mkPlot.py --pycfg=configuration_BPogSFUp.py --inputFile=./rootFile/plots_TopSFUp.root
+#mkPlot.py --pycfg=configuration_DD.py --inputFile=./rootFile/plots_TopDD.root
