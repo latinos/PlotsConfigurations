2fbad173f44515df1e1016e39dc4a411ab561e97
amassiro <massironi.andrea@gmail.com>
2016-02-19 19:42:41 +0100

    migration to 76x

--- Configurations/ggH/samples.py
+++ Configurations/ggH/samples.py
@@ -135,6 +135,14 @@ samples['qqH_hww']  = {    'name': ['latino_VBFHToWWTo2L2Nu_M125.root'],
                            'weights': ['1']            
                   }
 
+#samples['ggZH_hww']  = {    'name': [
+                               #'latino_ggZH_HToWW_M125.root',
+                               #],      
+                           #'weight' : 'puW*baseW*bPogSF*effTrigW*std_vector_lepton_idisoW[0]*std_vector_lepton_idisoW[1]',          
+                           ##'weights': ['1']            
+                  #}
+
+
 #samples['WH_hww']  = {    'name': ['latino_HWminusJ_HToWW_M125.root', 'latino_HWplusJ_HToWW_M125.root'],      
                            #'weight' : 'puW*baseW*bPogSF*effTrigW*std_vector_lepton_idisoW[0]*std_vector_lepton_idisoW[1]*GEN_weight_SM/abs(GEN_weight_SM)',          
                   #}
