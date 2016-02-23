128de5b391b54c5c3fd80def5ccf1fb2bfd6e6c4
amassiro <massironi.andrea@gmail.com>
2016-02-23 19:05:06 +0100

    otherwise it will crash

--- Configurations/ggH/samples.py
+++ Configurations/ggH/samples.py
@@ -70,11 +70,14 @@ samples['top'] = {   'name': [
                    }
 
 
-samples['WW']  = {    'name': ['latino_WWTo2L2Nu.root', 'latino_WWTo2L2NuHerwigPS.root'],      
+samples['WW']  = {    'name': [
+                                  'latino_WWTo2L2Nu.root'
+                                #, 'latino_WWTo2L2NuHerwigPS.root'
+                                ],      
                       'weight' : 'nllW*puW*baseW*bPogSF*effTrigW*std_vector_lepton_idisoW[0]*std_vector_lepton_idisoW[1]',          
                       #'weight' : 'puW*12.178/1965200.*1000.*bPogSF*effTrigW*std_vector_lepton_idisoW[0]*std_vector_lepton_idisoW[1]',          
                       #'weights': ['abs(nllW)'] ,           
-                      'weights': ['1.000', '0.000'] ,           
+                      #'weights': ['1.000', '0.000'] ,           
                   }
 
 #samples['WW_Herwig']  = {    'name': ['latino_WWTo2L2NuHerwigPS.root'],      
@@ -149,11 +152,12 @@ samples['H_htt']  = {      'name': ['latino_GluGluHToTauTau_M125.root',
 # HWW 
 
 samples['ggH_hww']  = {    'name': [
-                               'latino_GluGluHToWWTo2L2NuPowheg_M125.root', 'latino_GluGluHToWWTo2L2NuHerwigPS_M125.root'
+                               'latino_GluGluHToWWTo2L2NuPowheg_M125.root'
+                               #, 'latino_GluGluHToWWTo2L2NuHerwigPS_M125.root'
                                #'latino_GluGluHToWWTo2L2Nu_M125.root'
                                ],      
                            'weight' : 'puW*baseW*bPogSF*effTrigW*std_vector_lepton_idisoW[0]*std_vector_lepton_idisoW[1]',          
-                           'weights': ['1', '0']            
+                           #'weights': ['1', '0']            
                   }
 
 samples['qqH_hww']  = {    'name': ['latino_VBFHToWWTo2L2Nu_M125.root'],      
