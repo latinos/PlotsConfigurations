88bcc0a864555b9aa02d54339493432f0ac51267
amassiro <massironi.andrea@gmail.com>
2016-02-25 10:33:19 +0100

    new lumi

--- Configurations/ggH/configuration.py
+++ Configurations/ggH/configuration.py
@@ -22,7 +22,8 @@ plotFile = 'plot.py'
 
 
 # luminosity to normalize to (in 1/fb)
-lumi = 2.264
+# lumi = 2.264
+lumi = 2.397
 
 # used by mkPlot to define output directory for plots
 # different from "outputDir" to do things more tidy
