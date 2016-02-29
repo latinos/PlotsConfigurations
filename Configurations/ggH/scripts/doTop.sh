840a07dc48549e29e0cc20615d1836ed5031a231
Lorenzo Viliani <lorenzo.viliani@cern.ch>
2016-02-29 11:17:01 +0100

    Top scripts update

--- Configurations/ggH/scripts/doTop.sh
+++ Configurations/ggH/scripts/doTop.sh
@@ -9,21 +9,21 @@ outputFileLH=${2}
 
 # extract SF for Top 0 jets
 echo "~~~~~~~~~~ Top 0 jets SF ~~~~~~~~~~~~~" >> ${outputFileSF}
-combine -M MaxLikelihoodFit ${inputCardCOMB} --redefineSignalPOIs Topnorm0j --freezeNuisances=r >> ${outputFileSF}
+echo `combine -M MaxLikelihoodFit ${inputCardCOMB} --redefineSignalPOIs Topnorm0j --freezeNuisances=r` >> ${outputFileSF}
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" >> ${outputFileSF}
 
 # extract SF for Top 1 jets
 echo "~~~~~~~~~~ Top 1 jet SF ~~~~~~~~~~~~~" >> ${outputFileSF}
-combine -M MaxLikelihoodFit ${inputCardCOMB} --redefineSignalPOIs Topnorm1j --freezeNuisances=r >> ${outputFileSF}
+echo `combine -M MaxLikelihoodFit ${inputCardCOMB} --redefineSignalPOIs Topnorm1j --freezeNuisances=r` >> ${outputFileSF}
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" >> ${outputFileSF}
 
 # perform the Likelihood scan for Top 0 jets
 echo "~~~~~~~~~~ Top 0 jets Likelihood SCan ~~~~~~~~~~~~~" >> ${outputFileLH}
-combine -M MultiDimFit ${inputCardCOMB} --algo=grid --points 100 --redefineSignalPOIs Topnorm0j --freezeNuisances=r --setPhysicsModelParameterRanges Topnorm0j=0.01,2 -n "LHScanTopnorm0j" >> ${outputFileLH}
+echo `combine -M MultiDimFit ${inputCardCOMB} --algo=grid --points 100 --redefineSignalPOIs Topnorm0j --freezeNuisances=r --setPhysicsModelParameterRanges Topnorm0j=0.01,2 -n "LHScanTopnorm0j"` >> ${outputFileLH}
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" >> ${outputFileLH}
 
 # perform the Likelihood scan for Top 1 jet
 echo "~~~~~~~~~~ Top 1 jet Likelihood Scan ~~~~~~~~~~~~~" >> ${outputFileLH}
-combine -M MultiDimFit ${inputCardCOMB} --algo=grid --points 100 --redefineSignalPOIs Topnorm1j --freezeNuisances=r --setPhysicsModelParameterRanges Topnorm1j=0.01,2 -n "LHScanTopnorm1j" >> ${outputFileLH}
+echo `combine -M MultiDimFit ${inputCardCOMB} --algo=grid --points 100 --redefineSignalPOIs Topnorm1j --freezeNuisances=r --setPhysicsModelParameterRanges Topnorm1j=0.01,2 -n "LHScanTopnorm1j"` >> ${outputFileLH}
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" >> ${outputFileLH}
 
