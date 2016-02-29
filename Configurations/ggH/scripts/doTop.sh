8c7e7dce5144052cb98d5953feb9e5476c01a8a4
Lorenzo Viliani <lorenzo.viliani@cern.ch>
2016-02-29 11:14:02 +0100

    Top scripts update

--- Configurations/ggH/scripts/doTop.sh
+++ Configurations/ggH/scripts/doTop.sh
@@ -2,27 +2,28 @@
 
 ### need to setup combine before
 
-inputCard=${1}
-outputFileSF=${2}
-outputFileLH=${3}
+### update combined datacard path
+inputCardCOMB="/afs/cern.ch/user/a/amassiro/public/xLatinos/Moriond2016-28Feb/hww2l2v_13TeV_top_of0j/events/datacard.txt"
+outputFileSF=${1}
+outputFileLH=${2}
 
 # extract SF for Top 0 jets
 echo "~~~~~~~~~~ Top 0 jets SF ~~~~~~~~~~~~~" >> ${outputFileSF}
-combine -M MaxLikelihoodFit ${inputCard} --redefineSignalPOIs Topnorm0j --freezeNuisances=r >> ${outputFileSF}
+combine -M MaxLikelihoodFit ${inputCardCOMB} --redefineSignalPOIs Topnorm0j --freezeNuisances=r >> ${outputFileSF}
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" >> ${outputFileSF}
 
 # extract SF for Top 1 jets
 echo "~~~~~~~~~~ Top 1 jet SF ~~~~~~~~~~~~~" >> ${outputFileSF}
-combine -M MaxLikelihoodFit ${inputCard} --redefineSignalPOIs Topnorm1j --freezeNuisances=r >> ${outputFileSF}
+combine -M MaxLikelihoodFit ${inputCardCOMB} --redefineSignalPOIs Topnorm1j --freezeNuisances=r >> ${outputFileSF}
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" >> ${outputFileSF}
 
 # perform the Likelihood scan for Top 0 jets
 echo "~~~~~~~~~~ Top 0 jets Likelihood SCan ~~~~~~~~~~~~~" >> ${outputFileLH}
-combine -M MultiDimFit ${inputCard} --algo=grid --points 100 --redefineSignalPOIs Topnorm0j --freezeNuisances=r --setPhysicsModelParameterRanges Topnorm0j=0.01,2 -n "LHScanTopnorm0j" >> ${outputFileLH}
+combine -M MultiDimFit ${inputCardCOMB} --algo=grid --points 100 --redefineSignalPOIs Topnorm0j --freezeNuisances=r --setPhysicsModelParameterRanges Topnorm0j=0.01,2 -n "LHScanTopnorm0j" >> ${outputFileLH}
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" >> ${outputFileLH}
 
 # perform the Likelihood scan for Top 1 jet
 echo "~~~~~~~~~~ Top 1 jet Likelihood Scan ~~~~~~~~~~~~~" >> ${outputFileLH}
-combine -M MultiDimFit ${inputCard} --algo=grid --points 100 --redefineSignalPOIs Topnorm1j --freezeNuisances=r --setPhysicsModelParameterRanges Topnorm1j=0.01,2 -n "LHScanTopnorm1j" >> ${outputFileLH}
+combine -M MultiDimFit ${inputCardCOMB} --algo=grid --points 100 --redefineSignalPOIs Topnorm1j --freezeNuisances=r --setPhysicsModelParameterRanges Topnorm1j=0.01,2 -n "LHScanTopnorm1j" >> ${outputFileLH}
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" >> ${outputFileLH}
 
