d127cc7354efb7ea2b5968110020e1a6a07a64a8
amassiro <massironi.andrea@gmail.com>
2016-03-08 10:47:02 +0100

    fix lumi

--- Configurations/ggH/nuisances.py
+++ Configurations/ggH/nuisances.py
@@ -7,9 +7,22 @@
 
 nuisances['lumi']  = {
                'name'  : 'lumi_13TeV', 
-               'all'   : 1 ,     # apply to all samples
+               'samples'  : {
+                   'ggH_hww'  : '1.027',
+                   'qqH_hww'  : '1.027',
+                   'WH_hww'   : '1.027',
+                   'ZH_hww'   : '1.027',
+                   'H_htt'    : '1.027',
+                   'H_hww'    : '1.027',
+                   'WH_hww'   : '1.027',
+                   #'DY'       : '1.027',
+                   'VVV'      : '1.027',
+                   'VZ'       : '1.027',
+                   #'WW'       : '1.027',
+                   #'top'      : '1.027',
+                   'Vg'       : '1.027',
+                   },
                'type'  : 'lnN',
-               'value' : '1.027' 
               }
 
 
