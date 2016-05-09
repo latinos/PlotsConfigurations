03d2f639efcbbbed312e840ab685c67e13f8ac0c
amassiro <massironi.andrea@gmail.com>
2016-05-09 18:28:48 +0200

    update

---
+++ Configurations/Differential/ggH/scripts/doGGH_doMultisignal.sh
@@ -0,0 +1,50 @@
+
+
+# cd ~/Framework/CMSSW_7_1_15/src/
+cd ~/Framework/Combine/CMSSW_7_4_7/src/
+
+eval `scramv1 runtime -sh`
+cd -
+
+cat ggH/datacards/hww2l2v_13TeV_top_of0j/events/datacard.txt.pruned.txt  | grep -v "_DY0jet_ibin_1" | grep -v "_DY1jet_ibin_1" | grep -v "_Vg_ibin_1" | grep -v "_VVV_ibin_1" &> ggH/datacards/hww2l2v_13TeV_top_of0j/events/datacard.test.txt.pruned.txt
+mv ggH/datacards/hww2l2v_13TeV_top_of0j/events/datacard.test.txt.pruned.txt ggH/datacards/hww2l2v_13TeV_top_of0j/events/datacard.txt.pruned.txt
+
+cat ggH/datacards/hww2l2v_13TeV_top_of1j/events/datacard.txt.pruned.txt  | grep -v "_DY0jet_ibin_1" | grep -v "_DY1jet_ibin_1" | grep -v "_Vg_ibin_1" | grep -v "_VVV_ibin_1" &> ggH/datacards/hww2l2v_13TeV_top_of1j/events/datacard.test.txt.pruned.txt
+mv ggH/datacards/hww2l2v_13TeV_top_of1j/events/datacard.test.txt.pruned.txt ggH/datacards/hww2l2v_13TeV_top_of1j/events/datacard.txt.pruned.txt
+
+cat ggH/datacards/hww2l2v_13TeV_dytt_of0j/events/datacard.txt.pruned.txt   | grep -v "_VVV_ibin_1"  &> ggH/datacards/hww2l2v_13TeV_dytt_of0j/events/datacard.test.txt.pruned.txt
+mv ggH/datacards/hww2l2v_13TeV_dytt_of0j/events/datacard.test.txt.pruned.txt ggH/datacards/hww2l2v_13TeV_dytt_of0j/events/datacard.txt.pruned.txt
+
+cat ggH/datacards/hww2l2v_13TeV_dytt_of1j/events/datacard.txt.pruned.txt   | grep -v "_VVV_ibin_1"  &> ggH/datacards/hww2l2v_13TeV_dytt_of1j/events/datacard.test.txt.pruned.txt
+mv ggH/datacards/hww2l2v_13TeV_dytt_of1j/events/datacard.test.txt.pruned.txt ggH/datacards/hww2l2v_13TeV_dytt_of1j/events/datacard.txt.pruned.txt
+
+
+
+
+# combine
+ 
+combineCards.py   em01j13=ggH/datacards/hww2l2v_13TeV_em_01j/mllVSmth/datacard.txt.pruned.txt \
+                  me01j13=ggH/datacards/hww2l2v_13TeV_me_01j/mllVSmth/datacard.txt.pruned.txt \
+                  of0j13Top=ggH/datacards/hww2l2v_13TeV_top_of0j/events/datacard.txt.pruned.txt \
+                  of1j13Top=ggH/datacards/hww2l2v_13TeV_top_of1j/events/datacard.txt.pruned.txt \
+                  of0j13DYtt=ggH/datacards/hww2l2v_13TeV_dytt_of0j/events/datacard.txt.pruned.txt \
+                  of1j13DYtt=ggH/datacards/hww2l2v_13TeV_dytt_of1j/events/datacard.txt.pruned.txt \
+                  >   Moriond2016.v1.txt.pruned.txt
+
+
+# results
+
+
+text2workspace.py -P HiggsAnalysis.CombinedLimit.PhysicsModel:multiSignalModel --PO verbose  \
+                   --PO 'map=.*/ggH_hww_pt1:r1[1,0,10]' --PO 'map=.*/ggH_hww_pt2:r2[1,0,10]' --PO 'map=.*/ggH_hww_pt3:r3[1,0,10]' \
+                   Moriond2016.v1.txt.pruned.txt  -o  workspace.Moriond2016.v1.txt.pruned.root
+
+
+combine -M MultiDimFit -t -1 \
+     --setPhysicsModelParameters    r1=1,r2=1,r3=1 \
+     workspace.Moriond2016.v1.txt.pruned.root            >   result.MultiDimFit.Moriond2016.v1.txt.pruned.txt
+     
+#      
+#    --expectSignal 1   
+#      
+
