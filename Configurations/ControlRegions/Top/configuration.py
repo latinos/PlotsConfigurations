1a423c651831aa8d81efff68159f4f7d89f15edd
amassiro <massironi.andrea@gmail.com>
2016-06-21 14:36:41 +0200

    Top control region inclusive

---
+++ Configurations/ControlRegions/Top/configuration.py
@@ -0,0 +1,47 @@
+# example of configuration file
+
+tag = 'Top'
+
+
+# used by mkShape to define output directory for root files
+outputDir = 'rootFile'
+
+
+# file with list of variables
+variablesFile = 'variables.py'
+
+# file with list of cuts
+cutsFile = 'cuts.py' 
+
+# file with list of samples
+samplesFile = 'samples.py' 
+
+# file with list of samples
+plotFile = 'plot.py' 
+
+
+
+# luminosity to normalize to (in 1/fb)
+# luminosity to normalize to (in 1/fb)
+# lumi = 2.264
+#lumi = 2.318
+lumi = 0.8
+
+# used by mkPlot to define output directory for plots
+# different from "outputDir" to do things more tidy
+# outputDirPlots = '~/www/plotCR'
+outputDirPlots = 'plotCR'
+
+
+# used by mkDatacards to define output directory for datacards
+outputDirDatacard = 'datacards'
+
+
+# structure file for datacard
+structureFile = 'structure.py'
+
+
+# nuisances file for mkDatacards and for mkShape
+nuisancesFile = 'nuisances.py'
+
+
