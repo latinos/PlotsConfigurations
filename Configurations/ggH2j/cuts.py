7c3a177d02b343a7e6a39a97814cb5c2ceb4d053
amassiro <massironi.andrea@gmail.com>
2016-06-23 18:39:20 +0200

    up for ICHEP

--- Configurations/ggH2j/cuts.py
+++ Configurations/ggH2j/cuts.py
@@ -15,7 +15,7 @@ cuts['hww2l2v_13TeV_top_of2j']  = '(std_vector_lepton_flavour[0] * std_vector_le
                 && (abs(std_vector_lepton_flavour[1]) == 13 || std_vector_lepton_pt[1]>13) \
                 && mll>50 \
                 && mth>=60 \
-                && mpmet > 20 \
+                && mtw2 > 30 \
                 && ( std_vector_jet_pt[0] >= 30 ) \
                 && ( std_vector_jet_pt[1] >= 30 ) \
                 && (mjj<65 || (mjj > 105 && mjj<400)) \
@@ -49,7 +49,7 @@ cuts['hww2l2v_13TeV_of2j']  = '(std_vector_lepton_flavour[0] * std_vector_lepton
                                && (abs(std_vector_lepton_flavour[1]) == 13 || std_vector_lepton_pt[1]>13) \
                                && (njet>=2) \
                                && (std_vector_jet_pt[0]>30 && std_vector_jet_pt[1]>30) \
-                               && mpmet > 20 \
+                               && mtw2 > 30 \
                                && mth>=60 \
                                && (mjj<65 || (mjj > 105 && mjj<400)) \
                                && ( std_vector_jet_pt[0] < 20 || std_vector_jet_cmvav2[0] < -0.715 ) \
