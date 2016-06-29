0212318d308ea32dd5437df36038e712f6ee4c2f
Piergiulio <piergiulio.lenzi@cern.ch>
2016-06-29 15:16:35 +0200

    added config to comput bpog sf

---
+++ Configurations/ControlRegions/BPogSF/bins.py
@@ -0,0 +1,5 @@
+
+# -1 means inf
+ptranges = [20, 30, -1]
+#ptranges = [30, -1]
+etaranges = [0., 2.5]
---
+++ Configurations/ControlRegions/BPogSF/wps.py
@@ -0,0 +1,12 @@
+
+# -1 means inf
+algos=["csvv2ivf", "cmvav2"]
+#algos=["cmvav2"]
+nicknames = ["CSV", "CMVA"]
+#nicknames = ["CMVA"]
+wps={
+"CMVA":
+  {"L": -0.715, "M": 0.185, "T": 0.875},
+"CSV":
+  {"L":  0.460, "M": 0.800, "T": 0.935}
+}
