a872d866dc4de6569bfffedcd5150c26313493db
SangEun Lee <d4space@gmail.com>
2016-07-03 22:58:03 +0900

    codes for Wgstar k-factor at PlotsConfigurations/Configurations/ControlRegions/WgS

---
+++ Configurations/ControlRegions/WgS/torqueBatch/configuration8.py
@@ -0,0 +1,44 @@
+# example of configuration file
+
+tag = 'Met'
+
+
+# used by mkShape to define output directory for root files
+outputDir = 'rootFile8'
+
+
+# file with list of variables
+variablesFile = '../variables.py'
+
+# file with list of cuts
+cutsFile = '../cuts.py' 
+
+# file with list of samples
+samplesFile = 'samples8.py' 
+
+# file with list of samples
+plotFile = 'plot.py' 
+
+
+
+# luminosity to normalize to (in 1/fb)
+# lumi = 2.264
+lumi = 0.8
+
+# used by mkPlot to define output directory for plots
+# different from "outputDir" to do things more tidy
+outputDirPlots = 'plotMet'
+
+
+# used by mkDatacards to define output directory for datacards
+outputDirDatacard = 'datacards'
+
+
+# structure file for datacard
+structureFile = 'structure.py'
+
+
+# nuisances file for mkDatacards and for mkShape
+nuisancesFile = 'nuisances.py'
+
+
---
+++ Configurations/ControlRegions/WgS/torqueBatch/samples5.py
@@ -0,0 +1,14 @@
+
+samples['ggWW']  = {    'name': ['latino_GluGluWWTo2L2Nu_MCFM.root'],      
+                      'weight' : 'puW*baseW*effTrigW*std_vector_lepton_idisoW[0]*std_vector_lepton_idisoW[1]',          
+                      #'weights': ['abs(nllW)'] ,           
+                      #'weights': ['1.000'] ,           
+                      'isData': ['0'],                            
+                  }
+
+
+
+# during tree production: 1.4 k-factor has been applied to both samples
+# ggWW sample: k = 1.4 +/- 15%
+# ggWW interference: k = 1.87 +/- 25%
+
