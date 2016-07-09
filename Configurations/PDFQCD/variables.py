a8c1a92cc6683611562a334aae57571977f497ad
amassiro <massironi.andrea@gmail.com>
2016-07-09 14:31:38 +0200

    update full

--- Configurations/PDFQCD/variables.py
+++ Configurations/PDFQCD/variables.py
@@ -5,7 +5,7 @@
 #'fold' : # 0 = not fold (default), 1 = fold underflowbin, 2 = fold overflow bin, 3 = fold underflow and overflow
 
 
-for ipdf in range(0,10):
+for ipdf in range(0,111):
   variables['weight_' + str(ipdf)]  = {   'name': 'std_vector_LHE_weight[' + str(ipdf) +'] / std_vector_LHE_weight[0]',    
                                            'range' : (100,0,2),  
                                            'xaxis' : 'weight ' + str(ipdf),
