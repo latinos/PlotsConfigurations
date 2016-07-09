2f8f157a47df02229d25fb7de99b52a9733c8454
amassiro <massironi.andrea@gmail.com>
2016-07-09 09:24:09 +0200

    fix for VBF

--- Configurations/VBF/cuts.py
+++ Configurations/VBF/cuts.py
@@ -71,8 +71,8 @@ cuts['hww2l2v_13TeV_dytt_of2j_vbf']  = '(std_vector_lepton_flavour[0] * std_vect
                 && (abs(std_vector_lepton_flavour[1]) == 13 || std_vector_lepton_pt[1]>13) \
                 && ( mth<60) \
                 && mll>30 && mll<80 \
+                && ( std_vector_jet_pt[0] >= 30 ) \
                 && ( std_vector_jet_pt[1] >= 30 ) \
-                && ( std_vector_jet_pt[2] >= 30 ) \
                 && (detajj>3.5 && mjj>400) \
                 && ( std_vector_jet_pt[0] < 20 || std_vector_jet_cmvav2[0] < -0.715 ) \
                 && ( std_vector_jet_pt[1] < 20 || std_vector_jet_cmvav2[1] < -0.715 ) \
