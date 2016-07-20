b3f374599c21ed1711ecf5a1a23c16332d571d13
Lorenzo Viliani <lorenzo.viliani@cern.ch>
2016-07-20 15:03:35 +0200

    fix

--- Configurations/EXO/WWlvlv_VBF/variables.py
+++ Configurations/EXO/WWlvlv_VBF/variables.py
@@ -9,27 +9,15 @@ variables['events']  = {   'name': '1',
                         'xaxis' : 'events',
                          'fold' : 3
                         }
-'''  
-variables['mll']  = {   'name': 'mll',            #   variable name    
-                        'range' : ([50,100,150,200,250,300,500,1000],),    #   variable range
-                        'xaxis' : 'm_{ll} [GeV]',  #   x axis name
-                         'fold' : 3
-                        }
 
-variables['mllVSmti'] = { 'name' : 'mll:mTi',
-                          'range' : ([50,100,150,200,250,300,500,1000],[100,150,200,250,300,500,1000]),
-                          'xaxis' : 'm_{ll} : m_{T}^{i}', #   x axis name
-                          'fold' : 3
-                        }
-'''
 variables['mTi']  = {   'name': 'mTi',            #   variable name    
-                        'range' : ([100,150,200,250,300,350,400,450,500,550,600,700,800,1000],),    #   variable range
+                        'range' : ([100,150,200,250,300,350,400,450,500,600,700,1000],),    #   variable range
                         'xaxis' : 'm_{T,i}^{H} [GeV]',  #   x axis name
                         'fold' : 3
                         }
 
 variables['mTi_VBF']  = {   'name': 'mTi',            #   variable name    
-                        'range' : ([100,150,200,250,300,350,400,450,500,600,800,1000],),    #   variable range
+                        'range' : ([100,150,200,250,300,350,400,500,700,1000],),    #   variable range
                         'xaxis' : 'm_{T,i}^{H} [GeV]',  #   x axis name
                         'fold' : 3
                         }
