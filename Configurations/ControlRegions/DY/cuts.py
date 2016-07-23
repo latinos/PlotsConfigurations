7c07a6fe77e9b76c1688c37c500290dc6d0b77d1
amassiro <massironi.andrea@gmail.com>
2016-07-24 00:04:56 +0200

    test

--- Configurations/ControlRegions/DY/cuts.py
+++ Configurations/ControlRegions/DY/cuts.py
@@ -18,21 +18,27 @@ cuts['DYmm']  = '(std_vector_lepton_flavour[0] * std_vector_lepton_flavour[1] ==
                  && mll>60 \
                '
 
-#cuts['DYee2lepEB']  = '(std_vector_lepton_flavour[0] * std_vector_lepton_flavour[1] == -11*11)   \
-                 #&& mll>60 \
-                 #&& std_vector_lepton_pt[0]>30 && std_vector_lepton_pt[1]>20 \
-                 #&& abs(std_vector_lepton_eta[0])<1.46  \
-                 #&& abs(std_vector_lepton_eta[1])<1.46  \
-               #'
+cuts['DYee2lepEB']  = '(std_vector_lepton_flavour[0] * std_vector_lepton_flavour[1] == -11*11)   \
+                 && mll>60 \
+                 && std_vector_lepton_pt[0]>30 && std_vector_lepton_pt[1]>20 \
+                 && abs(std_vector_lepton_eta[0])<1.46  \
+                 && abs(std_vector_lepton_eta[1])<1.46  \
+               '
 
-#cuts['DYee2lepEE']  = '(std_vector_lepton_flavour[0] * std_vector_lepton_flavour[1] == -11*11)   \
-                 #&& mll>60 \
-                 #&& std_vector_lepton_pt[0]>30 && std_vector_lepton_pt[1]>20 \
-                 #&& abs(std_vector_lepton_eta[0])<1.46  \
-                 #&& abs(std_vector_lepton_eta[1])>1.46  \
-               #'
+cuts['DYee2lepEE']  = '(std_vector_lepton_flavour[0] * std_vector_lepton_flavour[1] == -11*11)   \
+                 && mll>60 \
+                 && std_vector_lepton_pt[0]>30 && std_vector_lepton_pt[1]>20 \
+                 && abs(std_vector_lepton_eta[0])>1.46  \
+                 && abs(std_vector_lepton_eta[1])>1.46  \
+               '
 
 
+cuts['DYee2lepEBEE']  = '(std_vector_lepton_flavour[0] * std_vector_lepton_flavour[1] == -11*11)   \
+                 && mll>60 \
+                 && std_vector_lepton_pt[0]>30 && std_vector_lepton_pt[1]>20 \
+                 && abs(std_vector_lepton_eta[0])>1.46  \
+                 && abs(std_vector_lepton_eta[1])<1.46  \
+               '
 
 
 #cuts['DYee0j']  = '(std_vector_lepton_flavour[0] * std_vector_lepton_flavour[1] == -11*11)   \
