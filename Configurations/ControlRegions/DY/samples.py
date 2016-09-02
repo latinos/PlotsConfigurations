bb2b155192f7897f21a659eacb9cfed993669dd0
amassiro <massironi.andrea@gmail.com>
2016-09-02 14:40:04 +0200

    up track efficiency

--- Configurations/ControlRegions/DY/samples.py
+++ Configurations/ControlRegions/DY/samples.py
@@ -2,6 +2,24 @@
 
 #samples = {}
 
+#samples['DY']  = {    'name': [
+                             #'latino_DYJetsToLL_M-10to50.root',
+                             #'latino_DYJetsToLL_M-50_0000__part0.root',
+                             #'latino_DYJetsToLL_M-50_0000__part1.root',
+                             #'latino_DYJetsToLL_M-50_0000__part2.root',
+                             #'latino_DYJetsToLL_M-50_0000__part3.root',
+                             #'latino_DYJetsToLL_M-50_0000__part4.root',
+                             #'latino_DYJetsToLL_M-50_0001__part0.root',
+                             #'latino_DYJetsToLL_M-50_0001__part1.root',
+                             #'latino_DYJetsToLL_M-50_0001__part2.root',
+                             #'latino_DYJetsToLL_M-50_0001__part3.root',
+                             #'latino_DYJetsToLL_M-50_0001__part4.root',
+                             #'latino_DYJetsToLL_M-50_0002__part0.root',
+                            #],    
+                      #'weight' : 'baseW*puW*(1.08683 * (0.95 - 0.0657370*TMath::Erf((gen_ptll-12.5151)/5.51582)))*effTrigW*std_vector_lepton_idisoW[0]*std_vector_lepton_idisoW[1]*std_vector_lepton_recoW[0]*std_vector_lepton_recoW[1]*GEN_weight_SM/abs(GEN_weight_SM)*std_vector_lepton_genmatched[0]*std_vector_lepton_genmatched[1]',  
+                   #}
+
+
 samples['DY']  = {    'name': [
                              'latino_DYJetsToLL_M-10to50.root',
                              'latino_DYJetsToLL_M-50_0000__part0.root',
@@ -16,8 +34,17 @@ samples['DY']  = {    'name': [
                              'latino_DYJetsToLL_M-50_0001__part4.root',
                              'latino_DYJetsToLL_M-50_0002__part0.root',
                             ],    
-                      'weight' : 'baseW*puW*(1.08683 * (0.95 - 0.0657370*TMath::Erf((gen_ptll-12.5151)/5.51582)))*effTrigW*std_vector_lepton_idisoW[0]*std_vector_lepton_idisoW[1]*std_vector_lepton_recoW[0]*std_vector_lepton_recoW[1]*GEN_weight_SM/abs(GEN_weight_SM)*std_vector_lepton_genmatched[0]*std_vector_lepton_genmatched[1]',  
+                      'weight' : 'baseW*puW*(1.08683 * (0.95 - 0.0657370*TMath::Erf((gen_ptll-12.5151)/5.51582)))*((abs(std_vector_lepton_flavour[0]) == 11 ) + (abs(std_vector_lepton_flavour[0]) == 13 ) * (0.992739 + 0.00152678 * std_vector_lepton_eta[0] + 0.00402821 * std_vector_lepton_eta[0]*std_vector_lepton_eta[0] -0.000557167 * std_vector_lepton_eta[0]*std_vector_lepton_eta[0]*std_vector_lepton_eta[0] - 0.00133539 *std_vector_lepton_eta[0]*std_vector_lepton_eta[0]*std_vector_lepton_eta[0]*std_vector_lepton_eta[0])) * ((abs(std_vector_lepton_flavour[1]) == 11 ) + (abs(std_vector_lepton_flavour[1]) == 13 ) * (0.992739 + 0.00152678 * std_vector_lepton_eta[1] + 0.00402821 * std_vector_lepton_eta[1]*std_vector_lepton_eta[1] -0.000557167 * std_vector_lepton_eta[1]*std_vector_lepton_eta[1]*std_vector_lepton_eta[1] - 0.00133539 *std_vector_lepton_eta[1]*std_vector_lepton_eta[1]*std_vector_lepton_eta[1]*std_vector_lepton_eta[1]))*effTrigW*std_vector_lepton_idisoW[0]*std_vector_lepton_idisoW[1]*std_vector_lepton_recoW[0]*std_vector_lepton_recoW[1]*GEN_weight_SM/abs(GEN_weight_SM)*std_vector_lepton_genmatched[0]*std_vector_lepton_genmatched[1]',  
                    }
+
+
+#((abs(std_vector_lepton_flavour[0]) == 11 ) + (abs(std_vector_lepton_flavour[0]) == 13 ) * (0.992739 + 0.00152678 * std_vector_lepton_eta[0] + 0.00402821 * std_vector_lepton_eta[0]*std_vector_lepton_eta[0] -0.000557167 * std_vector_lepton_eta[0]*std_vector_lepton_eta[0]*std_vector_lepton_eta[0] - 0.00133539 *std_vector_lepton_eta[0]*std_vector_lepton_eta[0]*std_vector_lepton_eta[0]*std_vector_lepton_eta[0])) * ((abs(std_vector_lepton_flavour[1]) == 11 ) + (abs(std_vector_lepton_flavour[1]) == 13 ) * (0.992739 + 0.00152678 * std_vector_lepton_eta[1] + 0.00402821 * std_vector_lepton_eta[1]*std_vector_lepton_eta[1] -0.000557167 * std_vector_lepton_eta[1]*std_vector_lepton_eta[1]*std_vector_lepton_eta[1] - 0.00133539 *std_vector_lepton_eta[1]*std_vector_lepton_eta[1]*std_vector_lepton_eta[1]*std_vector_lepton_eta[1]))
+
+
+
+#(0.992739 + 0.00152678 * x + 0.00402821 * x*x -0.000557167 * x*x*x - 0.00133539 *x*x*x*x)
+
+
 
 
 #samples['DYnoReco']  = {    'name': [
