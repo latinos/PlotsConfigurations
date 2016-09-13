253507d8b93e7f9a0b6caaebebbcaea05a6794da
NTrevisa <n.trevisani@campus.unimib.it>
2016-09-13 17:12:55 +0200

    12/fb

--- Configurations/monoHWW/cuts.py
+++ Configurations/monoHWW/cuts.py
@@ -7,16 +7,31 @@
 
 supercut = 'std_vector_lepton_pt[0]>20 && std_vector_lepton_pt[1]>20 \
             && std_vector_lepton_pt[2]<10 \
+'
+
+# Two leptons level 
+
+cuts['monoH_TwoLep_em'] = 'njet >= 0 \
+            && (std_vector_lepton_flavour[0] * std_vector_lepton_flavour[1] == -11*13) \
+'
+
+# cuts['monoH_TwoLep_ee'] = 'njet >= 0 \
+#             && (std_vector_lepton_flavour[0] * std_vector_lepton_flavour[1] == -11*11) \
+# '
+
+# cuts['monoH_TwoLep_mm'] = 'njet >= 0 \
+#             && (std_vector_lepton_flavour[0] * std_vector_lepton_flavour[1] == -13*13) \
+# '
+
+# BVeto level 
+
+cuts['monoH_BVeto_em'] = ' njet >= 0 \
             && (std_vector_lepton_flavour[0] * std_vector_lepton_flavour[1] == -11*13) \
             && mll>12  \
             && metPfType1 > 20 \
             && mpmet > 20 \
             && ptll > 30 \
             && mll < 100 \
-'
-
-#cuts['ww_BVetoIn_em'] = 'njet >= 0'
-cuts['monoH_BVeto_em'] = ' njet >= 0 \
             && ( std_vector_jet_pt[0] < 20 || std_vector_jet_cmvav2[0] < -0.715 ) \
             && ( std_vector_jet_pt[1] < 20 || std_vector_jet_cmvav2[1] < -0.715 ) \
             && ( std_vector_jet_pt[2] < 20 || std_vector_jet_cmvav2[2] < -0.715 ) \
@@ -29,7 +44,53 @@ cuts['monoH_BVeto_em'] = ' njet >= 0 \
             && ( std_vector_jet_pt[9] < 20 || std_vector_jet_cmvav2[9] < -0.715 ) \
 '
 
+# cuts['monoH_BVeto_ee'] = ' njet >= 0 \
+#             && (std_vector_lepton_flavour[0] * std_vector_lepton_flavour[1] == -11*11) \
+#             && mll>12  \
+#             && metPfType1 > 20 \
+#             && mpmet > 20 \
+#             && ptll > 30 \
+#             && mll < 100 \
+#             && ( std_vector_jet_pt[0] < 20 || std_vector_jet_cmvav2[0] < -0.715 ) \
+#             && ( std_vector_jet_pt[1] < 20 || std_vector_jet_cmvav2[1] < -0.715 ) \
+#             && ( std_vector_jet_pt[2] < 20 || std_vector_jet_cmvav2[2] < -0.715 ) \
+#             && ( std_vector_jet_pt[3] < 20 || std_vector_jet_cmvav2[3] < -0.715 ) \
+#             && ( std_vector_jet_pt[4] < 20 || std_vector_jet_cmvav2[4] < -0.715 ) \
+#             && ( std_vector_jet_pt[5] < 20 || std_vector_jet_cmvav2[5] < -0.715 ) \
+#             && ( std_vector_jet_pt[6] < 20 || std_vector_jet_cmvav2[6] < -0.715 ) \
+#             && ( std_vector_jet_pt[7] < 20 || std_vector_jet_cmvav2[7] < -0.715 ) \
+#             && ( std_vector_jet_pt[8] < 20 || std_vector_jet_cmvav2[8] < -0.715 ) \
+#             && ( std_vector_jet_pt[9] < 20 || std_vector_jet_cmvav2[9] < -0.715 ) \
+# '
+
+# cuts['monoH_BVeto_mm'] = ' njet >= 0 \
+#             && (std_vector_lepton_flavour[0] * std_vector_lepton_flavour[1] == -13*13) \
+#             && mll>12  \
+#             && metPfType1 > 20 \
+#             && mpmet > 20 \
+#             && ptll > 30 \
+#             && mll < 100 \
+#             && ( std_vector_jet_pt[0] < 20 || std_vector_jet_cmvav2[0] < -0.715 ) \
+#             && ( std_vector_jet_pt[1] < 20 || std_vector_jet_cmvav2[1] < -0.715 ) \
+#             && ( std_vector_jet_pt[2] < 20 || std_vector_jet_cmvav2[2] < -0.715 ) \
+#             && ( std_vector_jet_pt[3] < 20 || std_vector_jet_cmvav2[3] < -0.715 ) \
+#             && ( std_vector_jet_pt[4] < 20 || std_vector_jet_cmvav2[4] < -0.715 ) \
+#             && ( std_vector_jet_pt[5] < 20 || std_vector_jet_cmvav2[5] < -0.715 ) \
+#             && ( std_vector_jet_pt[6] < 20 || std_vector_jet_cmvav2[6] < -0.715 ) \
+#             && ( std_vector_jet_pt[7] < 20 || std_vector_jet_cmvav2[7] < -0.715 ) \
+#             && ( std_vector_jet_pt[8] < 20 || std_vector_jet_cmvav2[8] < -0.715 ) \
+#             && ( std_vector_jet_pt[9] < 20 || std_vector_jet_cmvav2[9] < -0.715 ) \
+# '
+
+# Top CR
+
 cuts['monoH_TopCR_em'] = 'njet >= 0 \
+            && (std_vector_lepton_flavour[0] * std_vector_lepton_flavour[1] == -11*13) \
+            && mll>12  \
+            && metPfType1 > 20 \
+            && mpmet > 20 \
+            && ptll > 30 \
+            && mll < 100 \
             && mth > 40 \
             && (( std_vector_jet_pt[0] > 20 && std_vector_jet_cmvav2[0] > -0.715 )  \
             ||  ( std_vector_jet_pt[1] > 20 && std_vector_jet_cmvav2[1] > -0.715 )  \
@@ -42,6 +103,49 @@ cuts['monoH_TopCR_em'] = 'njet >= 0 \
             ||  ( std_vector_jet_pt[8] > 20 && std_vector_jet_cmvav2[8] > -0.715 )  \
             ||  ( std_vector_jet_pt[9] > 20 && std_vector_jet_cmvav2[9] > -0.715 )) \
 '
+
+# cuts['monoH_TopCR_ee'] = 'njet >= 0 \
+#             && (std_vector_lepton_flavour[0] * std_vector_lepton_flavour[1] == -11*11) \
+#             && mll>12  \
+#             && metPfType1 > 20 \
+#             && mpmet > 20 \
+#             && ptll > 30 \
+#             && mll < 100 \
+#             && mth > 40 \
+#             && (( std_vector_jet_pt[0] > 20 && std_vector_jet_cmvav2[0] > -0.715 )  \
+#             ||  ( std_vector_jet_pt[1] > 20 && std_vector_jet_cmvav2[1] > -0.715 )  \
+#             ||  ( std_vector_jet_pt[2] > 20 && std_vector_jet_cmvav2[2] > -0.715 )  \
+#             ||  ( std_vector_jet_pt[3] > 20 && std_vector_jet_cmvav2[3] > -0.715 )  \
+#             ||  ( std_vector_jet_pt[4] > 20 && std_vector_jet_cmvav2[4] > -0.715 )  \
+#             ||  ( std_vector_jet_pt[5] > 20 && std_vector_jet_cmvav2[5] > -0.715 )  \
+#             ||  ( std_vector_jet_pt[6] > 20 && std_vector_jet_cmvav2[6] > -0.715 )  \
+#             ||  ( std_vector_jet_pt[7] > 20 && std_vector_jet_cmvav2[7] > -0.715 )  \
+#             ||  ( std_vector_jet_pt[8] > 20 && std_vector_jet_cmvav2[8] > -0.715 )  \
+#             ||  ( std_vector_jet_pt[9] > 20 && std_vector_jet_cmvav2[9] > -0.715 )) \
+# '
+
+# cuts['monoH_TopCR_mm'] = 'njet >= 0 \
+#             && (std_vector_lepton_flavour[0] * std_vector_lepton_flavour[1] == -13*13) \
+#             && mll>12  \
+#             && metPfType1 > 20 \
+#             && mpmet > 20 \
+#             && ptll > 30 \
+#             && mll < 100 \
+#             && mth > 40 \
+#             && (( std_vector_jet_pt[0] > 20 && std_vector_jet_cmvav2[0] > -0.715 )  \
+#             ||  ( std_vector_jet_pt[1] > 20 && std_vector_jet_cmvav2[1] > -0.715 )  \
+#             ||  ( std_vector_jet_pt[2] > 20 && std_vector_jet_cmvav2[2] > -0.715 )  \
+#             ||  ( std_vector_jet_pt[3] > 20 && std_vector_jet_cmvav2[3] > -0.715 )  \
+#             ||  ( std_vector_jet_pt[4] > 20 && std_vector_jet_cmvav2[4] > -0.715 )  \
+#             ||  ( std_vector_jet_pt[5] > 20 && std_vector_jet_cmvav2[5] > -0.715 )  \
+#             ||  ( std_vector_jet_pt[6] > 20 && std_vector_jet_cmvav2[6] > -0.715 )  \
+#             ||  ( std_vector_jet_pt[7] > 20 && std_vector_jet_cmvav2[7] > -0.715 )  \
+#             ||  ( std_vector_jet_pt[8] > 20 && std_vector_jet_cmvav2[8] > -0.715 )  \
+#             ||  ( std_vector_jet_pt[9] > 20 && std_vector_jet_cmvav2[9] > -0.715 )) \
+# '
+
+
+
 
 #mll > 12GeV
 # cuts['monoH_mll_em'] = 'mll>12  \
