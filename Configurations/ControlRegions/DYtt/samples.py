e6279bce894c3466c026928b8344cd583d2299b4
NTrevisa <n.trevisani@campus.unimib.it>
2016-09-14 17:48:40 +0200

    new DYtt sample included

--- Configurations/ControlRegions/DYtt/samples.py
+++ Configurations/ControlRegions/DYtt/samples.py
@@ -7,21 +7,29 @@ samples['DY']  = {    'name': [
                             #'latino_DYJetsToLL_M-10to50ext3.root',
                             #
                             #'latino_DYJetsToLL_M-50_0000__part0_genVar.root'
+
+                            # 'latino_DYJetsToLL_M-10to50.root',
+                            # 'latino_DYJetsToLL_M-50_0000__part0.root',
+                            # 'latino_DYJetsToLL_M-50_0000__part1.root',
+                            # 'latino_DYJetsToLL_M-50_0000__part2.root',
+                            # 'latino_DYJetsToLL_M-50_0000__part3.root',
+                            # 'latino_DYJetsToLL_M-50_0000__part4.root',
+                            # 'latino_DYJetsToLL_M-50_0001__part0.root',
+                            # 'latino_DYJetsToLL_M-50_0001__part1.root',
+                            # 'latino_DYJetsToLL_M-50_0001__part2.root',
+                            # 'latino_DYJetsToLL_M-50_0001__part3.root',
+                            # 'latino_DYJetsToLL_M-50_0001__part4.root',
+                            # 'latino_DYJetsToLL_M-50_0002__part0.root',
+       
                             'latino_DYJetsToLL_M-10to50.root',
-                            'latino_DYJetsToLL_M-50_0000__part0.root',
-                            'latino_DYJetsToLL_M-50_0000__part1.root',
-                            'latino_DYJetsToLL_M-50_0000__part2.root',
-                            'latino_DYJetsToLL_M-50_0000__part3.root',
-                            'latino_DYJetsToLL_M-50_0000__part4.root',
-                            'latino_DYJetsToLL_M-50_0001__part0.root',
-                            'latino_DYJetsToLL_M-50_0001__part1.root',
-                            'latino_DYJetsToLL_M-50_0001__part2.root',
-                            'latino_DYJetsToLL_M-50_0001__part3.root',
-                            'latino_DYJetsToLL_M-50_0001__part4.root',
-                            'latino_DYJetsToLL_M-50_0002__part0.root',
+                            'latino_DYJetsToTT_MuEle_M-50.root',
                             ],    
                       #'weight' : '(1.02852 - 0.0949640*TMath::Erf((gen_ptll-19.0422)/10.4487) + 0.0758834*TMath::Erf((gen_ptll-56.1146)/41.1653))*puW*effTrigW*std_vector_lepton_idisoW[0]*std_vector_lepton_idisoW[1]*std_vector_lepton_recoW[0]*std_vector_lepton_recoW[1]*GEN_weight_SM/abs(GEN_weight_SM)',  
                       'weight' : 'puW*baseW*(1.08683 * (0.95 - 0.0657370*TMath::Erf((gen_ptll-12.5151)/5.51582)))*bPogSF*effTrigW*std_vector_lepton_idisoW[0]*std_vector_lepton_idisoW[1]*std_vector_lepton_recoW[0]*std_vector_lepton_recoW[1]*GEN_weight_SM/abs(GEN_weight_SM)*std_vector_lepton_genmatched[0]*std_vector_lepton_genmatched[1]',  
+                       'weights' :  [
+                                     '1', 
+                                     '1.26645',
+                                     ],
                     }
 
 
