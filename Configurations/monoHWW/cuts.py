921ddd5c3c38d035cf52d8efe5f088fa5b1ed765
NTrevisa <n.trevisani@campus.unimib.it>
2016-09-20 09:20:52 +0200

    WW level

--- Configurations/monoHWW/cuts.py
+++ Configurations/monoHWW/cuts.py
@@ -11,9 +11,9 @@ supercut = 'std_vector_lepton_pt[0]>20 && std_vector_lepton_pt[1]>20 \
 
 # Two leptons level 
 
-cuts['monoH_TwoLep_em'] = 'njet >= 0 \
-            && (std_vector_lepton_flavour[0] * std_vector_lepton_flavour[1] == -11*13) \
-'
+# cuts['monoH_TwoLep_em'] = 'njet >= 0 \
+#             && (std_vector_lepton_flavour[0] * std_vector_lepton_flavour[1] == -11*13) \
+# '
 
 # cuts['monoH_TwoLep_ee'] = 'njet >= 0 \
 #             && (std_vector_lepton_flavour[0] * std_vector_lepton_flavour[1] == -11*11) \
@@ -31,7 +31,7 @@ cuts['monoH_BVeto_em'] = 'njet >= 0 \
             && metPfType1 > 20 \
             && mpmet > 20 \
             && ptll > 30 \
-            && mll < 100 \
+            && mth > 60 \
             && ( std_vector_jet_pt[0] < 20 || std_vector_jet_cmvav2[0] < -0.715 ) \
             && ( std_vector_jet_pt[1] < 20 || std_vector_jet_cmvav2[1] < -0.715 ) \
             && ( std_vector_jet_pt[2] < 20 || std_vector_jet_cmvav2[2] < -0.715 ) \
@@ -43,6 +43,7 @@ cuts['monoH_BVeto_em'] = 'njet >= 0 \
             && ( std_vector_jet_pt[8] < 20 || std_vector_jet_cmvav2[8] < -0.715 ) \
             && ( std_vector_jet_pt[9] < 20 || std_vector_jet_cmvav2[9] < -0.715 ) \
 '
+#            && mll < 100 \
 
 # cuts['monoH_BVeto_ee'] = ' njet >= 0 \
 #             && (std_vector_lepton_flavour[0] * std_vector_lepton_flavour[1] == -11*11) \
@@ -84,25 +85,25 @@ cuts['monoH_BVeto_em'] = 'njet >= 0 \
 
 # Top CR
 
-cuts['monoH_TopCR_em'] = 'njet >= 0 \
-            && (std_vector_lepton_flavour[0] * std_vector_lepton_flavour[1] == -11*13) \
-            && mll>12  \
-            && metPfType1 > 20 \
-            && mpmet > 20 \
-            && ptll > 30 \
-            && mll < 100 \
-            && mth > 40 \
-            && (( std_vector_jet_pt[0] > 20 && std_vector_jet_cmvav2[0] > -0.715 )  \
-            ||  ( std_vector_jet_pt[1] > 20 && std_vector_jet_cmvav2[1] > -0.715 )  \
-            ||  ( std_vector_jet_pt[2] > 20 && std_vector_jet_cmvav2[2] > -0.715 )  \
-            ||  ( std_vector_jet_pt[3] > 20 && std_vector_jet_cmvav2[3] > -0.715 )  \
-            ||  ( std_vector_jet_pt[4] > 20 && std_vector_jet_cmvav2[4] > -0.715 )  \
-            ||  ( std_vector_jet_pt[5] > 20 && std_vector_jet_cmvav2[5] > -0.715 )  \
-            ||  ( std_vector_jet_pt[6] > 20 && std_vector_jet_cmvav2[6] > -0.715 )  \
-            ||  ( std_vector_jet_pt[7] > 20 && std_vector_jet_cmvav2[7] > -0.715 )  \
-            ||  ( std_vector_jet_pt[8] > 20 && std_vector_jet_cmvav2[8] > -0.715 )  \
-            ||  ( std_vector_jet_pt[9] > 20 && std_vector_jet_cmvav2[9] > -0.715 )) \
-'
+# cuts['monoH_TopCR_em'] = 'njet >= 0 \
+#             && (std_vector_lepton_flavour[0] * std_vector_lepton_flavour[1] == -11*13) \
+#             && mll>12  \
+#             && metPfType1 > 20 \
+#             && mpmet > 20 \
+#             && ptll > 30 \
+#             && mll < 100 \
+#             && mth > 40 \
+#             && (( std_vector_jet_pt[0] > 20 && std_vector_jet_cmvav2[0] > -0.715 )  \
+#             ||  ( std_vector_jet_pt[1] > 20 && std_vector_jet_cmvav2[1] > -0.715 )  \
+#             ||  ( std_vector_jet_pt[2] > 20 && std_vector_jet_cmvav2[2] > -0.715 )  \
+#             ||  ( std_vector_jet_pt[3] > 20 && std_vector_jet_cmvav2[3] > -0.715 )  \
+#             ||  ( std_vector_jet_pt[4] > 20 && std_vector_jet_cmvav2[4] > -0.715 )  \
+#             ||  ( std_vector_jet_pt[5] > 20 && std_vector_jet_cmvav2[5] > -0.715 )  \
+#             ||  ( std_vector_jet_pt[6] > 20 && std_vector_jet_cmvav2[6] > -0.715 )  \
+#             ||  ( std_vector_jet_pt[7] > 20 && std_vector_jet_cmvav2[7] > -0.715 )  \
+#             ||  ( std_vector_jet_pt[8] > 20 && std_vector_jet_cmvav2[8] > -0.715 )  \
+#             ||  ( std_vector_jet_pt[9] > 20 && std_vector_jet_cmvav2[9] > -0.715 )) \
+# '
 
 # cuts['monoH_TopCR_ee'] = 'njet >= 0 \
 #             && (std_vector_lepton_flavour[0] * std_vector_lepton_flavour[1] == -11*11) \
