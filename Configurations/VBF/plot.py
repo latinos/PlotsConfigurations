6f5fbfa35078a2c17301dfda2d2d2295a8781367
amassiro <massironi.andrea@gmail.com>
2016-10-31 13:24:18 +0100

    unblond control regions

--- Configurations/VBF/plot.py
+++ Configurations/VBF/plot.py
@@ -364,7 +364,7 @@ plot['DATA']  = {
                   'color': 1 ,  
                   'isSignal' : 0,
                   'isData'   : 1 ,
-                  'isBlind'  : 1
+                  #'isBlind'  : 1
               }
 
 
