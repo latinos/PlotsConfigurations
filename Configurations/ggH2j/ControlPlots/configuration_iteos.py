958d156dcdd392321ca33bde4bb421aa67138ac5
amassiro <massironi.andrea@gmail.com>
2016-11-11 10:48:34 +0100

    update fix lumi

---
+++ Configurations/ggH2j/ControlPlots/configuration_iteos.py
@@ -0,0 +1,46 @@
+# example of configuration file
+
+tag = 'ggH2j'
+
+
+# used by mkShape to define output directory for root files
+outputDir = 'rootFile'
+
+
+# file with list of variables
+variablesFile = 'variables.py'
+
+# file with list of cuts
+cutsFile = 'cuts.py' 
+
+# file with list of samples
+samplesFile = 'samples_iteos.py' 
+
+# file with list of samples
+plotFile = 'plot.py' 
+
+
+
+# luminosity to normalize to (in 1/fb)
+#lumi = 6.264
+#lumi = 4.3
+# lumi = 5
+lumi = 12.8890
+
+# used by mkPlot to define output directory for plots
+# different from "outputDir" to do things more tidy
+outputDirPlots = 'plotggH2j'
+
+
+# used by mkDatacards to define output directory for datacards
+outputDirDatacard = 'datacards'
+
+
+# structure file for datacard
+structureFile = 'structure.py'
+
+
+# nuisances file for mkDatacards and for mkShape
+nuisancesFile = 'nuisances_iteos.py'
+
+
