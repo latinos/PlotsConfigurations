792c6cad17f0f187e48b344b645a54f586287cdd
amassiro <massironi.andrea@gmail.com>
2017-01-07 12:19:35 +0100

    nicer

--- Configurations/ggH/scripts/doImpact.sh
+++ Configurations/ggH/scripts/doImpact.sh
@@ -15,7 +15,91 @@ cd -
 cd ..
 
 
-text2workspace.py superCombination.Total.txt.pruned.txt -m 125
+text2workspace.py superCombination.Total.txt.pruned.txt   -o    superCombination.Total.txt.pruned.txt.workspace.root
+
+combine -M MultiDimFit -n _initialFit_Test --algo singles --redefineSignalPOIs r --robustFit 1 -t -1 --expectSignal=1 -m 125 -d superCombination.Total.txt.pruned.txt.workspace.root
+
+
+
+combine /afs/cern.ch/user/a/amassiro/Framework/CMSSW_8_0_5/src/PlotsConfigurations/Configurations/Combined.vbf.pruned.txt       -M MaxLikelihoodFit -t -1 --expectSignal 1   \
+           -n    /afs/cern.ch/user/a/amassiro/Framework/CMSSW_8_0_5/src/PlotsConfigurations/Configurations/Combined.vbf.pruned.txt.MaxLikelihoodFit.root
+
+combine Combined.vbf.pruned.txt       -M MaxLikelihoodFit -t -1 --expectSignal 1   \
+           -n   Combined.vbf.pruned.txt.MaxLikelihoodFit.root
+
+combine Combined.vbf.pruned.txt       -M MaxLikelihoodFit   \
+           -n   Combined.vbf.pruned.txt.MaxLikelihoodFit.root
+           
+           
+combine Combined.vbf.pruned.txt       -M MaxLikelihoodFit   -t -1 --expectSignal=1.5  --toysFreq \
+           -n   Combined.vbf.pruned.txt.MaxLikelihoodFit.1.5.root
+
+combine Combined.vbf.pruned.txt       -M MaxLikelihoodFit   -t -1 --expectSignal=0.5  --toysFreq \
+           -n   Combined.vbf.pruned.txt.MaxLikelihoodFit.0.5.root
+
+           
+python diffNuisances.py -a mlfitCombined.vbf.pruned.txt.MaxLikelihoodFit.root.root -g plots.root      &>           mlfitCombined.vbf.pruned.txt.MaxLikelihoodFit.root.diffNuisances.txt
+
+python ggH/scripts/diffNuisances.py -a mlfitCombined.vbf.pruned.txt.MaxLikelihoodFit.root.root          -g plots.root  -o vbf.root  
+python ggH/scripts/diffNuisances.py -a mlfitCombined.vbf.pruned.txt.MaxLikelihoodFit.1.5.root.root      -g plots.root  -o vbf.1.5.root  
+python ggH/scripts/diffNuisances.py -a mlfitCombined.vbf.pruned.txt.MaxLikelihoodFit.0.5.root.root      -g plots.root  -o vbf.0.5.root  
+
+
+python ggH/scripts/diffNuisances.py -a mlfitCombined.vbf.pruned.txt.MaxLikelihoodFit.0.5.root.root      -g plots.root  -o vbf.0.5.root  -i ggH/scripts/listNuisances.json
+
+
+
+ 
+combine -M MultiDimFit \
+     --redefineSignalPOIs fake_syst --freezeNuisances=r  \
+     --setPhysicsModelParameterRanges fake_syst=-2,1   \
+     --setPhysicsModelParameters    fake_syst=0 \
+     --algo=grid --points=100  \
+     superCombination.Total.txt.pruned.txt    \
+     -n "LH1D_fake_syst_combined"     >   result.MultiDimFit.grid.workspace.superCombination.Total.txt.pruned.txt.fakeSyst.txt
+
+
+     
+ 
+combine -M MultiDimFit \
+     --redefineSignalPOIs CMS_idiso_ele --freezeNuisances=r  \
+     --setPhysicsModelParameterRanges CMS_idiso_ele=-1.5,1.5   \
+     --setPhysicsModelParameters    CMS_idiso_ele=0 \
+     --algo=grid --points=100  \
+     superCombination.Total.txt.pruned.txt    \
+     -n "LH1D_CMS_idiso_ele_combined"     >   result.MultiDimFit.grid.workspace.superCombination.Total.txt.pruned.txt.CMS_idiso_ele.txt
+
+
+    
+combine -M MultiDimFit \
+     --redefineSignalPOIs CMS_fake_ele_stat_hww --freezeNuisances=r  \
+     --setPhysicsModelParameterRanges CMS_fake_ele_stat_hww=-3.0,1.0   \
+     --setPhysicsModelParameters    CMS_fake_ele_stat_hww=0 \
+     --algo=grid --points=100  \
+     superCombination.Total.txt.pruned.txt    \
+     -n "LH1D_CMS_fake_ele_stat_hww_combined"     >   result.MultiDimFit.grid.workspace.superCombination.Total.txt.pruned.txt.CMS_fake_ele_stat_hww.txt
+
+
+
+    
+combine -M MultiDimFit \
+     --redefineSignalPOIs WWnorm0j,fake_syst --freezeNuisances=r  \
+     --setPhysicsModelParameterRanges WWnorm0j=0.5,1.5:fake_syst=-2.0,1.0   \
+     --setPhysicsModelParameters    WWnorm0j=1,fake_syst=0 \
+     --algo=grid --points=200  \
+     Combined.0jet.txt    \
+     -n "LH2D_WWnorm0j_fake_syst_combined"     >   result.MultiDimFit.grid.workspace.Combined.0jet.txt.WWnorm0j_fake_syst.txt
+     
+     
+     
+ 
+ 
+     
+     
+     
+
+
+text2workspace.py superCombination.Total.txt.pruned.txt   -o  superCombination.Total.txt.pruned.root  
 
 
 
@@ -89,7 +173,7 @@ combineTool.py -M Impacts -d superCombination.Total.txt.pruned.root -m 125 --doI
 
 
 # the next one may takes ... hours!
-combineTool.py -M Impacts -d superCombination.Total.txt.pruned.root -m 125 --robustFit 1 --doFits --parallel 20  --expectSignal=1  -t -1 --toysFreq  \
+combineTool.py -M Impacts -d superCombination.Total.txt.pruned.root -m 125 --robustFit 1 --doFits --parallel 20  --expectSignal=1  -t -1 --toysFreq   --job-mode lxbatch --task-name lxbatch-test --sub-opts='-q 1nw'    \
                --named WWnorm0j,Topnorm0j,DYttnorm0j,WWnorm1j,Topnorm1j,DYttnorm1j,WWnorm2j,Topnorm2j,DYttnorm2j,WWnorm2jvbf,Topnorm2jvbf,DYttnorm2jvbf,WWnorm2jvh,Topnorm2jvh,DYttnorm2jvh,Zg3lnorm,WZ3lnorm  \
                --setPhysicsModelParameterRanges WWnorm0j=-3,4:Topnorm0j=-3,4:DYttnorm0j=-3,4:WWnorm1j=-3,4:Topnorm1j=-3,4:DYttnorm1j=-3,4:WWnorm2j=-3,4:Topnorm2j=-3,4:DYttnorm2j=-3,4:WWnorm2jvbf=-3,4:Topnorm2jvbf=-3,4:DYttnorm2jvbf=-3,4:WWnorm2jvh=-3,4:Topnorm2jvh=-3,4:DYttnorm2jvh=-3,4:Zg3lnorm=-3,4:WZ3lnorm=-3:4
 
@@ -259,12 +343,12 @@ combineCards.py   of2jvh2j13Top=VH2j/Moriond/datacards/hww2l2v_13TeV_top_of2j_vh
                   ICHEP_of2jvh2j13DYtt=VH2j/datacards/hww2l2v_13TeV_dytt_of2j_vh2j/events/datacard.txt.pruned.txt \
                   >   superCombination.ControlRegions.vh2j.txt.pruned.txt
  
-text2workspace.py superCombination.ControlRegions.Total.txt.pruned.txt -m 125
-text2workspace.py superCombination.ControlRegions.0j.txt.pruned.txt -m 125
-text2workspace.py superCombination.ControlRegions.1j.txt.pruned.txt -m 125
-text2workspace.py superCombination.ControlRegions.2j.txt.pruned.txt -m 125
-text2workspace.py superCombination.ControlRegions.vbf.txt.pruned.txt -m 125
-text2workspace.py superCombination.ControlRegions.vh2j.txt.pruned.txt -m 125
+# text2workspace.py superCombination.ControlRegions.Total.txt.pruned.txt -m 125
+# text2workspace.py superCombination.ControlRegions.0j.txt.pruned.txt -m 125
+# text2workspace.py superCombination.ControlRegions.1j.txt.pruned.txt -m 125
+# text2workspace.py superCombination.ControlRegions.2j.txt.pruned.txt -m 125
+# text2workspace.py superCombination.ControlRegions.vbf.txt.pruned.txt -m 125
+# text2workspace.py superCombination.ControlRegions.vh2j.txt.pruned.txt -m 125
 
  
    
@@ -280,7 +364,7 @@ text2workspace.py superCombination.ControlRegions.vh2j.txt.pruned.txt -m 125
                    
 combine -M MultiDimFit \
      --redefineSignalPOIs Topnorm2jvh,DYttnorm2jvh --freezeNuisances=r,WWnorm2jvh  \
-     --setPhysicsModelParameterRanges DYttnorm2jvh=0.3,1.7:Topnorm2jvh=0.80,1.20   \
+     --setPhysicsModelParameterRanges DYttnorm2jvh=0.2,1.8:Topnorm2jvh=0.90,1.10   \
      --setPhysicsModelParameters    Topnorm2jvh=1,DYttnorm2jvh=1 \
      --algo=grid --points=200  \
      superCombination.ControlRegions.vh2j.txt.pruned.txt    \
@@ -299,7 +383,7 @@ r99t higgsCombineLH2D_ControlRegion_vh2j_combined.MultiDimFit.mH120.root   ggH/s
                
 combine -M MultiDimFit \
      --redefineSignalPOIs Topnorm2jvbf,DYttnorm2jvbf --freezeNuisances=r,WWnorm2jvbf  \
-     --setPhysicsModelParameterRanges DYttnorm2jvbf=0.1,4.0:Topnorm2jvbf=0.50,1.50   \
+     --setPhysicsModelParameterRanges DYttnorm2jvbf=0.1,2.0:Topnorm2jvbf=0.50,1.50   \
      --setPhysicsModelParameters    Topnorm2jvbf=1,DYttnorm2jvbf=1 \
      --algo=grid --points=200  \
      superCombination.ControlRegions.vbf.txt.pruned.txt    \
@@ -350,7 +434,7 @@ r99t higgsCombineLH2D_ControlRegion_0j_combined.MultiDimFit.mH120.root   ggH/scr
  
 combine -M MultiDimFit \
      --redefineSignalPOIs Topnorm1j,DYttnorm1j --freezeNuisances=r,WWnorm1j  \
-     --setPhysicsModelParameterRanges DYttnorm1j=0.6,1.4:Topnorm1j=0.85,1.15   \
+     --setPhysicsModelParameterRanges DYttnorm1j=0.6,1.4:Topnorm1j=0.75,1.35   \
      --setPhysicsModelParameters    Topnorm1j=1,DYttnorm1j=1 \
      --algo=grid --points=200  \
      superCombination.ControlRegions.1j.txt.pruned.txt    \
