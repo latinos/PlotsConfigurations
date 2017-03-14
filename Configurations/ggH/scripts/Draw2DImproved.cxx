c306cd9d7a52a706dda89e97d4eab04d73221eb1
amassiro <massironi.andrea@gmail.com>
2017-03-14 16:49:15 +0100

    fancy colours

--- Configurations/ggH/scripts/Draw2DImproved.cxx
+++ Configurations/ggH/scripts/Draw2DImproved.cxx
@@ -40,6 +40,7 @@ void Draw2DImproved(std::string xName = "r_{1}", std::string yName = "r_{3}", st
   
   TString whatToDraw = Form ("2*deltaNLL:%s:%s", xNameVar.c_str(), yNameVar.c_str());
   
+//   TString cutToDraw = Form ("((deltaNLL<10) *1 + (deltaNLL>=10)*10) && (%s)", cut.c_str());
   TString cutToDraw = Form ("(deltaNLL<10) && (%s)", cut.c_str());
   
   std::cout << " whatToDraw = " << whatToDraw.Data() << std::endl;
@@ -80,8 +81,18 @@ void Draw2DImproved(std::string xName = "r_{1}", std::string yName = "r_{3}", st
   graphScan->GetHistogram()->GetYaxis()->SetTitle(yName.c_str());
   graphScan->GetHistogram()->GetZaxis()->SetTitle("- 2#Delta logL");
   // //   graphScan->Draw("cont4z");
+  graphScan->GetHistogram()->GetZaxis()->SetRangeUser(0,9.99);  
+  
+  for (int i=0; i<graphScan->GetHistogram()->GetSize(); i++) {
+//     std::cout << " [ " << i << " ] = " << graphScan->GetHistogram()->GetBinContent(i+1) << std::endl;
+    if (graphScan->GetHistogram()->GetBinContent(i+1) == 0) { 
+      graphScan->GetHistogram()->SetBinContent(i+1, 100);
+    }
+  }
+  
   graphScan->Draw("colz");
   
+
   
   TCanvas* cc2 = new TCanvas ("cc2","",800,600); 
   cc2->SetRightMargin(0.15);
@@ -96,6 +107,7 @@ void Draw2DImproved(std::string xName = "r_{1}", std::string yName = "r_{3}", st
   HistStreamFn_ph2->SetContour(2, contours);
   HistStreamFn_ph2->SetLineWidth(2);
   HistStreamFn_ph2->SetLineStyle(2);
+//   HistStreamFn_ph2->GetZaxis()->SetRangeUser(0,9.99);  
   HistStreamFn_ph2->Draw("CONT1 LIST SAME");
   
 //   gStyle->SetPadRightMargin(0.2);
@@ -143,7 +155,7 @@ void Draw2DImproved(std::string xName = "r_{1}", std::string yName = "r_{3}", st
   
   crossMin->Draw("P");
   
-  cc2->SetGrid();
+//   cc2->SetGrid();
   
   
   
