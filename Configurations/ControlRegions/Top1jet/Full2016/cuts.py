f7660ea815e4732c7780f23d0c2c1d365d6c1312
Janssen Xavier <xavier.janssen@uantwerpen.be>
2017-03-17 12:01:40 +0100

    Full2016 Top1jet

---
+++ Configurations/ControlRegions/Top1jet/Full2016/cuts.py
@@ -0,0 +1,34 @@
+# cuts
+supercut = 'mll>12  \
+            && std_vector_lepton_pt[0]>20 && std_vector_lepton_pt[1]>10 \
+            && std_vector_lepton_pt[2]<10 \
+            && metPfType1 > 20 \
+            && ptll > 30 \
+'
+
+cuts['hww2l2v_13TeV_top_of1j']  = '(std_vector_lepton_flavour[0] * std_vector_lepton_flavour[1] == -11*13)    \
+                && (abs(std_vector_lepton_flavour[1]) == 13 || std_vector_lepton_pt[1]>13) \
+                && mll>50 \
+                && ( mtw2>30) \
+                && ( std_vector_jet_pt[0] >= 30 ) \
+                && ( std_vector_jet_pt[1] < 30 ) \
+                && std_vector_jet_cmvav2[0]>-0.715 \
+                '
+
+cuts['hww2l2v_13TeV_top_eu1j']  = '(abs(std_vector_lepton_flavour[0]) == 11 && abs(std_vector_lepton_flavour[1]) == 13)    \
+                && (abs(std_vector_lepton_flavour[1]) == 13 || std_vector_lepton_pt[1]>13) \
+                && mll>50 \
+                && ( mtw2>30) \
+                && ( std_vector_jet_pt[0] >= 30 ) \
+                && ( std_vector_jet_pt[1] < 30 ) \
+                && std_vector_jet_cmvav2[0]>-0.715 \
+                '
+
+cuts['hww2l2v_13TeV_top_ue1j']  = '(abs(std_vector_lepton_flavour[0]) == 13 && abs(std_vector_lepton_flavour[1]) == 11)    \
+                && (abs(std_vector_lepton_flavour[1]) == 13 || std_vector_lepton_pt[1]>13) \
+                && mll>50 \
+                && ( mtw2>30) \
+                && ( std_vector_jet_pt[0] >= 30 ) \
+                && ( std_vector_jet_pt[1] < 30 ) \
+                && std_vector_jet_cmvav2[0]>-0.715 \
+                '
