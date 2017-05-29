a2b2c20e66750663d9ef0985af03ae824e21537c
NTrevisa <n.trevisani@campus.unimib.it>
2017-05-29 09:46:57 +0200

    put comments in script for limit extraction

--- Configurations/monoHWW/Full2016/scriptMonoH.py
+++ Configurations/monoHWW/Full2016/scriptMonoH.py
@@ -14,27 +14,33 @@ print variable
 cut = sys.argv[3]
 print cut
 
-print "combineCards.py datacards/monoH_" + cut + "_" + channel + "/" + variable + "/datacard.txt.pruned.txt datacards/monoH_" + cut + "_WW_" + channel + "/events/datacard.txt.pruned.txt datacards/monoH_" + cut + "_Top_" + channel + "/events/datacard.txt.pruned.txt datacards/monoH_" + cut + "_DYtt_" + channel + "/events/datacard.txt.pruned.txt > datacards/monoH_" + cut + "_" + channel + "/" + variable + "/datacard_combined.txt"
+# Combine datacards
+print "+++++ Combining Datacards +++++"
 os.system("combineCards.py datacards/monoH_" + cut + "_" + channel + "/" + variable + "/datacard.txt.pruned.txt datacards/monoH_" + cut + "_WW_" + channel + "/events/datacard.txt.pruned.txt datacards/monoH_" + cut + "_Top_" + channel + "/events/datacard.txt.pruned.txt datacards/monoH_" + cut + "_DYtt_" + channel + "/events/datacard.txt.pruned.txt > datacards/monoH_" + cut + "_" + channel + "/" + variable + "/datacard_combined.txt")
 
 
+# Create folders for results
+print "+++++ Creating Folders for Results +++++"
 os.system('mkdir -p  combine_' + channel + '_' + cut + '/')
 os.system('mkdir -p  limits_' + channel + '_' + cut + '/')
 os.system('mkdir -p  pulls_' + channel + '_' + cut + '/')
+
+# Loop over 2HDM model mass points
+print "+++++ 2HDM Mass Points +++++"
 for mZp in ZpMasses :
     for mA0 in A0Masses :
-        print('text2workspace.py -P HiggsAnalysis.CombinedLimit.PhysicsModel:multiSignalModel  --PO verbose --PO \'map=.*/monoH_*:0\' --PO \'map=.*/monoH_' + mZp + '_' + mA0 + ':r[1,0,10]\' datacards/monoH_' + cut + '_' + channel + '/' + variable + '/datacard_combined.txt -o monoH_' + mZp + '_' + mA0 + '.root')
+        # text2workspace step
+        print "+++++ Translating Datacards to Rootfiles +++++"
         os.system('text2workspace.py -P HiggsAnalysis.CombinedLimit.PhysicsModel:multiSignalModel  --PO verbose --PO \'map=.*/monoH_*:0\' --PO \'map=.*/monoH_' + mZp + '_' + mA0 + ':r[1,0,10]\' datacards/monoH_' + cut + '_' + channel + '/' + variable + '/datacard_combined.txt -o monoH_' + mZp + '_' + mA0 + '.root')
-        print('mv monoH_' + mZp + '_' + mA0 + '.root combine_' + channel + '_' + cut + '/monoH_' + mZp + '_' + mA0 + '_' + variable + '.root')
         os.system('mv monoH_' + mZp + '_' + mA0 + '.root combine_' + channel + '_' + cut + '/monoH_' + mZp + '_' + mA0 + '_' + variable + '.root')
         
-        print('combine -M Asymptotic -m ' + mZp + '.' + mA0 + ' -t -1 --expectSignal 1 --rMin=0.001 --run expected combine_' + channel + '_' + cut + '/monoH_' + mZp + '_' + mA0 + '_' + variable + '.root &> limits_' + channel + '_' + cut + '/monoH_' + mZp + '_' + mA0 + '_' + variable + '.txt')
+        # Use combine to calculate limits
+        print "+++++ Extracting Limits +++++"
         os.system('combine -M Asymptotic -m ' + mZp + '.' + mA0 + ' -t -1 --expectSignal 1 --run expected combine_' + channel + '_' + cut + '/monoH_' + mZp + '_' + mA0 + '_' + variable + '.root &> limits_' + channel + '_' + cut + '/monoH_' + mZp + '_' + mA0 + '_' + variable + '.txt')
         os.system('mv higgsCombineTest.Asymptotic* combine_' + channel + '_' + cut + '/higgsCombineTest.Asymptotic.mH' + mZp + '_' + mA0 + '_' + variable + '.root')
 
-        print('combine -M MaxLikelihoodFit -t -1 --expectSignal 1 -d combine_' + channel + '_' + cut + '/monoH_' + mZp + '_' + mA0 + '_' + variable + '.root')
-        print('mv mlfit.root combine_' + channel + '_' + cut + '/mlfit_' + mZp + '_' + mA0 + '_' + variable + '.root')
-        print('python ~/work/CMSSW_7_4_7/src/HiggsAnalysis/CombinedLimit/test/diffNuisances.py combine_' + channel + '_' + cut + '/mlfit_' + mZp + '_' + mA0 + '_' + variable + '.root --histogram pulls_' + channel + '_' + cut + '/pulls_' + mZp + '_' + mA0 + '_' + variable + '.root')
+        # Produce pulls
+        print "+++++ Producing Pulls +++++"
         os.system('combine -M MaxLikelihoodFit -t -1 --expectSignal 1 -d combine_' + channel + '_' + cut + '/monoH_' + mZp + '_' + mA0 + '_' + variable + '.root')
         os.system('mv mlfit.root combine_' + channel + '_' + cut + '/mlfit_' + mZp + '_' + mA0 + '_' + variable + '.root')
         os.system('python ~/work/CMSSW_7_4_7/src/HiggsAnalysis/CombinedLimit/test/diffNuisances.py combine_' + channel + '_' + cut + '/mlfit_' + mZp + '_' + mA0 + '_' + variable + '.root --histogram pulls_' + channel + '_' + cut + '/pulls_' + mZp + '_' + mA0 + '_' + variable + '.root')
@@ -42,23 +48,21 @@ for mZp in ZpMasses :
 
 zbMassPoints={"ZB_10000_50","ZB_10000_500","ZB_1000_1","ZB_1000_1000","ZB_1000_150","ZB_995_500","ZB_100_1","ZB_100_10","ZB_10_1","ZB_10_1000","ZB_10_50","ZB_10_500","ZB_15_10","ZB_200_150","ZB_300_1","ZB_300_50","ZB_500_150","ZB_500_500","ZB_50_1","ZB_50_10","ZB_50_50"}
 
+# Loop over Z'B model mass points
+print "+++++ Z'B Mass Points +++++"
 for masses in zbMassPoints :
-        print('text2workspace.py -P HiggsAnalysis.CombinedLimit.PhysicsModel:multiSignalModel  --PO verbose --PO \'map=.*/monoH_*:0\' --PO \'map=.*/monoH_' + masses + ':r[1,0,10]\' datacards/monoH_' + cut + '_' + channel + '/' + variable + '/datacard_combined.txt -o monoH_' + masses + '.root')
-        os.system('text2workspace.py -P HiggsAnalysis.CombinedLimit.PhysicsModel:multiSignalModel  --PO verbose --PO \'map=.*/monoH_*:0\' --PO \'map=.*/monoH_' + masses + ':r[1,0,10]\' datacards/monoH_' + cut + '_' + channel + '/' + variable + '/datacard_combined.txt -o monoH_' + masses + '.root')
-        print('mv monoH_' + masses + '.root combine_' + channel + '_' + cut + '/monoH_' + masses + '_' + variable + '.root')
-        os.system('mv monoH_' + masses + '.root combine_' + channel + '_' + cut + '/monoH_' + masses + '_' + variable + '.root')
-        
-        print('combine -M Asymptotic -m ' + mZp + '.' + mA0 + ' -t -1 --expectSignal 1 --rMin=0.001 --run expected combine_' + channel + '_' + cut + '/monoH_' + masses + '_' + variable + '.root &> limits_' + channel + '_' + cut + '/monoH_' + masses + '_' + variable + '.txt')
-        os.system('combine -M Asymptotic -m ' + mZp + '.' + mA0 + ' -t -1 --expectSignal 1 --run expected combine_' + channel + '_' + cut + '/monoH_' + masses + '_' + variable + '.root &> limits_' + channel + '_' + cut + '/monoH_' + masses + '_' + variable + '.txt')
-        os.system('mv higgsCombineTest.Asymptotic* combine_' + channel + '_' + cut + '/higgsCombineTest.Asymptotic.mH' + masses + '_' + variable + '.root')
-
-        print('combine -M MaxLikelihoodFit -t -1 --expectSignal 1 -d combine_' + channel + '_' + cut + '/monoH_' + masses + '_' + variable + '.root')
-        print('mv mlfit.root combine_' + channel + '_' + cut + '/mlfit_' + masses + '_' + variable + '.root')
-        print('python ~/work/CMSSW_7_4_7/src/HiggsAnalysis/CombinedLimit/test/diffNuisances.py combine_' + channel + '_' + cut + '/mlfit_' + masses + '_' + variable + '.root --histogram pulls_' + channel + '_' + cut + '/pulls_' + masses + '_' + variable + '.root')
-        os.system('combine -M MaxLikelihoodFit -t -1 --expectSignal 1 -d combine_' + channel + '_' + cut + '/monoH_' + masses + '_' + variable + '.root')
-        os.system('mv mlfit.root combine_' + channel + '_' + cut + '/mlfit_' + masses + '_' + variable + '.root')
-        os.system('python ~/work/CMSSW_7_4_7/src/HiggsAnalysis/CombinedLimit/test/diffNuisances.py combine_' + channel + '_' + cut + '/mlfit_' + masses + '_' + variable + '.root --histogram pulls_' + channel + '_' + cut + '/pulls_' + masses + '_' + variable + '.root')
-
-
-# combine -M MaxLikelihoodFit -t -1 --expectSignal 1 -d combine_em/monoH_800_300.root
-# python ~/work/CMSSW_7_4_7/src/HiggsAnalysis/CombinedLimit/test/diffNuisances.py mlfit.root --histogram output.root
+    # text2workspace step
+    print "+++++ Translating Datacards to Rootfiles +++++"
+    os.system('text2workspace.py -P HiggsAnalysis.CombinedLimit.PhysicsModel:multiSignalModel  --PO verbose --PO \'map=.*/monoH_*:0\' --PO \'map=.*/monoH_' + masses + ':r[1,0,10]\' datacards/monoH_' + cut + '_' + channel + '/' + variable + '/datacard_combined.txt -o monoH_' + masses + '.root')
+    os.system('mv monoH_' + masses + '.root combine_' + channel + '_' + cut + '/monoH_' + masses + '_' + variable + '.root')
+    
+    # Use combine to calculate limits
+    print "+++++ Extracting Limits +++++"
+    os.system('combine -M Asymptotic -m ' + mZp + '.' + mA0 + ' -t -1 --expectSignal 1 --run expected combine_' + channel + '_' + cut + '/monoH_' + masses + '_' + variable + '.root &> limits_' + channel + '_' + cut + '/monoH_' + masses + '_' + variable + '.txt')
+    os.system('mv higgsCombineTest.Asymptotic* combine_' + channel + '_' + cut + '/higgsCombineTest.Asymptotic.mH' + masses + '_' + variable + '.root')
+    
+    # Produce pulls
+    print "+++++ Producing Pulls +++++"
+    os.system('combine -M MaxLikelihoodFit -t -1 --expectSignal 1 -d combine_' + channel + '_' + cut + '/monoH_' + masses + '_' + variable + '.root')
+    os.system('mv mlfit.root combine_' + channel + '_' + cut + '/mlfit_' + masses + '_' + variable + '.root')
+    os.system('python ~/work/CMSSW_7_4_7/src/HiggsAnalysis/CombinedLimit/test/diffNuisances.py combine_' + channel + '_' + cut + '/mlfit_' + masses + '_' + variable + '.root --histogram pulls_' + channel + '_' + cut + '/pulls_' + masses + '_' + variable + '.root')
