2be734394dcccf9187236be28d3c9dd1d403d9b9
Page Yu <page011235813@gmail.com>
2017-07-21 08:58:07 +0200

    Add ZZ control region.

--- Configurations/ZH4l/cuts.py
+++ Configurations/ZH4l/cuts.py
@@ -1,14 +1,15 @@
 # cuts
 
 #cuts = {}
-eleWP='cut_WP_Tight80X'
-#'cut_WP_Tight80X'
-#'cut_WP_Tight80X_SS
+eleWP='cut_WP_Tight80X' 
+#'cut_WP_Tight80X_SS'  
+#'cut_WP_Tight80X_SS'  
 #'mva_80p_Iso2015'
 #'mva_80p_Iso2016'
 #'mva_90p_Iso2015'
 #'mva_90p_Iso2016'
 
+# supercut = '1'
 supercut = 'std_vector_lepton_pt[0]>25 && std_vector_lepton_pt[1]>15 && std_vector_lepton_pt[2]>10 && std_vector_lepton_pt[3]>10 \
             && std_vector_lepton_pt[4]<10 \
             && chllll_zh4l == 0\
@@ -25,3 +26,9 @@ cuts['zh4l_XSF_13TeV']  = 'flagZ1SF_zh4l==1\
 cuts['zh4l_XDF_13TeV']  = 'flagZ1SF_zh4l==0\
         && z1Mass_zh4l < 75 && z1Mass_zh4l >10 \
         && fabs(z0Mass_zh4l-91.1876)< 15'
+
+cuts['zh4l_ZZ_13TeV']  = 'flagZ1SF_zh4l==1\
+        && z1Mass_zh4l < 105 && z1Mass_zh4l >75 \
+        && metPfType1 < 35 \
+        && fabs(z0Mass_zh4l-91.1876)< 15'
+
