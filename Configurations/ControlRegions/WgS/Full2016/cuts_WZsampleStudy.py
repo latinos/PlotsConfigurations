81892ac86f71b5d46f01c73f8fbcb9c5240690f5
SangEun Lee <d4space@gmail.com>
2017-09-23 13:07:44 +0900

    WgS Full2016 config skeleton

---
+++ Configurations/ControlRegions/WgS/Full2016/cuts_WZsampleStudy.py
@@ -0,0 +1,41 @@
+# cuts from DY
+
+#supercut = '  \
+#             std_vector_lepton_pt[0]>0 && std_vector_lepton_pt[1]>0 \
+#            '
+#
+cuts['NoCut'] = '1==1 '
+
+cuts['TwoLep'] = ' std_vector_lepton_pt[0]>0 && std_vector_lepton_pt[1]>0 '
+
+
+#cuts['Zmm']  = '(std_vector_lepton_flavour[0] * std_vector_lepton_flavour[1] == -13*13)   \
+#                 && mll>80 && mll<100 \
+#               '
+#
+#cuts['Zee0j']  = '(std_vector_lepton_flavour[0] * std_vector_lepton_flavour[1] == -11*11)   \
+#                   && std_vector_lepton_pt[0]>25 && std_vector_lepton_pt[1]>13 \
+#                   && mll>80 && mll<100 \
+#                   && ( std_vector_jet_pt[0] < 30 ) \
+#                 '
+#
+#cuts['Zmm0j']  = '(std_vector_lepton_flavour[0] * std_vector_lepton_flavour[1] == -13*13)   \
+#                   && mll>80 && mll<100 \
+#                   && ( std_vector_jet_pt[0] < 30 ) \
+#                 '
+#
+#cuts['Zee1j']  = '(std_vector_lepton_flavour[0] * std_vector_lepton_flavour[1] == -11*11)   \
+#                   && std_vector_lepton_pt[0]>25 && std_vector_lepton_pt[1]>13 \
+#                   && mll>80 && mll<100 \
+#                   && ( std_vector_jet_pt[0] >= 30 ) \
+#                   && ( std_vector_jet_pt[1] < 30 ) \
+#                 '
+#
+#cuts['Zmm1j']  = '(std_vector_lepton_flavour[0] * std_vector_lepton_flavour[1] == -13*13)   \
+#                   && mll>80 && mll<100 \
+#                   && ( std_vector_jet_pt[0] >= 30 ) \
+#                   && ( std_vector_jet_pt[1] < 30 ) \
+#                 '
+
+
+
