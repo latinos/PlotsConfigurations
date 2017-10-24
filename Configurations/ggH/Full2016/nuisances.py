603c358759e469b773b08b69e9efa2957d6cbcdc
amassiro <massironi.andrea@gmail.com>
2017-10-24 09:45:02 +0200

    small fix for new autoMCstat settings

--- Configurations/ggH/Full2016/nuisances.py
+++ Configurations/ggH/Full2016/nuisances.py
@@ -1047,8 +1047,8 @@ nuisances['TopPtRew']  = {
 ### * autoMCStats 10 1
 
 nuisances['stat']  = {
-              'type'  : 'auto'
-              'maxPoiss'  : '10'
+              'type'  : 'auto',
+              'maxPoiss'  : '10',
               'includeSignal'  : '1'
               #  nuisance ['maxPoiss'] =  Number of threshold events for Poisson modelling
               #  nuisance ['includeSignal'] =  Include MC stat nuisances on signal processes (1=True, 0=False)
