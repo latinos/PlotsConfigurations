b69625c1873abc0bfc924171f17b6a6d41382994
NTrevisa <n.trevisani@campus.unimib.it>
2017-10-24 13:33:42 +0200

    re-introducing workspaces

--- Configurations/monoHWW/Apr2017/scriptMonoHSplit.py
+++ Configurations/monoHWW/Apr2017/scriptMonoHSplit.py
@@ -47,6 +47,7 @@ print "+++++ Combining Datacards +++++"
 
 print "Full Signal Region"  # datacard_600_300.txt.pruned.txt
 os.system("combineCards.py signal=datacards/monoH_" + cut + "_" + channel + "/" + variable + "/datacard_" + mZp + "_" + mA0 + ".txt.pruned.txt WW=datacards/monoH_" + cut + "_WW_" + channel + "/events/datacard_" + mZp + "_" + mA0 + ".txt.pruned.txt Top=datacards/monoH_" + cut + "_Top_" + channel + "/events/datacard_" + mZp + "_" + mA0 + ".txt.pruned.txt DYtt=datacards/monoH_" + cut + "_DYtt_" + channel + "/events/datacard_" + mZp + "_" + mA0 + ".txt.pruned.txt > datacards/monoH_" + cut + "_" + channel + "/" + variable + "/datacard_" + mZp + "_" + mA0 + "_combined.txt")
+print("combineCards.py signal=datacards/monoH_" + cut + "_" + channel + "/" + variable + "/datacard_" + mZp + "_" + mA0 + ".txt.pruned.txt WW=datacards/monoH_" + cut + "_WW_" + channel + "/events/datacard_" + mZp + "_" + mA0 + ".txt.pruned.txt Top=datacards/monoH_" + cut + "_Top_" + channel + "/events/datacard_" + mZp + "_" + mA0 + ".txt.pruned.txt DYtt=datacards/monoH_" + cut + "_DYtt_" + channel + "/events/datacard_" + mZp + "_" + mA0 + ".txt.pruned.txt > datacards/monoH_" + cut + "_" + channel + "/" + variable + "/datacard_" + mZp + "_" + mA0 + "_combined.txt")
 
 print "1/15 Signal Region"
 os.system("combineCards.py signal=datacards_blindData/monoH_" + cut + "_" + channel + "/" + variable + "/datacard_" + mZp + "_" + mA0 + ".txt.pruned.txt WW=datacards/monoH_" + cut + "_WW_" + channel + "/events/datacard_" + mZp + "_" + mA0 + ".txt.pruned.txt Top=datacards/monoH_" + cut + "_Top_" + channel + "/events/datacard_" + mZp + "_" + mA0 + ".txt.pruned.txt DYtt=datacards/monoH_" + cut + "_DYtt_" + channel + "/events/datacard_" + mZp + "_" + mA0 + ".txt.pruned.txt > datacards/monoH_" + cut + "_" + channel + "/" + variable + "/datacard_" + mZp + "_" + mA0 + "_blind.txt")
@@ -58,25 +59,14 @@ os.system('mkdir -p  combine_' + channel + '_' + cut + '/')
 os.system('mkdir -p  limits_' + channel + '_' + cut + '/')
 os.system('mkdir -p  pulls_' + channel + '_' + cut + '/')
 
-##if "Zbar" not in variable: 
+# text2workspace step
+print "+++++ Translating Datacards to Rootfiles +++++"
 
-##    print "Running on 2HDM Model"
+print "Full Signal Region"
+os.system("text2workspace.py --channel-masks datacards/monoH_" + cut + "_" + channel + "/" + variable + "/datacard_" + mZp + "_" + mA0 + "_combined.txt")
 
-    # # Loop over 2HDM model mass points
-    # print "+++++ 2HDM Mass Points +++++"
-    # for mZp in ZpMasses :
-    #     for mA0 in A0Masses :
-            
-# # text2workspace step
-# print "+++++ Translating Datacards to Rootfiles +++++"
-
-# print "Full Signal Region"
-# os.system('text2workspace.py -P HiggsAnalysis.CombinedLimit.PhysicsModel:multiSignalModel  --PO verbose --PO \'map=.*/monoH_*:0\' --PO \'map=.*/monoH_' + mZp + '_' + mA0 + ':r[1,0,10]\' --channel-masks datacards/monoH_' + cut + '_' + channel + '/' + variable + '/datacard_combined.txt -o monoH_' + mZp + '_' + mA0 + '.root')
-# os.system('mv monoH_' + mZp + '_' + mA0 + '.root combine_' + channel + '_' + cut + '/monoH_' + mZp + '_' + mA0 + '_' + variable + '.root')
-
-# print "1/15 Signal Region"
-# os.system('text2workspace.py -P HiggsAnalysis.CombinedLimit.PhysicsModel:multiSignalModel  --PO verbose --PO \'map=.*/monoH_*:0\' --PO \'map=.*/monoH_' + mZp + '_' + mA0 + ':r[1,0,10]\' --channel-masks datacards/monoH_' + cut + '_' + channel + '/' + variable + '/datacard_blind.txt -o monoH_' + mZp + '_' + mA0 + '_blind.root')
-# os.system('mv monoH_' + mZp + '_' + mA0 + '_blind.root combine_' + channel + '_' + cut + '/monoH_' + mZp + '_' + mA0 + '_' + variable + '_blind.root')
+print "1/15 Signal Region"
+os.system("text2workspace.py --channel-masks datacards/monoH_" + cut + "_" + channel + "/" + variable + "/datacard_" + mZp + "_" + mA0 + "_blind.txt")
 
 # Use combine to calculate limits
 
@@ -85,13 +75,15 @@ print "+++++ Extracting Limits +++++"
 print "Full Signal Region"
 
 # datacards/monoH_" + cut + "_" + channel + "/" + variable + "/datacard_" + mZp + "_" + mA0 + "_combined.txt
-os.system('combine -M Asymptotic -n ' + mZp + '_' + mA0 + '_' + variable + ' -t -1 --expectSignal 1 --run expected datacards/monoH_' + cut + '_' + channel + '/' + variable + '/datacard_' + mZp + '_' + mA0 + '_combined.txt &> limits_' + channel + '_' + cut + '/monoH_' + mZp + '_' + mA0 + '_' + variable + '.txt')
+os.system('combine -M Asymptotic -n ' + mZp + '_' + mA0 + '_' + variable + ' -t -1 --expectSignal 1 --run expected datacards/monoH_' + cut + '_' + channel + '/' + variable + '/datacard_' + mZp + '_' + mA0 + '_combined.root &> limits_' + channel + '_' + cut + '/monoH_' + mZp + '_' + mA0 + '_' + variable + '.txt')
 os.system('mv higgsCombine' + mZp + '_' + mA0 + '_' + variable + '.Asymptotic.mH120.root combine_' + channel + '_' + cut + '/')
 
 # datacards/monoH_" + cut + "_" + channel + "/" + variable + "/datacard_" + mZp + "_" + mA0 + "_blind.txt
 print "1/15 Signal Region"
-os.system('combine -M Asymptotic -n ' + mZp + '_' + mA0 + '_' + variable + '_blind datacards/monoH_' + cut + '_' + channel + '/' + variable + '/datacard_' + mZp + '_' + mA0 + '_blind.txt &> limits_' + channel + '_' + cut + '/monoH_' + mZp + '_' + mA0 + '_' + variable + '_blind.txt')
+os.system('combine -M Asymptotic -n ' + mZp + '_' + mA0 + '_' + variable + '_blind datacards/monoH_' + cut + '_' + channel + '/' + variable + '/datacard_' + mZp + '_' + mA0 + '_blind.root &> limits_' + channel + '_' + cut + '/monoH_' + mZp + '_' + mA0 + '_' + variable + '_blind.txt')
 os.system('mv higgsCombine' + mZp + '_' + mA0 + '_' + variable + '_blind.Asymptotic.mH120.root combine_' + channel + '_' + cut + '/')
+
+
 
 if (doPulls=="True") :
     # Produce pulls
@@ -99,7 +91,7 @@ if (doPulls=="True") :
     
     # SB = Signal and Background Asimov for Signal and Control Regions
     print "+++++ Signal and Control Regions. S + B +++++"
-    os.system('combine -M MaxLikelihoodFit -t -1 --expectSignal 1 --robustFit 1 -n _' + mZp + '_' + mA0 + '_' + variable + '_SB --saveShapes --saveWithUncertainties -d datacards/monoH_' + cut + '_' + channel + '/' + variable + '/datacard_' + mZp + '_' + mA0 + '_combined.txt')
+    os.system('combine -M MaxLikelihoodFit -t -1 --expectSignal 1 --robustFit 1 -n _' + mZp + '_' + mA0 + '_' + variable + '_SB --saveShapes --saveWithUncertainties -d datacards/monoH_' + cut + '_' + channel + '/' + variable + '/datacard_' + mZp + '_' + mA0 + '_combined.root')
     os.system('mv mlfit_' + mZp + '_' + mA0 + '_' + variable + '_SB.root combine_' + channel + '_' + cut + '/')
     os.system('python ~/work/CMSSW_7_4_7/src/HiggsAnalysis/CombinedLimit/test/diffNuisances.py combine_' + channel + '_' + cut + '/mlfit_' + mZp + '_' + mA0 + '_' + variable + '_SB.root -a -f latex --histogram pulls_' + channel + '_' + cut + '/pulls_' + mZp + '_' + mA0 + '_' + variable + '_SB.root > pulls_' + channel + '_' + cut + '/pulls_' + mZp + '_' + mA0 + '_' + variable + '_SB.tex')
     os.system("mv higgsCombine_" + mZp + "_" + mA0 + "_" + variable + "_SB.MaxLikelihoodFit.mH120.root combine_" + channel + "_" + cut + "/")
@@ -107,7 +99,7 @@ if (doPulls=="True") :
     
     # B = Background-only Asimov for Signal and Control Regions
     print "+++++ Signal and Control Regions. B Only Asimov +++++"
-    os.system('combine -M MaxLikelihoodFit -t -1 --expectSignal 0 --robustFit 1 -n _' + mZp + '_' + mA0 + '_' + variable + '_B --saveShapes --saveWithUncertainties -d datacards/monoH_' + cut + '_' + channel + '/' + variable + '/datacard_' + mZp + '_' + mA0 + '_combined.txt')
+    os.system('combine -M MaxLikelihoodFit -t -1 --expectSignal 0 --robustFit 1 -n _' + mZp + '_' + mA0 + '_' + variable + '_B --saveShapes --saveWithUncertainties -d datacards/monoH_' + cut + '_' + channel + '/' + variable + '/datacard_' + mZp + '_' + mA0 + '_combined.root')
     os.system('mv mlfit_' + mZp + '_' + mA0 + '_' + variable + '_B.root combine_' + channel + '_' + cut + '/')
     os.system('python ~/work/CMSSW_7_4_7/src/HiggsAnalysis/CombinedLimit/test/diffNuisances.py combine_' + channel + '_' + cut + '/mlfit_' + mZp + '_' + mA0 + '_' + variable + '_B.root -a -f latex --histogram pulls_' + channel + '_' + cut + '/pulls_' + mZp + '_' + mA0 + '_' + variable + '_B.root > pulls_' + channel + '_' + cut + '/pulls_' + mZp + '_' + mA0 + '_' + variable + '_B.tex')
     os.system("mv higgsCombine_" + mZp + "_" + mA0 + "_" + variable + "_B.MaxLikelihoodFit.mH120.root combine_" + channel + "_" + cut + "/")
@@ -115,7 +107,7 @@ if (doPulls=="True") :
     
     # SR = Background-only fit for Data in Signal and Control Regions
     print "+++++ Signal and Control Regions. Unblind +++++"
-    os.system('combine -M MaxLikelihoodFit --robustFit 1 -n _' + mZp + '_' + mA0 + '_' + variable + '_SR --saveShapes --saveWithUncertainties --setPhysicsModelParameters mask_signal=0 -d datacards/monoH_' + cut + '_' + channel + '/' + variable + '/datacard_' + mZp + '_' + mA0 + '_combined.txt')
+    os.system('combine -M MaxLikelihoodFit --robustFit 1 -n _' + mZp + '_' + mA0 + '_' + variable + '_SR --saveShapes --saveWithUncertainties --setPhysicsModelParameters mask_signal=0 -d datacards/monoH_' + cut + '_' + channel + '/' + variable + '/datacard_' + mZp + '_' + mA0 + '_combined.root')
     os.system('mv mlfit_' + mZp + '_' + mA0 + '_' + variable + '_SR.root combine_' + channel + '_' + cut + '/')
     os.system('python ~/work/CMSSW_7_4_7/src/HiggsAnalysis/CombinedLimit/test/diffNuisances.py combine_' + channel + '_' + cut + '/mlfit_' + mZp + '_' + mA0 + '_' + variable + '_SR.root -a -f latex --histogram pulls_' + channel + '_' + cut + '/pulls_' + mZp + '_' + mA0 + '_' + variable + '_SR.root > pulls_' + channel + '_' + cut + '/pulls_' + mZp + '_' + mA0 + '_' + variable + '_SR.tex')
     os.system("mv higgsCombine_" + mZp + "_" + mA0 + "_" + variable + "_SR.MaxLikelihoodFit.mH120.root combine_" + channel + "_" + cut + "/")
@@ -123,7 +115,7 @@ if (doPulls=="True") :
     
     # CR = Background-only fit for Data in Control Regions
     print "+++++ Control Regions Only +++++"
-    os.system('combine -M MaxLikelihoodFit --robustFit 1 -n _' + mZp + '_' + mA0 + '_' + variable + '_CR --saveShapes --saveWithUncertainties --setPhysicsModelParameters mask_signal=1 -d datacards/monoH_' + cut + '_' + channel + '/' + variable + '/datacard_' + mZp + '_' + mA0 + '_combined.txt')
+    os.system('combine -M MaxLikelihoodFit --robustFit 1 -n _' + mZp + '_' + mA0 + '_' + variable + '_CR --saveShapes --saveWithUncertainties --setPhysicsModelParameters mask_signal=1 -d datacards/monoH_' + cut + '_' + channel + '/' + variable + '/datacard_' + mZp + '_' + mA0 + '_combined.root')
     os.system('mv mlfit_' + mZp + '_' + mA0 + '_' + variable + '_CR.root combine_' + channel + '_' + cut + '/')
     os.system('python ~/work/CMSSW_7_4_7/src/HiggsAnalysis/CombinedLimit/test/diffNuisances.py combine_' + channel + '_' + cut + '/mlfit_' + mZp + '_' + mA0 + '_' + variable + '_CR.root -a -f latex --histogram pulls_' + channel + '_' + cut + '/pulls_' + mZp + '_' + mA0 + '_' + variable + '_CR.root > pulls_' + channel + '_' + cut + '/pulls_' + mZp + '_' + mA0 + '_' + variable + '_CR.tex')
     os.system("mv higgsCombine_" + mZp + "_" + mA0 + "_" + variable + "_CR.MaxLikelihoodFit.mH120.root combine_" + channel + "_" + cut + "/")
@@ -131,7 +123,7 @@ if (doPulls=="True") :
     
     # BSR = Background-only fit for Data in Signal and Control Regions
     print "+++++ 1/15 Signal Region and Control Regions. Unblind +++++"
-    os.system('combine -M MaxLikelihoodFit --robustFit 1 -n _' + mZp + '_' + mA0 + '_' + variable + '_BSR --saveShapes --saveWithUncertainties --setPhysicsModelParameters mask_signal=0 -d datacards/monoH_' + cut + '_' + channel + '/' + variable + '/datacard_' + mZp + '_' + mA0 + '_blind.txt')
+    os.system('combine -M MaxLikelihoodFit --robustFit 1 -n _' + mZp + '_' + mA0 + '_' + variable + '_BSR --saveShapes --saveWithUncertainties --setPhysicsModelParameters mask_signal=0 -d datacards/monoH_' + cut + '_' + channel + '/' + variable + '/datacard_' + mZp + '_' + mA0 + '_blind.root')
     os.system('mv mlfit_' + mZp + '_' + mA0 + '_' + variable + '_BSR.root combine_' + channel + '_' + cut + '/')
     os.system('python ~/work/CMSSW_7_4_7/src/HiggsAnalysis/CombinedLimit/test/diffNuisances.py combine_' + channel + '_' + cut + '/mlfit_' + mZp + '_' + mA0 + '_' + variable + '_BSR.root -a -f latex --histogram pulls_' + channel + '_' + cut + '/pulls_' + mZp + '_' + mA0 + '_' + variable + '_BSR.root > pulls_' + channel + '_' + cut + '/pulls_' + mZp + '_' + mA0 + '_' + variable + '_BSR.tex')
     os.system("mv higgsCombine_" + mZp + "_" + mA0 + "_" + variable + "_BSR.MaxLikelihoodFit.mH120.root combine_" + channel + "_" + cut + "/")
@@ -139,13 +131,13 @@ if (doPulls=="True") :
     
     # Produce Goodness of Fit test
     print "+++++ Producing Goodness of Fit test +++++"
-    os.system('combine -M GoodnessOfFit -n _' + mZp + '_' + mA0 + '_' + variable + '_AD datacards/monoH_' + cut + '_' + channel + '/' + variable + '/datacard_' + mZp + '_' + mA0 + '_combined.txt --algo=AD --setPhysicsModelParameters mask_signal=0 > goodnessOfFit_' + channel + '_' + cut + '/monoH_' + mZp + '_' + mA0 + '_' + variable + '_AD.txt')
+    os.system('combine -M GoodnessOfFit -n _' + mZp + '_' + mA0 + '_' + variable + '_AD datacards/monoH_' + cut + '_' + channel + '/' + variable + '/datacard_' + mZp + '_' + mA0 + '_combined.root --algo=AD --setPhysicsModelParameters mask_signal=0 > goodnessOfFit_' + channel + '_' + cut + '/monoH_' + mZp + '_' + mA0 + '_' + variable + '_AD.txt')
     os.system('mv higgsCombine_' + mZp + '_' + mA0 + '_' + variable + '_AD.GoodnessOfFit.mH120.root goodnessOfFit_' + channel +'_' + cut + '/')
     
-    os.system('combine -M GoodnessOfFit -n _' + mZp + '_' + mA0 + '_' + variable + '_KS datacards/monoH_' + cut + '_' + channel + '/' + variable + '/datacard_' + mZp + '_' + mA0 + '_combined.txt --algo=KS --setPhysicsModelParameters mask_signal=0 >> goodnessOfFit_' + channel + '_' + cut + '/monoH_' + mZp + '_' + mA0 + '_' + variable + '_KS.txt')
+    os.system('combine -M GoodnessOfFit -n _' + mZp + '_' + mA0 + '_' + variable + '_KS datacards/monoH_' + cut + '_' + channel + '/' + variable + '/datacard_' + mZp + '_' + mA0 + '_combined.root --algo=KS --setPhysicsModelParameters mask_signal=0 >> goodnessOfFit_' + channel + '_' + cut + '/monoH_' + mZp + '_' + mA0 + '_' + variable + '_KS.txt')
     os.system('mv higgsCombine_' + mZp + '_' + mA0 + '_' + variable + '_KS.GoodnessOfFit.mH120.root goodnessOfFit_' + channel +'_' + cut + '/')
     
-    os.system('combine -M GoodnessOfFit -n _' + mZp + '_' + mA0 + '_' + variable + '_SAT datacards/monoH_' + cut + '_' + channel + '/' + variable + '/datacard_' + mZp + '_' + mA0 + '_combined.txt --algo=saturated --setPhysicsModelParameters mask_signal=0 >> goodnessOfFit_' + channel + '_' + cut + '/monoH_' + mZp + '_' + mA0 + '_' + variable + '_saturated.txt')
+    os.system('combine -M GoodnessOfFit -n _' + mZp + '_' + mA0 + '_' + variable + '_SAT datacards/monoH_' + cut + '_' + channel + '/' + variable + '/datacard_' + mZp + '_' + mA0 + '_combined.root --algo=saturated --setPhysicsModelParameters mask_signal=0 >> goodnessOfFit_' + channel + '_' + cut + '/monoH_' + mZp + '_' + mA0 + '_' + variable + '_saturated.txt')
     os.system('mv higgsCombine_' + mZp + '_' + mA0 + '_' + variable + '_SAT.GoodnessOfFit.mH120.root goodnessOfFit_' + channel +'_' + cut + '/')
 
 
