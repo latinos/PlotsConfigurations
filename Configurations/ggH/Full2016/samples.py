8df8bac0a63f9a76c869f7c0053470667b0c0603
Lorenzo Viliani <lorenzo.viliani@cern.ch>
2017-11-24 12:01:16 +0100

    fix

--- Configurations/ggH/Full2016/samples.py
+++ Configurations/ggH/Full2016/samples.py
@@ -56,13 +56,13 @@ Nlep='2'
 ################################################
 
 XSWeight      = 'XSWeight'
-#SFweight      = 'SFweight'+Nlep+'l'
-SFweight      = 'puW*\
-                 effTrigW*\
-                 std_vector_lepton_recoW[0]*\
-                 std_vector_lepton_recoW[1]*\
-                 electron_etaW_2l*electron_ptW_2l*\
-                 veto_EMTFBug'
+SFweight      = 'SFweight'+Nlep+'l'
+#SFweight      = 'puW*\
+#                 effTrigW*\
+#                 std_vector_lepton_recoW[0]*\
+#                 std_vector_lepton_recoW[1]*\
+#                 electron_etaW_2l*electron_ptW_2l*\
+#                 veto_EMTFBug'
 
 GenLepMatch   = 'GenLepMatch'+Nlep+'l'
 
