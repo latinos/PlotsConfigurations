7450ae603c0f73505dee77e49335d04fe65a4e2e
lorenzo russo <l.russo@cern.ch>
2018-04-13 15:52:59 +0200

    HM fix nuis SF

--- Configurations/EXO/WWlvlv_VBF_SF_embeddedWeights/Full2016/configuration_ee.py
+++ Configurations/EXO/WWlvlv_VBF_SF_embeddedWeights/Full2016/configuration_ee.py
@@ -1,10 +1,9 @@
 
 # example of configuration file
-tag = 'HWWhighMass2016_SF_embeddedWeights_ee_VBF_V3'
+tag = 'HWWhighMass2016_SF_embeddedWeights_ee_VBF'
 
 # used by mkShape to define output directory for root files
-outputDir = 'rootFile_SF_ee_VBF_V3'
-
+outputDir = 'rootFile_SF_ee_VBF'
 
 # file with list of variables
 variablesFile = 'variables_SF.py'
@@ -23,18 +22,14 @@ lumi = 35.9
 
 # used by mkPlot to define output directory for plots
 # different from "outputDir" to do things more tidy
-outputDirPlots = 'plotHWWhighMass_SF_ee_V3'
-
+outputDirPlots = 'plotHWWhighMass_SF_ee'
 
 # used by mkDatacards to define output directory for datacards
 outputDirDatacard = 'datacards_SF_ee_VBF'
 
-
 # structure file for datacard
 structureFile = 'structure_SF_merge.py'
 
-
 # nuisances file for mkDatacards and for mkShape
 nuisancesFile = 'nuisances_SF_ee.py'
-
 #nuisancesFile = 'nuisances_VUOTO.py'
