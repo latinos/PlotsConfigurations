d08efa088a973f689177af895a423f7129879f09
Lorenzo Viliani <lorenzo.viliani@cern.ch>
2018-05-15 19:08:19 +0200

    useful scripts

--- Configurations/ggH/Full2016/scripts/printPrePostFit.C
+++ Configurations/ggH/Full2016/scripts/printPrePostFit.C
@@ -5,19 +5,61 @@ void printPrePostFit(std::string process = "ggH_hww") {
   RooArgSet*     post = (RooArgSet*) _file0->Get("norm_fit_s");
   RooArgSet*     pre = (RooArgSet*) _file0->Get("norm_prefit");
 
-  cuts.push_back("of0j_DYtt");
-  cuts.push_back("of1j_DYtt");
-  cuts.push_back("of0j_Top");
-  cuts.push_back("of1j_Top");
-  cuts.push_back("em_mp_0j");
-  cuts.push_back("me_mp_0j");
-  cuts.push_back("em_pm_0j");
-  cuts.push_back("me_pm_0j");
-  cuts.push_back("em_mp_1j");
-  cuts.push_back("me_mp_1j");
-  cuts.push_back("em_pm_1j");
-  cuts.push_back("me_pm_1j");
+  cuts.push_back("ggHtag_of0j_em_mp_0j_pt2ge20"); 
+  cuts.push_back("ggHtag_of0j_em_mp_0j_pt2lt20");
+  cuts.push_back("ggHtag_of0j_em_pm_0j_pt2ge20");
+  cuts.push_back("ggHtag_of0j_em_pm_0j_pt2lt20");
+  cuts.push_back("ggHtag_of0j_me_mp_0j_pt2ge20");
+  cuts.push_back("ggHtag_of0j_me_mp_0j_pt2lt20");
+  cuts.push_back("ggHtag_of0j_me_pm_0j_pt2ge20");
+  cuts.push_back("ggHtag_of0j_me_pm_0j_pt2lt20");
+  cuts.push_back("ggHtag_of0j_of0j_DYtt");
+  cuts.push_back("ggHtag_of0j_of0j_Top");
+  cuts.push_back("ggHtag_of1j_em_mp_1j_pt2ge20");
+  cuts.push_back("ggHtag_of1j_em_mp_1j_pt2lt20");
+  cuts.push_back("ggHtag_of1j_em_pm_1j_pt2ge20");
+  cuts.push_back("ggHtag_of1j_em_pm_1j_pt2lt20");
+  cuts.push_back("ggHtag_of1j_me_mp_1j_pt2ge20");
+  cuts.push_back("ggHtag_of1j_me_mp_1j_pt2lt20");
+  cuts.push_back("ggHtag_of1j_me_pm_1j_pt2ge20");
+  cuts.push_back("ggHtag_of1j_me_pm_1j_pt2lt20");
+  cuts.push_back("ggHtag_of1j_of1j_DYtt");
+  cuts.push_back("ggHtag_of1j_of1j_Top");
+  cuts.push_back("ggHtag_of2j_of2j_DYtt");
+  cuts.push_back("ggHtag_of2j_of2j_Top");
+  cuts.push_back("ggHtag_of2j_of_2j");
+  cuts.push_back("ggHtag_sf0j_sf_0j_ee_Top");
+  cuts.push_back("ggHtag_sf0j_sf_0j_ee_WW");
+  cuts.push_back("ggHtag_sf0j_sf_0j_ee_pt2ge20");
+  cuts.push_back("ggHtag_sf0j_sf_0j_ee_pt2lt20");
+  cuts.push_back("ggHtag_sf0j_sf_0j_mm_Top");
+  cuts.push_back("ggHtag_sf0j_sf_0j_mm_WW");
+  cuts.push_back("ggHtag_sf0j_sf_0j_mm_pt2ge20");
+  cuts.push_back("ggHtag_sf0j_sf_0j_mm_pt2lt20");
+  cuts.push_back("ggHtag_sf1j_sf_1j_Top");
+  cuts.push_back("ggHtag_sf1j_sf_1j_ee");
+  cuts.push_back("ggHtag_sf1j_sf_1j_ee_WW");
+  cuts.push_back("ggHtag_sf1j_sf_1j_mm");
+  cuts.push_back("ggHtag_sf1j_sf_1j_mm_WW");
+  cuts.push_back("vbftag_of2j_vbf_2j_DYtt");
+  cuts.push_back("vbftag_of2j_vbf_2j_Top");
+  cuts.push_back("vbftag_of2j_vbf_2j_highmjj");
+  cuts.push_back("vbftag_of2j_vbf_2j_lowmjj");
+  cuts.push_back("vhtag_of2j_vh2j_DYtt");
+  cuts.push_back("vhtag_of2j_vh2j_Top");
+  cuts.push_back("vhtag_of2j_vh_2j");
+  cuts.push_back("wh3ltag_wh3l_13TeV_ossf");
+  cuts.push_back("wh3ltag_wh3l_sssf");
+  cuts.push_back("wh3ltag_wh3l_wz_13TeV");
+  cuts.push_back("wh3ltag_wh3l_zg_13TeV");
+  cuts.push_back("zh4ltag_zh4l_XDF_13TeV");
+  cuts.push_back("zh4ltag_zh4l_XSF_13TeV");
+  cuts.push_back("zh4ltag_zh4l_ZZ_13TeV");
 
+  cuts.push_back("vh_2j");
+
+
+  std::cout << std::left << setw(30) << "category/process " << std::left << setw(30) << " prefit " << std::left << setw(30) << " postfit " << std::left << setw(30) << " postfit/prefit " << std::endl;
   for (int icut = 0; icut < cuts.size(); icut++) {  
     RooRealVar *norm_pre, *norm_post;
           
@@ -28,7 +70,14 @@ void printPrePostFit(std::string process = "ggH_hww") {
     norm_post = (RooRealVar* ) post->find(name.Data()); 
     norm_pre  = (RooRealVar* ) pre->find(name.Data());
 
-    std::cout << cuts.at(icut).c_str() << "/" << process.c_str() << " --> prefit = " << norm_pre->getVal() << " postfit = " << norm_post->getVal() << " postfit/prefit = " << norm_post->getVal()/norm_pre->getVal() << std::endl;
+    TString pre   = Form ("%.2f +/- %.2f", norm_pre->getVal(), norm_pre->getError());
+    TString post  = Form ("%.2f +/- %.2f", norm_post->getVal(), norm_post->getError());
+    TString ratio = Form ("%.2f", norm_post->getVal()/norm_pre->getVal());
+
+    std::cout << std::left << setw(30) << name << setw(30) << pre << setw(33) << post << setw(20) << ratio << endl; 
+//    std::cout <<  setw(30) << setprecision(3) << norm_pre->getVal() << " +/- " << setprecision(3) << norm_pre->getError() <<  setw(30) << setprecision(3) << norm_post->getVal() << " +/- " << setprecision(3) << norm_post->getError() <<  setw(30) << setprecision(3) << norm_post->getVal()/norm_pre->getVal()<< std::endl;
+//    std::cout <<  setw(30) << setprecision(3) << norm_post->getVal() << setw(2)  << " +/- " << setw(5) << setprecision(3) << norm_post->getError();
+//    std::cout <<  setw(30) << setprecision(3) << norm_post->getVal()/norm_pre->getVal()<< std::endl;
   }
 }
 
