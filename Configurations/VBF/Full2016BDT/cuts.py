9a4d38b47e99f6a607ba3b753dcb81d7e6553546
arun <arun.kumar@cern.ch>
2018-10-15 22:07:30 +0200

    final BDT configuration including ggH-VBF discriminator

--- Configurations/VBF/Full2016BDT/cuts.py
+++ Configurations/VBF/Full2016BDT/cuts.py
@@ -9,16 +9,109 @@ supercut = 'mll>12  \
             && (std_vector_lepton_flavour[0] * std_vector_lepton_flavour[1] == -11*13) \
             '
 
-
 cuts['hww2l2v_13TeV_of2j_vbf']  = '(std_vector_lepton_flavour[0] * std_vector_lepton_flavour[1] == -11*13)    \
                                       && (abs(std_vector_lepton_flavour[1]) == 13 || std_vector_lepton_pt[1]>13) \
                                       && (abs((std_vector_lepton_eta[0] - (jeteta1+jeteta2)/2)/detajj) < 0.5) \
                                       && (abs((std_vector_lepton_eta[1] - (jeteta1+jeteta2)/2)/detajj) < 0.5) \
                                       && (abs(std_vector_jet_eta[0])<4.7) && (abs(std_vector_jet_eta[1])<4.7) \
+                                      && (std_vector_jet_pt[0] > 30. && std_vector_jet_pt[1]>30.) \
                                       && (njet==2) \
+                                      && metPfType1 > 20 \
                                       && (detajj>3.0 && mjj>300) \
                                       && '+bVeto+' \
+                                      && hww_VBF_mvaBDT_ggH(mjj,jetpt1,jetpt2,detajj,jeteta1,jeteta2) > 0.5 \
                                       '
+
+#cuts['hww2l2v_13TeV_of2j_vbf_noMVAShape']  = '(std_vector_lepton_flavour[0] * std_vector_lepton_flavour[1] == -11*13)    \
+#                                      && (abs(std_vector_lepton_flavour[1]) == 13 || std_vector_lepton_pt[1]>13) \
+#                                      && (abs((std_vector_lepton_eta[0] - (jeteta1+jeteta2)/2)/detajj) < 0.5) \
+#                                      && (abs((std_vector_lepton_eta[1] - (jeteta1+jeteta2)/2)/detajj) < 0.5) \
+#                                      && (abs(std_vector_jet_eta[0])<4.7) && (abs(std_vector_jet_eta[1])<4.7) \
+#                                      && (std_vector_jet_pt[0] > 30. && std_vector_jet_pt[1]>30.) \
+#                                      && (njet==2) \
+#                                      && hww_VBF_mvaBDT(mll,mjj,jetpt1,jetpt2,detajj,mth,dphill,dphillmet,mR) > 0. \
+#                                      && metPfType1 > 20 \
+#                                      && (detajj>3.0 && mjj>300) \
+#                                      && '+bVeto+' \
+#                                      && hww_VBF_mvaBDT_ggH(mjj,jetpt1,jetpt2,detajj,jeteta1,jeteta2) > 0.5 \
+#                                      '
+
+#cuts['hww2l2v_13TeV_of2j_vbf']  = '(std_vector_lepton_flavour[0] * std_vector_lepton_flavour[1] == -11*13)    \
+#                                      && (abs(std_vector_lepton_flavour[1]) == 13 || std_vector_lepton_pt[1]>13) \
+#                                      && (abs((std_vector_lepton_eta[0] - (jeteta1+jeteta2)/2)/detajj) < 0.5) \
+#                                      && (abs((std_vector_lepton_eta[1] - (jeteta1+jeteta2)/2)/detajj) < 0.5) \
+#                                      && (abs(std_vector_jet_eta[0])<4.7) && (abs(std_vector_jet_eta[1])<4.7) \
+#                                      && (njet==2) \
+#                                      && (detajj>3.5 && mjj>400) \
+#                                      && '+bVeto+' \
+#                                      '
+#
+#
+#cuts['hww2l2v_13TeV_of2j_vbf_detajj']  = '(std_vector_lepton_flavour[0] * std_vector_lepton_flavour[1] == -11*13)    \
+#                                      && (abs(std_vector_lepton_flavour[1]) == 13 || std_vector_lepton_pt[1]>13) \
+#                                      && (abs((std_vector_lepton_eta[0] - (jeteta1+jeteta2)/2)/detajj) < 0.5) \
+#                                      && (abs((std_vector_lepton_eta[1] - (jeteta1+jeteta2)/2)/detajj) < 0.5) \
+#                                      && (abs(std_vector_jet_eta[0])<4.7) && (abs(std_vector_jet_eta[1])<4.7) \
+#                                      && (njet==2) \
+#                                      && (detajj>3.0 && mjj>400) \
+#                                      && '+bVeto+' \
+#                                      '
+#
+#
+#cuts['hww2l2v_13TeV_of2j_vbf_mjj']  = '(std_vector_lepton_flavour[0] * std_vector_lepton_flavour[1] == -11*13)    \
+#                                      && (abs(std_vector_lepton_flavour[1]) == 13 || std_vector_lepton_pt[1]>13) \
+#                                      && (abs((std_vector_lepton_eta[0] - (jeteta1+jeteta2)/2)/detajj) < 0.5) \
+#                                      && (abs((std_vector_lepton_eta[1] - (jeteta1+jeteta2)/2)/detajj) < 0.5) \
+#                                      && (abs(std_vector_jet_eta[0])<4.7) && (abs(std_vector_jet_eta[1])<4.7) \
+#                                      && (njet==2) \
+#                                      && (detajj>3.5 && mjj>300) \
+#                                      && '+bVeto+' \
+#                                      '
+#
+#cuts['hww2l2v_13TeV_of2j_vbf_detajj_mjj']  = '(std_vector_lepton_flavour[0] * std_vector_lepton_flavour[1] == -11*13)    \
+#                                      && (abs(std_vector_lepton_flavour[1]) == 13 || std_vector_lepton_pt[1]>13) \
+#                                      && (abs((std_vector_lepton_eta[0] - (jeteta1+jeteta2)/2)/detajj) < 0.5) \
+#                                      && (abs((std_vector_lepton_eta[1] - (jeteta1+jeteta2)/2)/detajj) < 0.5) \
+#                                      && (abs(std_vector_jet_eta[0])<4.7) && (abs(std_vector_jet_eta[1])<4.7) \
+#                                      && (njet==2) \
+#                                      && (detajj>3.0 && mjj>300) \
+#                                      && '+bVeto+' \
+#                                      '
+#
+#cuts['hww2l2v_13TeV_of2j_vbf_met']  = '(std_vector_lepton_flavour[0] * std_vector_lepton_flavour[1] == -11*13)    \
+#                                      && (abs(std_vector_lepton_flavour[1]) == 13 || std_vector_lepton_pt[1]>13) \
+#                                      && (abs((std_vector_lepton_eta[0] - (jeteta1+jeteta2)/2)/detajj) < 0.5) \
+#                                      && (abs((std_vector_lepton_eta[1] - (jeteta1+jeteta2)/2)/detajj) < 0.5) \
+#                                      && (abs(std_vector_jet_eta[0])<4.7) && (abs(std_vector_jet_eta[1])<4.7) \
+#                                      && (njet==2) \
+#                                      && metPfType1 > 20 \
+#                                      && (detajj>3.5 && mjj>400) \
+#                                      && '+bVeto+' \
+#                                      '
+
+#cuts['hww2l2v_13TeV_of2j_vbf_detajj_mjj_met']  = '(std_vector_lepton_flavour[0] * std_vector_lepton_flavour[1] == -11*13)    \
+#                                      && (abs(std_vector_lepton_flavour[1]) == 13 || std_vector_lepton_pt[1]>13) \
+#                                      && (abs((std_vector_lepton_eta[0] - (jeteta1+jeteta2)/2)/detajj) < 0.5) \
+#                                      && (abs((std_vector_lepton_eta[1] - (jeteta1+jeteta2)/2)/detajj) < 0.5) \
+#                                      && (abs(std_vector_jet_eta[0])<4.7) && (abs(std_vector_jet_eta[1])<4.7) \
+#                                      && (njet==2) \
+#                                      && metPfType1 > 20 \
+#                                      && (detajj>3.0 && mjj>300) \
+#                                      && '+bVeto+' \
+#                                      '
+
+#cuts['hww2l2v_13TeV_of2j_vbf_detajj_mjj_met_jetpt']  = '(std_vector_lepton_flavour[0] * std_vector_lepton_flavour[1] == -11*13)    \
+#                                      && (abs(std_vector_lepton_flavour[1]) == 13 || std_vector_lepton_pt[1]>13) \
+#                                      && (abs((std_vector_lepton_eta[0] - (jeteta1+jeteta2)/2)/detajj) < 0.5) \
+#                                      && (abs((std_vector_lepton_eta[1] - (jeteta1+jeteta2)/2)/detajj) < 0.5) \
+#                                      && (abs(std_vector_jet_eta[0])<4.7) && (abs(std_vector_jet_eta[1])<4.7) \
+#                                      && (std_vector_jet_pt[0] > 30. && std_vector_jet_pt[1]>30.) \
+#                                      && (njet==2) \
+#                                      && metPfType1 > 20 \
+#                                      && (detajj>3.0 && mjj>300) \
+#                                      && '+bVeto+' \
+#                                      '
+
 #
 # control regions
 #
--- Configurations/VBF/Full2016BDT/variables.py
+++ Configurations/VBF/Full2016BDT/variables.py
@@ -4,266 +4,34 @@
     
 #'fold' : # 0 = not fold (default), 1 = fold underflowbin, 2 = fold overflow bin, 3 = fold underflow and overflow
 
-#variables['mucca_BDT'] = {
-#     'name': 'hww_VBF_mvaBDT(mll,mjj,jetpt1,jetpt2,detajj,jeteta1,jeteta2,metPfType1,mth)',
-#     'range' : (20,-1.,1.),
-#     'xaxis' : 'MVA discriminant',
-#     'fold' : 3,
-#     'linesToAdd' : ['.L /afs/cern.ch/work/a/arun/Latinos/HWW_Full2016/CMSSW_8_0_26_patch1/src/PlotsConfigurations/Configurations/VBF/Full2016BDT/mymacros/hww_VBF_mvaBDT.C+', 'initmyreaderBDT()']
-#     }
-
-variables['mucca_BDT12'] = {
-     'name': 'hww_VBF_mvaBDT12(mll,mjj,jetpt1,jetpt2,detajj,jeteta1,jeteta2,metPfType1,mth)',
-     'range' : (20,-1.,1.),
+variables['mva_vbf_ggH'] = {
+     'name': 'hww_VBF_mvaBDT_ggH(mjj,jetpt1,jetpt2,detajj,jeteta1,jeteta2)',
+     'range' : (15,-1.,1.),
      'xaxis' : 'MVA discriminant',
      'fold' : 3,
-     'linesToAdd' : ['.L /afs/cern.ch/work/a/arun/Latinos/HWW_Full2016/CMSSW_8_0_26_patch1/src/PlotsConfigurations/Configurations/VBF/Full2016BDT/mymacros/hww_VBF_mvaBDT12.C+', 'initmyreaderBDT12()']
+     'linesToAdd' : ['.L /afs/cern.ch/work/a/arun/Latinos/HWW_Full2016/CMSSW_8_0_26_patch1/src/PlotsConfigurations/Configurations/VBF/Full2016BDT/mymacros/hww_VBF_mvaBDT_ggH.C+', 'initmyreaderBDT1()']
      }
 
-#
-#variables['mucca_BDT2'] = {
-#     'name': 'hww_VBF_mvaBDT2(mll,mjj,jetpt1,jetpt2,detajj,jeteta1,jeteta2,metPfType1,mth)',
-#     'range' : (20,-1.,1.),
-#     'xaxis' : 'MVA discriminant',
-#     'fold' : 3,
-#     'linesToAdd' : ['.L /afs/cern.ch/work/a/arun/Latinos/HWW_Full2016/CMSSW_8_0_26_patch1/src/PlotsConfigurations/Configurations/VBF/MyChecks/SignalRegion/MVATesting/hww_VBF_mvaBDT2.C+', 'initmyreaderBDT2()']
-#     }
-#
-#variables['mucca_BDT3'] = {
-#     'name': 'hww_VBF_mvaBDT3(mll,mjj,jetpt1,jetpt2,detajj,jeteta1,jeteta2,metPfType1,mth)',
-#     'range' : (20,-1.,1.),
-#     'xaxis' : 'MVA discriminant',
-#     'fold' : 3,
-#     'linesToAdd' : ['.L /afs/cern.ch/work/a/arun/Latinos/HWW_Full2016/CMSSW_8_0_26_patch1/src/PlotsConfigurations/Configurations/VBF/MyChecks/SignalRegion/MVATesting/hww_VBF_mvaBDT3.C+', 'initmyreaderBDT3()']
-#     }
-#
-#variables['mucca_BDT4'] = {
-#     'name': 'hww_VBF_mvaBDT4(mll,mjj,jetpt1,jetpt2,detajj,jeteta1,jeteta2,metPfType1,mth)',
-#     'range' : (20,-1.,1.),
-#     'xaxis' : 'MVA discriminant',
-#     'fold' : 3,
-#     'linesToAdd' : ['.L /afs/cern.ch/work/a/arun/Latinos/HWW_Full2016/CMSSW_8_0_26_patch1/src/PlotsConfigurations/Configurations/VBF/MyChecks/SignalRegion/MVATesting/hww_VBF_mvaBDT4.C+', 'initmyreaderBDT4()']
-#     }
-#
-#variables['mucca_BDT5'] = {
-#     'name': 'hww_VBF_mvaBDT5(mll,mjj,jetpt1,jetpt2,detajj,jeteta1,jeteta2,metPfType1,mth)',
-#     'range' : (20,-1.,1.),
-#     'xaxis' : 'MVA discriminant',
-#     'fold' : 3,
-#     'linesToAdd' : ['.L /afs/cern.ch/work/a/arun/Latinos/HWW_Full2016/CMSSW_8_0_26_patch1/src/PlotsConfigurations/Configurations/VBF/MyChecks/SignalRegion/MVATesting/hww_VBF_mvaBDT5.C+', 'initmyreaderBDT5()']
-#     }
-#
-#variables['mucca_BDT6'] = {
-#     'name': 'hww_VBF_mvaBDT6(mll,mjj,jetpt1,jetpt2,detajj,jeteta1,jeteta2,metPfType1,mth)',
-#     'range' : (20,-1.,1.),
-#     'xaxis' : 'MVA discriminant',
-#     'fold' : 3,
-#     'linesToAdd' : ['.L /afs/cern.ch/work/a/arun/Latinos/HWW_Full2016/CMSSW_8_0_26_patch1/src/PlotsConfigurations/Configurations/VBF/MyChecks/SignalRegion/MVATesting/hww_VBF_mvaBDT6.C+', 'initmyreaderBDT6()']
-#     }
-#
-#variables['mucca_BDT7'] = {
-#     'name': 'hww_VBF_mvaBDT7(mll,mjj,jetpt1,jetpt2,detajj,jeteta1,jeteta2,metPfType1,mth)',
-#     'range' : (20,-1.,1.),
-#     'xaxis' : 'MVA discriminant',
-#     'fold' : 3,
-#     'linesToAdd' : ['.L /afs/cern.ch/work/a/arun/Latinos/HWW_Full2016/CMSSW_8_0_26_patch1/src/PlotsConfigurations/Configurations/VBF/MyChecks/SignalRegion/MVATesting/hww_VBF_mvaBDT7.C+', 'initmyreaderBDT7()']
-#     }
-#
-#variables['mucca_BDT8'] = {
-#     'name': 'hww_VBF_mvaBDT8(mll,mjj,jetpt1,jetpt2,detajj,jeteta1,jeteta2,metPfType1,mth)',
-#     'range' : (20,-1.,1.),
-#     'xaxis' : 'MVA discriminant',
-#     'fold' : 3,
-#     'linesToAdd' : ['.L /afs/cern.ch/work/a/arun/Latinos/HWW_Full2016/CMSSW_8_0_26_patch1/src/PlotsConfigurations/Configurations/VBF/MyChecks/SignalRegion/MVATesting/hww_VBF_mvaBDT8.C+', 'initmyreaderBDT8()']
-#     }
-
-#variables['mucca_BDT11'] = {
-#     'name': 'hww_VBF_mvaBDT11(mll,mjj,jetpt1,jetpt2,detajj,jeteta1,jeteta2,metPfType1,mth)',
-#     'range' : (20,-1.,1.),
-#     'xaxis' : 'MVA discriminant',
-#     'fold' : 3,
-#     'linesToAdd' : ['.L /afs/cern.ch/work/a/arun/Latinos/HWW_Full2016/CMSSW_8_0_26_patch1/src/PlotsConfigurations/Configurations/VBF/MyChecks/SignalRegion/MVATesting/hww_VBF_mvaBDT11.C+', 'initmyreaderBDT11()']
-#     }
-
-#variables['mucca_BDT14'] = {
-#     'name': 'hww_VBF_mvaBDT14(mll,mjj,jetpt1,jetpt2,detajj,jeteta1,jeteta2,metPfType1,mth)',
-#     'range' : (20,-1.,1.),
-#     'xaxis' : 'MVA discriminant',
-#     'fold' : 3,
-#     'linesToAdd' : ['.L /afs/cern.ch/work/a/arun/Latinos/HWW_Full2016/CMSSW_8_0_26_patch1/src/PlotsConfigurations/Configurations/VBF/MyChecks/SignalRegion/MVATesting/hww_VBF_mvaBDT14.C+', 'initmyreaderBDT14()']
-#     }
-#
-#variables['mucca_BDT15'] = {
-#     'name': 'hww_VBF_mvaBDT15(mll,mjj,jetpt1,jetpt2,detajj,jeteta1,jeteta2,metPfType1,mth)',
-#     'range' : (20,-1.,1.),
-#     'xaxis' : 'MVA discriminant',
-#     'fold' : 3,
-#     'linesToAdd' : ['.L /afs/cern.ch/work/a/arun/Latinos/HWW_Full2016/CMSSW_8_0_26_patch1/src/PlotsConfigurations/Configurations/VBF/MyChecks/SignalRegion/MVATesting/hww_VBF_mvaBDT15.C+', 'initmyreaderBDT15()']
-#     }
-#
-#variables['mucca_BDT16'] = {
-#     'name': 'hww_VBF_mvaBDT16(mll,mjj,jetpt1,jetpt2,detajj,jeteta1,jeteta2,metPfType1,mth)',
-#     'range' : (20,-1.,1.),
-#     'xaxis' : 'MVA discriminant',
-#     'fold' : 3,
-#     'linesToAdd' : ['.L /afs/cern.ch/work/a/arun/Latinos/HWW_Full2016/CMSSW_8_0_26_patch1/src/PlotsConfigurations/Configurations/VBF/MyChecks/SignalRegion/MVATesting/hww_VBF_mvaBDT16.C+', 'initmyreaderBDT16()']
-#     }
-
-#variables['mucca_BDTG'] = {
-#     'name': 'hww_VBF_mvaBDTG(mll,mjj,jetpt1,jetpt2,detajj,jeteta1,jeteta2,metPfType1,mth)',
-#     'range' : (20,-1.,1.),
-#     'xaxis' : 'MVA discriminant',
-#     'fold' : 3,
-#     'linesToAdd' : ['.L /afs/cern.ch/work/a/arun/Latinos/HWW_Full2016/CMSSW_8_0_26_patch1/src/PlotsConfigurations/Configurations/VBF/MyChecks/SignalRegion/MVATesting/hww_VBF_mvaBDTG.C+', 'initmyreaderBDTG()']
-#     }
-#
-#variables['mucca_BDTG2'] = {
-#     'name': 'hww_VBF_mvaBDTG2(mll,mjj,jetpt1,jetpt2,detajj,jeteta1,jeteta2,metPfType1,mth)',
-#     'range' : (20,-1.,1.),
-#     'xaxis' : 'MVA discriminant',
-#     'fold' : 3,
-#     'linesToAdd' : ['.L /afs/cern.ch/work/a/arun/Latinos/HWW_Full2016/CMSSW_8_0_26_patch1/src/PlotsConfigurations/Configurations/VBF/MyChecks/SignalRegion/MVATesting/hww_VBF_mvaBDTG2.C+', 'initmyreaderBDTG2()']
-#     }
-#
-#variables['mucca_BDTG4'] = {
-#     'name': 'hww_VBF_mvaBDTG4(mll,mjj,jetpt1,jetpt2,detajj,jeteta1,jeteta2,metPfType1,mth)',
-#     'range' : (20,-1.,1.),
-#     'xaxis' : 'MVA discriminant',
-#     'fold' : 3,
-#     'linesToAdd' : ['.L /afs/cern.ch/work/a/arun/Latinos/HWW_Full2016/CMSSW_8_0_26_patch1/src/PlotsConfigurations/Configurations/VBF/MyChecks/SignalRegion/MVATesting/hww_VBF_mvaBDTG4.C+', 'initmyreaderBDTG4()']
-#     }
-#
-#variables['mucca_BDTG5'] = {
-#     'name': 'hww_VBF_mvaBDTG5(mll,mjj,jetpt1,jetpt2,detajj,jeteta1,jeteta2,metPfType1,mth)',
-#     'range' : (20,-1.,1.),
-#     'xaxis' : 'MVA discriminant',
-#     'fold' : 3,
-#     'linesToAdd' : ['.L /afs/cern.ch/work/a/arun/Latinos/HWW_Full2016/CMSSW_8_0_26_patch1/src/PlotsConfigurations/Configurations/VBF/MyChecks/SignalRegion/MVATesting/hww_VBF_mvaBDTG5.C+', 'initmyreaderBDTG5()']
-#     }
-#
-#variables['mucca_BDTG6'] = {
-#     'name': 'hww_VBF_mvaBDTG6(mll,mjj,jetpt1,jetpt2,detajj,jeteta1,jeteta2,metPfType1,mth)',
-#     'range' : (20,-1.,1.),
-#     'xaxis' : 'MVA discriminant',
-#     'fold' : 3,
-#     'linesToAdd' : ['.L /afs/cern.ch/work/a/arun/Latinos/HWW_Full2016/CMSSW_8_0_26_patch1/src/PlotsConfigurations/Configurations/VBF/MyChecks/SignalRegion/MVATesting/hww_VBF_mvaBDTG6.C+', 'initmyreaderBDTG6()']
-#     }
-
-#variables['mucca_BDTG7'] = {
-#     'name': 'hww_VBF_mvaBDTG7(mll,mjj,jetpt1,jetpt2,detajj,jeteta1,jeteta2,metPfType1,mth)',
-#     'range' : (20,-1.,1.),
-#     'xaxis' : 'MVA discriminant',
-#     'fold' : 3,
-#     'linesToAdd' : ['.L /afs/cern.ch/work/a/arun/Latinos/HWW_Full2016/CMSSW_8_0_26_patch1/src/PlotsConfigurations/Configurations/VBF/MyChecks/SignalRegion/MVATesting/hww_VBF_mvaBDTG7.C+', 'initmyreaderBDTG7()']
-#     }
-
-#variables['mucca_BDTG11'] = {
-#     'name': 'hww_VBF_mvaBDTG11(mll,mjj,jetpt1,jetpt2,detajj,jeteta1,jeteta2,metPfType1,mth)',
-#     'range' : (20,-1.,1.),
-#     'xaxis' : 'MVA discriminant',
-#     'fold' : 3,
-#     'linesToAdd' : ['.L /afs/cern.ch/work/a/arun/Latinos/HWW_Full2016/CMSSW_8_0_26_patch1/src/PlotsConfigurations/Configurations/VBF/MyChecks/SignalRegion/MVATesting/hww_VBF_mvaBDTG11.C+', 'initmyreaderBDTG11()']
-#     }
-#
-#variables['mucca_BDTG12'] = {
-#     'name': 'hww_VBF_mvaBDTG12(mll,mjj,jetpt1,jetpt2,detajj,jeteta1,jeteta2,metPfType1,mth)',
-#     'range' : (20,-1.,1.),
-#     'xaxis' : 'MVA discriminant',
-#     'fold' : 3,
-#     'linesToAdd' : ['.L /afs/cern.ch/work/a/arun/Latinos/HWW_Full2016/CMSSW_8_0_26_patch1/src/PlotsConfigurations/Configurations/VBF/MyChecks/SignalRegion/MVATesting/hww_VBF_mvaBDTG12.C+', 'initmyreaderBDTG12()']
-#     }
-#
-#variables['mucca_BDTG13'] = {
-#     'name': 'hww_VBF_mvaBDTG13(mll,mjj,jetpt1,jetpt2,detajj,jeteta1,jeteta2,metPfType1,mth)',
-#     'range' : (20,-1.,1.),
-#     'xaxis' : 'MVA discriminant',
-#     'fold' : 3,
-#     'linesToAdd' : ['.L /afs/cern.ch/work/a/arun/Latinos/HWW_Full2016/CMSSW_8_0_26_patch1/src/PlotsConfigurations/Configurations/VBF/MyChecks/SignalRegion/MVATesting/hww_VBF_mvaBDTG13.C+', 'initmyreaderBDTG13()']
-#     }
-#
-#variables['mucca_BDTG14'] = {
-#     'name': 'hww_VBF_mvaBDTG14(mll,mjj,jetpt1,jetpt2,detajj,jeteta1,jeteta2,metPfType1,mth)',
-#     'range' : (20,-1.,1.),
-#     'xaxis' : 'MVA discriminant',
-#     'fold' : 3,
-#     'linesToAdd' : ['.L /afs/cern.ch/work/a/arun/Latinos/HWW_Full2016/CMSSW_8_0_26_patch1/src/PlotsConfigurations/Configurations/VBF/MyChecks/SignalRegion/MVATesting/hww_VBF_mvaBDTG14.C+', 'initmyreaderBDTG14()']
-#     }
-#
-#
-#variables['mucca_BDTG15'] = {
-#     'name': 'hww_VBF_mvaBDTG15(mll,mjj,jetpt1,jetpt2,detajj,jeteta1,jeteta2,metPfType1,mth)',
-#     'range' : (20,-1.,1.),
-#     'xaxis' : 'MVA discriminant',
-#     'fold' : 3,
-#     'linesToAdd' : ['.L /afs/cern.ch/work/a/arun/Latinos/HWW_Full2016/CMSSW_8_0_26_patch1/src/PlotsConfigurations/Configurations/VBF/MyChecks/SignalRegion/MVATesting/hww_VBF_mvaBDTG15.C+', 'initmyreaderBDTG15()']
-#     }
-
-#variables['mucca_BDTG16'] = {
-#     'name': 'hww_VBF_mvaBDTG16(mll,mjj,jetpt1,jetpt2,detajj,jeteta1,jeteta2,metPfType1,mth)',
-#     'range' : (20,-1.,1.),
-#     'xaxis' : 'MVA discriminant',
-#     'fold' : 3,
-#     'linesToAdd' : ['.L /afs/cern.ch/work/a/arun/Latinos/HWW_Full2016/CMSSW_8_0_26_patch1/src/PlotsConfigurations/Configurations/VBF/MyChecks/SignalRegion/MVATesting/hww_VBF_mvaBDTG16.C+', 'initmyreaderBDTG16()']
-#     }
-#
-#
-#variables['mucca_BDTG17'] = {
-#     'name': 'hww_VBF_mvaBDTG17(mll,mjj,jetpt1,jetpt2,detajj,jeteta1,jeteta2,metPfType1,mth)',
-#     'range' : (20,-1.,1.),
-#     'xaxis' : 'MVA discriminant',
-#     'fold' : 3,
-#     'linesToAdd' : ['.L /afs/cern.ch/work/a/arun/Latinos/HWW_Full2016/CMSSW_8_0_26_patch1/src/PlotsConfigurations/Configurations/VBF/MyChecks/SignalRegion/MVATesting/hww_VBF_mvaBDTG17.C+', 'initmyreaderBDTG17()']
-#     }
-
-#variables['mucca_BDTB'] = {
-#   #  'name': 'hww_VBF_mvaBDT((*std_vector_lepton_pt)[0],(*std_vector_lepton_pt)[1],mll,mjj,jetpt1,jetpt2,detajj,ptll,jeteta1,jeteta2)',
-##     'name': 'hww_VBF_mvaBDTB(mll,mjj,jetpt1,jetpt2,detajj,ptll,jeteta1,jeteta2,metPfType1,mth)',
-#     'name': 'hww_VBF_mvaBDTB(mll,mjj,jetpt1,jetpt2,detajj,jeteta1,jeteta2,metPfType1,mth)',
-#     'range' : (20,-1.,1.),  
-#     'xaxis' : 'MVA discriminant',
-#     'fold' : 3,
-#     'linesToAdd' : ['.L /afs/cern.ch/work/a/arun/Latinos/HWW_Full2016/CMSSW_8_0_26_patch1/src/PlotsConfigurations/Configurations/VBF/MyChecks/SignalRegion/MVATesting/hww_VBF_mvaBDTB.C+', 'initmyreaderBDTB()']
-#     }
-#
-#variables['mucca_BDTB2'] = {
-#     'name': 'hww_VBF_mvaBDTB2(mll,mjj,jetpt1,jetpt2,detajj,jeteta1,jeteta2,metPfType1,mth)',
-#     'range' : (20,-1.,1.),
-#     'xaxis' : 'MVA discriminant',
-#     'fold' : 3,
-#     'linesToAdd' : ['.L /afs/cern.ch/work/a/arun/Latinos/HWW_Full2016/CMSSW_8_0_26_patch1/src/PlotsConfigurations/Configurations/VBF/MyChecks/SignalRegion/MVATesting/hww_VBF_mvaBDTB2.C+', 'initmyreaderBDTB2()']
-#     }
-#
-#variables['mucca_BDTB3'] = {
-#     'name': 'hww_VBF_mvaBDTB3(mll,mjj,jetpt1,jetpt2,detajj,jeteta1,jeteta2,metPfType1,mth)',
-#     'range' : (20,-1.,1.),
-#     'xaxis' : 'MVA discriminant',
-#     'fold' : 3,
-#     'linesToAdd' : ['.L /afs/cern.ch/work/a/arun/Latinos/HWW_Full2016/CMSSW_8_0_26_patch1/src/PlotsConfigurations/Configurations/VBF/MyChecks/SignalRegion/MVATesting/hww_VBF_mvaBDTB3.C+', 'initmyreaderBDTB3()']
-#     }
-#
-#variables['mucca_BDTB4'] = {
-#     'name': 'hww_VBF_mvaBDTB4(mll,mjj,jetpt1,jetpt2,detajj,jeteta1,jeteta2,metPfType1,mth)',
-#     'range' : (20,-1.,1.),
-#     'xaxis' : 'MVA discriminant',
-#     'fold' : 3,
-#     'linesToAdd' : ['.L /afs/cern.ch/work/a/arun/Latinos/HWW_Full2016/CMSSW_8_0_26_patch1/src/PlotsConfigurations/Configurations/VBF/MyChecks/SignalRegion/MVATesting/hww_VBF_mvaBDTB4.C+', 'initmyreaderBDTB4()']
-#     }
-#
-#variables['mucca_BDTD'] = {
-#     'name': 'hww_VBF_mvaBDTD(mll,mjj,jetpt1,jetpt2,detajj,jeteta1,jeteta2,metPfType1,mth)',
-#     'range' : (20,-1.,1.),
-#     'xaxis' : 'MVA discriminant',
-#     'fold' : 3,
-#     'linesToAdd' : ['.L /afs/cern.ch/work/a/arun/Latinos/HWW_Full2016/CMSSW_8_0_26_patch1/src/PlotsConfigurations/Configurations/VBF/MyChecks/SignalRegion/MVATesting/hww_VBF_mvaBDTD.C+', 'initmyreaderBDTD()']
-#     }
-
+variables['mucca_BDT4'] = { 'name': 'hww_VBF_mvaBDT(mll,mjj,jetpt1,jetpt2,detajj,mth,dphill,dphillmet,mR)',
+                            'range' : ([-1.,-0.75,-0.5,-0.25,0.,0.15,0.25,0.35,0.50,0.65,1.],),    #   variable range : 
+#Significance: 2.31872
+#Best fit r: 1  -0.437689/+0.472283  (68% CL)
+#                            'range' : ([-1.,-0.75,-0.5,-0.25,0.,0.10,0.20,0.30,0.40,0.50,0.60,1.],),    #   variable range 
+                          #'range' : (15,-1.,1.),
+                            'xaxis' : 'MVA discriminant',
+                            'fold' : 3,
+                            'linesToAdd' : ['.L /afs/cern.ch/work/a/arun/Latinos/HWW_Full2016/CMSSW_8_0_26_patch1/src/PlotsConfigurations/Configurations/VBF/MyChecks/SignalRegion/MVATesting/hww_VBF_mvaBDT.C+', 'initmyreaderBDT()'],
+                            'doWeight' : 1,
+                            'binX'     : 1,
+                            'binY'     : 11
+                          }
 
 #variables['events']  = {   'name': '1',      
 #                        'range' : (1,0,2),  
 #                        'xaxis' : 'events', 
 #                         'fold' : 3
 #                        }
-#
+
 #variables['mll_optim']  = { 'name': 'mll',            #   variable name
 #                            'range' : ([12,30,50,70,90,110,150,200],),    #   variable range
 #                            'xaxis' : 'mll [GeV]',  #   x axis name
