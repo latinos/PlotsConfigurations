aae89df5524deb319e88b1206ebad172db8fb174
Janssen Xavier <xavier.janssen@uantwerpen.be>
2018-10-18 14:23:08 +0200

    Remove compiled objects and fixing paths

--- Configurations/WW/Full2016_ACReco/samples_em.py
+++ Configurations/WW/Full2016_ACReco/samples_em.py
@@ -322,7 +322,7 @@ samples['WW']  =  {    'name'   : getSampleFiles(directory,'WWTo2L2Nu_aTGC_0-400
                                    + getSampleFiles(directory,'WWTo2L2Nu_aTGC_800-Inf')
                                    ,
                         'weight' : XSWeight+'*'+SFweight+'*'+GenLepMatch+'*'+METFilter_MC+'*'+SMWeight+'*'+ggWW_Scale+'*'+EWK_corr,
-                      'linesToAdd' : ['.L '+os.environ['CMSSW_BASE']+'/src/PlotsConfigurations/Configurations/WW/Full2016/onTheFly/addEWKcorr.C+', 'initaddEWKcorr("ratio_Ptlm")'],
+                      'linesToAdd' : ['.L '+os.environ['CMSSW_BASE'].replace('/storage_mnt/storage','')+'/src/PlotsConfigurations/Configurations/WW/Full2016_ACReco/onTheFly/addEWKcorr.C+', 'initaddEWKcorr("ratio_Ptlm")'],
                        'suppressNegative':['all'],
                        'suppressNegativeNuisances' :['all'],
                   }
