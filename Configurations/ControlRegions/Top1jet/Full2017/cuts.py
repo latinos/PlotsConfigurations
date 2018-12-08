675f58f2e365ca2baa93c025b8d91f07c3f40bed
Piergiulio <piergiulio.lenzi@cern.ch>
2018-12-08 15:59:08 +0100

    ask btag in all top CR

--- Configurations/ControlRegions/Top1jet/Full2017/cuts.py
+++ Configurations/ControlRegions/Top1jet/Full2017/cuts.py
@@ -7,11 +7,13 @@ supercut = 'mll>60 \
             abs(Lepton_eta[0])<2.5 && abs(Lepton_eta[1])<2.5 && \
             LepCut2l==1 && \
             Alt$(CleanJet_pt[0],0)>30 &&\
+            Alt$(CleanJet_pt[1],0)>20 &&\
             Alt$(CleanJet_pt[1],100)<30\
             '
 
 
 cuts['hww2l2v_13TeV_top_of1j']  = '(Lepton_pdgId[0]*Lepton_pdgId[1] == -11*13) \
                                     && Jet_btagCSVV2[CleanJet_jetIdx[0]] > 0.5803 \
+                                    && Jet_btagCSVV2[CleanJet_jetIdx[1]] > 0.5803 \
                                     '
 
