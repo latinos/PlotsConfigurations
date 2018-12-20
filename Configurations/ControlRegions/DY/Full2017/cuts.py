9f2f0fd9406dd13c225b1dfbc6fbdb70ea3bc4d9
xjanssen <xavier.janssen@cern.ch>
2018-12-20 15:40:56 +0100

    Adding Fakes

--- Configurations/ControlRegions/DY/Full2017/cuts.py
+++ Configurations/ControlRegions/DY/Full2017/cuts.py
@@ -1,6 +1,6 @@
 # cuts
 
-supercut = 'mll>60 && Lepton_pt[0]>20 && Lepton_pt[1]>10 && (nLepton>=2 && Alt$(Lepton_pt[2],0)<10) && abs(Lepton_eta[0])<2.5 && abs(Lepton_eta[1])<2.5 && LepCut2l==1'
+supercut = 'mll>60 && Lepton_pt[0]>20 && Lepton_pt[1]>10 && (nLepton>=2 && Alt$(Lepton_pt[2],0)<10) && abs(Lepton_eta[0])<2.5 && abs(Lepton_eta[1])<2.5'
 
 
 cuts['Zee']  = '(Lepton_pdgId[0] * Lepton_pdgId[1] == -11*11)   \
