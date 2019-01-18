4a31304a38e081ba4f9f247f51f7841650a2281b
Yutaro <yutaro.iiyama@cern.ch>
2019-01-18 18:49:52 +0100

    setup for ptH measurement - nuisances not complete yet

--- Configurations/Differential/ggH2016/aliases.py
+++ Configurations/Differential/ggH2016/aliases.py
@@ -4,6 +4,8 @@
 # samples
 
 mc = [skey for skey in samples if skey not in ('Fake', 'DATA')]
+signal = [skey for skey in samples if '_hww' in skey]
+top = [skey for skey in samples if skey.startswith('top')]
 
 ##############################
 
@@ -24,6 +26,23 @@ eleWP='mva_90p_Iso2016'
 muWP='cut_Tight80x'
 
 ##############################
+
+## categorization
+aliases['leade'] = {
+    'expr': 'abs(std_vector_lepton_flavour[0]) == 11'
+}
+
+aliases['leadm'] = {
+    'expr': 'abs(std_vector_lepton_flavour[0]) == 13'
+}
+
+aliases['highptTrail'] = {
+    'expr': 'std_vector_lepton_pt[1] > 20.'
+}
+
+aliases['lowptTrail'] = {
+    'expr': 'std_vector_lepton_pt[1] < 20.'
+}
 
 # Trailing lepton is a muon (with pt > 10 GeV implied) or an electron with pt > 13 GeV
 aliases['trailingE13'] = {
@@ -51,6 +70,26 @@ aliases['oneJet'] = {
     'expr': 'std_vector_jet_pt[0] >= 30. && std_vector_jet_pt[1] < 30.'
 }
 
+# >=2 jets with pt > 30 GeV
+aliases['manyJet'] = {
+    'expr': 'std_vector_jet_pt[1] >= 30.'
+}
+
+## >=2 jets with pt > 30 GeV limiting to "ggh" configuration
+#aliases['manyJetGGH'] = {
+#    'expr': 'manyJet && (mjj < 65. || (mjj > 105. && mjj < 400.))'
+#}
+
+## number of jets capped at 2
+#aliases['njetCapped'] = {
+#    'expr': 'njet * (njet < 3) + 2 * (njet > 2)'
+#}
+
+## number of bjets
+#aliases['nbjet'] = {
+#    'expr': 'Sum$(std_vector_jet_pt > 30. && std_vector_jet_cmvav2 > -0.5884)'
+#}
+
 # b-jet veto
 aliases['bVeto'] = {
     'expr': 'bveto_CMVA' + bWP
@@ -58,43 +97,43 @@ aliases['bVeto'] = {
 
 aliases['toprwgt'] = {
     'expr': 'TMath::Sqrt(TMath::Exp(0.0615-0.0005*topLHEpt) * TMath::Exp(0.0615-0.0005*antitopLHEpt))',
-    'samples': ['top']
+    'samples': top
 }
 
 # Lepton px, py
-aliases['px0'] = {
-    'expr': 'std_vector_lepton_pt[0] * cos(std_vector_lepton_phi[0])'
-}
-aliases['py0'] = {
-    'expr': 'std_vector_lepton_pt[0] * sin(std_vector_lepton_phi[0])'
-}
-aliases['px1'] = {
-    'expr': 'std_vector_lepton_pt[1] * cos(std_vector_lepton_phi[1])'
-}
-aliases['py1'] = {
-    'expr': 'std_vector_lepton_pt[1] * sin(std_vector_lepton_phi[1])'
-}
+#aliases['px0'] = {
+#    'expr': 'std_vector_lepton_pt[0] * cos(std_vector_lepton_phi[0])'
+#}
+#aliases['py0'] = {
+#    'expr': 'std_vector_lepton_pt[0] * sin(std_vector_lepton_phi[0])'
+#}
+#aliases['px1'] = {
+#    'expr': 'std_vector_lepton_pt[1] * cos(std_vector_lepton_phi[1])'
+#}
+#aliases['py1'] = {
+#    'expr': 'std_vector_lepton_pt[1] * sin(std_vector_lepton_phi[1])'
+#}
 
 # pxll, pyll
-aliases['pxll'] = {
-    'expr': 'px0 + px1'
-}
-aliases['pyll'] = {
-    'expr': 'py0 + py1'
-}
+#aliases['pxll'] = {
+#    'expr': 'px0 + px1'
+#}
+#aliases['pyll'] = {
+#    'expr': 'py0 + py1'
+#}
 
 # pxH, pyH
-aliases['pxH'] = {
-    'expr': 'pxll + metPfType1 * cos(metPfType1Phi)'
-}
-aliases['pyH'] = {
-    'expr': 'pyll + metPfType1 * sin(metPfType1Phi)'
-}
+#aliases['pxH'] = {
+#    'expr': 'pxll + metPfType1 * cos(metPfType1Phi)'
+#}
+#aliases['pyH'] = {
+#    'expr': 'pyll + metPfType1 * sin(metPfType1Phi)'
+#}
 
-# pTH
-aliases['ptH'] = {
-    'expr': 'sqrt(pxH * pxH + pyH + pyH)'
-}
+# pTH - identical to pTWW
+#aliases['ptH'] = {
+#    'expr': 'sqrt(pxH * pxH + pyH * pyH)'
+#}
 
 # Lepton & b-jet id efficiency scale factor
 aliases['sfWeight'] = {
@@ -220,13 +259,28 @@ aliases['genPyll'] = {
     'samples': mc
 }
 
+# Gen met x, y
+aliases['genMetx'] = {
+    'expr': 'metGenpt * cos(metGenphi)',
+    'samples': mc
+}
+aliases['genMety'] = {
+    'expr': 'metGenpt * sin(metGenphi)',
+    'samples': mc
+}
+
 # Gen pxH, pyH
 aliases['genPxH'] = {
-    'expr': 'genPxll + metGenpt * cos(metGenphi)',
+    'expr': 'genPxll + genMetx',
     'samples': mc
 }
 aliases['genPyH'] = {
-    'expr': 'genPyll + metGenpt * sin(metGenphi)',
+    'expr': 'genPyll + genMety',
+    'samples': mc
+}
+
+aliases['genPth'] = {
+    'expr': 'sqrt(genPxH * genPxH + genPyH * genPyH)',
     'samples': mc
 }
 
@@ -236,11 +290,29 @@ aliases['genPtll'] = {
     'samples': mc
 }
 
-# Gen pTH
-aliases['genPtH'] = {
-    'expr': 'sqrt(genPxH * genPxH + genPyH + genPyH)',
-    'samples': mc
-}
+## Gen H pz
+#aliases['genPzH'] = {
+#    'expr': 'higgsGenpt * TMath::SinH(higgsGeneta)',
+#    'samples': mc
+#}
+#
+## Gen H energy
+#aliases['genEH'] = {
+#    'expr': 'TMath::Sqrt(higgsGenmass * higgsGenmass + TMath::Power(higgsGenpt * TMath::CosH(higgsGeneta), 2.))',
+#    'samples': mc
+#}
+#
+## Gen yH
+#aliases['genYH'] = {
+#    'expr': 'TMath::Log((genEH + genPzH) / (higgsGenmass * higgsGenmass + higgsGenpt * higgsGenpt))',
+#    'samples': mc
+#}
+#
+## Gen yH
+#aliases['absGenYH'] = {
+#    'expr': 'abs(genYH)',
+#    'samples': mc
+#}
 
 # Gen mth
 aliases['genMth'] = {
@@ -254,67 +326,108 @@ aliases['genMtw2'] = {
     'samples': mc
 }
 
-# No gen jet with pt > 30 GeV
-aliases['nGenJet'] = {
-    'expr': 'Sum$(std_vector_jetGen_pt > 30 && TMath::Power(std_vector_jetGen_eta - std_vector_dressedLeptonGen_eta[0], 2.) + TMath::Power(TVector2::Phi_mpi_pi(std_vector_jetGen_phi - std_vector_dressedLeptonGen_phi[0]), 2.) > 0.16 && TMath::Power(std_vector_jetGen_eta - std_vector_dressedLeptonGen_eta[1], 2.) + TMath::Power(TVector2::Phi_mpi_pi(std_vector_jetGen_phi - std_vector_dressedLeptonGen_phi[1]), 2.) > 0.16)',
+# Overlap cleaning for gen jets
+aliases['genJetClean'] = {
+    'expr': 'TMath::Power(std_vector_jetGen_eta - std_vector_dressedLeptonGen_eta[0], 2.) + TMath::Power(TVector2::Phi_mpi_pi(std_vector_jetGen_phi - std_vector_dressedLeptonGen_phi[0]), 2.) > 0.16 && TMath::Power(std_vector_jetGen_eta - std_vector_dressedLeptonGen_eta[1], 2.) + TMath::Power(TVector2::Phi_mpi_pi(std_vector_jetGen_phi - std_vector_dressedLeptonGen_phi[1]), 2.) > 0.16',
+    'samples': mc
+}
+
+aliases['genJet0Clean'] = {
+    'expr': 'TMath::Power(std_vector_jetGen_eta[0] - std_vector_dressedLeptonGen_eta[0], 2.) + TMath::Power(TVector2::Phi_mpi_pi(std_vector_jetGen_phi[0] - std_vector_dressedLeptonGen_phi[0]), 2.) > 0.16 && TMath::Power(std_vector_jetGen_eta[0] - std_vector_dressedLeptonGen_eta[1], 2.) + TMath::Power(TVector2::Phi_mpi_pi(std_vector_jetGen_phi[0] - std_vector_dressedLeptonGen_phi[1]), 2.) > 0.16',
+    'samples': mc
+}
+
+aliases['genJet1Clean'] = {
+    'expr': 'TMath::Power(std_vector_jetGen_eta[1] - std_vector_dressedLeptonGen_eta[0], 2.) + TMath::Power(TVector2::Phi_mpi_pi(std_vector_jetGen_phi[1] - std_vector_dressedLeptonGen_phi[0]), 2.) > 0.16 && TMath::Power(std_vector_jetGen_eta[1] - std_vector_dressedLeptonGen_eta[1], 2.) + TMath::Power(TVector2::Phi_mpi_pi(std_vector_jetGen_phi[1] - std_vector_dressedLeptonGen_phi[1]), 2.) > 0.16',
     'samples': mc
 }
 
 # Components for the fiducial cut
 aliases['genLeptonPt'] = {
     'expr': 'std_vector_dressedLeptonGen_pt[0]>25 && std_vector_dressedLeptonGen_pt[1]>10 && std_vector_dressedLeptonGen_pt[2]<10',
-    'samples': ['ggH_hww']
+    'samples': signal
 }
 aliases['genOSOF'] = {
     'expr': 'std_vector_dressedLeptonGen_pid[0] * std_vector_dressedLeptonGen_pid[1] == -11 * 13',
-    'samples': ['ggH_hww']
+    'samples': signal
 }
 aliases['genTrailingE13'] = {
     'expr': '(abs(std_vector_dressedLeptonGen_pid[1]) == 13 || std_vector_dressedLeptonGen_pt[1]>13)',
-    'samples': ['ggH_hww']
+    'samples': signal
 }
-aliases['genMjj'] = {
-    'expr': 'sqrt(2 * std_vector_jetGen_pt[0] * std_vector_jetGen_pt[1] * (cosh(std_vector_jetGen_eta[0]-std_vector_jetGen_eta[1])-cos(std_vector_jetGen_phi[0]-std_vector_jetGen_phi[1])))',
-    'samples': ['ggH_hww']
-}
+
+#expr = 'sqrt(2 * std_vector_jetGen_pt[0] * std_vector_jetGen_pt[1] * (cosh(std_vector_jetGen_eta[0]-std_vector_jetGen_eta[1])-cos(std_vector_jetGen_phi[0]-std_vector_jetGen_phi[1]))) * (genJet0Clean && genJet1Clean) + '
+#expr += 'sqrt(2 * std_vector_jetGen_pt[0] * std_vector_jetGen_pt[2] * (cosh(std_vector_jetGen_eta[0]-std_vector_jetGen_eta[2])-cos(std_vector_jetGen_phi[0]-std_vector_jetGen_phi[2]))) * (genJet0Clean && !genJet1Clean) + '
+#expr += 'sqrt(2 * std_vector_jetGen_pt[1] * std_vector_jetGen_pt[2] * (cosh(std_vector_jetGen_eta[1]-std_vector_jetGen_eta[2])-cos(std_vector_jetGen_phi[1]-std_vector_jetGen_phi[2]))) * (!genJet0Clean && genJet1Clean) + '
+#expr += 'sqrt(2 * std_vector_jetGen_pt[2] * std_vector_jetGen_pt[3] * (cosh(std_vector_jetGen_eta[2]-std_vector_jetGen_eta[3])-cos(std_vector_jetGen_phi[2]-std_vector_jetGen_phi[3]))) * (!genJet0Clean && !genJet1Clean)'
+#
+#aliases['genMjj'] = {
+#    'expr': expr,
+#    'samples': mc
+#}
+#aliases['genDetajj'] = {
+#    'expr': 'abs(std_vector_jetGen_eta[0] - std_vector_jetGen_eta[1])',
+#    'samples': mc
+#}
+
+## Number of gen jets with pt > 30 GeV
+#aliases['nGenJet'] = {
+#    'expr': 'Sum$(std_vector_jetGen_pt > 30 && genJetClean)',
+#    'samples': mc
+#}
+
+#aliases['nGenJetCapped'] = {
+#    'expr': 'nGenJet * (nGenJet < 3) + 2 * (nGenJet > 2)',
+#    'samples': mc
+#}
+
+#aliases['nGenJetGGH'] = {
+#    'expr': 'nGenJet * (nGenJet < 2) + nGenJet * (nGenJet >= 2 && (genMjj < 65. || (genMjj > 105. && genMjj < 400.)))',
+#    'samples': mc
+#}
 
 # Fiducial cut for differential measurements
 aliases['fiducial'] = {
-    'expr': 'genLeptonPt && genOSOF && genTrailingE13 && genMll>12 && metGenpt>20 && genPtll>30 && genMth>=60 && genMtw2>30',
-    'samples': ['ggH_hww']
+    #'expr': 'genLeptonPt && genOSOF && genTrailingE13 && genMll>12 && metGenpt>20 && genPtll>30 && genMth>=60 && genMtw2>30'
+    'expr': 'genLeptonPt && genOSOF && genTrailingE13 && genMll>12 && genPtll>30 && genMth>=60 && genMtw2>30',
+    'samples': signal
 }
 
-# Signal region splitting
-aliases['genPtHcut0'] = {
-    'expr': 'genPtH < 35',
-    'samples': ['ggH_hww']
-}
-aliases['genPtHcut1'] = {
-    'expr': 'genPtH >= 35 && genPtH < 50',
-    'samples': ['ggH_hww']
-}
-aliases['genPtHcut2'] = {
-    'expr': 'genPtH >= 50 && genPtH < 65',
-    'samples': ['ggH_hww']
-}
-aliases['genPtHcut3'] = {
-    'expr': 'genPtH >= 65 && genPtH < 80',
-    'samples': ['ggH_hww']
-}
-aliases['genPtHcut4'] = {
-    'expr': 'genPtH >= 80',
-    'samples': ['ggH_hww']
-}
-
-aliases['njetcut0'] = {
-    'expr': 'nGenJet == 0',
-    'samples': ['ggH_hww']
-}
-aliases['njetcut1'] = {
-    'expr': 'nGenJet == 1',
-    'samples': ['ggH_hww']
-}
-aliases['njetcut2'] = {
-    'expr': 'nGenJet >= 2 && (genMjj<65 || (genMjj>105 && genMjj<400))',
-    'samples': ['ggH_hww']
-}
+#aliases['gen_STXS_VBF'] = {
+#    'expr': 'higgsGenpt < 200. && genMjj > 400. && genDetajj > 2.8',
+#    'samples': signal
+#}
+#aliases['bin_njet_2_STXS_ggF_gg'] = {
+#    'expr': 'nGenJet >= 2 && !gen_STXS_VBF',
+#    'samples': signal
+#}
+#aliases['bin_njet_2_STXS_ggF_VBF'] = {
+#    'expr': 'nGenJet >= 2 && gen_STXS_VBF',
+#    'samples': signal
+#}
+#
+## Reco cuts for STXS
+#aliases['STXS_VBF'] = {
+#    'expr': 'pTWW < 200. && mjj > 400. && detajj > 2.8'
+#}
+#
+## Reco cuts used by HWW 2016
+#aliases['historical_gg'] = {
+#    'expr': 'mjj < 65. || (mjj > 105. && mjj < 400.)'
+#}
+#aliases['historical_VBF'] = {
+#    'expr': 'mjj > 400.'
+#}
+#aliases['historical_VBF'] = {
+#    'expr': 'mjj > 65. && mjj < 105.'
+#}
+#
+## >=2 jet with pt > 30 GeV, ggH tag
+#aliases['manyJet_STXS_ggF_gg'] = {
+#    'expr': 'manyJet && !STXS_VBF'
+#}
+#
+## >=2 jet with pt > 30 GeV, ggH tag
+#aliases['manyJet_STXS_ggF_VBF'] = {
+#    'expr': 'manyJet && STXS_VBF'
+#}
---
+++ Configurations/Differential/ggH2016/structure_pth.py
@@ -0,0 +1,82 @@
+# structure configuration for datacard
+
+#structure = {}
+
+# imported from samples.py:
+# samples, pthBinning1, pthBinning2
+# imported from cuts.py
+# cuts
+
+# keys here must match keys in samples.py
+#
+
+# remove samples we won't use in limit setting
+samples.pop('DY')
+samples.pop('top')
+
+mc = [skey for skey in samples if skey not in ('Fake', 'DATA')]
+dy = [skey for skey in samples if skey.startswith('DY')]
+top = [skey for skey in samples if skey.startswith('top')]
+signal = [skey for skey in samples if '_hww' in skey]
+ggh = [skey for skey in samples if skey.startswith('ggH_hww')]
+xh = [skey for skey in samples if skey.startswith('XH_hww')]
+
+topcr = [ckey for ckey in cuts if ckey.startswith('topcr')]
+dycr = [ckey for ckey in cuts if ckey.startswith('dycr')]
+sr = [ckey for ckey in cuts if ckey.startswith('sr')]
+
+for sname in ['WW', 'Fake', 'Wjets', 'ggWW', 'Vg', 'WZgS_L', 'WZgS_H', 'VZ', 'VVV']:
+    structure[sname] = {
+        'isSignal' : 0,
+        'isData'   : 0
+    }
+
+#for sname in ['ggH_htt', 'qqH_htt', 'ZH_htt', 'WH_htt']:
+for sname in ['H_htt']:
+    structure[sname] = {
+        'isSignal' : 0,
+        'isData'   : 0,
+        'removeFromCuts': crs
+    }
+
+for sname in ['ggH_hww', 'XH_hww']:
+    #for fid in ['fid', 'nonfid']:
+    for fid in ['incl']:
+        for ipt in range(len(pthBinning1) - 1):
+            low, high = pthBinning1[ipt:ipt+2]
+            suffix = '_pth_%.0f_%.0f_%s' % (low, high, fid)
+            structure[sname + suffix] = {
+                'isSignal' : 1,
+                'isData'   : 0,
+                'removeFromCuts': crs
+            }
+
+        # drop the other pthBinning
+        for ipt in range(len(pthBinning2) - 1):
+            low, high = pthBinning2[ipt:ipt+2]
+            suffix = '_pth_%.0f_%.0f_%s' % (low, high, fid)
+            samples.pop(sname + suffix)
+
+    # drop fid and nonfid samples
+    for fid in ['fid', 'nonfid']:
+        for ipt in range(len(pthBinning1) - 1):
+            low, high = pthBinning1[ipt:ipt+2]
+            suffix = '_pth_%.0f_%.0f_%s' % (low, high, fid)
+            samples.pop(sname + suffix)
+
+        for ipt in range(len(pthBinning2) - 1):
+            low, high = pthBinning2[ipt:ipt+2]
+            suffix = '_pth_%.0f_%.0f_%s' % (low, high, fid)
+            samples.pop(sname + suffix)    
+
+    # drop other signal bins
+    for key in samples.keys():
+        if key == sname or (key.startswith(sname) and '_pth_' not in key):
+            samples.pop(key)
+
+# data
+
+structure['DATA']  = {
+    'isSignal' : 0,
+    'isData'   : 1
+}
