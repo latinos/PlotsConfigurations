78d2e209883a442184754b3f10fc94b35e232e65
Pietro <pietro.govoni@cern.ch>
2019-01-23 10:44:26 +0100

    WIP HWW configuration

--- Configurations/HH/WWbb_lvjj/cuts.py
+++ Configurations/HH/WWbb_lvjj/cuts.py
@@ -2,15 +2,19 @@
 
 #cuts = {}
   
-supercut = 'std_vector_jet_pt[1]>40 && std_vector_jet_pt[2]>25' 
+supercut = 'std_vector_jet_pt[3]>25'
+#cuts['wwlvjj_13TeV_e']  = 'std_vector_jet_pt[0]>25'
+#cuts['wwlvjj_13TeV_m']  = 'std_vector_jet_pt[0]>25'
+
+#&& std_vector_lepton_pt[0]>20 && (1*(std_vector_jet_cmvav2[0]>-0.715) + 1*(std_vector_jet_cmvav2[1]>-0.715) + 1*(std_vector_jet_cmvav2[2]>-0.715) + 1*(std_vector_jet_cmvav2[3]>-0.715) + 1*(std_vector_jet_cmvav2[4]>-0.715) + 1*(std_vector_jet_cmvav2[5]>-0.715))>=1 
 #cuts['wwlvjj_13TeV_e']  = 'abs(std_vector_lepton_flavour[0]) == 11'
 #cuts['wwlvjj_13TeV_m']  = 'abs(std_vector_lepton_flavour[0]) == 13'
 
 #cuts['wwlvjj_13TeV_e']  = 'abs(std_vector_lepton_flavour[0]) == 11 && std_vector_fatjet_pt[0]>0 && std_vector_lepton_pt[1] <=0'
 #cuts['wwlvjj_13TeV_m']  = 'abs(std_vector_lepton_flavour[0]) == 13 && std_vector_fatjet_pt[0]>0 && std_vector_lepton_pt[1] <=0'
 
-cuts['wwlvjj_13TeV_e']  = 'abs(std_vector_lepton_flavour[0]) == 11'
-cuts['wwlvjj_13TeV_m']  = 'abs(std_vector_lepton_flavour[0]) == 13'
+#cuts['wwlvjj_13TeV_e']  = 'abs(std_vector_lepton_flavour[0]) == 11'
+#cuts['wwlvjj_13TeV_m']  = 'abs(std_vector_lepton_flavour[0]) == 13'
 
 
 
