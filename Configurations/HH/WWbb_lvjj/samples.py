352057e208b9bee382ebd91d51bfad481ad455ae
Arianna <arianna.chiapparini@gmail.com>
2019-02-13 14:10:19 +0100

    Configuration for HH bkg

--- Configurations/HH/WWbb_lvjj/samples.py
+++ Configurations/HH/WWbb_lvjj/samples.py
@@ -14,32 +14,32 @@ from LatinoAnalysis.Tools.commonTools import *
 #directory = '/gwteras/cms/store/group/OneLepton/Apr2017_summer16/lepSel__MCWeights__bSFLpTEffMulti__cleanTauMC/'
 directory = '/gwterap/cms/store/group/OneLepton/Apr2017_summer16/lepSel__MCWeights__bSFLpTEffMulti__cleanTauMC/'
 
-samples['Wjets'] = { 'name' :     getSampleFiles(directory, 'WJetsToLNu_HT100_200')\
-				+ getSampleFiles(directory, 'WJetsToLNu_HT200_400')\
-				+ getSampleFiles(directory, 'WJetsToLNu_HT400_600')\
-				+ getSampleFiles(directory, 'WJetsToLNu_HT600_800')\
+#samples['Wjets'] = { 'name' :     getSampleFiles(directory, 'WJetsToLNu_HT100_200')\
+#				+ getSampleFiles(directory, 'WJetsToLNu_HT200_400')\
+#				+ getSampleFiles(directory, 'WJetsToLNu_HT400_600')\
+#				+ getSampleFiles(directory, 'WJetsToLNu_HT600_800')\
 #				+ getSampleFiles(directory, 'WJetsToLNu_HT800_1200')\
-				+ getSampleFiles(directory, 'WJetsToLNu_HT1200_2500')\
-				+ getSampleFiles(directory, 'WJetsToLNu_HT2500_inf'),
-				'weight': '1.',
-			}
+#				+ getSampleFiles(directory, 'WJetsToLNu_HT1200_2500')\
+#				+ getSampleFiles(directory, 'WJetsToLNu_HT2500_inf'),
+#				'weight': '1.',
+#			}
 
-samples['TT']  = {    'name'   : getSampleFiles(directory, 'TTToSemiLepton') ,
-                                                'weight' :   '1.' ,
-                 }
+#samples['TT']  = {    'name'   : getSampleFiles(directory, 'TTToSemiLepton') ,
+#                                                'weight' :   '1.' ,
+#                 }
 
 
-samples['TTW']  = {    'name'   : getSampleFiles(directory, 'TTWJetsToLNu') ,
-                                                'weight' :   '1.' ,
-	                 }
+#samples['TTW']  = {    'name'   : getSampleFiles(directory, 'TTWJetsToLNu') ,
+#                                                'weight' :   '1.' ,
+#	                 }
 
-samples['WZTo1L1Nu2Q']  = {    'name'   : getSampleFiles(directory, 'WZTo1L1Nu2Q') ,
-                                                'weight' :   '1.' ,
-                 }
+#samples['WZTo1L1Nu2Q']  = {    'name'   : getSampleFiles(directory, 'WZTo1L1Nu2Q') ,
+#                                                'weight' :   '1.' ,
+#                 }
 
 samples['WZTo1L3Nu']  = {    'name'   : ['latino_WZTo1L3Nu__part0.root',
 					'latino_WZTo1L3Nu__part1.root'] ,
-                                                'weight' :   '1.' ,
+                                                'weight' :   'XSWeight*SFweight1l*GenLepMatchMETFilter_MC' ,
                  }
 
 samples['WWW']  = {    'name'   : ['latino_WWW__part0.root'] ,
