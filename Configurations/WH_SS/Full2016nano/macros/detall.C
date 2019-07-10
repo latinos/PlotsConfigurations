c52426db4004f1b2299ed87ee5dea7d86e40955d
arun <arun.kumar@cern.ch>
2019-07-10 19:46:07 +0200

    more cleanup

--- Configurations/WH_SS/Full2016nano/macros/detall.C
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-using namespace std;
-
-float detall(float lep1eta,float lep2eta){
-  return  fabs(lep1eta-lep2eta);
-}
--- Configurations/WH_SS/Full2016nano/macros/minmtw.C
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-using namespace std;
-
-float minmtw(float mt1,float mt2){
-if(mt1 < mt2) return mt1;
-else return mt2;
-}
