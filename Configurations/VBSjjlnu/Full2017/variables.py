52ebdaffda01fc2d4dc99cf426ef8bc530c78f9c
Andrea Salati <a.salati1@campus.unimib.it>
2019-07-13 18:19:26 +0200

    Added FatJet_pt and nFatJet to the variables

--- Configurations/VBSjjlnu/Full2017/variables.py
+++ Configurations/VBSjjlnu/Full2017/variables.py
@@ -17,3 +17,17 @@ variables["FatJet_eta"] = {
                         'fold' : 3  # both underflow and overflow bin are moved inside the histogram
                 }
 
+variables['nFatJet'] = {   'name': 'nFatJet',      
+                        'range' : (5,0,4),  
+                        'xaxis' : 'nFatJet', 
+                        'fold' : 3
+                        }
+
+variables['FatJet_pt'] = {   'name': 'FatJet_pt[0]',      
+                        'range' : (40,150,350),  
+                        'xaxis' : 'FatJet_pt', 
+                        'fold' : 3
+                        }
+
+#variables['FatJet_tau2'] = {   'name': 'FatJet_pt[0]',      
+
