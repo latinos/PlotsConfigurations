a798b430e791481db40f868570cecf4560b5c350
Lorenzo Viliani <lorenzo.viliani@cern.ch>
2019-07-25 18:58:48 +0200

    2016 nanoAODv4 HTXS stage1

--- Configurations/ggH/Full2016_nanoAODv4/HTXS_Stage1/HTXS_stage1_categories.py
+++ Configurations/ggH/Full2016_nanoAODv4/HTXS_Stage1/HTXS_stage1_categories.py
@@ -1,4 +1,4 @@
-HTXSStage1Categories = {
+HTXSStage1Categories : {
       'UNKNOWN'  : 0,
       # Gluon fusion
       'GG2H_FWDH' : 100,
@@ -36,3 +36,64 @@ HTXSStage1Categories = {
       # tH
       'TH_FWDH' : 800, 'TH' : 801
 }
+
+HTXSStage1_1Categories : {
+      'UNKNOWN' : 0,
+      # Gluon fusion
+      'GG2H_FWDH' : 100,
+      'GG2H_PTH_GT200' : 101,
+      'GG2H_0J_PTH_0_10' : 102,
+      'GG2H_0J_PTH_GT10' : 103,
+      'GG2H_1J_PTH_0_60' : 104,
+      'GG2H_1J_PTH_60_120' : 105,
+      'GG2H_1J_PTH_120_200' : 106,
+      'GG2H_GE2J_MJJ_0_350_PTH_0_60' : 107,
+      'GG2H_GE2J_MJJ_0_350_PTH_60_120' : 108,
+      'GG2H_GE2J_MJJ_0_350_PTH_120_200' : 109,
+      'GG2H_MJJ_350_700_PTHJJ_0_25' : 110,
+      'GG2H_MJJ_350_700_PTHJJ_GT25' : 111,
+      'GG2H_MJJ_GT700_PTHJJ_0_25' : 112,
+      'GG2H_MJJ_GT700_PTHJJ_GT25' : 113,
+      # "VBF"
+      'QQ2HQQ_FWDH' : 200,
+      'QQ2HQQ_0J' : 201,
+      'QQ2HQQ_1J' : 202,
+      'QQ2HQQ_MJJ_0_60' : 203,
+      'QQ2HQQ_MJJ_60_120' : 204,
+      'QQ2HQQ_MJJ_120_350' : 205,
+      'QQ2HQQ_MJJ_GT350_PTH_GT200' : 206,
+      'QQ2HQQ_MJJ_350_700_PTHJJ_0_25' : 207,
+      'QQ2HQQ_MJJ_350_700_PTHJJ_GT25' : 208,
+      'QQ2HQQ_MJJ_GT700_PTHJJ_0_25' : 209,
+      'QQ2HQQ_MJJ_GT700_PTHJJ_GT25' : 210,
+      # qq -> WH
+      'QQ2HLNU_FWDH' : 300,
+      'QQ2HLNU_PTV_0_75' : 301,
+      'QQ2HLNU_PTV_75_150' : 302,
+      'QQ2HLNU_PTV_150_250_0J' : 303,
+      'QQ2HLNU_PTV_150_250_GE1J' : 304,
+      'QQ2HLNU_PTV_GT250' : 305,
+      # qq -> ZH
+      'QQ2HLL_FWDH' : 400,
+      'QQ2HLL_PTV_0_75' : 401,
+      'QQ2HLL_PTV_75_150' : 402,
+      'QQ2HLL_PTV_150_250_0J' : 403,
+      'QQ2HLL_PTV_150_250_GE1J' : 404,
+      'QQ2HLL_PTV_GT250' : 405,
+      # gg -> ZH
+      'GG2HLL_FWDH' : 500,
+      'GG2HLL_PTV_0_75' : 501,
+      'GG2HLL_PTV_75_150' : 502,
+      'GG2HLL_PTV_150_250_0J' : 503,
+      'GG2HLL_PTV_150_250_GE1J' : 504,
+      'GG2HLL_PTV_GT250' : 505,
+      # ttH
+      'TTH_FWDH' : 600,
+      'TTH' : 601,
+      # bbH
+      'BBH_FWDH' : 700,
+      'BBH' : 701,
+      # tH
+      'TH_FWDH' : 800,
+      'TH' : 801
+    }
