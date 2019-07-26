c8c3a643992e7b9ded28ce454d72d8a2dabf5a99
Lorenzo Viliani <lorenzo.viliani@cern.ch>
2019-07-26 16:32:45 +0200

    2017 HTXS stage1

--- Configurations/ggH/Full2017/HTXS_Stage1/samples.py
+++ Configurations/ggH/Full2017/HTXS_Stage1/samples.py
@@ -83,11 +83,10 @@ METFilter_DATA = 'METFilter_DATA'
 
 DataRun = [
             ['B','Run2017B-31Mar2018-v1'] ,
-                                                ['C','Run2017C-31Mar2018-v1'] ,
-                                                ['D','Run2017D-31Mar2018-v1'] ,
-                                                ['E','Run2017E-31Mar2018-v1'] ,
-                                                ['F','Run2017F-31Mar2018-v1']
-
+            ['C','Run2017C-31Mar2018-v1'] ,
+            ['D','Run2017D-31Mar2018-v1'] ,
+            ['E','Run2017E-31Mar2018-v1'] ,
+            ['F','Run2017F-31Mar2018-v1']
           ]
 
 DataSets = ['MuonEG','DoubleMuon','SingleMuon','DoubleEG','SingleElectron']
