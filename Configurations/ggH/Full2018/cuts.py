a6cecd503f600bd6140e91b7f5faa8a74b0989fb
Yutaro <yutaro.iiyama@cern.ch>
2019-08-06 09:58:39 +0200

    adding btag SF patch + fixed cuts

--- Configurations/ggH/Full2018/cuts.py
+++ Configurations/ggH/Full2018/cuts.py
@@ -15,7 +15,7 @@ supercut = '   mll>12 \
 ### Unsplitted signal regions
 
 cutsr = '(mth>60 && mtw2>30 && bVeto)'
-cuttt = '(mll>50 && mtw2>30 && bReq)'
+cuttt = '(mll>50 && mtw2>30 && ((zeroJet && !bVeto) || bReq))'
 cutdy = '(mth<60 && mll>40 && mll<80 && bVeto)'
 cutww = '(mth>60 && mtw2>30 && mll>100 && bVeto)'
 
@@ -26,10 +26,9 @@ categories=['zeroJet', 'oneJet', 'twoJet']
 def addcut(name, cut, categories):
     cuts[name] = { 'expr': cut}
     cuts[name]["categories"] = categories
-    cuts[name]["categorization"] = '0'
+    cuts[name]["categorization"] = '-1'
     for i,cat in enumerate(categories):
-      cuts[name]["categorization"] += "+%d*(%s)" % (i, cat)
-
+      cuts[name]["categorization"] += "+%d*(%s)" % (i + 1, cat)
 
 #addcut('hww2l2v_13TeV_of', cutsr, categories)
 addcut('hww2l2v_13TeV_top', cuttt, categories)
