fc5cce0192a48114f7826352fc2ebba6d3cb7f20
Davide Di Croce <davide.di.croce@cern.ch>
2019-08-06 09:29:18 +0200

    Adding config for 2017 HWW SF maximization: DYMVA DNN

---
+++ Configurations/ggH_SF/Full2017_nAODv5/maxDNN/aliases.py
@@ -0,0 +1,105 @@
+#aliases = {}
+
+mc = [skey for skey in samples if skey not in ('Fake' ,'Fake_ee', 'Fake_mm' , 'DATA')]
+
+bAlgo = 'DeepB'
+bWP = '0.1522'
+
+#aliases['sameFlavor'] = {
+#'expr': '((Lepton_pdgId[0]*Lepton_pdgId[1] == -13*13) || (Lepton_pdgId[0]*Lepton_pdgId[1] == -11*11))'
+#}
+
+aliases['0j'] = {
+'expr': '( Alt$(CleanJet_pt[0],0)<30 )'
+}
+
+aliases['1j'] = {
+'expr': 'Alt$(CleanJet_pt[0],0)>=30 && Alt$(CleanJet_pt[1],0)<30'
+}
+
+aliases['2jggH'] = {
+'expr': '( Alt$(CleanJet_pt[0],0)>=30 && Alt$(CleanJet_pt[1],0)>=30 && ( abs(detajj)<3.5 || ( mjj<400 && abs(detajj)>=3.5 ) ) )'
+}
+
+aliases['2jVBF'] = {
+'expr': '(Alt$(CleanJet_pt[0],0)>=30 && Alt$(CleanJet_pt[1],0)>=30 && (mjj>=400 && abs(detajj)>=3.5))'
+}
+
+aliases['Higgs0jetsf'] = {
+'expr': '(mll < 70 && mth > 30)'
+}
+
+aliases['Higgs1jetsf'] = {
+'expr': '(mll < 70 && mth > 30)'
+}
+
+aliases['Higgs2jetsf'] = {
+'expr': '(mll < 70 && mth > 30)'
+}
+
+aliases['Higgsvbfsf'] = {
+'expr': '(mll < 70 && mth > 30)'
+}
+
+aliases['ZVeto'] = {
+'expr': '(fabs(91.1876 - mll) > 15)'
+}
+
+aliases['bVeto'] = {
+'expr': '(Sum$(CleanJet_pt > 20. && abs(CleanJet_eta)<2.5 && Jet_btagDeepB[CleanJet_jetIdx] > 0.1522) == 0)'
+}
+
+aliases['btag0'] = {
+'expr': '( Alt$(CleanJet_pt[0],0)<30  \
+           && Sum$(CleanJet_pt > 20. && abs(CleanJet_eta)<2.5 && Jet_btagDeepB[CleanJet_jetIdx] > 0.1522)>0 \
+         )'
+}
+
+aliases['btag1'] = {
+'expr': '(    Alt$(CleanJet_pt[0],0)>30 && Alt$(abs(CleanJet_eta[0]),99)<2.5\
+           && Alt$(CleanJet_pt[1],0)<30 \
+           && Alt$(Jet_btagDeepB[CleanJet_jetIdx[0]], 0) > 0.1522 )'
+}
+
+aliases['btag2'] = {
+'expr': '(    Alt$(CleanJet_pt[0],0)>30 \
+           && Alt$(CleanJet_pt[1],0)>30 \
+           && ( ( Alt$(abs(CleanJet_eta[0]),99)<2.5 && Alt$(Jet_btagDeepB[CleanJet_jetIdx[0]],0) > 0.1522 ) \
+             || ( Alt$(abs(CleanJet_eta[1]),99)<2.5 && Alt$(Jet_btagDeepB[CleanJet_jetIdx[1]],0) > 0.1522 ) ) \
+         )' 
+}
+
+aliases['bVetoSF'] = {
+'expr': '( TMath::Exp(Sum$( TMath::Log( (CleanJet_pt>20 && abs(CleanJet_eta)<2.5)*Jet_btagSF_shape[CleanJet_jetIdx]+1*(CleanJet_pt<20 || abs(CleanJet_eta)>2.5) ) ) ) )',
+'samples': mc
+}
+
+
+aliases['btag0SF'] = {
+'expr': '( TMath::Exp(Sum$( TMath::Log( (CleanJet_pt>20 && CleanJet_pt<30 && abs(CleanJet_eta)<2.5)*Jet_btagSF_shape[CleanJet_jetIdx]+1*(CleanJet_pt<20 || CleanJet_pt>30 || abs(CleanJet_eta)>2.5) ) ) ) )',
+'samples': mc
+}
+
+aliases['btag1SF'] = {
+'expr': '( ( Alt$(CleanJet_pt[0], 0)>30 && Alt$(abs(CleanJet_eta[0]),99)<2.5 )*( Alt$(Jet_btagSF_shape[CleanJet_jetIdx[0]], 1) ) + ( Alt$(CleanJet_pt[0], 0)<30 || Alt$(abs(CleanJet_eta[0]),99)>2.5 ) )',
+'samples': mc
+}
+
+aliases['btag2SF'] = {
+'expr': '( ( ( Alt$(CleanJet_pt[0], 0)>30 && Alt$(abs(CleanJet_eta[0]),99)<2.5 )*( Alt$(Jet_btagSF_shape[CleanJet_jetIdx[0]], 1) ) + ( Alt$(CleanJet_pt[0], 0)<30 || Alt$(abs(CleanJet_eta[0]),99)>2.5 ) )* \
+           ( ( Alt$(CleanJet_pt[1], 0)>30 && Alt$(abs(CleanJet_eta[1]),99)<2.5 )*( Alt$(Jet_btagSF_shape[CleanJet_jetIdx[1]], 1) ) + ( Alt$(CleanJet_pt[1], 0)<30 || Alt$(abs(CleanJet_eta[1]),99)>2.5 ) ) )\
+        ',
+'samples': mc
+}
+
+aliases['btagSF'] = {
+'expr': '( bVetoSF*bVeto + btag0SF*btag0 + btag1SF*btag1 + btag2SF*btag2 + ( (!bVeto) && (!btag0) && (!btag1) && (!btag2) ) )',
+'samples': mc
+}
+
+
+systs = ['jes','lf','hf','lfstats1','lfstats2','hfstats1','hfstats2','cferr1','cferr2']
+
+for s in systs:
+  aliases['btagSF'+s+'up'] = { 'expr': '( bVeto*'+aliases['bVetoSF']['expr'].replace('shape','shape_up_'+s)+'+btag0*'+aliases['btag0SF']['expr'].replace('shape','shape_up_'+s)+'+btag1*'+aliases['btag1SF']['expr'].replace('shape','shape_up_'+s)+'+btag2*'+aliases['btag2SF']['expr'].replace('shape','shape_up_'+s)+' + ( (!bVeto) && (!btag0) && (!btag1) && (!btag2) ) )', 'samples':mc  }
+  aliases['btagSF'+s+'down'] = { 'expr': '( bVeto*'+aliases['bVetoSF']['expr'].replace('shape','shape_down_'+s)+'+btag0*'+aliases['btag0SF']['expr'].replace('shape','shape_down_'+s)+'+btag1*'+aliases['btag1SF']['expr'].replace('shape','shape_down_'+s)+'+btag2*'+aliases['btag2SF']['expr'].replace('shape','shape_down_'+s)+' + ( (!bVeto) && (!btag0) && (!btag1) && (!btag2) ) )', 'samples':mc  }
