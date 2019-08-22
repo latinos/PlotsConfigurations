33929d26d13e119df2cd5ad4a70e37e7e0dd3ed0
arun <arun.kumar@cern.ch>
2019-08-22 13:28:22 +0200

    variable update for whss

--- Configurations/WH_SS/Full2016nano/variables.py
+++ Configurations/WH_SS/Full2016nano/variables.py
@@ -3,19 +3,29 @@
 #variables = {}
     
 
-
+'''
 variables['events']  = {   'name': '1',      
                         'range' : (1,0,2),  
                         'xaxis' : 'events', 
                         'fold' : 3
                         }
 
+
 variables['mlljj20_whss']  = {   'name': 'mlljj20_whss',
                         'range' : (20,0,200),
                         'xaxis' : 'mlljj20_whss [GeV]',
                         'fold' : 0
                         }
+'''
 
+variables['mlljj20_whss_2j']  = {   'name': 'mlljj20_whss',
+                        'range' : ([50.,100.,110.,125.,130.,135.,140.,150.,160.,175.,200.,250.,300.],),
+                        'xaxis' : 'mlljj20_whss [GeV]',
+                        'fold' : 3
+                        }
+
+
+'''
 variables['mlljj30_whss']  = {   'name': 'mlljj30_whss',
                         'range' : (40,0,200),
                         'xaxis' : 'mlljj30_whss [GeV]',
@@ -135,7 +145,7 @@ variables['jetpt2']  = {
 #                        'fold'  : 0
 #                        }
 
-'''
+
 variables['mtw1']  = {  'name': 'mtw1',
                         'range' : (40,0,200),
                         'xaxis' : 'm_{T}^{W_{1}} [GeV]',
