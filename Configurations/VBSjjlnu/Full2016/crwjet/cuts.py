4abda53857f97696f6e0651bfe878f39a91f909d
Dario Mapelli <mapelli.dario@gmail.com>
2019-08-27 23:22:26 +0200

    [vbsjjlnu] small changes

--- Configurations/VBSjjlnu/Full2016/crwjet/cuts.py
+++ Configurations/VBSjjlnu/Full2016/crwjet/cuts.py
@@ -1,11 +1,13 @@
 # cuts
 
 #cuts = {}
-  
+
 l1pt = '(std_vector_lepton_pt[0]>30)'
 # l2vetotight = '(  std_vector_muon_isTightLepton_cut_Tight80x[1]<0.5 &&  std_vector_electron_isTightLepton_cut_WP_Tight80X[1]<0.5 )'
 l2veto = '(std_vector_lepton_pt[1]<0)'
-supercut = l1pt + ' && ' + l2veto
+crwjet = '(mjj_vjet > 40 && mjj_vjet < 65) || (mjj_vjet > 105 && mjj_vjet < 150)'
+supercut = l1pt + ' && ' + l2veto + ' && ' + crwjet
+
 
 cuts["events"] = '1.'
 cuts['electron']  = '(abs(std_vector_lepton_flavour[0]) == 11)'
