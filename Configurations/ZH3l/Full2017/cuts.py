99c5811970294f54113fcfcc417e41a48eee43db
Susan Dittmer <Susan.Dittmer@cern.ch>
2019-09-03 13:14:09 +0200

    Adding ZH3l nanoAODv5 configs

---
+++ Configurations/ZH3l/Full2017/cuts.py
@@ -0,0 +1,87 @@
+# cuts
+
+supercut = 'MinIf$( WH3l_mOSll[], WH3l_mOSll[Iteration$] > 0) > 12 \
+            && Alt$(Lepton_pt[0],0)>25 \
+            && Alt$(Lepton_pt[1],0)>20 \
+            && Alt$(Lepton_pt[2],0)>15 \
+            && Alt$(Lepton_pt[3],0)<10 \
+            && abs(WH3l_chlll) == 1 \
+           '
+
+cuts['preselection']   = '1'
+
+cuts['zmass_cut'] = ' WH3l_ZVeto < 25 '
+
+cuts['2jet_cut'] = ' Alt$( CleanJet_pt[0], 0) >= 30 \
+                  && Alt$( CleanJet_pt[1], 0) >= 30 \
+                  && WH3l_ZVeto < 25 \
+                  '
+
+cuts['bveto'] = ' Alt$( CleanJet_pt[0], 0) >= 30 \
+               && Alt$( CleanJet_pt[1], 0) >= 30 \
+               && WH3l_ZVeto < 25 \
+               && bVeto \
+               '
+
+cuts['z4lveto'] = ' Alt$( CleanJet_pt[0], 0) >= 30 \
+                 && Alt$( CleanJet_pt[1], 0) >= 30 \
+                 && WH3l_ZVeto < 25 \
+                 && bVeto \
+                 && ZH3l_Z4lveto > 20 \
+                 '
+
+cuts['zh3l_SR_2j'] = ' Alt$( CleanJet_pt[0], 0) >= 30 \
+                    && Alt$( CleanJet_pt[1], 0) >= 30 \
+                    && WH3l_ZVeto < 25 \
+                    && bVeto \
+                    && ZH3l_Z4lveto > 20 \
+                    && ZH3l_dphilmetjj < 3.14159/2 \
+                    '
+
+cuts['zh3l_WZ_CR_2j'] = ' Alt$( CleanJet_pt[0], 0) >= 30 \
+                       && Alt$( CleanJet_pt[1], 0) >= 30 \
+                       && WH3l_ZVeto < 25 \
+                       && bVeto \
+                       && ZH3l_Z4lveto > 20 \
+                       && ZH3l_dphilmetjj > 3.14159/2 \
+                       '
+
+cuts['1jet_cut'] = ' Alt$( CleanJet_pt[0], 0) >= 30 \
+                  && Alt$( CleanJet_pt[1], 0) < 30 \
+                  && WH3l_ZVeto < 25 \
+                  '
+
+cuts['bveto_1j'] = ' Alt$( CleanJet_pt[0], 0) >= 30 \
+                  && Alt$( CleanJet_pt[1], 0) < 30 \
+                  && WH3l_ZVeto < 25 \
+                  && bVeto \
+                  '
+
+cuts['z4lveto_1j'] = ' Alt$( CleanJet_pt[0], 0) >= 30 \
+                    && Alt$( CleanJet_pt[1], 0) < 30 \
+                    && WH3l_ZVeto < 25 \
+                    && bVeto \
+                    && ZH3l_Z4lveto > 20 \
+                    '
+
+cuts['zh3l_SR_1j'] = ' Alt$( CleanJet_pt[0], 0) >= 30 \
+                    && Alt$( CleanJet_pt[1], 0) < 30 \
+                    && WH3l_ZVeto < 25 \
+                    && bVeto \
+                    && ZH3l_Z4lveto > 20 \
+                    && ZH3l_dphilmetj < 3.14159/2 \
+                    '
+
+cuts['zh3l_WZ_CR_1j'] = ' Alt$( CleanJet_pt[0], 0) >= 30 \
+                       && Alt$( CleanJet_pt[1], 0) < 30 \
+                       && WH3l_ZVeto < 25 \
+                       && bVeto \
+                       && ZH3l_Z4lveto > 20 \
+                       && ZH3l_dphilmetj > 3.14159/2 \
+                       '
+
+'''
+ #11 = e
+# 13 = mu
+# 15 = tau
+'''
---
+++ Configurations/ZH3l/Full2018/variables.py
@@ -0,0 +1,79 @@
+# variables
+
+#variables = {}
+    
+#'fold' : # 0 = not fold (default), 1 = fold underflowbin, 2 = fold overflow bin, 3 = fold underflow and overflow
+   
+variables['events']     = { 'name': '1',      
+                            'range' : (1,0,2),  
+                            'xaxis' : 'events', 
+                            'fold' : 3
+                        }
+
+variables['njet']       = { 'name'  : 'ZH3l_njet',
+                            'range' : (10,0,10),
+                            'xaxis' : 'N_{jet}',
+                            'fold' : 0
+                        }
+
+variables['pt1']        = { 'name': 'Lepton_pt[0]',
+                            'range' : (10,0.,200),
+                            'xaxis' : 'lept1_p_{T} [GeV]',
+                            'fold' : 0
+                        }
+
+variables['dphilmetj'] = {     'name' : 'ZH3l_dphilmetj',
+                                'range' : (16,0,3.14159),
+                                'xaxis' : 'dphilmetj',
+                                'fold' : 0
+                            }
+
+variables['dphilmetjj'] = { 'name' : 'ZH3l_dphilmetjj',
+                            'range' : (16,0,3.14159),
+                            'xaxis' : 'dphilmetjj',
+                            'fold' : 0
+                        }
+
+variables['pTlmetj'] = {     'name' : 'ZH3l_pTlmetj',
+                                'range' : (20,0,400),
+                                'xaxis' : 'pTlmetj',
+                                'fold' : 0
+                            }
+
+variables['pTlmetjj'] = {     'name' : 'ZH3l_pTlmetjj',
+                                'range' : (20,0,400),
+                                'xaxis' : 'pTlmetjj',
+                                'fold' : 0
+                            }
+
+variables['mTlmetjj']   = { 'name' : 'ZH3l_mTlmetjj',
+                            'range' : (25,0,500),
+                            'xaxis' : 'mTlmetjj',
+                            'fold' : 0
+                        }
+
+variables['ptz']        = { 'name' : 'ZH3l_pTZ',
+                            'range' : (20,0,400),
+                            'xaxis' : 'ptz',
+                            'fold' : 0
+                        }
+
+variables['mtw_notZ']   = { 'name' : 'ZH3l_mTlmet',
+                            'range' : (20,0,200),
+                            'xaxis' : 'mTlmet',
+                            'fold' : 0
+                        }
+
+variables['mtw_fit']    = { 'name' : 'ZH3l_mTlmet',
+                            'range' : (8,0,160),
+                            'xaxis' : 'mTlmet',
+                            'fold' : 2
+                        }
+
+variables['checkmZ']    = { 'name' : 'ZH3l_checkmZ',
+                            'range' : (20,0,200),
+                            'xaxis' : 'checkmZ',
+                            'fold' : 0
+                        }
+
+
