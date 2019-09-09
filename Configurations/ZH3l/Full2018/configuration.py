6c4a15456d56605cf8e4cef24bb08a286203cfc1
corrinne mills <cmills10@uic.edu>
2019-09-09 23:21:51 +0200

    update lumi

--- Configurations/ZH3l/Full2018/configuration.py
+++ Configurations/ZH3l/Full2018/configuration.py
@@ -21,7 +21,7 @@ samplesFile = 'samples.py'
 plotFile = 'plot.py' 
 
 # luminosity to normalize to (in 1/fb)
-lumi = 35.867
+lumi = 59.74
 
 # used by mkPlot to define output directory for plots
 outputDirPlots = 'plot_'+tag
