899e6c151c6e8db88462d3b4face0af1a8a9a645
adrianalvarezf <adrian.alvarez.fernandez@cern.ch>
2019-09-16 11:53:26 +0200

    STXS 2017 completed

--- Configurations/ggH/Full2017/HTXS_Stage1/variables.py
+++ Configurations/ggH/Full2017/HTXS_Stage1/variables.py
@@ -9,7 +9,7 @@ variables['events']  = {   'name': '1',
                         'xaxis' : 'events', 
                         'fold' : 3
                         }
-'''
+
 variables['mllVSmth_pt2ge20'] = {   'name': 'mll:mth',            #   variable name    
                              'range' : ([60,80,90,100,110,120,130,150,200],[12,25,35,40,45,50,55,70,90,210],),            #   variable range
                              'xaxis' : 'm_{ll} : m_{T}^{H}',      #   x axis name
@@ -32,4 +32,4 @@ variables['mllVSmth_pt2lt20'] = {   'name': 'mll:mth',            #   variable n
                              #
                              }
 
-'''
+
