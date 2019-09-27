3810d45ca3b063a2a407643b421b97354a79396d
Davide Di Croce <davide.di.croce@cern.ch>
2019-09-27 15:53:44 +0200

    Updating HWW SF with new DYMVA

---
+++ Configurations/ggH_SF/Full2018/maxDNN/configuration_0j_dymvaOptim.py
@@ -0,0 +1,44 @@
+# example of configuration file
+treeName= 'Events'
+
+tag = 'ggH_SF_2018_0j_dymvaOptim'
+
+# used by mkShape to define output directory for root files
+outputDir = 'rootFile_0j_dymvaOptim'
+
+# file with TTree aliases
+aliasesFile = 'aliases.py'
+
+# file with list of variables
+variablesFile = 'variables.py'
+
+# file with list of cuts
+cutsFile = 'cuts_0j_dymvaOptim.py' 
+
+# file with list of samples
+samplesFile = 'samples.py' 
+
+# file with list of samples
+plotFile = 'plot.py' 
+
+
+
+# luminosity to normalize to (in 1/fb)
+lumi = 59.74
+
+# used by mkPlot to define output directory for plots
+# different from "outputDir" to do things more tidy
+outputDirPlots = 'plotGGH_SF_signal'
+
+
+# used by mkDatacards to define output directory for datacards
+outputDirDatacard = 'datacards'
+
+
+# structure file for datacard
+structureFile = 'structure.py'
+
+
+# nuisances file for mkDatacards and for mkShape
+nuisancesFile = 'nuisances.py'
+
