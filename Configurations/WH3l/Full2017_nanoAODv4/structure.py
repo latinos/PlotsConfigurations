3d922666d0f3e3fea19b36d7960fcd941a1e0e6e
pageyu <pyu@cern.ch>
2019-10-10 00:44:09 +0200

    Update new config with nanoAODv4.

---
+++ Configurations/WH3l/Full2017_nanoAODv4/structure.py
@@ -0,0 +1,111 @@
+# structure configuration for datacard
+
+#structure = {}
+
+# keys here must match keys in samples.py
+#
+structure['Fake']  = {
+                  'isSignal' : 0,
+                  'isData'   : 0
+              }
+
+#structure['DY']  = {
+#                  'isSignal' : 0,
+#                  'isData'   : 0
+#                  }
+#
+#structure['top']  = {
+#                  'isSignal' : 0,
+#                  'isData'   : 0
+#                  }
+
+structure['WW']  = {
+                  'isSignal' : 0,
+                  'isData'   : 0
+                  }
+
+structure['ggWW']  = {
+                  'isSignal' : 0,
+                  'isData'   : 0
+                  }
+
+
+structure['Vg']  = {
+                  'isSignal' : 0,
+                  'isData'   : 0
+                  }
+
+structure['VgS']  = {
+                  'isSignal' : 0,
+                  'isData'   : 0
+                  }
+
+
+structure['WZ']  = {
+                  'isSignal' : 0,
+                  'isData'   : 0
+                  }
+
+
+structure['VVV']  = {
+                  'isSignal' : 0,
+                  'isData'   : 0
+                  }
+
+structure['ZZ']  = {
+                  'isSignal' : 0,
+                  'isData'   : 0
+                  }
+
+#structure['ggZZ_tt']  = {
+#                  'isSignal' : 0,
+#                  'isData'   : 0
+#                  }
+#
+#structure['ggZZ_em']  = {
+#                  'isSignal' : 0,
+#                  'isData'   : 0
+#                  }
+#
+#structure['ggZZ_ee']  = {
+#                  'isSignal' : 0,
+#                  'isData'   : 0
+#                  }
+#
+#structure['ggZZ_mm']  = {
+#                  'isSignal' : 0,
+#                  'isData'   : 0
+#                  }
+
+structure['WH_hww'] = {
+                  'isSignal' : 1,
+                  'isData'   : 0
+                  }
+
+structure['ZH_hww'] = {
+                  'isSignal' : 1,
+                  'isData'   : 0
+                  }
+
+structure['ggZH_hww'] = {
+                  'isSignal' : 1,
+                  'isData'   : 0
+                  }
+
+structure['WH_htt'] = {
+                  'isSignal' : 1,
+                  'isData'   : 0
+                  }
+
+
+# data
+
+
+structure['DATA']  = {
+                  'isSignal' : 0,
+                  'isData'   : 1
+              }
+
+
+
+
