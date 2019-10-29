e94f349afab41ac05a8e627d37cfdaedd2487d7d
Sebastian Siebert <sebastian.siebert@rwth-aachen.de>
2019-10-29 14:31:33 +0100

    Attempt at running topcr with v5, uncategorized

--- Configurations/HWWSemiLepHighMass/Full2018/cuts.py
+++ Configurations/HWWSemiLepHighMass/Full2018/cuts.py
@@ -9,60 +9,60 @@ supercut = 'LepWPCut \
          '
 
 
-## Signal regions -> bVeto
-cuts['hwwlv2q_13TeV'] = {
-   'expr': 'bVeto',
-   # Define the sub-categorization of sr
-   'categories' : {
-      #
-      'VBF_boosted'  : 'VBFtagged \
-                     && 65 < FatJet_msoftdrop[0] && FatJet_msoftdrop[0] < 105 \
-                     && boostedWtagged \
-                     ',
-      'VBF_resolved' : 'VBFtagged \
-                     && !boostedWtagged \
-                     && 65 < GenW_Ak4_mass && GenW_Ak4_mass < 105 \
-                     && resolvedWtagged \
-                     ',
-      #
-      'ggF_boosted'  : '!VBFtagged \
-                     && ggFtagged \
-                     && 65 < FatJet_msoftdrop[0] && FatJet_msoftdrop[0] < 105 \
-                     &&  boostedWtagged \
-                     ',
-      'ggF_resolved' : '!VBFtagged \
-                     && ggFtagged \
-                     && ! boostedWtagged \
-                     && 65 < GenW_Ak4_mass && GenW_Ak4_mass < 105 \
-                     && resolvedWtagged \
-                     ',
-      #
-      'untagged_boosted' :'!VBFtagged \
-                        && !ggFtagged \
-                        && 65 < FatJet_msoftdrop[0] && FatJet_msoftdrop[0] < 105 \
-                        && boostedWtagged \
-                        ',
-      'untagged_resolved':'!VBFtagged \
-                        && !ggFtagged \
-                        && !boostedWtagged \
-                        && 65 < GenW_Ak4_mass && GenW_Ak4_mass < 105 \
-                        &&  resolvedWtagged \
-                        '
-   }
-}
+# ## Signal regions -> bVeto
+# cuts['hwwlv2q_13TeV'] = {
+#    'expr': 'bVeto',
+#    # Define the sub-categorization of sr
+#    'categories' : {
+#       #
+#       'VBF_boosted'  : 'VBFtagged \
+#                      && 65 < FatJet_msoftdrop[0] && FatJet_msoftdrop[0] < 105 \
+#                      && boostedWtagged \
+#                      ',
+#       'VBF_resolved' : 'VBFtagged \
+#                      && !boostedWtagged \
+#                      && 65 < GenW_Ak4_mass && GenW_Ak4_mass < 105 \
+#                      && resolvedWtagged \
+#                      ',
+#       #
+#       'ggF_boosted'  : '!VBFtagged \
+#                      && ggFtagged \
+#                      && 65 < FatJet_msoftdrop[0] && FatJet_msoftdrop[0] < 105 \
+#                      &&  boostedWtagged \
+#                      ',
+#       'ggF_resolved' : '!VBFtagged \
+#                      && ggFtagged \
+#                      && ! boostedWtagged \
+#                      && 65 < GenW_Ak4_mass && GenW_Ak4_mass < 105 \
+#                      && resolvedWtagged \
+#                      ',
+#       #
+#       'untagged_boosted' :'!VBFtagged \
+#                         && !ggFtagged \
+#                         && 65 < FatJet_msoftdrop[0] && FatJet_msoftdrop[0] < 105 \
+#                         && boostedWtagged \
+#                         ',
+#       'untagged_resolved':'!VBFtagged \
+#                         && !ggFtagged \
+#                         && !boostedWtagged \
+#                         && 65 < GenW_Ak4_mass && GenW_Ak4_mass < 105 \
+#                         &&  resolvedWtagged \
+#                         '
+#    }
+# }
 
 ## Top control region --> FIXME: definition of region?
 cuts['hwwlv2q_13TeV_top'] = {
    'expr' : 'topcr',
    'categories' : {
-      # FIXME --> to be like above
+      'any' : '1'
    }
 }
 
-## W+jets control region
-cuts['hwwlv2q_13TeV_wjets'] = {
-   'expr' : 'wjetscr',
-   'categories' : {
-      # FIXME --> to be like above
-   }
-}
+# ## W+jets control region
+# cuts['hwwlv2q_13TeV_wjets'] = {
+#    'expr' : 'wjetscr',
+#    'categories' : {
+#       # FIXME --> to be like above
+#    }
+# }
