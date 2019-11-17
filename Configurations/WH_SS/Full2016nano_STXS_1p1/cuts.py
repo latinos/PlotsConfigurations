c36572d49adf81613c5dc5ad2ce4aecb6f84d072
SiewYan <siew.yan.hoh@cern.ch>
2019-11-17 22:45:03 +0100

    fix njet

--- Configurations/WH_SS/Full2016nano_STXS_1p1/cuts.py
+++ Configurations/WH_SS/Full2016nano_STXS_1p1/cuts.py
@@ -38,8 +38,10 @@ whcuts['hww2l2v_13TeV_of2j_WH_SS_uu_1j'] = 'uu[0] && oneJet'
 whcuts['hww2l2v_13TeV_of2j_WH_SS_ee_1j'] = 'ee[0] && oneJet && abs(mll-91.2)>15'
 whcuts['hww2l2v_13TeV_of2j_WH_SS_eu_1j'] = 'eu[0] && oneJet'
 
+
 for key,value in whcuts.iteritems():
     for cat,val in categorization_wh.iteritems():
+        njet='1==1'
         if '_2j' in key:
             if '_0J' in cat:
                 njet='Sum$(CleanJet_pt>30)==2'
