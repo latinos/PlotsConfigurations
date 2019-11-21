11b741523d469c11516fbb2a3c9127282ed5b9be
piedra <piedra@cern.ch>
2019-11-21 13:19:58 +0100

    Include tables for VH2j control regions

--- Configurations/VH2j/scripts/prepareTables.py
+++ Configurations/VH2j/scripts/prepareTables.py
@@ -18,10 +18,16 @@ if not os.path.exists(output) :
 
 list_datacards = {}
 
-#                 name                               datacard                           blind
-list_datacards [ 'mll_2016' ] = [ pathToDatacards + '2016/VH_2j_emu/mll/datacard.txt', 'True' ]  
-list_datacards [ 'mll_2017' ] = [ pathToDatacards + '2017/VH_2j_emu/mll/datacard.txt', 'True' ]
-list_datacards [ 'mll_2018' ] = [ pathToDatacards + '2018/VH_2j_emu/mll/datacard.txt', 'True' ]
+#                 name                                              datacard                                blind
+list_datacards [ '2016_mll_VH_2j_topemu'   ] = [ pathToDatacards + '2016/VH_2j_topemu/mll/datacard.txt',   'True' ]  
+list_datacards [ '2016_mll_VH_2j_emu'      ] = [ pathToDatacards + '2016/VH_2j_emu/mll/datacard.txt',      'True' ]  
+list_datacards [ '2016_mll_VH_2j_DYtautau' ] = [ pathToDatacards + '2016/VH_2j_DYtautau/mll/datacard.txt', 'True' ]  
+list_datacards [ '2017_mll_VH_2j_topemu'   ] = [ pathToDatacards + '2017/VH_2j_topemu/mll/datacard.txt',   'True' ]  
+list_datacards [ '2017_mll_VH_2j_emu'      ] = [ pathToDatacards + '2017/VH_2j_emu/mll/datacard.txt',      'True' ]  
+list_datacards [ '2017_mll_VH_2j_DYtautau' ] = [ pathToDatacards + '2017/VH_2j_DYtautau/mll/datacard.txt', 'True' ]  
+list_datacards [ '2018_mll_VH_2j_topemu'   ] = [ pathToDatacards + '2018/VH_2j_topemu/mll/datacard.txt',   'True' ]  
+list_datacards [ '2018_mll_VH_2j_emu'      ] = [ pathToDatacards + '2018/VH_2j_emu/mll/datacard.txt',      'True' ]  
+list_datacards [ '2018_mll_VH_2j_DYtautau' ] = [ pathToDatacards + '2018/VH_2j_DYtautau/mll/datacard.txt', 'True' ]  
 
 for datacardName, datacard in list_datacards.iteritems() :
   print ""
