1de44011c06f73b5c45b5eb949138cef87a4c13c
Siew Yan Hoh <shoh@lxplus759.cern.ch>
2019-12-10 00:43:12 +0100

    merge bin

--- Configurations/WH3l/FullRunII_BDT/Full2016_STXS_1p1/scripts/doFit.py
+++ Configurations/WH3l/FullRunII_BDT/Full2016_STXS_1p1/scripts/doFit.py
@@ -14,7 +14,7 @@ for cat in HTXSStage1_1Categories:
     #sampleNames.append(cat.replace('QQ2HLNU','WH_lep_htt'))
 
 os.chdir('Combination')
-
+'''
 #No merging
 command="combine -M MultiDimFit --algo=singles --X-rtd MINIMIZER_analytic Full2016_WH3l_HTXS_Stage1.root -t -1 --setParameters "
 poi =''
@@ -26,22 +26,26 @@ for sample in sampleNames:
 
 command = command[:-1]
 print(command)
-os.system(command)
-
+#os.system(command)
 '''
+
 #Merge some bins
-command="combine -M MultiDimFit --algo=singles --X-rtd MINIMIZER_analytic Full2017_ggH_HTXS_Stage1_merged.root -t -1 --setParameters "
+command="combine -M MultiDimFit --algo=singles --X-rtd MINIMIZER_analytic Full2016_WH3l_HTXS_Stage1_merged.root -t -1 --setParameters "
 poi = ''
 for sample in sampleNames:
-  if 'ggH_hww' not in sample: continue
+  if 'WH_hww' not in sample: continue
+  if 'FWDH' in sample: continue
+  if 'GE1J' in sample: continue
+  if ('PTV_0_75' in sample or 'PTV_75_150' in sample): poi = 'r_WH_hww_LE150'
+  if ('PTV_150_250_0J' in sample or 'PTV_GT250' in sample): poi = 'r_WH_hww_GE150'
+  #else: poi = 'r_'+sample
 
-  if 'GE2J' in sample: poi = 'r_ggH_hww_GE2J'
-  elif 'VBFTOPO' in sample: poi = 'r_ggH_hww_VBFTOPO'
-  elif ('1J_PTH_120_200' in sample or '1J_PTH_GT200' in sample): poi = 'r_ggH_hww_1J_PTH_GT120'
-  else: poi = 'r_'+sample
-  
   command+="{}=1,".format(poi)
 
 command = command[:-1]
+command+=' > fitresults.txt'
+print command
 os.system(command)
-'''
+os.system('cat fitresults.txt |grep 68% | sed \'s/(68%)/ /g\' |tr -d \'-\'| tr -d \'/\' | tr -d ":" | sed \'s/+/ /g\' > fit.txt')
+os.system('awk \'{ print $0 $1 }\' < fit.txt | sed \'s/r_ggH_hww/ggH/g\' | sort -b > fit_ready.txt')
+os.system('cat fit_ready.txt')
