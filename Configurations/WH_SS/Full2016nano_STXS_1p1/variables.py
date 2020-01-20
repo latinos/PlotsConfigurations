827d9b5e11fa9b5a96e5b8373e440607c4bc9b28
Siew Yan Hoh <shoh@lxplus742.cern.ch>
2020-01-20 22:21:37 +0100

    Properly merge Gen bin

--- Configurations/WH_SS/Full2016nano_STXS_1p1/variables.py
+++ Configurations/WH_SS/Full2016nano_STXS_1p1/variables.py
@@ -8,35 +8,35 @@ variables['events']  = {   'name': '1',
                            'fold' : 3
                        }
  
-variables['WlepPt_v1'] = { 'name': 'WlepPt_v1',
-                               'range' : (60,0,300),
-                             'xaxis' : 'Wlep Pt [GeV]',
-                             'fold' : 1
-                         }
+#variables['WlepPt_v1'] = { 'name': 'WlepPt_v1',
+#                               'range' : (60,0,300),
+#                             'xaxis' : 'Wlep Pt [GeV]',
+#                             'fold' : 1
+#                         }
 
-variables['WlepPt_v2'] = { 'name': 'WlepPt_v2',
-                             'range' : (60,0,300),
-                             'xaxis' : 'Wlep Pt [GeV]',
-                             'fold' : 1
-                         }
+#variables['WlepPt_v2'] = { 'name': 'WlepPt_v2',
+#                             'range' : (60,0,300),
+#                             'xaxis' : 'Wlep Pt [GeV]',
+#                             'fold' : 1
+#                         }
 
-variables['WlepPt_v3'] = { 'name': 'WlepPt_v3',
-                             'range' : (60,0,300),
-                             'xaxis' : 'Wlep Pt [GeV]',
-                             'fold' : 1
-                         }
+#variables['WlepPt_v3'] = { 'name': 'WlepPt_v3',
+#                             'range' : (60,0,300),
+#                             'xaxis' : 'Wlep Pt [GeV]',
+#                             'fold' : 1
+#                         }
 
-variables['WlepMt_whss'] = { 'name': 'WlepMt_whss',
-                             'range' : (60,0,300),
-                             'xaxis' : 'Wlep Mt [GeV]',
-                             'fold' : 1
-                         }
+#variables['WlepMt_whss'] = { 'name': 'WlepMt_whss',
+#                             'range' : (60,0,300),
+#                             'xaxis' : 'Wlep Mt [GeV]',
+#                             'fold' : 1
+#                         }
 
-variables['mlljj20_whss_bin4']  = {   'name': 'mlljj20_whss',
-                        'range' : ([60.,70.,80.,90.,100.,110.,120.,130.,140.,150.,160.,170.,180.,190.,200.,250.,300.],),
-                        'xaxis' : 'mlljj20_whss [GeV]',
-                        'fold' : 0
-                        }
+#variables['mlljj20_whss_bin4']  = {   'name': 'mlljj20_whss',
+#                        'range' : ([60.,70.,80.,90.,100.,110.,120.,130.,140.,150.,160.,170.,180.,190.,200.,250.,300.],),
+#                        'xaxis' : 'mlljj20_whss [GeV]',
+#                        'fold' : 0
+#                        }
 
 variables['mlljj20_whss_bin5']  = {   'name': 'mlljj20_whss',
                         'range' : ([60.,100.,110.,120.,130.,140.,150.,175.,200.,250.,300.],),
@@ -44,8 +44,8 @@ variables['mlljj20_whss_bin5']  = {   'name': 'mlljj20_whss',
                         'fold' : 0
                         }
 
-variables['njet']  = {  'name': 'Sum$(CleanJet_pt>30)',
-                        'range' : (5,0,5),
-                        'xaxis' : 'Number of jets',
-                        'fold' : 2
-                        }
+#variables['njet']  = {  'name': 'Sum$(CleanJet_pt>30)',
+#                        'range' : (5,0,5),
+#                        'xaxis' : 'Number of jets',
+#                        'fold' : 2
+#                        }
