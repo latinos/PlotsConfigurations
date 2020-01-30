1fcd84a1271696131b050be5f78c8b15bb4ab317
Siew Yan Hoh <shoh@lxplus738.cern.ch>
2020-01-30 21:24:03 +0100

    DY admixture sample: point DY HT binned sample to eos space

--- Configurations/WH_SS/Full2016nano_STXS_1p1/samples.py
+++ Configurations/WH_SS/Full2016nano_STXS_1p1/samples.py
@@ -37,13 +37,13 @@ def makeMCDirectory(var=''):
     if var:
         #return os.path.join(treeBaseDir, mcProduction, mcSteps.format(var='__' + var))
         #return '/afs/cern.ch/user/d/ddicroce/public/Summer16/l2tightOR__{var}'.format(var=var)
-        return '/afs/cern.ch/user/s/shoh/public/latino_STXS/WH2l/2016/wwSel'
+        return '/afs/cern.ch/user/s/shoh/public/'+var+'/WH2l/2016/wwSel'
     else:
         #return os.path.join(treeBaseDir, mcProduction, mcSteps.format(var=''))
         return '/afs/cern.ch/user/d/ddicroce/public/Summer16/l2tightOR'
 
-mcDirectory   = makeMCDirectory()
-mcStxs        = makeMCDirectory('stxs')
+#mcDirectory   = makeMCDirectory()
+mcStxs        = makeMCDirectory('latino_STXS')
 
 ################################################
 ############ NUMBER OF LEPTONS #################
@@ -143,8 +143,8 @@ ptllDYW_NLO = '(0.876979+gen_ptll*(4.11598e-03)-(2.35520e-05)*gen_ptll*gen_ptll)
 ptllDYW_LO  = '(8.61313e-01+gen_ptll*4.46807e-03-1.52324e-05*gen_ptll*gen_ptll)*(1.08683 * (0.95 - 0.0657370*TMath::Erf((gen_ptll-11.)/5.51582)))*(gen_ptll<140)+1.141996*(gen_ptll>=140)'
 
 if useDYtt:
-    files = nanoGetSampleFiles(mcDirectory, 'DYJetsToTT_MuEle_M-50') + \
-            nanoGetSampleFiles(mcDirectory, 'DYJetsToLL_M-10to50-LO')
+    files = getSampleFiles(directory, 'DYJetsToTT_MuEle_M-50',False,'nanoLatino_') + \
+            getSampleFiles(directory, 'DYJetsToLL_M-10to50-LO',False,'nanoLatino_')
 
     samples['DY'] = {
         'name': files,
@@ -155,8 +155,8 @@ if useDYtt:
     addSampleWeight(samples,'DY','DYJetsToLL_M-10to50-LO',ptllDYW_LO)
 
 else:
-    files = nanoGetSampleFiles(mcDirectory, 'DYJetsToLL_M-50_ext2') + \
-            nanoGetSampleFiles(mcDirectory, 'DYJetsToLL_M-10to50-LO')
+    files = getSampleFiles(directory, 'DYJetsToLL_M-50_ext2',False,'nanoLatino_') + \
+            getSampleFiles(directory, 'DYJetsToLL_M-10to50-LO',False,'nanoLatino_')
 
     samples['DY'] = {
         'name': files,
@@ -168,19 +168,19 @@ else:
     }
     # Add DY HT Samples
     if useDYHT :
-        samples['DY']['name'] +=   nanoGetSampleFiles(mcDirectory, 'DYJetsToLL_M-5to50_HT-70to100') \
-                                 + nanoGetSampleFiles(mcDirectory, 'DYJetsToLL_M-5to50_HT-100to200_ext1') \
-                                 + nanoGetSampleFiles(mcDirectory, 'DYJetsToLL_M-5to50_HT-200to400_ext1') \
-                                 + nanoGetSampleFiles(mcDirectory, 'DYJetsToLL_M-5to50_HT-400to600_ext1') \
-                                 + nanoGetSampleFiles(mcDirectory, 'DYJetsToLL_M-5to50_HT-600toinf') \
-                                 + nanoGetSampleFiles(mcDirectory, 'DYJetsToLL_M-50_HT-70to100') \
-                                 + nanoGetSampleFiles(mcDirectory, 'DYJetsToLL_M-50_HT-100to200_ext1') \
-                                 + nanoGetSampleFiles(mcDirectory, 'DYJetsToLL_M-50_HT-200to400_ext1') \
-                                 + nanoGetSampleFiles(mcDirectory, 'DYJetsToLL_M-50_HT-400to600_ext1') \
-                                 + nanoGetSampleFiles(mcDirectory, 'DYJetsToLL_M-50_HT-600to800') \
-                                 + nanoGetSampleFiles(mcDirectory, 'DYJetsToLL_M-50_HT-800to1200') \
-                                 + nanoGetSampleFiles(mcDirectory, 'DYJetsToLL_M-50_HT-1200to2500') \
-                                 + nanoGetSampleFiles(mcDirectory, 'DYJetsToLL_M-50_HT-2500toinf')
+        samples['DY']['name'] +=   getSampleFiles(directory, 'DYJetsToLL_M-5to50_HT-70to100',False,'nanoLatino_') \
+                                 + getSampleFiles(directory, 'DYJetsToLL_M-5to50_HT-100to200_ext1',False,'nanoLatino_') \
+                                 + getSampleFiles(directory, 'DYJetsToLL_M-5to50_HT-200to400_ext1',False,'nanoLatino_') \
+                                 + getSampleFiles(directory, 'DYJetsToLL_M-5to50_HT-400to600_ext1',False,'nanoLatino_') \
+                                 + getSampleFiles(directory, 'DYJetsToLL_M-5to50_HT-600toinf',False,'nanoLatino_') \
+                                 + getSampleFiles(directory, 'DYJetsToLL_M-50_HT-70to100',False,'nanoLatino_') \
+                                 + getSampleFiles(directory, 'DYJetsToLL_M-50_HT-100to200_ext1',False,'nanoLatino_') \
+                                 + getSampleFiles(directory, 'DYJetsToLL_M-50_HT-200to400_ext1',False,'nanoLatino_') \
+                                 + getSampleFiles(directory, 'DYJetsToLL_M-50_HT-400to600_ext1',False,'nanoLatino_') \
+                                 + getSampleFiles(directory, 'DYJetsToLL_M-50_HT-600to800',False,'nanoLatino_') \
+                                 + getSampleFiles(directory, 'DYJetsToLL_M-50_HT-800to1200',False,'nanoLatino_') \
+                                 + getSampleFiles(directory, 'DYJetsToLL_M-50_HT-1200to2500',False,'nanoLatino_') \
+                                 + getSampleFiles(directory, 'DYJetsToLL_M-50_HT-2500toinf',False,'nanoLatino_')
 
     addSampleWeight(samples,'DY','DYJetsToLL_M-50_ext2',ptllDYW_NLO)
     addSampleWeight(samples,'DY','DYJetsToLL_M-10to50-LO',ptllDYW_LO)
