b19a80e13b0f2a1f36b81e134511787197d5a130
pageyu <pyu@cern.ch>
2020-01-30 16:49:30 +0100

    Remove PU in nuisances source.

--- Configurations/WH3l/FullRunII_BDT/Full2017/nuisances.py
+++ Configurations/WH3l/FullRunII_BDT/Full2017/nuisances.py
@@ -234,22 +234,6 @@ nuisances['met'] = {
 }
 
 
-##### Pileup
-
-nuisances['PU'] = {
-    'name': 'CMS_PU_2017',
-    'kind': 'weight',
-    'type': 'shape',
-    'samples': {
-        'DY': ['0.993259983266*(puWeightUp/puWeight)', '0.997656381501*(puWeightDown/puWeight)'],
-        'top': ['1.00331969187*(puWeightUp/puWeight)', '0.999199609528*(puWeightDown/puWeight)'],
-        'WW': ['1.0033022059*(puWeightUp/puWeight)', '0.997085330608*(puWeightDown/puWeight)'],
-        'ggH_hww': ['1.0036768006*(puWeightUp/puWeight)', '0.995996570285*(puWeightDown/puWeight)'],
-        'qqH_hww': ['1.00374694528*(puWeightUp/puWeight)', '0.995878596852*(puWeightDown/puWeight)'],
-    },
-    'AsLnN': '1',
-}
-
 ##### PS and UE --> NO AVAILABLE FOR VH2j! 
 nuisances['PS']  = {
     'name'  : 'PS',
