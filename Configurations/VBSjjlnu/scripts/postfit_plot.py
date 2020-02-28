8f2808faa92b4de8aba12a962eb88b795122031f
Davide Valsecchi <davide.valsecchi@cern.ch>
2020-02-28 17:24:36 +0100

    Postfit script: does the plots for all combine regions

--- Configurations/VBSjjlnu/scripts/postfit_plot.py
+++ Configurations/VBSjjlnu/scripts/postfit_plot.py
@@ -14,8 +14,9 @@ parser.add_argument("-d","--datacards", help="Datacard names", type=str)
 parser.add_argument("-b","--basedir", help="Baseline folder", type=str)
 parser.add_argument("-o","--outputdir", help="Output folder", type=str)
 parser.add_argument("--plot-config", help="Path of plot configuration.py", required=True, type=str)
-parser.add_argument("--plot-var", help="Path of plot configuration.py", required=True, type=str)
+parser.add_argument("--region-name", help="Combine region in the datacard to plot", required=False, type=str)
 parser.add_argument("--year", help="Year", required=True, type=str)
+parser.add_argument("--debug", help="Debug", required=False, action="store_true")
 args = parser.parse_args()
 
 #Loading plot configuration
@@ -37,42 +38,50 @@ def post_fit_plots(datac):
         return
     plotbasedir = os.path.dirname(args.plot_config)
     basedircomb = os.getcwd() +"/"+ args.basedir + "/" + datac["datacard_name"]
-    cmds = ["cd "+ plotbasedir]
+    cmds = []
 
     combconf = None
-    for var in datac["phase_spaces"]:
-        if var["var"] == args.plot_var:
-            combconf = var 
-    if not combconf:
-        print("> Error! Requested variable not found in datacard")
-        return 
-    
-    cmds.append("""mkPostFitPlot.py --inputFileCombine={} \\
-        --outputFile={}/postfit_latino_{}.root --variable {} \\
-        --cutNameInOriginal={} --cut={} \\
-        --inputFile={}/{} --kind=b --pycfg={}""".format(
-            basedircomb+"/fitDiagnostics."+datac["datacard_name"]+".root",
-            outputDir,
-            datac["datacard_name"],
-            args.plot_var,
-            combconf["cut"],
-            combconf["name"] + "_"+args.year, #cut name in the datacard
-            outputDir, "plots_{}.root".format(tag),
-            args.plot_config.split("/")[-1]
-        ))
-    cmds.append("""mkPlot.py  --pycfg={} \\
-        --inputFile={}/postfit_latino_{}.root \\
-        --showIntegralLegend=1 --showRelativeRatio --plotNormalizedDistributions=True --minLogC 0.3 --maxLogC 1e2 \\
-        --minLogCratio 0.3 --maxLogCratio 1e2 --postFit=p --outputDirPlots={} \\
-        --onlyVariable={} --onlyCut={}""".format(
-            args.plot_config.split("/")[-1],
-            outputDir, 
-            datac["datacard_name"],
-            datac["outputdir"],
-            combconf["var"], combconf["cut"]
-        ))
+    for combconf in datac["phase_spaces"]:
+        # The baseline is to plot all regions and variables
+        # If not specified
+        if args.region_name != None and var["name"] != args.region_name: continue 
 
-    os.system("\n".join(cmds))
+        print("Post fit of variable {}, cut {}".format(combconf["var"], combconf["cut"]))
+        
+        cmdpost1 = """cd {9};\\
+            mkPostFitPlot.py --inputFileCombine={0} \\
+            --outputFile={1}/postfit_latino_{2}.root --variable {3} \\
+            --cutNameInOriginal={4} --cut={5} \\
+            --inputFile={6}/{7} --kind=b --pycfg={8}""".format(
+                basedircomb+"/fitDiagnostics."+datac["datacard_name"]+".root",
+                outputDir,
+                datac["datacard_name"],
+                combconf["var"],
+                combconf["cut"],
+                combconf["name"] + "_"+args.year, #cut name in the datacard
+                outputDir, "plots_{}.root".format(tag),
+                args.plot_config.split("/")[-1],
+                plotbasedir
+            )
+
+        os.makedirs(datac["outputdir"]+"/"+combconf["name"])
+        cmdpost2 = """mkPlot.py  --pycfg={0} \\
+            --inputFile={1}/postfit_latino_{2}.root \\
+            --showIntegralLegend=1 --showRelativeRatio --plotNormalizedDistributions=True --minLogC 0.3 --maxLogC 1e2 \\
+            --minLogCratio 0.3 --maxLogCratio 1e2 --postFit=p --outputDirPlots={3} \\
+            --onlyVariable={4} --onlyCut={5}""".format(
+                args.plot_config.split("/")[-1],
+                outputDir, 
+                datac["datacard_name"],
+                datac["outputdir"]+"/"+combconf["name"],
+                combconf["var"], combconf["cut"]
+            )
+        cmds.append(cmdpost1)
+        cmds.append(cmdpost2)
+        if args.debug: 
+            print(cmdpost1)
+            print(cmdpost2)
+        os.system("\n".join([cmdpost1, cmdpost2]))
 
     with open("{}/log_postfit_plots.txt".format(basedircomb), "w") as wf:
         wf.write("\n".join(cmds))
