402187a60d4568b40860aedf9c06a0bcdcc1d7e8
Lorenzo Viliani <lorenzo.viliani@cern.ch>
2020-03-11 12:22:30 +0100

    Add 2017 SS configuration using new WPs

---
+++ Configurations/ControlRegions/SS/Full2017_ForNewWPs/nuisances.py
@@ -0,0 +1,46 @@
+#### FAKES
+
+## FIXME: check the 30% lnN
+nuisances['fake_syst'] = {
+    'name': 'CMS_fake_syst',
+    'type': 'lnN',
+    'samples': {
+        'Fake': '1.3'
+    },
+}
+
+nuisances['fake_ele'] = {
+    'name': 'CMS_fake_e_2017',
+    'kind': 'weight',
+    'type': 'shape',
+    'samples': {
+        'Fake': ['fakeWEleUp', 'fakeWEleDown'],
+    }
+}
+
+nuisances['fake_ele_stat'] = {
+    'name': 'CMS_fake_stat_e_2017',
+    'kind': 'weight',
+    'type': 'shape',
+    'samples': {
+        'Fake': ['fakeWStatEleUp', 'fakeWStatEleDown']
+    }
+}
+
+nuisances['fake_mu'] = {
+    'name': 'CMS_fake_m_2017',
+    'kind': 'weight',
+    'type': 'shape',
+    'samples': {
+        'Fake': ['fakeWMuUp', 'fakeWMuDown'],
+    }
+}
+
+nuisances['fake_mu_stat'] = {
+    'name': 'CMS_fake_stat_m_2017',
+    'kind': 'weight',
+    'type': 'shape',
+    'samples': {
+        'Fake': ['fakeWStatMuUp', 'fakeWStatMuDown'],
+    }
+}
