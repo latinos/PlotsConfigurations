94370d0d7faa7369ad1bf81d9e709eaf9f0f143b
arun <arun.kumar@cern.ch>
2020-03-18 15:54:44 +0100

    WHSS v6 configs up

---
+++ Configurations/WH_SS/Full2016nanov6/cuts.py
@@ -0,0 +1,75 @@
+# cuts
+
+supercut = 'mll>12  \
+            && Lepton_pt[0]>25 && Lepton_pt[1]>20 \
+            && (nLepton>=2 && Alt$(Lepton_pt[2],0)<10) \
+            && bVeto \
+            && PuppiMET_pt > 30 \
+            '
+## 2jets
+
+cuts['hww2l2v_13TeV_of2j_WH_SS_uu_2j'] = '(Lepton_pdgId[0]*Lepton_pdgId[1] == 13*13) \
+                                       && Alt$(CleanJet_pt[0],0)>30 \
+                                       && Alt$(CleanJet_pt[1],0)>30 \
+                                       && mjj < 100 \
+                                       && abs(Lepton_eta[0] - Lepton_eta[1])<2.0 \
+                                       && abs(mll-91.2)>15 \
+                                       && mlljj20_whss > 50. \
+                                       '
+cuts['hww2l2v_13TeV_of2j_WH_SS_eu_2j'] = '(Lepton_pdgId[0]*Lepton_pdgId[1] == 11*13) \
+                                       && Alt$(CleanJet_pt[0],0)>30 \
+                                       && Alt$(CleanJet_pt[1],0)>30 \
+                                       && mjj < 100 \
+                                       && abs(Lepton_eta[0] - Lepton_eta[1])<2.0 \
+                                       && mlljj20_whss > 50. \
+                                       '
+## 1jet
+
+cuts['hww2l2v_13TeV_of2j_WH_SS_uu_1j'] = '(Lepton_pdgId[0]*Lepton_pdgId[1] == 13*13) \
+                                       && Alt$(CleanJet_pt[0],0)>30 \
+                                       && Alt$(CleanJet_pt[1],0)<30 \
+                                       && abs(Lepton_eta[0] - Lepton_eta[1])<2.0 \
+                                       && abs(mll-91.2)>15 \
+                                       && mlljj20_whss > 50. \
+                                       '
+cuts['hww2l2v_13TeV_of2j_WH_SS_eu_1j'] = '(Lepton_pdgId[0]*Lepton_pdgId[1] == 11*13) \
+                                       && Alt$(CleanJet_pt[0],0)>30 \
+                                       && Alt$(CleanJet_pt[1],0)<30 \
+                                       && abs(Lepton_eta[0] - Lepton_eta[1])<2.0 \
+                                       && mlljj20_whss > 50. \
+                                       '
+'''
+cuts['hww2l2v_13TeV_of2j_WH_SS_WZ_1j'] = '((Lepton_pdgId[0]*Lepton_pdgId[1] == 13*13) || (Lepton_pdgId[0]*Lepton_pdgId[1] == 11*13))\
+                                       && Alt$(CleanJet_pt[0],0)>30 \
+                                       && Alt$(CleanJet_pt[1],0)<30 \
+                                       && WH3l_mlll > 100 \
+                                       && abs(WH3l_chlll) == 1 \
+                                       '
+cuts['hww2l2v_13TeV_of2j_WH_SS_WZ_2j'] = '((Lepton_pdgId[0]*Lepton_pdgId[1] == 13*13) || (Lepton_pdgId[0]*Lepton_pdgId[1] == 11*13)) \
+                                       && Alt$(CleanJet_pt[0],0)>30 \
+                                       && Alt$(CleanJet_pt[1],0)>30 \
+                                       && WH3l_mlll > 100 \
+                                       && abs(WH3l_chlll) == 1 \
+                                       '
+'''
+'''
+## Signal regions
+cuts['hww2l2v_13TeV_of2j_WH_SS_ll_1j'] = '((Lepton_pdgId[0] * Lepton_pdgId[1] == 13*13) || (Lepton_pdgId[0] * Lepton_pdgId[1] == 11*11)) \
+                                       && abs(mll-91.2)>12 \
+                                       && Alt$(CleanJet_pt[0],0)>30 \
+                                       && Alt$(CleanJet_pt[1],0)<30 \
+                                       '
+cuts['hww2l2v_13TeV_of2j_WH_SS_ll_2j'] = '((Lepton_pdgId[0] * Lepton_pdgId[1] == 13*13) || (Lepton_pdgId[0] * Lepton_pdgId[1] == 11*11)) \
+                                       && abs(mll-91.2)>12 \
+                                       && Alt$(CleanJet_pt[0],0)>30 \
+                                       && Alt$(CleanJet_pt[1],0)>30 \
+                                       '
+cuts['hww2l2v_13TeV_of2j_WH_SS_eu_1j'] = '(Lepton_pdgId[0]*Lepton_pdgId[1] == 11*13) \
+                                       && Alt$(CleanJet_pt[0],0)>30 \
+                                       && Alt$(CleanJet_pt[1],0)<30 \
+                                       '
+cuts['hww2l2v_13TeV_of2j_WH_SS_eu_2j'] = '(Lepton_pdgId[0]*Lepton_pdgId[1] == 11*13) \
+                                       && Alt$(CleanJet_pt[0],0)>30 \
+                                       && Alt$(CleanJet_pt[1],0)>30 \
+                                       '
+'''
---
+++ Configurations/WH_SS/Full2018nanov6/cuts.py
@@ -0,0 +1,73 @@
+# cuts
+
+'''
+supercut = 'mll>12  \
+            && Lepton_pt[0]>25 && Lepton_pt[1]>20 \
+            && (nLepton>=2 && Alt$(Lepton_pt[2],0)<10) \
+            && PuppiMET_pt > 30 \
+            && abs(Lepton_eta[0] - Lepton_eta[1])<2.0 \
+            '
+'''
+supercut = 'mll>12  \
+            && Lepton_pt[0]>25 && Lepton_pt[1]>20 \
+            && (nLepton>=2 && Alt$(Lepton_pt[2],0)<10) \
+            && bVeto \
+            && PuppiMET_pt > 30 \
+            '
+            #&& mllThird < 10 \
+            #&& mtw2 > 40 \
+## 2jets
+
+cuts['hww2l2v_13TeV_of2j_WH_SS_uu_2j'] = '(Lepton_pdgId[0]*Lepton_pdgId[1] == 13*13) \
+                                       && Alt$(CleanJet_pt[0],0)>30 \
+                                       && Alt$(CleanJet_pt[1],0)>30 \
+                                       && mjj < 100 \
+                                       && abs(Lepton_eta[0] - Lepton_eta[1])<2.0 \
+                                       && mlljj20_whss > 50. \
+                                       && abs(mll-91.2)>15 \
+                                       '
+cuts['hww2l2v_13TeV_of2j_WH_SS_eu_2j'] = '(Lepton_pdgId[0]*Lepton_pdgId[1] == 11*13) \
+                                       && Alt$(CleanJet_pt[0],0)>30 \
+                                       && Alt$(CleanJet_pt[1],0)>30 \
+                                       && mjj < 100 \
+                                       && abs(Lepton_eta[0] - Lepton_eta[1])<2.0 \
+                                       && mlljj20_whss > 50. \
+                                       '
+
+## 1jet
+
+cuts['hww2l2v_13TeV_of2j_WH_SS_uu_1j'] = '(Lepton_pdgId[0]*Lepton_pdgId[1] == 13*13) \
+                                       && Alt$(CleanJet_pt[0],0)>30 \
+                                       && Alt$(CleanJet_pt[1],0)<30 \
+                                       && abs(Lepton_eta[0] - Lepton_eta[1])<2.0 \
+                                       && mlljj20_whss > 50. \
+                                       && abs(mll-91.2)>15 \
+                                       '
+cuts['hww2l2v_13TeV_of2j_WH_SS_eu_1j'] = '(Lepton_pdgId[0]*Lepton_pdgId[1] == 11*13) \
+                                       && Alt$(CleanJet_pt[0],0)>30 \
+                                       && Alt$(CleanJet_pt[1],0)<30 \
+                                       && abs(Lepton_eta[0] - Lepton_eta[1])<2.0 \
+                                       && mlljj20_whss > 50. \
+                                       '
+
+'''
+## Signal regions
+cuts['hww2l2v_13TeV_of2j_WH_SS_ll_1j'] = '((Lepton_pdgId[0] * Lepton_pdgId[1] == 13*13) || (Lepton_pdgId[0] * Lepton_pdgId[1] == 11*11)) \
+                                       && abs(mll-91.2)>12 \
+                                       && Alt$(CleanJet_pt[0],0)>30 \
+                                       && Alt$(CleanJet_pt[1],0)<30 \
+                                       '
+cuts['hww2l2v_13TeV_of2j_WH_SS_ll_2j'] = '((Lepton_pdgId[0] * Lepton_pdgId[1] == 13*13) || (Lepton_pdgId[0] * Lepton_pdgId[1] == 11*11)) \
+                                       && abs(mll-91.2)>12 \
+                                       && Alt$(CleanJet_pt[0],0)>30 \
+                                       && Alt$(CleanJet_pt[1],0)>30 \
+                                       '
+cuts['hww2l2v_13TeV_of2j_WH_SS_eu_1j'] = '(Lepton_pdgId[0]*Lepton_pdgId[1] == 11*13) \
+                                       && Alt$(CleanJet_pt[0],0)>30 \
+                                       && Alt$(CleanJet_pt[1],0)<30 \
+                                       '
+cuts['hww2l2v_13TeV_of2j_WH_SS_eu_2j'] = '(Lepton_pdgId[0]*Lepton_pdgId[1] == 11*13) \
+                                       && Alt$(CleanJet_pt[0],0)>30 \
+                                       && Alt$(CleanJet_pt[1],0)>30 \
+                                       '
+'''
