831d66b87aa15a53cd95ec28ee4e0cca50c1d008
arun <arun.kumar@cern.ch>
2020-04-09 01:04:03 +0200

    WHSS up

--- Configurations/WH_SS/Full2016nanov6/nuisances.py
+++ Configurations/WH_SS/Full2016nanov6/nuisances.py
@@ -198,7 +198,7 @@ for js in jes_systs:
       'type': 'shape',
       'mapUp': js+'up',
       'mapDown': js+'do',
-      'samples': dict((skey, ['1', '1']) for skey in mc if skey not in ['WZ','ZZ','Wg','Zg,''WgS']),
+      'samples': dict((skey, ['1', '1']) for skey in mc),
       'folderUp': makeMCDirectory('JESup_suffix'),
       'folderDown': makeMCDirectory('JESdo_suffix'),
       'AsLnN': '1'
@@ -483,7 +483,7 @@ nuisances['QCDscale_VV'] = {
         'Zg': variations,
         'Wg': variations,
         'ZZ': variations,
-        'WZhas': variations,
+        'WZ': variations,
         'WgS': variations,
         'ZgS': variations
     }
