430b20bf8e495699d0e188d83f28a0ea047213d1
kaura <a.kaur@cern.ch>
2020-04-21 18:51:16 +0200

    up

--- Configurations/ZH4l/nano_config/v6/Full2017/samples.py
+++ Configurations/ZH4l/nano_config/v6/Full2017/samples.py
@@ -239,12 +239,12 @@ samples['ggWW'] = {
 ############ Vg ############
 
 samples['Wg']  =  {     'name'   :   nanoGetSampleFiles(mcDirectory,'Wg_MADGRAPHMLM'),
-                        'weight' : XSWeight+'*'+SFweight+'*'+PromptGenLepMatch3l+'*'+METFilter_MC+'* !(Gen_ZGstar_mass > 0 && Gen_ZGstar_MomId == 22 )',
+                        'weight' : XSWeight+'*'+SFweight+'*'+METFilter_MC+'* (!(Gen_ZGstar_mass > 0))',
                         'FilesPerJob' : 5 ,
                   }
 
 samples['Zg']  =  {     'name'   : nanoGetSampleFiles(mcDirectory,'ZGToLLG'),
-                        'weight' : XSWeight+'*'+SFweight+'*'+PromptGenLepMatch3l+'*'+METFilter_MC+'* !(Gen_ZGstar_mass > 0 && Gen_ZGstar_MomId == 22 )',
+                        'weight' : XSWeight+'*'+SFweight+'*'+METFilter_MC+'* (!(Gen_ZGstar_mass > 0))',
                         'FilesPerJob' : 5 ,
                   }
 
@@ -262,12 +262,19 @@ samples['Zg']  =  {     'name'   : nanoGetSampleFiles(mcDirectory,'ZGToLLG'),
 
 #################### ZZ ##############
 
-samples['ZZ']  = {  'name'   :   nanoGetSampleFiles(mcDirectory,'ZZTo4L'),
+samples['ZZ']  = {  'name'   :   nanoGetSampleFiles(mcDirectory,'ZZTo4L')
+                              +nanoGetSampleFiles(mcDirectory,'ggZZ2m2t')
+                              +nanoGetSampleFiles(mcDirectory,'ggZZ2e2t_ext1')
+                            #  +nanoGetSampleFiles(mcDirectory,'ggZZ2m2t')
+                              +nanoGetSampleFiles(mcDirectory,'ggZZ2e2m_ext1')
+                             # +nanoGetSampleFiles(mcDirectory,'ggZZ4t')
+                             # +nanoGetSampleFiles(mcDirectory,'ggZZ4e')
+                              +nanoGetSampleFiles(mcDirectory,'ggZZ4m_ext2'),
                     'weight' : XSWeight+'*'+SFweight+'*'+PromptGenLepMatch+'*'+METFilter_MC ,
                     'FilesPerJob' : 5,
                  }
                             
-
+'''
 samples['ggZZ']  = {  'name'   : nanoGetSampleFiles(mcDirectory,'ggZZ2m2t')
                               +nanoGetSampleFiles(mcDirectory,'ggZZ2e2t_ext1')
                             #  +nanoGetSampleFiles(mcDirectory,'ggZZ2m2t')
@@ -278,13 +285,13 @@ samples['ggZZ']  = {  'name'   : nanoGetSampleFiles(mcDirectory,'ggZZ2m2t')
                     'weight' : XSWeight+'*'+SFweight+'*'+PromptGenLepMatch+'*'+METFilter_MC ,
                     'FilesPerJob' : 5,
                  }
-
+'''
 addSampleWeight(samples,'ZZ','ZZTo4L',"1.07") ## The NNLO/NLO k-factor, cited from https://arxiv.org/abs/1405.2219v1
-addSampleWeight(samples,'ggZZ','ggZZ2e2t_ext1',"1.68") ## The NLO/LO k-factor, cited from https://arxiv.org/abs/1509.06734v1
-#addSampleWeight(samples,'ggZZ','ggZZ2m2t',"1.68") 
-addSampleWeight(samples,'ggZZ','ggZZ2e2m_ext1',"1.68")
+addSampleWeight(samples,'ZZ','ggZZ2e2t_ext1',"1.68") ## The NLO/LO k-factor, cited from https://arxiv.org/abs/1509.06734v1
+addSampleWeight(samples,'ZZ','ggZZ2m2t',"1.68") 
+addSampleWeight(samples,'ZZ','ggZZ2e2m_ext1',"1.68")
 #addSampleWeight(samples,'ggZZ','ggZZ4e',"1.68")
-addSampleWeight(samples,'ggZZ','ggZZ4m_ext2',"1.68")
+addSampleWeight(samples,'ZZ','ggZZ4m_ext2',"1.68")
 #addSampleWeight(samples,'ggZZ','ggZZ4t',"1.68")
 
 ############ VZ ############
@@ -342,11 +349,11 @@ samples['qqH_hww']  = {  'name'   :   nanoGetSampleFiles(mcDirectory,'VBFHToWWTo
 samples['ZH_hww']  = {  'name'   :   nanoGetSampleFiles(mcDirectory,'HZJ_HToWWTo2L2Nu_M125'), #FIXME replace with 125 GeV sample when available
                         'weight' : XSWeight+'*'+SFweight+'*'+PromptGenLepMatch+'*'+METFilter_MC ,
                      }
-
+'''
 samples['ggZH_hww']  = {  'name'   :   nanoGetSampleFiles(mcDirectory,'GluGluZH_HToWWTo2L2Nu_M125'),
                         'weight' : XSWeight+'*'+SFweight+'*'+PromptGenLepMatch+'*'+METFilter_MC ,
                      }
-
+'''
 ############ WH H->WW ############
 
 samples['WH_hww']  = {  'name'   :   nanoGetSampleFiles(mcDirectory,'HWplusJ_HToWW_M125')
