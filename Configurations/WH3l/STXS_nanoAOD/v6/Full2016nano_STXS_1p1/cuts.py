e484d3ceb2488e280dc7484cc98c2bd069decbe5
SiewYan <siew.yan.hoh@cern.ch>
2020-04-22 15:40:10 +0200

    update STXS WH3l

--- Configurations/WH3l/STXS_nanoAOD/v6/Full2016nano_STXS_1p1/cuts.py
+++ Configurations/WH3l/STXS_nanoAOD/v6/Full2016nano_STXS_1p1/cuts.py
@@ -23,6 +23,8 @@ cuts['wh3l_13TeV_ossf']  = {
     }
 }
 
+cuts['wh3l_13TeV_merged_ptv_gt150'] = '( ( %s ) || ( %s ) ) && WH3l_pTW[0] >150' %( cuts['wh3l_13TeV_sssf']['expr'] , cuts['wh3l_13TeV_ossf']['expr'] )
+
 cuts['wh3l_wz_13TeV'] = 'WH3l_flagOSSF == 1\
                          && PuppiMET_pt > 45\
                          && WH3l_ZVeto < 20\
