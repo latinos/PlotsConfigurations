baab80f739e51cea1195bb3dc04e744be8396923
piedra <piedra@cern.ch>
2020-06-15 13:06:19 +0200

    Minor changes

--- Configurations/VH2j/scripts/copyDatacards.sh
+++ Configurations/VH2j/scripts/copyDatacards.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
 
-# The final destination is
-# /eos/user/l/lviliani/HWWRun2LegacyCombination/VH2jets
-
-
 # 2016
 mkdir -p /afs/cern.ch/user/p/piedra/public/forLatinos/VH2jets/2016/VH_2j_emu
 mkdir -p /afs/cern.ch/user/p/piedra/public/forLatinos/VH2jets/2016/VH_2j_topemu
@@ -34,4 +30,10 @@ mkdir -p /afs/cern.ch/user/p/piedra/public/forLatinos/VH2jets/2018/VH_2j_DYtauta
 cp -r ../Full2018_v6/datacards/VH_2j_emu/mll         /afs/cern.ch/user/p/piedra/public/forLatinos/VH2jets/2018/VH_2j_emu/.
 cp -r ../Full2018_v6/datacards/VH_2j_topemu/events   /afs/cern.ch/user/p/piedra/public/forLatinos/VH2jets/2018/VH_2j_topemu/.
 cp -r ../Full2018_v6/datacards/VH_2j_DYtautau/events /afs/cern.ch/user/p/piedra/public/forLatinos/VH2jets/2018/VH_2j_DYtautau/.
+
+
+# Copy to eos latino
+
+rm -rf /eos/user/l/lviliani/HWWRun2LegacyCombination/VH2jets
+cp -r /afs/cern.ch/user/p/piedra/public/forLatinos/VH2jets /eos/user/l/lviliani/HWWRun2LegacyCombination/.
 
