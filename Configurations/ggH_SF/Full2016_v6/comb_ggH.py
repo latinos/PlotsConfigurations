c0ccc8bef994354f4f529938d465db8a7187b4b6
Davide Di Croce <davide.di.croce@cern.ch>
2020-06-17 17:23:23 +0200

    HWW SF 2016

---
+++ Configurations/ggH_SF/Full2016_v6/comb_ggH.py
@@ -0,0 +1,54 @@
+## Combinations to use
+#
+##comb = {}
+#
+##
+
+combs['hww2l2v_13TeV_ggH_0j'] = {
+                  'hww2l2v_13TeV_0j_ee' : 'events' ,
+                  'hww2l2v_13TeV_WW_0j_ee' : 'events' ,
+                  'hww2l2v_13TeV_top_0j_ee' : 'events' ,
+                  'hww2l2v_13TeV_0j_mm' : 'events' ,
+                  'hww2l2v_13TeV_WW_0j_mm' : 'events' ,
+                  'hww2l2v_13TeV_top_0j_mm' : 'events' ,
+                }
+
+combs['hww2l2v_13TeV_ggH_1j'] = {
+                  'hww2l2v_13TeV_1j_ee' : 'events' ,
+                  'hww2l2v_13TeV_WW_1j_ee' : 'events' ,
+                  'hww2l2v_13TeV_1j_mm' : 'events' ,
+                  'hww2l2v_13TeV_WW_1j_mm' : 'events' ,
+                  'hww2l2v_13TeV_top_1j_ee' : 'events' ,
+                  'hww2l2v_13TeV_top_1j_mm' : 'events' ,
+                }
+
+combs['hww2l2v_13TeV_ggH_2j'] = {
+                  'hww2l2v_13TeV_2j_ee' : 'events' ,
+                  'hww2l2v_13TeV_WW_2j_ee' : 'events' ,
+                  'hww2l2v_13TeV_2j_mm' : 'events' ,
+                  'hww2l2v_13TeV_WW_2j_mm' : 'events' ,
+                  'hww2l2v_13TeV_top_2j_ee' : 'events' ,
+                  'hww2l2v_13TeV_top_2j_mm' : 'events' ,
+                }
+
+combs['hww2l2v_13TeV_ggH'] = {
+                  'hww2l2v_13TeV_0j_ee' : 'events' ,
+                  'hww2l2v_13TeV_WW_0j_ee' : 'events' ,
+                  'hww2l2v_13TeV_top_0j_ee' : 'events' ,
+                  'hww2l2v_13TeV_0j_mm' : 'events' ,
+                  'hww2l2v_13TeV_WW_0j_mm' : 'events' ,
+                  'hww2l2v_13TeV_top_0j_mm' : 'events' ,
+                  'hww2l2v_13TeV_1j_ee' : 'events' ,
+                  'hww2l2v_13TeV_WW_1j_ee' : 'events' ,
+                  'hww2l2v_13TeV_1j_mm' : 'events' ,
+                  'hww2l2v_13TeV_WW_1j_mm' : 'events' ,
+                  'hww2l2v_13TeV_top_1j_ee' : 'events' ,
+                  'hww2l2v_13TeV_top_1j_mm' : 'events' ,
+                  'hww2l2v_13TeV_2j_ee' : 'events' ,
+                  'hww2l2v_13TeV_WW_2j_ee' : 'events' ,
+                  'hww2l2v_13TeV_2j_mm' : 'events' ,
+                  'hww2l2v_13TeV_WW_2j_mm' : 'events' ,
+                  'hww2l2v_13TeV_top_2j_ee' : 'events' ,
+                  'hww2l2v_13TeV_top_2j_mm' : 'events' ,
+                }
+
