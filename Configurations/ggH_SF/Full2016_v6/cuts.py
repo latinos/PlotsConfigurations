6433b594295579bacd2225643571777132b59bbd
calderon <calderon@cern.ch>
2020-06-25 12:03:15 +0200

    add DY CR

--- Configurations/ggH_SF/Full2016_v6/cuts.py
+++ Configurations/ggH_SF/Full2016_v6/cuts.py
@@ -1,5 +1,4 @@
-
-supercut = '    mll>12 \
+supercut = '   mll>12 \
             && Lepton_pt[0]>25 \
             && Lepton_pt[1]>10 \
             && (abs(Lepton_pdgId[1])==13 || Lepton_pt[1]>13) \
@@ -111,3 +110,15 @@ cuts['hww2l2v_13TeV_wwAcc']  = {
       '2j_mm' : '  2jggH && (Lepton_pdgId[0]*Lepton_pdgId[1] == -13*13) && dymva_alt_dnn_2j > 0.90',
    }
 } 
+
+## DY CR: No H Sel , mll>80, tight DYMva                                                                                                   
+cuts['hww2l2v_13TeV_DY']  = {                                                                                                             
+   'expr' : 'dycr * (Lepton_pdgId[0]==-Lepton_pdgId[1])',                                                                                     'categories' : {                                                                                                                      
+      '0j_ee' : 'zeroJet && (Lepton_pdgId[0]*Lepton_pdgId[1] == -11*11) && dymva_alt_dnn_0j < 0.90 ',                                     
+      '0j_mm' : 'zeroJet && (Lepton_pdgId[0]*Lepton_pdgId[1] == -13*13) && dymva_alt_dnn_0j < 0.90 ',                                     
+      '1j_ee' : ' oneJet && (Lepton_pdgId[0]*Lepton_pdgId[1] == -11*11) && dymva_alt_dnn_0j < 0.90 ',                                     
+      '1j_mm' : ' oneJet && (Lepton_pdgId[0]*Lepton_pdgId[1] == -13*13) && dymva_alt_dnn_0j < 0.90 ',                                     
+      '2j_ee' : '  2jggH && (Lepton_pdgId[0]*Lepton_pdgId[1] == -11*11) && dymva_alt_dnn_0j < 0.90 ',                                    
+      '2j_mm' : '  2jggH && (Lepton_pdgId[0]*Lepton_pdgId[1] == -13*13) && dymva_alt_dnn_0j < 0.90 ',                                     
+   }                                                                                                                                      
+}                                                                             
