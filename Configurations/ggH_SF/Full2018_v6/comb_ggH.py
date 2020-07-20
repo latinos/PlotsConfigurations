7b8de31615babbe6b836ac9c1edfdf62451d78ed
calderon <calderon@cern.ch>
2020-07-20 16:20:58 +0200

    add comination file

---
+++ Configurations/ggH_SF/Full2018_v6/comb_ggH.py
@@ -0,0 +1,91 @@
+## Combinations to use
+#
+##comb = {}
+#
+##
+
+combs['hww2l2v_13TeV_ggH_0j'] = {
+                  'hww2l2v_13TeV_0j_ee' : 'events' ,
+                  'hww2l2v_13TeV_WW_0j_ee' : 'events' ,
+                  'hww2l2v_13TeV_top_0j_ee' : 'events' ,
+                  'hww2l2v_13TeV_0j_mm' : 'events' ,
+                  'hww2l2v_13TeV_WW_0j_mm' : 'events' ,
+                  'hww2l2v_13TeV_top_0j_mm' : 'events' ,
+                }
+
+combs['hww2l2v_13TeV_ggH_1j'] = {
+                  'hww2l2v_13TeV_1j_ee' : 'events' ,
+                  'hww2l2v_13TeV_WW_1j_ee' : 'events' ,
+                  'hww2l2v_13TeV_1j_mm' : 'events' ,
+                  'hww2l2v_13TeV_WW_1j_mm' : 'events' ,
+                  'hww2l2v_13TeV_top_1j_ee' : 'events' ,
+                  'hww2l2v_13TeV_top_1j_mm' : 'events' ,
+                }
+
+combs['hww2l2v_13TeV_ggH_2j'] = {
+                  'hww2l2v_13TeV_2j_ee' : 'events' ,
+                  'hww2l2v_13TeV_WW_2j_ee' : 'events' ,
+                  'hww2l2v_13TeV_2j_mm' : 'events' ,
+                  'hww2l2v_13TeV_WW_2j_mm' : 'events' ,
+                  'hww2l2v_13TeV_top_2j_ee' : 'events' ,
+                  'hww2l2v_13TeV_top_2j_mm' : 'events' ,
+                }
+
+combs['hww2l2v_13TeV_ggH'] = {
+                  'hww2l2v_13TeV_0j_ee' : 'events' ,
+                  'hww2l2v_13TeV_WW_0j_ee' : 'events' ,
+                  'hww2l2v_13TeV_top_0j_ee' : 'events' ,
+                  'hww2l2v_13TeV_0j_mm' : 'events' ,
+                  'hww2l2v_13TeV_WW_0j_mm' : 'events' ,
+                  'hww2l2v_13TeV_top_0j_mm' : 'events' ,
+                  'hww2l2v_13TeV_1j_ee' : 'events' ,
+                  'hww2l2v_13TeV_WW_1j_ee' : 'events' ,
+                  'hww2l2v_13TeV_1j_mm' : 'events' ,
+                  'hww2l2v_13TeV_WW_1j_mm' : 'events' ,
+                  'hww2l2v_13TeV_top_1j_ee' : 'events' ,
+                  'hww2l2v_13TeV_top_1j_mm' : 'events' ,
+                  'hww2l2v_13TeV_2j_ee' : 'events' ,
+                  'hww2l2v_13TeV_WW_2j_ee' : 'events' ,
+                  'hww2l2v_13TeV_2j_mm' : 'events' ,
+                  'hww2l2v_13TeV_WW_2j_mm' : 'events' ,
+                  'hww2l2v_13TeV_top_2j_ee' : 'events' ,
+                  'hww2l2v_13TeV_top_2j_mm' : 'events' ,
+                }
+
+#combs['hww2l2v_13TeV_HWW'] = {
+#                  'hww2l2v_13TeV_0j_ee_pt2lt20' : 'events' ,
+#                  'hww2l2v_13TeV_0j_ee_pt2ge20' : 'events' ,
+#                  'hww2l2v_13TeV_WW_0j_ee' : 'events' ,
+#                  'hww2l2v_13TeV_top_0j_ee' : 'events' ,
+#                  'hww2l2v_13TeV_0j_mm_pt2lt20' : 'events' ,
+#                  'hww2l2v_13TeV_0j_mm_pt2ge20' : 'events' ,
+#                  'hww2l2v_13TeV_WW_0j_mm' : 'events' ,
+#                  'hww2l2v_13TeV_top_0j_mm' : 'events' ,
+#                  'hww2l2v_13TeV_1j_ee' : 'events' ,
+#                  'hww2l2v_13TeV_WW_1j_ee' : 'events' ,
+#                  'hww2l2v_13TeV_1j_mm' : 'events' ,
+#                  'hww2l2v_13TeV_WW_1j_mm' : 'events' ,
+#                  'hww2l2v_13TeV_top_1j_ee' : 'events' ,
+#                  'hww2l2v_13TeV_top_1j_mm' : 'events' ,
+#                  'hww2l2v_13TeV_2j_ee' : 'events' ,
+#                  'hww2l2v_13TeV_2j_ee_vh2j' : 'events' ,
+#                  'hww2l2v_13TeV_WW_2j_ee' : 'events' ,
+#                  'hww2l2v_13TeV_WW_2j_ee_vh2j' : 'events' ,
+#                  'hww2l2v_13TeV_2j_mm' : 'events' ,
+#                  'hww2l2v_13TeV_2j_mm_vh2j' : 'events' ,
+#                  'hww2l2v_13TeV_WW_2j_mm' : 'events' ,
+#                  'hww2l2v_13TeV_WW_2j_mm_vh2j' : 'events' ,
+#                  'hww2l2v_13TeV_top_2j_ee' : 'events' ,
+#                  'hww2l2v_13TeV_top_2j_ee_vh2j' : 'events' ,
+#                  'hww2l2v_13TeV_top_2j_mm' : 'events' ,
+#                  'hww2l2v_13TeV_top_2j_mm_vh2j' : 'events' ,
+#                  'hww2l2v_13TeV_2j_ee_vbf_lowmjj' : 'events' ,
+#                  'hww2l2v_13TeV_2j_ee_vbf_highmjj' : 'events' ,
+#                  'hww2l2v_13TeV_2j_mm_vbf_lowmjj' : 'events' ,
+#                  'hww2l2v_13TeV_2j_mm_vbf_highmjj' : 'events' ,
+#                  'hww2l2v_13TeV_WW_2j_ee_vbf' : 'events' ,
+#                  'hww2l2v_13TeV_WW_2j_mm_vbf' : 'events' ,
+#                  'hww2l2v_13TeV_top_2j_ee_vbf' : 'events' ,
+#                  'hww2l2v_13TeV_top_2j_mm_vbf' : 'events' ,
+#                }
+
