b1e6b56601126bec1ba9e6ed9188c7d23cce25a9
calderon <calderon@cern.ch>
2020-07-21 17:42:31 +0200

    help making tables

---
+++ Configurations/ggH_SF/Full2016_v6/merging_map.py
@@ -0,0 +1,6 @@
+categories_to_merge = [{'new_name' : 'ggH_13TeV_0j',
+                        'old_names' : ['hww2l2v_13TeV_0j_mm', 'hww2l2v_13TeV_0j_ee']},
+                       {'new_name' : 'ggH_13TeV_1j',                                                                                                         
+                       'old_names' : ['hww2l2v_13TeV_1j_mm', 'hww2l2v_13TeV_1j_ee']},
+                       {'new_name' : 'ggH_13TeV_2j',                                                                                                         
+                        'old_names' : ['hww2l2v_13TeV_2j_mm', 'hww2l2v_13TeV_2j_ee']}]                    
