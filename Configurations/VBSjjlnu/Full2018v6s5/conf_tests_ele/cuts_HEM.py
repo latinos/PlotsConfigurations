0ea10049e2727dcd87fc4ffd57dfb32c1044a353
Davide Valsecchi <davide.valsecchi@cern.ch>
2020-07-22 09:56:04 +0200

    Test genBins and jetPuIdSF checks

---
+++ Configurations/VBSjjlnu/Full2018v6s5/conf_tests_ele/cuts_HEM.py
@@ -0,0 +1,112 @@
+# cuts
+
+# Second lepton veto already done in post-processing 
+#and Lepton WP setup in samples.py
+supercut = '(   (abs(Lepton_pdgId[0])==11 && Lepton_pt[0]>35)\
+             || (abs(Lepton_pdgId[0])==13 && Lepton_pt[0]>30 ) ) \
+            && vbs_0_pt_corrHEM > 50 && vbs_1_pt_corrHEM > 30 \
+            && PuppiMET_pt_corrHEM > 30 \
+            && deltaeta_vbs >= 2.5  \
+            && mjj_vbs_corrHEM >= 500 \
+            && Mtw_lep < 185 \
+            '
+
+
+
+cuts["res_wjetcr_ele_incl"] = 'VBS_category==1 \
+                                && abs(Lepton_pdgId[0])==11 \
+                                && vjet_0_pt_corrHEM > 30 && vjet_1_pt_corrHEM > 30 \
+                                && (mjj_vjet_corrHEM <= 65 || mjj_vjet_corrHEM >= 105) \
+                                && bVeto \
+                                && whad_pt_corrHEM < 200 \
+                                '
+
+
+cuts["res_wjetcr_ele_incl_loweta"] = 'VBS_category==1 \
+                                && abs(Lepton_pdgId[0])==11 \
+                                && vjet_0_pt_corrHEM > 30 && vjet_1_pt_corrHEM > 30 \
+                                && (mjj_vjet_corrHEM <= 65 || mjj_vjet_corrHEM >= 105) \
+                                && bVeto \
+                                && whad_pt_corrHEM < 200 \
+                                && abs(Lepton_eta[0])< 2\
+                                '
+
+cuts["res_wjetcr_ele_incl_higheta"] = 'VBS_category==1 \
+                                && abs(Lepton_pdgId[0])==11 \
+                                && vjet_0_pt_corrHEM > 30 && vjet_1_pt_corrHEM > 30 \
+                                && (mjj_vjet_corrHEM <= 65 || mjj_vjet_corrHEM >= 105) \
+                                && bVeto \
+                                && whad_pt_corrHEM < 200 \
+                                && abs(Lepton_eta[0]) >= 2 \
+                                '
+
+cuts["res_wjetcr_ele_incl_loweta_highnvtx"] = 'VBS_category==1 \
+                                && abs(Lepton_pdgId[0])==11 \
+                                && vjet_0_pt_corrHEM > 30 && vjet_1_pt_corrHEM > 30 \
+                                && (mjj_vjet_corrHEM <= 65 || mjj_vjet_corrHEM >= 105) \
+                                && bVeto \
+                                && whad_pt_corrHEM < 200 \
+                                && abs(Lepton_eta[0])< 2\
+                                && PV_npvs >= 25\
+                                '
+
+cuts["res_wjetcr_ele_incl_higheta_highnvtx"] = 'VBS_category==1 \
+                                && abs(Lepton_pdgId[0])==11 \
+                                && vjet_0_pt_corrHEM > 30 && vjet_1_pt_corrHEM > 30 \
+                                && (mjj_vjet_corrHEM <= 65 || mjj_vjet_corrHEM >= 105) \
+                                && bVeto \
+                                && whad_pt_corrHEM < 200 \
+                                && abs(Lepton_eta[0]) >= 2 \
+                                &&  PV_npvs >= 25\
+                                '
+
+cuts["res_wjetcr_ele_incl_loweta_lownvtx"] = 'VBS_category==1 \
+                                && abs(Lepton_pdgId[0])==11 \
+                                && vjet_0_pt_corrHEM > 30 && vjet_1_pt_corrHEM > 30 \
+                                && (mjj_vjet_corrHEM <= 65 || mjj_vjet_corrHEM >= 105) \
+                                && bVeto \
+                                && whad_pt_corrHEM < 200 \
+                                && abs(Lepton_eta[0])< 2\
+                                && PV_npvs < 25\
+                                '
+
+cuts["res_wjetcr_ele_incl_higheta_lownvtx"] = 'VBS_category==1 \
+                                && abs(Lepton_pdgId[0])==11 \
+                                && vjet_0_pt_corrHEM > 30 && vjet_1_pt_corrHEM > 30 \
+                                && (mjj_vjet_corrHEM <= 65 || mjj_vjet_corrHEM >= 105) \
+                                && bVeto \
+                                && whad_pt_corrHEM < 200 \
+                                && abs(Lepton_eta[0]) >= 2 \
+                                && PV_npvs < 25 \
+                                '
+
+
+
+cuts["boost_wjetcr_ele_incl"] = 'VBS_category==0 \
+                            && abs(Lepton_pdgId[0])==11 \
+                            && vjet_0_pt > 200 \
+                            && (mjj_vjet <= 65 || mjj_vjet >= 105)  \
+                            && bVeto \
+                            '
+
+
+
+cuts["boost_wjetcr_ele_loweta"] = 'VBS_category==0 \
+                            && abs(Lepton_pdgId[0])==11 \
+                            && vjet_0_pt > 200 \
+                            && (mjj_vjet <= 65 || mjj_vjet >= 105)  \
+                            && bVeto \
+                            && abs(Lepton_eta[0])< 2 \
+                             '
+
+
+
+cuts["boost_wjetcr_ele_higheta"] = 'VBS_category==0 \
+                            && abs(Lepton_pdgId[0])==11 \
+                            && vjet_0_pt > 200 \
+                            && (mjj_vjet <= 65 || mjj_vjet >= 105)  \
+                            && bVeto \
+                            && abs(Lepton_eta[0]) >= 2 \
+                            '
+
+                   
