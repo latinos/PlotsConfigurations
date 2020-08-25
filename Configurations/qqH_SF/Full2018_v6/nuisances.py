e5c8b31170e417fe00d0faf991c17282f0a3d1a4
piedra <piedra@cern.ch>
2020-08-25 13:30:28 +0200

    Updated muon ID for 2018 qqH_SF

--- Configurations/qqH_SF/Full2018_v6/nuisances.py
+++ Configurations/qqH_SF/Full2018_v6/nuisances.py
@@ -185,7 +185,7 @@ nuisances['eff_m'] = {
     'name': 'CMS_eff_m_2018',
     'kind': 'weight',
     'type': 'shape',
-    'samples': dict((skey, ['SFweightMuUp', 'SFweightMuDown']) for skey in mc if skey not in ['WW', 'top', 'DY'])
+    'samples': dict((skey, ['ttHMVA_2l_mu_SF_Up', 'ttHMVA_2l_mu_SF_Down']) for skey in mc if skey not in ['DY'])
 }
 
 nuisances['muonpt'] = {
--- Configurations/qqH_SF/Full2018_v6/samples.py
+++ Configurations/qqH_SF/Full2018_v6/samples.py
@@ -30,11 +30,14 @@ except NameError:
 ################################################
 
 mcProduction = 'Autumn18_102X_nAODv6_Full2018v6'
+
 dataReco = 'Run2018_102X_nAODv6_Full2018v6'
+
+fakeReco = 'Run2018_102X_nAODv6_Full2018v6_ForNewWPs'
 
 mcSteps = 'MCl1loose2018v6__MCCorr2018v6__l2loose__l2tightOR2018v6{var}'
 
-fakeSteps = 'DATAl1loose2018v6__l2loose__fakeW'
+fakeSteps = 'DATAl1loose2018v6__l2loose__fakeW__DYMVA'
 
 dataSteps = 'DATAl1loose2018v6__l2loose__l2tightOR2018v6'
 
@@ -55,7 +58,7 @@ def makeMCDirectory(var=''):
         return os.path.join(treeBaseDir, mcProduction, mcSteps.format(var=''))
 
 mcDirectory = makeMCDirectory()
-fakeDirectory = os.path.join(treeBaseDir, dataReco, fakeSteps)
+fakeDirectory = os.path.join(treeBaseDir, fakeReco, fakeSteps)
 dataDirectory = os.path.join(treeBaseDir, dataReco, dataSteps)
 
 ################################################
