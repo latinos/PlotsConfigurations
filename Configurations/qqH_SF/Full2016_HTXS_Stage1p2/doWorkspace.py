71399ddc067a4435d7ea24f2388298e14510663c
Nicolo' Trevisani <nicolo.trevisani@cern.ch>
2020-10-07 15:26:10 +0200

    workspace and fit as in ggH

--- Configurations/qqH_SF/Full2016_HTXS_Stage1p2/doWorkspace.py
+++ Configurations/qqH_SF/Full2016_HTXS_Stage1p2/doWorkspace.py
@@ -48,41 +48,85 @@ print("============")
 
 #No merging
 command="text2workspace.py datacards/Full2016_SF_qqH_HTXS_Stage1p2/comb/datacard.txt -o Combination/Full2016_SF_qqH_HTXS_Stage1p2.root -P HiggsAnalysis.CombinedLimit.PhysicsModel:multiSignalModel --PO verbose "
+
+poi=''
 for sample in sampleNames:
-  if 'FWDH'    in sample: continue
-  if '0J'      in sample or '1J' in sample: continue
-  # qqH --> 2 jets, mjj > 350
-  if 'qqH_hww' in sample:
-    # case pth < 200
+  if 'FWDH' in sample: continue
+  if 'htt' in sample: continue
+ 
+  if 'ggH_hww' in sample:
+    if 'GT200' in sample: continue
 
-    # case 350 < mjj < 700
-    if 'MJJ_350_700' in sample:
-      poi = 'qqH_hww_MJJ_350_700'
-      command+="--PO 'map=.*/{}:r_{}[1,-10,10]' ".format(sample,poi)
-      print("{0} - {1}".format(sample,poi))
-
-    # case mjj > 700
-    elif 'MJJ_GT700' in sample:
-      poi = 'qqH_hww_MJJ_GT700'
-      command+="--PO 'map=.*/{}:r_{}[1,-10,10]' ".format(sample,poi)
-      print("{0} - {1}".format(sample,poi))
-
-    # case pth > 200
-    elif 'PTH_GT200' in sample:
-      poi = 'qqH_hww_PTH_GT200'
-      command+="--PO 'map=.*/{}:r_{}[1,-10,10]' ".format(sample,poi)
-      print("{0} - {1}".format(sample,poi))
+    if (sample in ['ggH_hww_PTH_300_450','ggH_hww_PTH_450_650','ggH_hww_PTH_GT650']): poi = 'r_ggH_hww_PTH_GT300'
+    elif ('MJJ_0_350_PTH_0_60' in sample or 'MJJ_0_350_PTH_60_120' in sample): poi = 'r_ggH_hww_GE2J_MJJ_0_350_PTH_LT120'
+    elif ('MJJ_350_700' in sample): poi = 'r_ggH_hww_GE2J_MJJ_350_700'
+    elif ('MJJ_GT700' in sample): poi = 'r_ggH_hww_GE2J_MJJ_GT700'
+    elif ('PTH_200_300' in sample): poi = 'r_ggH_hww_PTH_200_300'
+    elif ('MJJ_0_350_PTH_120_200' in sample): poi = 'r_ggH_hww_GE2J_MJJ_0_350_PTH_120_200' 
     else: continue
 
-  # VH --> 2 jets, 60 < mjj < 120
-  elif 'WH_had_hww' in sample or 'ZH_had_hww' in sample:
-    if 'MJJ_60_120' in sample:
-      poi = 'VH_had_hww_MJJ_60_120'
-      command+="--PO 'map=.*/{}:r_{}[1,-10,10]' ".format(sample,poi)
-      print("{0} - {1}".format(sample,poi))
+  elif 'qqH_hww' in sample:
+    if 'MJJ_350_700' in sample: poi = 'r_qqH_hww_MJJ_350_700_PTH_LT200'
+    elif 'MJJ_GT700' in sample: poi = 'r_qqH_hww_MJJ_GT700_PTH_LT200'
+    elif 'MJJ_GT350_PTH_GT200' in sample: poi = 'r_qqH_hww_MJJ_GT350_PTH_GT200'
     else: continue
+
+  elif 'WH_had_hww_MJJ_60_120' in sample or 'ZH_had_hww_MJJ_60_120' in sample: poi = 'r_VH_had_hww_MJJ_60_120'
+
   else: continue
-print("============")
+
+  print sample,"->",poi
+
+  command+="--PO 'map=.*/{}:{}[1,-10,10]' ".format(sample,poi)
 
 print command
 os.system(command)
+
+##########################################################################
+# OLD
+# for sample in sampleNames:
+#   if 'FWDH'    in sample: continue
+#   if '0J'      in sample or '1J' in sample: continue
+#   # qqH --> 2 jets, mjj > 350
+#   if 'qqH_hww' in sample:
+#     # case pth < 200
+
+#     # case 350 < mjj < 700
+#     done_MJJ_350_700 = 0
+#     if 'MJJ_350_700' in sample and done_MJJ_350_700 == 0:
+#       poi = 'qqH_hww_MJJ_350_700'
+#       command+="--PO 'map=.*/{}:r_{}[1,-10,10]' ".format(sample,poi)
+#       print("{0} - {1}".format(sample,poi))
+#       done_MJJ_350_700 = 1
+
+#     # case mjj > 700
+#     done_MJJ_GT700 = 0
+#     elif 'MJJ_GT700' in sample and done_MJJ_GT700 == 0:
+#       poi = 'qqH_hww_MJJ_GT700'
+#       command+="--PO 'map=.*/{}:r_{}[1,-10,10]' ".format(sample,poi)
+#       print("{0} - {1}".format(sample,poi))
+#       done_MJJ_GT700 = 1
+
+#     # case pth > 200
+#     done_PTH_GT200 = 0
+#     elif 'PTH_GT200' in sample and done_PTH_GT200 == 0:
+#       poi = 'qqH_hww_PTH_GT200'
+#       command+="--PO 'map=.*/{}:r_{}[1,-10,10]' ".format(sample,poi)
+#       print("{0} - {1}".format(sample,poi))
+#       done_PTH_GT200 = 1
+#     else: continue
+
+#   # VH --> 2 jets, 60 < mjj < 120
+#   done_VH = 0
+#   elif ('WH_had_hww' in sample or 'ZH_had_hww' in sample) and done_VH == 0:
+#     if 'MJJ_60_120' in sample:
+#       poi = 'VH_had_hww_MJJ_60_120'
+#       command+="--PO 'map=.*/{}:r_{}[1,-10,10]' ".format(sample,poi)
+#       print("{0} - {1}".format(sample,poi))
+#       done_VH = 1
+#     else: continue
+#   else: continue
+# print("============")
+
+# print command
+# #os.system(command)
