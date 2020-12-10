2b389632b2efc267632fd28ec3b3417a5a4c2b5f
ntrevisa <nicolo.trevisani@cern.ch>
2020-12-10 20:34:41 +0100

    working cfg for HTXS SF 2016

---
+++ Configurations/ggH_SF/Full2016_HTXS_Stage1p2_v7/dropNuisances.sh
@@ -0,0 +1,7 @@
+echo "nuisance edit drop VZ sf_hpt_mm_pth650 QCDscale_VV" >> Combination/Full2016_SF_ggH_HTXS_Stage1p2_v7.txt
+echo "nuisance edit drop Vg sf_0j_ee_Top     QCDscale_VV" >> Combination/Full2016_SF_ggH_HTXS_Stage1p2_v7.txt
+echo "nuisance edit drop Vg sf_1j_ee_Top     QCDscale_VV" >> Combination/Full2016_SF_ggH_HTXS_Stage1p2_v7.txt
+#echo "Nothing to do! :)"
+
+
+
