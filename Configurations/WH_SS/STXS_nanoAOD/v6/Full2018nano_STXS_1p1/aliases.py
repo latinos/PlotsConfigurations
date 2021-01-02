ff827cec41d87825eb05d0d85bab064c0b34a8f3
SiewYan <siew.yan.hoh@cern.ch>
2021-01-02 15:53:36 +0100

    STXS WHSS pujetidsf update

--- Configurations/WH_SS/STXS_nanoAOD/v6/Full2018nano_STXS_1p1/aliases.py
+++ Configurations/WH_SS/STXS_nanoAOD/v6/Full2018nano_STXS_1p1/aliases.py
@@ -233,12 +233,13 @@ aliases['Top_pTrw'] = {
 
 # PU jet Id SF
 
-puidSFSource = '%s/src/LatinoAnalysis/NanoGardener/python/data/JetPUID_effcyandSF.root' % os.getenv('CMSSW_BASE')
+#puidSFSource = '%s/src/LatinoAnalysis/NanoGardener/python/data/JetPUID_effcyandSF.root' % os.getenv('CMSSW_BASE')
+puidSFSource = '{}/../../patches/PUID_80XTraining_EffSFandUncties.root'.format(configurations)
 
 aliases['PUJetIdSF'] = {
     'linesToAdd': [
         'gSystem->AddIncludePath("-I%s/src");' % os.getenv('CMSSW_BASE'),
-        '.L %s/../../patches/pujetidsf_event.cc+' % configurations
+        '.L %s/../../patches/pujetidsf_event_new.cc+' % configurations
     ],
     'class': 'PUJetIdEventSF',
     'args': (puidSFSource, '2018', 'loose'),
