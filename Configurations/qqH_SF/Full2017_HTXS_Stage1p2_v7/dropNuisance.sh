18c6f1eb59ab398da320f279aba196e84b31d3c4
ntrevisa <nicolo.trevisani@cern.ch>
2021-01-09 16:13:59 +0100

    qqH_SF HTXS v7

---
+++ Configurations/qqH_SF/Full2017_HTXS_Stage1p2_v7/dropNuisance.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "nuisance edit drop Vg sf_2j_mm_mjjGT350_pthGT200 QCDscale_VV" >> Combination/Full2017_SF_qqH_HTXS_Stage1p2_v7.txt
+echo "nuisance edit drop Vg sf_2j_mm_top_2j_hpt QCDscale_VV" >> Combination/Full2017_SF_qqH_HTXS_Stage1p2_v7.txt
