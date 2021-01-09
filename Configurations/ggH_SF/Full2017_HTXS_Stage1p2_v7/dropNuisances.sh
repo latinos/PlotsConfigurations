4650677bfa70f8b45fbf31219ec43bec2ef2009c
ntrevisa <nicolo.trevisani@cern.ch>
2021-01-09 16:19:11 +0100

    ggH_SF 2017 HTXS v7

---
+++ Configurations/ggH_SF/Full2017_HTXS_Stage1p2_v7/dropNuisances.sh
@@ -0,0 +1,12 @@
+echo "nuisance edit drop Vg    sf_2j_ee_mjj0_350_pth120_200 QCDscale_VV" >> Combination/Full2017_SF_ggH_HTXS_Stage1p2_v7.txt
+echo "nuisance edit drop VZ    sf_hpt_mm_pth650             QCDscale_VV" >> Combination/Full2017_SF_ggH_HTXS_Stage1p2_v7.txt
+echo "nuisance edit drop VgS_L sf_2j_mm_Top                 QCDscale_VV" >> Combination/Full2017_SF_ggH_HTXS_Stage1p2_v7.txt
+echo "nuisance edit drop Vg    sf_2j_ee_WW                  QCDscale_VV" >> Combination/Full2017_SF_ggH_HTXS_Stage1p2_v7.txt
+echo "nuisance edit drop Vg    sf_2j_mm_WW                  QCDscale_VV" >> Combination/Full2017_SF_ggH_HTXS_Stage1p2_v7.txt
+
+# echo "nuisance edit drop Vg sf_0j_ee_Top     QCDscale_VV" >> Combination/Full2016_SF_ggH_HTXS_Stage1p2_v7.txt
+# echo "nuisance edit drop Vg sf_1j_ee_Top     QCDscale_VV" >> Combination/Full2016_SF_ggH_HTXS_Stage1p2_v7.txt
+# echo "Nothing to do! :)"
+
+
+
