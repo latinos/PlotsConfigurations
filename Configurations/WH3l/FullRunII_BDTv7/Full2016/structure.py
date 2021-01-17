522cab11caa4755b7553a0a82ac698a8af5ee3a5
pageyu <pyu@cern.ch>
2021-01-17 02:33:39 +0100

    v7 updated.

---
+++ Configurations/WH3l/FullRunII_BDTv7/Full2016/structure.py
@@ -0,0 +1,135 @@
+# structure configuration for datacard
+
+#structure = {}
+
+# keys here must match keys in samples.py
+#
+structure['Fake']  = {
+                  'isSignal' : 0,
+                  'isData'   : 0
+              }
+
+#structure['DY']  = {
+#                  'isSignal' : 0,
+#                  'isData'   : 0
+#                  }
+#
+#structure['top']  = {
+#                  'isSignal' : 0,
+#                  'isData'   : 0
+#                  }
+
+structure['WW']  = {
+                  'isSignal' : 0,
+                  'isData'   : 0
+                  }
+
+structure['ggWW']  = {
+                  'isSignal' : 0,
+                  'isData'   : 0
+                  }
+
+
+structure['Wg']  = {
+                  'isSignal' : 0,
+                  'isData'   : 0
+                  }
+
+structure['WgS']  = {
+                  'isSignal' : 0,
+                  'isData'   : 0
+                  }
+
+structure['Zg']  = {
+                  'isSignal' : 0,
+                  'isData'   : 0
+                  }
+
+structure['ZgS']  = {
+                  'isSignal' : 0,
+                  'isData'   : 0
+                  }
+
+structure['Vg']  = {
+                  'isSignal' : 0,
+                  'isData'   : 0
+                  }
+
+structure['VgS']  = {
+                  'isSignal' : 0,
+                  'isData'   : 0
+                  }
+
+
+structure['WZ']  = {
+                  'isSignal' : 0,
+                  'isData'   : 0
+                  }
+
+
+structure['VVV']  = {
+                  'isSignal' : 0,
+                  'isData'   : 0
+                  }
+
+structure['ZZ']  = {
+                  'isSignal' : 0,
+                  'isData'   : 0
+                  }
+
+structure['ggH_hww'] = {
+                  'isSignal' : 1,
+                  'isData'   : 0    
+                  }
+
+structure['qqH_hww'] = {
+                  'isSignal' : 1,
+                  'isData'   : 0    
+                  }
+
+structure['WH_hww'] = {
+                  'isSignal' : 1,
+                  'isData'   : 0    
+                  }
+
+structure['ZH_hww'] = {
+                  'isSignal' : 1,
+                  'isData'   : 0    
+                  }
+
+structure['ggZH_hww'] = {
+                  'isSignal' : 1,
+                  'isData'   : 0    
+                  }
+
+structure['ggH_htt'] = {
+                  'isSignal' : 1,
+                  'isData'   : 0,
+                  }
+
+structure['qqH_htt'] = {
+                  'isSignal' : 1,
+                  'isData'   : 0,
+                  }
+
+structure['WH_htt'] = {
+                  'isSignal' : 1,
+                  'isData'   : 0,
+                  }
+
+structure['ZH_htt'] = {
+                  'isSignal' : 1,
+                  'isData'   : 0,
+                  }
+
+# data
+
+
+structure['DATA']  = {
+                  'isSignal' : 0,
+                  'isData'   : 1
+              }
+
+
+
+
