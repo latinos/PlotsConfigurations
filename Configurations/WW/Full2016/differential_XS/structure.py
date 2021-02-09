4d86bc501e31f731cf6b8ce62c0823e4fc66ec7b
fmanteca <fmanteca@ifca.unican.es>
2021-02-09 16:40:32 +0100

    added differential_XS cfg

---
+++ Configurations/WW/Full2016/differential_XS/structure.py
@@ -0,0 +1,256 @@
+# structure configuration for datacard
+
+#structure = {}
+
+# keys here must match keys in samples.py    
+#                    
+structure['DY']  = {  
+                  'isSignal' : 0,
+                  'isData'   : 0 
+              }
+
+
+structure['Wjets']  = {  
+                  'isSignal' : 0,
+                  'isData'   : 0 
+              }
+
+structure['Fake']  = {  
+                  'isSignal' : 0,
+                  'isData'   : 0 
+              }
+
+structure['ttbar'] = {   
+                  'isSignal' : 0,
+                  'isData'   : 0 
+                  }
+
+
+structure['singletop'] = {   
+                  'isSignal' : 0,
+                  'isData'   : 0 
+                  }
+
+structure['top'] = {   
+                  'isSignal' : 0,
+                  'isData'   : 0 
+                  }
+
+
+structure['WW0']  = {
+                  'isSignal' : 1,
+                  'isData'   : 0    
+                  }
+
+structure['WW1']  = {
+                  'isSignal' : 1,
+                  'isData'   : 0    
+                  }
+
+structure['WW2']  = {
+                  'isSignal' : 1,
+                  'isData'   : 0    
+                  }
+
+structure['WW3']  = {
+                  'isSignal' : 1,
+                  'isData'   : 0    
+                  }
+
+structure['WW4']  = {
+                  'isSignal' : 1,
+                  'isData'   : 0    
+                  }
+
+structure['WW5']  = {
+                  'isSignal' : 1,
+                  'isData'   : 0    
+                  }
+
+structure['WW6']  = {
+                  'isSignal' : 1,
+                  'isData'   : 0    
+                  }
+
+structure['WW7']  = {
+                  'isSignal' : 1,
+                  'isData'   : 0    
+                  }
+
+structure['WW8']  = {
+                  'isSignal' : 1,
+                  'isData'   : 0    
+                  }
+
+structure['WWnonfid']  = {
+                  'isSignal' : 0,
+                  'isData'   : 0    
+                  }
+
+structure['ggWW0']  = {
+                  'isSignal' : 1,
+                  'isData'   : 0    
+                  }
+
+structure['ggWW1']  = {
+                  'isSignal' : 1,
+                  'isData'   : 0    
+                  }
+
+structure['ggWW2']  = {
+                  'isSignal' : 1,
+                  'isData'   : 0    
+                  }
+
+structure['ggWW3']  = {
+                  'isSignal' : 1,
+                  'isData'   : 0    
+                  }
+
+structure['ggWW4']  = {
+                  'isSignal' : 1,
+                  'isData'   : 0    
+                  }
+
+structure['ggWW5']  = {
+                  'isSignal' : 1,
+                  'isData'   : 0    
+                  }
+
+structure['ggWW6']  = {
+                  'isSignal' : 1,
+                  'isData'   : 0    
+                  }
+
+structure['ggWW7']  = {
+                  'isSignal' : 1,
+                  'isData'   : 0    
+                  }
+
+structure['ggWW8']  = {
+                  'isSignal' : 1,
+                  'isData'   : 0    
+                  }
+
+structure['ggWWnonfid']  = {
+                  'isSignal' : 0,
+                  'isData'   : 0    
+                  }
+                  
+structure['ggWW_Int']  = {
+                  'isSignal' : 0,
+                  'isData'   : 0    
+                  }
+
+structure['Wg']  = { 
+                  'isSignal' : 0,
+                  'isData'   : 0 
+                  }
+
+structure['Vg']  = { 
+                  'isSignal' : 0,
+                  'isData'   : 0 
+                  }
+
+structure['VgS'] = { 
+                  'isSignal' : 0,
+                  'isData'   : 0 
+                  }
+
+structure['Zg']  = { 
+                  'isSignal' : 0,
+                  'isData'   : 0 
+                  }
+
+structure['VZ']  = { 
+                  'isSignal' : 0,
+                  'isData'   : 0 
+                  }
+
+structure['WZ']  = { 
+                  'isSignal' : 0,
+                  'isData'   : 0 
+                  }
+
+
+structure['VVV']  = { 
+                  'isSignal' : 0,
+                  'isData'   : 0 
+                  }
+
+structure['ZZ']  = {
+                  'isSignal' : 0,
+                  'isData'   : 0    
+                  }
+
+
+structure['ggH'] = {
+                  'isSignal' : 0,
+                  'isData'   : 0    
+                  }
+
+structure['ggH_hww'] = {
+                  'isSignal' : 0,
+                  'isData'   : 0    
+                  }
+
+structure['ggH_htt'] = {
+                  'isSignal' : 0,
+                  'isData'   : 0    
+                  }
+
+
+structure['qqH_hww'] = {
+                  'isSignal' : 0,
+                  'isData'   : 0    
+                  }
+
+structure['qqH_hww'] = {
+                  'isSignal' : 0,
+                  'isData'   : 0    
+                  }
+
+structure['WH_hww'] = {
+                  'isSignal' : 0,
+                  'isData'   : 0    
+                  }
+
+structure['ZH_hww'] = {
+                  'isSignal' : 0,
+                  'isData'   : 0    
+                  }
+
+structure['ggZH_hww'] = {
+                  'isSignal' : 0,
+                  'isData'   : 0    
+                  }
+
+structure['H_htt'] = {
+                  'isSignal' : 0,
+                  'isData'   : 0    
+                  }
+
+structure['H_hww'] = {
+                  'isSignal' : 0,
+                  'isData'   : 0    
+                  }
+
+
+
+structure['bbH_hww'] = {
+    'isSignal' : 0,
+    'isData' : 0
+    }
+                   
+
+
+# data
+
+structure['DATA']  = { 
+                  'isSignal' : 0,
+                  'isData'   : 1 
+              }
+
+
+
+
