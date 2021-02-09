ef3e680ef9e5f8e543753855a2ede8d466cc4795
lourdesurda <lourdes.urda@cern.ch>
2021-02-09 10:46:00 +0100

    TUtil::JHUGenMatEl: Number of daughters 1 or number of intermediate Vs 25, -9000 not supported

--- Configurations/patches/RecoLevelME_patch.cc
+++ Configurations/patches/RecoLevelME_patch.cc
@@ -1,3 +1,4 @@
+
 //  Temporary on-the-fly wlep1pt var for nanoLatino trees nAODv7_2016/2017/2018v7 or earlier.
 
 
@@ -11,8 +12,8 @@
 #include "ZZMatrixElement/MELA/interface/Mela.h"
 
 
-class RecoLevelME : public multidraw::TTreeFunction, public Mela {
-	
+//class RecoLevelME : public multidraw::TTreeFunction, public Mela {
+class RecoLevelME : public multidraw::TTreeFunction {
 	public:
 		RecoLevelME();
 		char const* getName() const override {return "RecoLevelME"; }
@@ -21,15 +22,19 @@ class RecoLevelME : public multidraw::TTreeFunction, public Mela {
 		double evaluate(unsigned) override;
 		
 		/*double LHCsqrts_ = 13.;
-		double mh_ = 125.;
+		double mh_ = 125.
 		TVar::VerbosityLevel verbosity_=TVar::ERROR;*/
 		//Mela(double LHCsqrts_=13., double mh_=125., TVar::VerbosityLevel verbosity_=TVar::ERROR);
 		//Mela(LHCsqrts_, mh_, verbosity_);
 		//MELA functions
-		Mela* setCandidateDecayMode(TVar::CandidateDecayMode mode);
+		/*Mela* setCandidateDecayMode(TVar::CandidateDecayMode mode);
 		Mela* setInputEvent(SimpleParticleCollection_t* pDaughters,SimpleParticleCollection_t* pAssociated=0, SimpleParticleCollection_t* pMothers=0, bool isGen=false);
 		Mela* setCurrentCandidateFromIndex();
-		Mela* resetInputEvent();
+		Mela* resetInputEvent();*/
+
+		//Double_t LHCsqrts_=13., mh_=125.;
+		//TVar::VerbosityLevel verbosity_=TVar::ERROR;
+		//Mela* mela  = new Mela(LHCsqrts_, mh_, verbosity_);
 
 	protected:
 		void bindTree_(multidraw::FunctionLibrary&) override;
@@ -62,20 +67,25 @@ class RecoLevelME : public multidraw::TTreeFunction, public Mela {
 		IntArrayReader* FatJet_subJetIdx1{};
 		IntArrayReader* FatJet_subJetIdx2{};
 
-		/*double LHCsqrts_ = 13.;
-		double mh_ = 125.;
-		TVar::VerbosityLevel verbosity_=TVar::ERROR;*/
+	private:
+		//Mela* mela  = new Mela();
+		Double_t LHCsqrts_=13., mh_=125.;
+		TVar::VerbosityLevel verbosity_=TVar::ERROR;
+		Mela* mela  = new Mela(LHCsqrts_, mh_, verbosity_);
+		//std::unique_ptr<Mela> mela(new Mela(13, 125));
+
 };
 
 	RecoLevelME::RecoLevelME():
-		TTreeFunction(),
-		Mela()
+		TTreeFunction()
+		//Mela()
 		//Mela(LHCsqrts_, mh_, verbosity_)
 	{}
 
 	double 
 	RecoLevelME::evaluate(unsigned)
 	{
+
 		TLorentzVector L1(0.,0.,0.,0.);
 		TLorentzVector L2(0.,0.,0.,0.);
 		TLorentzVector LL(0.,0.,0.,0.);
@@ -118,23 +128,45 @@ class RecoLevelME : public multidraw::TTreeFunction, public Mela {
 		J1.SetPtEtaPhiM(SubJet_pt->At(indexsubfatjet1), SubJet_eta->At(indexsubfatjet1), SubJet_phi->At(indexsubfatjet1), SubJet_mass->At(indexsubfatjet1));
 		J2.SetPtEtaPhiM(SubJet_pt->At(indexsubfatjet2), SubJet_eta->At(indexsubfatjet2), SubJet_phi->At(indexsubfatjet2), SubJet_mass->At(indexsubfatjet2));
 
-		//MELA
+		/*std::cout << "Higgs.M() " << hm << std::endl;
+		std::cout << "nunu " << NuNu.M() << std::endl;
+		std::cout << "Indexfatjet " << indexfatjet << std::endl;
+		std::cout << "Indexsubfatjet1 " << indexsubfatjet1 << " Indexsubfatjet2 " << indexsubfatjet2 << std::endl;
+		std::cout << "SUBJET1_PT " << SubJet_pt->At(indexsubfatjet1) << " SUBJET2_PT " << SubJet_pt->At(indexsubfatjet2) << std::endl;*/
+
 		SimpleParticleCollection_t daughter_coll;
 		SimpleParticleCollection_t associated_coll;	
 
-		SimpleParticle_t daughter(25, Higgs);
-		SimpleParticle_t associated1(0, J1);
-		SimpleParticle_t associated2(0, J2);
+		//daughter_coll.push_back(daughter);
+		daughter_coll.push_back(SimpleParticle_t(25,Higgs));
+		associated_coll.push_back(SimpleParticle_t(0,J1));
+		associated_coll.push_back(SimpleParticle_t(0,J2));	
 
-		daughter_coll.push_back(daughter);
-		associated_coll.push_back(associated1);
-		associated_coll.push_back(associated2);	
+		std::cout << "MELA 1 " << std::endl;
+		mela->setCandidateDecayMode(TVar::CandidateDecay_Stable);
+		std::cout << "MELA 2 " << std::endl;
+		//mela->setCandidateDecayMode(TVar::CandidateDecay_ZZ);
+		mela->setInputEvent(&daughter_coll, &associated_coll, 0, 0);
+		std::cout << "MELA 3 "  << daughter_coll.size() << "\t" << associated_coll.size()<< std::endl;
+		mela->setCurrentCandidateFromIndex(0);
+		std::cout << "MELA 4 " << std::endl;
+		mela->setProcess(TVar::HSMHiggs, TVar::JHUGen, TVar::Had_WH);
+		//mela->setProcess(TVar::HSMHiggs, TVar::MCFM, TVar::ZZGG);
+		//mela->setProcess();
+		std::cout << "MELA 5 " << std::endl;
+		float p_smH =0.;
+		mela->computeP(p_smH, false);
+		std::cout << "********matrix element p_smh ********" << p_smH << std::endl;
+ 		//float kd, ps, pb;
 
-		//setCandidateDecayMode(TVar::CandidateDecay_ZZ);
-		//Mela->setInputEvent(daughter_coll, associated_coll, 0, 0);
-		//Mela->setCurrentCandidateFromIndex(0);
-
+  		//mela->computeKD(210.437,91.872,51.459,0.981,0.175,0.934,-1.980,0.269,kd,ps,pb);
+ 
+  		//cout << endl << "KD=" << kd << endl;
+		std::cout << "MELA 6 " << std::endl;
+		mela->resetInputEvent();
+		std::cout << "MELA 7 " << std::endl;
 		return 0;
+
 		}
 		else return -9999;
 	}
