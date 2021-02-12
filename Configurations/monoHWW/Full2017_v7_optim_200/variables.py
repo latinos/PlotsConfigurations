975a6bb4d8806f22984db4f1b06b732868b10b16
fmanteca <fmanteca@ifca.unican.es>
2021-02-12 11:42:09 +0100

    updated

--- Configurations/monoHWW/Full2017_v7_optim_200/variables.py
+++ Configurations/monoHWW/Full2017_v7_optim_200/variables.py
@@ -14,3 +14,14 @@ variables['mtw2']  = {   'name': 'mtw2',
                          'xaxis' : 'm_{T}^{l2+MET} [GeV]',
                          'fold' : 3
                         }
+variables['mth']  = {   'name': 'mth',
+                        'range' : (30, 50,400),
+                        'xaxis' : 'm_{T}^{ll, p_{T}^{miss}} [GeV]',
+                        'fold' : 3
+                        }
+
+variables['mtw2_morebins']  = {   'name': 'mtw2',
+                        'range' : (30, 20,300),
+                         'xaxis' : 'm_{T}^{p_{T}^{min}, p_{T}^{miss}} [GeV]',
+                        'fold' : 3
+                        }
