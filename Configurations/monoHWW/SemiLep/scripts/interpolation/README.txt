6f330ef3496cd88ed5b826f4280065a0a0da1e77
Senne Van Putte <senne.vanputte@student.uantwerpen.be>
2021-05-28 18:51:27 +0200

    interpolation scripts

---
+++ Configurations/monoHWW/SemiLep/scripts/interpolation/README.txt
@@ -0,0 +1,2 @@
+Copied from Pedro for safety
+Originally from: PlotsConfigurations/Configurations/monoHWW/Full2017_v7_3d/onTheFly 
---
+++ Configurations/monoHWW/SemiLep/scripts/interpolation/interpolationWeight_mhs_180_mx_100_mZp_700.C
@@ -0,0 +1,15 @@
+#include "TString.h"
+#include "TROOT.h"
+#include "TH1F.h"
+#include "TFile.h"
+#include <iostream>
+
+using namespace std;
+
+TFile* myfile = new TFile("/afs/cern.ch/user/f/fernanpe/public/for_MonoH_analysis/Interpolation_mhs_180_mx_100_mZp_700.root");  
+
+TH1F* histo = (TH1F*) myfile->Get("Ratio");
+
+float interpolationWeight_mhs_180_mx_100_mZp_700(float higgsPt=100.){
+  return histo->GetBinContent(histo->FindBin(higgsPt));
+}
