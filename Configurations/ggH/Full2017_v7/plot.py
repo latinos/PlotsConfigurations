be07036d7446c1b5903acf3b5bc0a2e244e144f2
Roberto <roberto.seidita@cern.ch>
2021-11-12 11:09:23 +0100

    Updating ggH DF configs

--- Configurations/ggH/Full2017_v7/plot.py
+++ Configurations/ggH/Full2017_v7/plot.py
@@ -358,7 +358,7 @@ plot['DATA']  = {
                   'color': 1 ,  
                   'isSignal' : 0,
                   'isData'   : 1 ,
-                  'isBlind'  : 0
+                  'isBlind'  : 1
               }
 
 
@@ -366,7 +366,7 @@ plot['DATA']  = {
 
 # additional options
 
-legend['lumi'] = 'L = 59.7/fb'
+legend['lumi'] = 'L = 41.5/fb'
 
 legend['sqrt'] = '#sqrt{s} = 13 TeV'
 
--- Configurations/ggH/Full2018_v7/plot.py
+++ Configurations/ggH/Full2018_v7/plot.py
@@ -358,7 +358,7 @@ plot['DATA']  = {
                   'color': 1 ,  
                   'isSignal' : 0,
                   'isData'   : 1 ,
-                  'isBlind'  : 0
+                  'isBlind'  : 1
               }
 
 
