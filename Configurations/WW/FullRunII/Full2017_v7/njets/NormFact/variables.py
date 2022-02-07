fd0c3c8b0fe535f0c4fd56b68c4a93a19b2b3689
Susan Dittmer <Susan.Dittmer@cern.ch>
2022-02-07 09:11:15 +0100

    Updating WW njets configs

--- Configurations/WW/FullRunII/Full2017_v7/njets/NormFact/variables.py
+++
@@ -1,7 +0,0 @@
-# variables
-
-variables['events']  = {   'name': '1',      
-                        'range' : (1,0,2),  
-                        'xaxis' : 'events', 
-                        'fold' : 3
-                        }
