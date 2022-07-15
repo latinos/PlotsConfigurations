dbfc5fcb6dd122e3341e1d53e42bdc6aad02de76
arun <arun.kumar@cern.ch>
2022-07-15 15:22:00 +0200

    mll 2018 up

---
+++ Configurations/WW/FullRunII/Full2018_v7/mll/TheoUnc/variables.py
@@ -0,0 +1,7 @@
+# variables
+
+variables['nCleanGenJet']  = {   'name': 'nCleanGenJet',      
+                           'range' : (4,0,4),  
+                           'xaxis' : 'events', 
+                           'fold' : 3
+                       }
