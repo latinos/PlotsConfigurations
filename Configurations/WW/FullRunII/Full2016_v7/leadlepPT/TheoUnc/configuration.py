7ecc3aefa1ced52717a201641fad6ca5820e54c5
Saumya Phor <saumya.phor4252@gmail.com>
2022-07-22 22:06:10 +0200

    updating 2016 jsons for leptonic differential variables

---
+++ Configurations/WW/FullRunII/Full2016_v7/leadlepPT/TheoUnc/configuration.py
@@ -0,0 +1,26 @@
+# Configuration file to get normalizations of WW theoretical uncertainties
+
+treeName = 'Events'
+
+tag = 'WW2016_v7_leadlepPT_TheoUnc'
+
+# used by mkShape to define output directory for root files
+outputDir = 'rootFile'
+
+# file with TTree aliases
+aliasesFile = 'aliases.py'
+
+# file with list of variables
+variablesFile = 'variables.py'
+
+# file with list of cuts
+cutsFile = 'cuts.py' 
+
+# file with list of samples
+samplesFile = 'samples.py' 
+
+# luminosity to normalize to (in 1/fb)
+lumi = 35.867
+
+# nuisances file for mkDatacards and for mkShape
+nuisancesFile = 'nuisances.py'
