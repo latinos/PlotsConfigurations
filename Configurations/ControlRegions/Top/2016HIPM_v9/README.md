031e06bf92261f132483801e671ec4dd7974cb6b
ntrevisa <nicolo.trevisani@cern.ch>
2022-08-26 16:39:44 +0200

    cfg for Top CR in 2016 HIPM UL

---
+++ Configurations/ControlRegions/Top/2016HIPM_v9/README.md
@@ -0,0 +1,29 @@
+# Top Control Plots in 2016 preVFP UL
+
+Control plots in a Top phase space, using the first part of the 2016 Run 2 dataset.
+
+The instructions to produce the plots follow.
+
+### Produce distributions using mkShapesMulti.py in batch mode
+
+    mkShapesMulti.py --pycfg=configuration.py --doBatch=1 --batchSplit=Samples,Files --batchQueue=testmatch
+
+Resubmit failed jobs.
+
+    cd $HOME/scripts/jobs/mkShapes__Top_2016HIPM_v9/
+    for i in *jid; do condor_submit ${i/jid/jds}; done
+    cd -
+
+Or, if they failed because the wall clock time has been exceeded, resubmit them on a longer-time queue.
+
+    cd $HOME/scripts/jobs/mkShapes__Top_2016HIPM_v9/
+    for i in *jid; do sed -i "s/longlunch/workday/g" ${i/jid/jds}; condor_submit ${i/jid/jds}; done
+    cd -
+
+### Merge rootfiles using hadd
+
+    mkShapesMulti.py --pycfg=configuration.py --doHadd=1 --batchSplit=Samples,Files --doNotCleanup --nThreads=8
+
+### Plot distributions
+
+    mkPlot.py --pycfg=configuration.py --inputFile=rootFile/plots_Top_2016HIPM_v9.root --fileFormats=png --onlyPlot=cratio --showIntegralLegend=1
---
+++ Configurations/ControlRegions/Top/2016HIPM_v9/cuts.py
@@ -0,0 +1,47 @@
+# cuts
+
+
+supercut = 'mll>12 \
+         && Lepton_pt[0]>25 \
+         && Lepton_pt[1]>10 \
+         && (abs(Lepton_pdgId[1])==13 || Lepton_pt[1]>13) \
+         && (nLepton>=2 && Alt$(Lepton_pt[2],0)<10) \
+         && abs(Lepton_eta[0])<2.5 && abs(Lepton_eta[1])<2.5 \
+         && ptll>30 \
+         && PuppiMET_pt > 20 \
+         && (Lepton_pdgId[0]*Lepton_pdgId[1] == -11*13) \
+         '
+
+# # Inclusive regions
+# cuts['Top_ee_incl'] = '(Lepton_pdgId[0] * Lepton_pdgId[1] == -11*11) && mll > 110 && ((zeroJet && !bVeto) || bReq)'
+# cuts['Top_mm_incl'] = '(Lepton_pdgId[0] * Lepton_pdgId[1] == -13*13) && mll > 110 && ((zeroJet && !bVeto) || bReq)'
+# cuts['Top_df_incl'] = '(Lepton_pdgId[0] * Lepton_pdgId[1] == -13*11) && mll > 50  && ((zeroJet && !bVeto) || bReq)'
+
+
+# Jet bins regions
+cuts['Top_cr']  = {
+    'expr' : 'topcr',
+    'categories' : {
+        '0j' : 'zeroJet',
+        '1j' : 'oneJet && Alt$(CleanJet_pt[1],0)<30',
+        '2j' : '(mjj<65 || mjj>105) && mjj<120 && multiJet',
+    }
+}
+
+# cuts['Top_ee']  = {
+#     'expr' : '(Lepton_pdgId[0] * Lepton_pdgId[1] == -11*11) && mll > 110 && ((zeroJet && !bVeto) || bReq)',
+#     'categories' : {
+#         '0j' : 'zeroJet',
+#         '1j' : 'oneJet && Alt$(CleanJet_pt[1],0)<30',
+#         '2j' : 'multiJet',
+#     }
+# }
+
+# cuts['Top_mm']  = {
+#     'expr' : '(Lepton_pdgId[0] * Lepton_pdgId[1] == -13*13) && mll > 110 && ((zeroJet && !bVeto) || bReq)',
+#     'categories' : {
+#         '0j' : 'zeroJet',
+#         '1j' : 'oneJet && Alt$(CleanJet_pt[1],0)<30',
+#         '2j' : 'multiJet',
+#     }
+# }
