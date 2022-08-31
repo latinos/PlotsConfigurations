7999aee10680025c91e6c1888027ed1379d040cb
ntrevisa <nicolo.trevisani@cern.ch>
2022-08-31 10:55:27 +0200

    WW control region with fakes for UL

---
+++ Configurations/ControlRegions/WW/2016HIPM/README.md
@@ -0,0 +1,29 @@
+# WW Control Plots in 2016 preVFP UL
+
+Control plots in a WW phase space, using the first part of the 2016 Run 2 dataset.
+
+The instructions to produce the plots follow.
+
+### Produce distributions using mkShapesMulti.py in batch mode
+
+    mkShapesMulti.py --pycfg=configuration.py --doBatch=1 --batchSplit=Samples,Files --batchQueue=testmatch
+
+Resubmit failed jobs.
+
+    cd $HOME/scripts/jobs/mkShapes__WW_2016HIPM_v9/
+    for i in *jid; do condor_submit ${i/jid/jds}; done
+    cd -
+
+Or, if they failed because the wall clock time has been exceeded, resubmit them on a longer-time queue.
+
+    cd $HOME/scripts/jobs/mkShapes__WW_2016HIPM_v9/
+    for i in *jid; do sed -i "s/longlunch/workday/g" ${i/jid/jds}; condor_submit ${i/jid/jds}; done
+    cd -
+
+### Merge rootfiles using hadd
+
+    mkShapesMulti.py --pycfg=configuration.py --doHadd=1 --batchSplit=Samples,Files --doNotCleanup --nThreads=8
+
+### Plot distributions
+
+    mkPlot.py --pycfg=configuration.py --inputFile=rootFile/plots_WW_2016HIPM_v9.root --fileFormats=png --onlyPlot=cratio --showIntegralLegend=1 --minLogCratio=1
---
+++ Configurations/ControlRegions/WW/2016HIPM/configuration.py
@@ -0,0 +1,41 @@
+# Configuration file to produce initial root files -- has both merged and binned ggH samples
+
+treeName = 'Events'
+
+tag = 'WW_2016HIPM_v9'
+
+# used by mkShape to define output directory for root files
+outputDir = 'rootFile'
+
+# file with TTree aliases
+aliasesFile = 'aliases.py'
+
+# file with list of variables
+variablesFile = 'variables.py'
+
+# file with list of cuts
+cutsFile = 'cuts.py' 
+
+# file with list of samples
+samplesFile = 'samples.py' 
+
+# file with list of samples
+plotFile = 'plot.py' 
+
+# luminosity to normalize to (in 1/fb)
+# https://github.com/latinos/LatinoAnalysis/blob/UL_production/NanoGardener/python/data/TrigMaker_cfg.py#L27 (#95 #165)
+# 16.802739097 + 1.063261220 + 1.655228036 = 19.521228353 
+lumi = 19.52
+
+# used by mkPlot to define output directory for plots
+# different from "outputDir" to do things more tidy
+outputDirPlots = 'plots_' + tag
+
+# used by mkDatacards to define output directory for datacards
+outputDirDatacard = 'datacards'
+
+# structure file for datacard
+structureFile = 'structure.py'
+
+# nuisances file for mkDatacards and for mkShape
+nuisancesFile = 'nuisances.py'
