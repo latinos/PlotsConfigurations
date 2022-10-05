9889f57af85fbac2e9651e1704d89cdb04b7e0c0
ntrevisa <nicolo.trevisani@cern.ch>
2022-10-05 17:16:05 +0200

    WW CR Full2017_v9

---
+++ Configurations/ControlRegions/WW/Full2017_v9/cuts.py
@@ -0,0 +1,24 @@
+# cuts
+
+# https://github.com/latinos/PlotsConfigurations/blob/master/Configurations/ggH/Full2016_v7/cuts.py
+
+supercut = '   mll>12 \
+            && Lepton_pt[0]>25 \
+            && Lepton_pt[1]>10 \
+            && (abs(Lepton_pdgId[1])==13 || Lepton_pt[1]>13) \
+            && (nLepton>=2 && Alt$(Lepton_pt[2],0)<10) \
+            && abs(Lepton_eta[0])<2.5 && abs(Lepton_eta[1])<2.5 \
+            && ptll>30 \
+            && PuppiMET_pt > 20 \
+            && (Lepton_pdgId[0]*Lepton_pdgId[1] == -11*13) \
+'
+
+## WW control regions
+cuts['hww2l2v_13TeV_ww'] = {
+    'expr' : 'wwcr',
+    'categories' : {
+        '0j' : 'zeroJet',
+        '1j' : 'oneJet && Alt$(CleanJet_pt[1],0)<30',
+        '2j' : '(mjj<65 || mjj>105) && mjj<120 && multiJet'
+    }
+}
