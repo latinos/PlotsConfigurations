003d3cce8cede08e5e02ccdb68f65ee41ca3766a
ntrevisa <nicolo.trevisani@cern.ch>
2023-02-13 12:54:31 +0100

    DYee estimation in UL 2018

---
+++ Configurations/WH_chargeAsymmetry/UL/Full2018_v9/WHSS/DY_OS_CR/configuration_DY_OS.py
@@ -0,0 +1,42 @@
+# Configuration file for charge asymmetry WH_SS analysis
+treeName= 'Events'
+
+tag = 'WHSS_DY_OSCR_2018_v9_chargeAsymmetry' 
+
+# used by mkShape to define output directory for root files
+outputDir = 'rootFile_OS'
+
+# file with TTree aliases
+aliasesFile = 'aliases.py'
+
+# file with list of variables
+variablesFile = 'variables.py'
+
+# file with list of cuts
+cutsFile = 'cuts.py' 
+
+# file with list of samples
+samplesFile = 'samples_DY_OS.py' 
+
+# file with list of samples
+plotFile = 'plot.py' 
+
+# luminosity to normalize to (in 1/fb)
+# https://github.com/latinos/LatinoAnalysis/blob/UL_production/NanoGardener/python/data/TrigMaker_cfg.py#L868 (874)
+#  59.832475339
+lumi =  59.83
+
+# used by mkPlot to define output directory for plots
+# different from "outputDir" to do things more tidy
+outputDirPlots = 'plots_'+tag
+
+# used by mkDatacards to define output directory for datacards
+outputDirDatacard = 'datacards'
+
+# structure file for datacard
+structureFile = 'structure.py'
+
+# nuisances file for mkDatacards and for mkShape
+nuisancesFile = 'nuisances.py'
+
+
---
+++ Configurations/WH_chargeAsymmetry/UL/Full2018_v9/WHSS/DY_OS_CR/dict_os_ss_cuts.py
@@ -0,0 +1,26 @@
+# Dictionary with opposite-sign and corresponding same-sign selection cuts
+
+# dict structure = {"ss_cut": "os_cut"}
+
+dict_os_ss_cuts = {
+    # SR 2j
+    "hww2l2v_13TeV_WH_SS_ee_2j_plus_pt2ge20"    : "hww2l2v_13TeV_WH_OS_ee_2j_pt2ge20",
+    "hww2l2v_13TeV_WH_SS_ee_2j_minus_pt2ge20"   : "hww2l2v_13TeV_WH_OS_ee_2j_pt2ge20",
+    "hww2l2v_13TeV_WH_SS_ee_2j_plus_pt2lt20"    : "hww2l2v_13TeV_WH_OS_ee_2j_pt2lt20",
+    "hww2l2v_13TeV_WH_SS_ee_2j_minus_pt2lt20"   : "hww2l2v_13TeV_WH_OS_ee_2j_pt2lt20",
+    # SR 1j
+    "hww2l2v_13TeV_WH_SS_ee_1j_plus_pt2ge20"    : "hww2l2v_13TeV_WH_OS_ee_1j_pt2ge20",
+    "hww2l2v_13TeV_WH_SS_ee_1j_minus_pt2ge20"   : "hww2l2v_13TeV_WH_OS_ee_1j_pt2ge20",
+    "hww2l2v_13TeV_WH_SS_ee_1j_plus_pt2lt20"    : "hww2l2v_13TeV_WH_OS_ee_1j_pt2lt20",
+    "hww2l2v_13TeV_WH_SS_ee_1j_minus_pt2lt20"   : "hww2l2v_13TeV_WH_OS_ee_1j_pt2lt20",
+    # DY CR 2j
+    "hww2l2v_13TeV_WH_SS_DYee_2j_plus_pt2ge20"  : "hww2l2v_13TeV_WH_OS_ee_2j_DYeeCR_pt2ge20",
+    "hww2l2v_13TeV_WH_SS_DYee_2j_minus_pt2ge20" : "hww2l2v_13TeV_WH_OS_ee_2j_DYeeCR_pt2ge20",
+    "hww2l2v_13TeV_WH_SS_DYee_2j_plus_pt2lt20"  : "hww2l2v_13TeV_WH_OS_ee_2j_DYeeCR_pt2lt20",
+    "hww2l2v_13TeV_WH_SS_DYee_2j_minus_pt2lt20" : "hww2l2v_13TeV_WH_OS_ee_2j_DYeeCR_pt2lt20",
+    # DY CR 1j
+    "hww2l2v_13TeV_WH_SS_DYee_1j_plus_pt2ge20"  : "hww2l2v_13TeV_WH_OS_ee_1j_DYeeCR_pt2ge20",
+    "hww2l2v_13TeV_WH_SS_DYee_1j_minus_pt2ge20" : "hww2l2v_13TeV_WH_OS_ee_1j_DYeeCR_pt2ge20",
+    "hww2l2v_13TeV_WH_SS_DYee_1j_plus_pt2lt20"  : "hww2l2v_13TeV_WH_OS_ee_1j_DYeeCR_pt2lt20",
+    "hww2l2v_13TeV_WH_SS_DYee_1j_minus_pt2lt20" : "hww2l2v_13TeV_WH_OS_ee_1j_DYeeCR_pt2lt20",
+}
