4b040f6f39d670d4dd71085f9869148d1313d8d0
ntrevisa <nicolo.trevisani@cern.ch>
2023-03-03 16:18:42 +0100

    BDT WHSS 2018

---
+++ Configurations/WH_chargeAsymmetry/UL/Full2018_v9/BDTconfig_WHSS/configuration.py
@@ -0,0 +1,17 @@
+# example of configuration file
+
+import os
+
+tagName = ''
+
+# luminosity to normalize to (in 1/fb)
+lumi = 59.74
+
+# file with list of cuts
+cutsFile = 'cuts_BDTTrain.py'
+
+# file with list of samples
+samplesFile = 'samples_BDTTrain.py'
+
+# structure file for datacard
+structureFile = 'structure.py'
