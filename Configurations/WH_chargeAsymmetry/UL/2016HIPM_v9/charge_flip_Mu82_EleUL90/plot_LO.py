57208b8de6db4b3c225ed62349f904cc49725b04
ntrevisa <nicolo.trevisani@cern.ch>
2023-03-07 11:46:22 +0100

    charge flip 2016HIPM Mu82 EleUL90

---
+++ Configurations/WH_chargeAsymmetry/UL/2016HIPM_v9/charge_flip_Mu82_EleUL90/plot_LO.py
@@ -0,0 +1,50 @@
+# plot configuration
+
+# groupPlot = {}
+# 
+# Groups of samples to improve the plots.
+# If not defined, normal plots is used
+#
+groupPlot['DY_LO']  = {  
+    'nameHR'   : "DY",
+    'isSignal' : 0,
+    'color'    : 418,    # kGreen+2
+    'samples'  : ['DY','DY_LO']
+}
+
+
+#plot = {}
+
+# keys here must match keys in samples.py    
+     
+plot['DY']  = {  
+    'color'    : 418,    # kGreen+2
+    'isSignal' : 0,
+    'isData'   : 0, 
+    'scale'    : 0,
+}
+
+plot['DY_LO']  = {  
+    'color'    : 418,    # kGreen+2
+    'isSignal' : 0,
+    'isData'   : 0, 
+    'scale'    : 1.0,
+}
+
+########
+# Data #
+########
+
+plot['DATA']  = { 
+    'nameHR'   : 'Data',
+    'color'    : 1 ,  
+    'isSignal' : 0,
+    'isData'   : 1,
+    'isBlind'  : 0
+}
+
+
+# Additional options
+
+legend['lumi'] = 'L = 19.5 fb^{-1}'
+legend['sqrt'] = '#sqrt{s} = 13 TeV'
