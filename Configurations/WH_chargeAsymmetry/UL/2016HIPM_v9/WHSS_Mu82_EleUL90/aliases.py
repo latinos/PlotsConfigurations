47cf959d26af93f31b9f533fb5647132c3a250e0
ntrevisa <nicolo.trevisani@cern.ch>
2023-03-11 09:24:34 +0100

    WHSS 2016HIPM

---
+++ Configurations/WH_chargeAsymmetry/UL/2016HIPM_v9/WHSS_Mu82_EleUL90/aliases.py
@@ -0,0 +1,367 @@
+import os
+import copy
+import inspect
+
+# /afs/cern.ch/user/n/ntrevisa/work/latinos/unblinding/CMSSW_10_6_4/src/PlotsConfigurations/Configurations/WH_chargeAsymmetry/UL/2016HIPM_v9/WHSS_Mu82_EleUL90
+
+configurations = os.path.realpath(inspect.getfile(inspect.currentframe())) # this file
+configurations = os.path.dirname(configurations) # WHSS_Mu82_EleUL90
+configurations = os.path.dirname(configurations) # 2016HIPM_v9
+configurations = os.path.dirname(configurations) # UL
+configurations = os.path.dirname(configurations) # WH_chargeAsymmetry
+configurations = os.path.dirname(configurations) # Configurations
+
+#aliases = {}
+mc     = [skey for skey in samples if skey not in ('Fake', 'DATA', 'Dyemb')]
+mc_emb = [skey for skey in samples if skey not in ('Fake', 'DATA')]
+
+# LepCut2l__ele_mvaFall17V2Iso_WP90_tthmva_70__mu_cut_Tight_HWWW_tthmva_80
+eleWP = 'mvaFall17V2Iso_WP90_tthmva_70'
+muWP  = 'cut_Tight80x_tthmva_80'
+
+aliases['LepWPCut'] = {
+    'expr': 'LepCut2l__ele_mvaFall17V2Iso_WP90__mu_cut_Tight80x*\
+             ( ((abs(Lepton_pdgId[0])==13 && Muon_mvaTTH[Lepton_muonIdx[0]]>0.82) || (abs(Lepton_pdgId[0])==11 && Lepton_mvaTTH_UL[0]>0.90)) \
+            && ((abs(Lepton_pdgId[1])==13 && Muon_mvaTTH[Lepton_muonIdx[1]]>0.82) || (abs(Lepton_pdgId[1])==11 && Lepton_mvaTTH_UL[1]>0.90)) )',
+    'samples': mc_emb + ['DATA']
+}
+
+aliases['LepWPSF'] = {
+    'expr': 'LepSF2l__ele_'+eleWP+'__mu_'+muWP,
+    'samples': mc_emb
+}
+
+# Fake leptons transfer factor
+aliases['fakeW'] = {
+    'linesToAdd' : ['.L %s/WH_chargeAsymmetry/UL/macros/fake_rate_reader.C+' % configurations],
+    'class'      : 'fake_rate_reader',
+    'args'       : ('2016_HIPM', '90', '82', 0.90, 0.82, 'nominal', 2, 'std'),
+    'samples'    : ['Fake']
+}
+
+# And variations - already divided by central values in formulas !
+aliases['fakeWEleUp'] = {
+    'linesToAdd' : ['.L %s/WH_chargeAsymmetry/UL/macros/fake_rate_reader.C+' % configurations],
+    'class'      : 'fake_rate_reader',
+    'args'       : ('2016_HIPM', '90', '82', 0.90, 0.82, 'EleUp', 2, 'std'),
+    'samples'    : ['Fake']
+}
+
+aliases['fakeWEleDown'] = {
+    'linesToAdd' : ['.L %s/WH_chargeAsymmetry/UL/macros/fake_rate_reader.C+' % configurations],
+    'class'      : 'fake_rate_reader',
+    'args'       : ('2016_HIPM', '90', '82', 0.90, 0.82, 'EleDown', 2, 'std'),
+    'samples'    : ['Fake']
+}
+
+aliases['fakeWMuUp'] = {
+    'linesToAdd' : ['.L %s/WH_chargeAsymmetry/UL/macros/fake_rate_reader.C+' % configurations],
+    'class'      : 'fake_rate_reader',
+    'args'       : ('2016_HIPM', '90', '82', 0.90, 0.82, 'MuUp', 2, 'std'),
+    'samples'    : ['Fake']
+}
+
+aliases['fakeWMuDown'] = {
+    'linesToAdd' : ['.L %s/WH_chargeAsymmetry/UL/macros/fake_rate_reader.C+' % configurations],
+    'class'      : 'fake_rate_reader',
+    'args'       : ('2016_HIPM', '90', '82', 0.90, 0.82, 'MuDown', 2, 'std'),
+    'samples'    : ['Fake']
+}
+
+
+aliases['fakeWStatEleUp'] = {
+    'linesToAdd' : ['.L %s/WH_chargeAsymmetry/UL/macros/fake_rate_reader.C+' % configurations],
+    'class'      : 'fake_rate_reader',
+    'args'       : ('2016_HIPM', '90', '82', 0.90, 0.82, 'StatEleUp', 2, 'std'),
+    'samples'    : ['Fake']
+}
+
+aliases['fakeWStatEleDown'] = {
+    'linesToAdd' : ['.L %s/WH_chargeAsymmetry/UL/macros/fake_rate_reader.C+' % configurations],
+    'class'      : 'fake_rate_reader',
+    'args'       : ('2016_HIPM', '90', '82', 0.90, 0.82, 'StatEleDown', 2, 'std'),
+    'samples'    : ['Fake']
+}
+
+aliases['fakeWStatMuUp'] = {
+    'linesToAdd' : ['.L %s/WH_chargeAsymmetry/UL/macros/fake_rate_reader.C+' % configurations],
+    'class'      : 'fake_rate_reader',
+    'args'       : ('2016_HIPM', '90', '82', 0.90, 0.82, 'StatMuUp', 2, 'std'),
+    'samples'    : ['Fake']
+}
+
+aliases['fakeWStatMuDown'] = {
+    'linesToAdd' : ['.L %s/WH_chargeAsymmetry/UL/macros/fake_rate_reader.C+' % configurations],
+    'class'      : 'fake_rate_reader',
+    'args'       : ('2016_HIPM', '90', '82', 0.90, 0.82, 'StatMuDown', 2, 'std'),
+    'samples'    : ['Fake']
+}
+
+
+# No jet with pt > 30 GeV
+aliases['zeroJet'] = {
+    'expr': 'Alt$(CleanJet_pt[0], 0) < 30.'
+}
+
+aliases['oneJet'] = {
+    'expr': 'Alt$(CleanJet_pt[0], 0) > 30.'
+}
+
+aliases['multiJet'] = {
+    'expr': 'Alt$(CleanJet_pt[1], 0) > 30.'
+}
+
+##########################################################################################
+# b tagging WPs: https://twiki.cern.ch/twiki/bin/view/CMS/BtagRecommendation106XUL16preVFP
+##########################################################################################
+
+# DeepB = DeepCSV
+bWP_loose_deepB  = '0.2027'
+bWP_medium_deepB = '0.6001' 
+bWP_tight_deepB  = '0.8819'
+
+# DeepFlavB = DeepJet
+bWP_loose_deepFlavB  = '0.0508'
+bWP_medium_deepFlavB = '0.2598'
+bWP_tight_deepFlavB  = '0.6502'
+
+# Actual algo and WP definition. BE CONSISTENT!!
+bAlgo = 'DeepB' # ['DeepB','DeepFlavB']
+bWP   = bWP_loose_deepB
+bSF   = 'deepcsv' # ['deepcsv','deepjet']
+
+# b veto
+aliases['bVeto'] = {
+    'expr': 'Sum$(CleanJet_pt > 20. && abs(CleanJet_eta) < 2.5 && Jet_btag{}[CleanJet_jetIdx] > {}) == 0'.format(bAlgo, bWP)
+}
+
+aliases['bVetoSF'] = {
+    'expr': 'TMath::Exp(Sum$(TMath::Log((CleanJet_pt>20 && abs(CleanJet_eta)<2.5)*Jet_btagSF_{}_shape[CleanJet_jetIdx]+1*(CleanJet_pt<20 || abs(CleanJet_eta)>2.5))))'.format(bSF),
+    'samples': mc
+}
+
+# At least one b-tagged jet
+aliases['bReq'] = {
+    'expr': 'Sum$(CleanJet_pt > 30. && abs(CleanJet_eta) < 2.5 && Jet_btag{}[CleanJet_jetIdx] > {}) >= 1'.format(bAlgo, bWP)
+}
+
+aliases['bReqSF'] = {
+    'expr': 'TMath::Exp(Sum$(TMath::Log((CleanJet_pt>30 && abs(CleanJet_eta)<2.5)*Jet_btagSF_{}_shape[CleanJet_jetIdx]+1*(CleanJet_pt<30 || abs(CleanJet_eta)>2.5))))'.format(bSF),
+    'samples': mc
+}
+
+# Top control region
+aliases['topcr'] = {
+    'expr': 'mtw2>30 && mll>50 && ((zeroJet && !bVeto) || bReq)'
+}
+
+# WW control region
+aliases['wwcr'] = {
+    'expr': 'mth>60 && mtw2>30 && mll>100 && bVeto'
+}
+
+# Overall b tag SF
+aliases['btagSF'] = {
+    'expr': '(bVeto || (topcr && zeroJet))*bVetoSF + (topcr && !zeroJet)*bReqSF',
+    'samples': mc
+}
+
+for shift in ['jes', 'lf', 'hf', 'lfstats1', 'lfstats2', 'hfstats1', 'hfstats2', 'cferr1', 'cferr2']:
+    for targ in ['bVeto', 'bReq']:
+        alias = aliases['%sSF%sup' % (targ, shift)] = copy.deepcopy(aliases['%sSF' % targ])
+        alias['expr'] = alias['expr'].replace('btagSF_{}_shape'.format(bSF), 'btagSF_{}_shape_up_{}'.format(bSF, shift))
+
+        alias = aliases['%sSF%sdown' % (targ, shift)] = copy.deepcopy(aliases['%sSF' % targ])
+        alias['expr'] = alias['expr'].replace('btagSF_{}_shape'.format(bSF), 'btagSF_{}_shape_down_{}'.format(bSF, shift))
+
+    aliases['btagSF%sup' % shift] = {
+        'expr': aliases['btagSF']['expr'].replace('SF', 'SF' + shift + 'up'),
+        'samples': mc
+    }
+
+    aliases['btagSF%sdown' % shift] = {
+        'expr': aliases['btagSF']['expr'].replace('SF', 'SF' + shift + 'down'),
+        'samples': mc
+    }
+
+####################################################################################
+# End of b tagging pippone
+####################################################################################
+
+
+aliases['gstarLow'] = {
+    'expr': 'Gen_ZGstar_mass > 0 && Gen_ZGstar_mass < 4',
+    'samples': 'WgS'
+}
+
+aliases['gstarHigh'] = {
+    'expr': 'Gen_ZGstar_mass < 0 || Gen_ZGstar_mass > 4',
+    'samples': 'WZ'
+}
+
+# aliases['embedtotal'] = {
+#     'expr': 'embed_total_mva16',  # wrt. eleWP
+#     'samples': 'Dyemb'
+# }
+
+
+# gen-matching to prompt only (GenLepMatch2l matches to *any* gen lepton)
+aliases['PromptGenLepMatch2l'] = {
+    'expr': 'Alt$(Lepton_promptgenmatched[0]*Lepton_promptgenmatched[1], 0)',
+    'samples': mc
+}
+
+# PostProcessing did not create (anti)topGenPt for ST samples with _ext1
+lastcopy = (1 << 13)
+
+aliases['isTTbar'] = {
+    'expr': 'Sum$(TMath::Abs(GenPart_pdgId) == 6 && TMath::Odd(GenPart_statusFlags / %d)) == 2' % lastcopy,
+    'samples': ['top']
+}
+
+aliases['isSingleTop'] = {
+    'expr': 'Sum$(TMath::Abs(GenPart_pdgId) == 6 && TMath::Odd(GenPart_statusFlags / %d)) == 1' % lastcopy,
+    'samples': ['top']
+}
+
+aliases['Top_pTrw'] = {
+    'expr': 'isTTbar * (topGenPt * antitopGenPt > 0.) * (TMath::Sqrt((0.103*TMath::Exp(-0.0118*topGenPt) - 0.000134*topGenPt + 0.973) * (0.103*TMath::Exp(-0.0118*antitopGenPt) - 0.000134*antitopGenPt + 0.973))) + (topGenPt * antitopGenPt <= 0.)',
+    'samples': ['top']
+}
+
+
+# aliases['topGenPtOTF'] = {
+#     'expr': 'Sum$((GenPart_pdgId == 6 && TMath::Odd(GenPart_statusFlags / %d)) * GenPart_pt)' % lastcopy,
+#     'samples': ['top']
+# }
+
+# aliases['antitopGenPtOTF'] = {
+#     'expr': 'Sum$((GenPart_pdgId == -6 && TMath::Odd(GenPart_statusFlags / %d)) * GenPart_pt)' % lastcopy,
+#     'samples': ['top']
+# }
+
+
+
+##### DY Z pT reweighting
+aliases['nCleanGenJet'] = {
+    'linesToAdd': ['.L %s/src/PlotsConfigurations/Configurations/Differential/ngenjet.cc+' % os.getenv('CMSSW_BASE')],
+    'class': 'CountGenJet',
+    'samples': mc
+}
+
+# aliases['getGenZpt_OTF'] = {
+#     'linesToAdd':['.L %s/src/PlotsConfigurations/Configurations/patches/getGenZpt.cc+' % os.getenv('CMSSW_BASE')],
+#     'class': 'getGenZpt',
+#     'samples': ['DY']
+# }
+# handle = open('%s/src/PlotsConfigurations/Configurations/patches/DYrew30.py' % os.getenv('CMSSW_BASE'),'r')
+# exec(handle)
+# handle.close()
+# aliases['DY_NLO_pTllrw'] = {
+#     'expr': '('+DYrew['2016']['NLO'].replace('x', 'getGenZpt_OTF')+')*(nCleanGenJet == 0)+1.0*(nCleanGenJet > 0)',
+#     'samples': ['DY']
+# }
+# aliases['DY_LO_pTllrw'] = {
+#     'expr': '('+DYrew['2016']['LO'].replace('x', 'getGenZpt_OTF')+')*(nCleanGenJet == 0)+1.0*(nCleanGenJet > 0)',
+#     'samples': ['DY']
+# }
+
+
+# Jet bins
+# using Alt$(CleanJet_pt[n], 0) instead of Sum$(CleanJet_pt >= 30) because jet pt ordering is not strictly followed in JES-varied samples
+
+# data/MC scale factors
+aliases['SFweight'] = {
+    'expr': ' * '.join(['SFweight2l', 'LepWPCut', 'LepWPSF','PrefireWeight','Jet_PUIDSF_loose','btagSF']),
+    'samples': mc
+}
+
+# variations
+aliases['SFweightEleUp'] = {
+    'expr': 'LepSF2l__ele_'+eleWP+'__Up',
+    'samples': mc_emb
+}
+aliases['SFweightEleDown'] = {
+    'expr': 'LepSF2l__ele_'+eleWP+'__Do',
+    'samples': mc_emb
+}
+aliases['SFweightMuUp'] = {
+    'expr': 'LepSF2l__mu_'+muWP+'__Up',
+    'samples': mc_emb
+}
+aliases['SFweightMuDown'] = {
+    'expr': 'LepSF2l__mu_'+muWP+'__Do',
+    'samples': mc_emb
+}
+
+# TriggerSFWeight_2l:TriggerSFWeight_2l_u:TriggerSFWeight_2l_d
+aliases['SFtriggUp'] = {
+    'expr': 'TriggerSFWeight_2l_u/TriggerSFWeight_2l',
+    'samples': mc_emb
+}
+aliases['SFtriggDown'] = {
+    'expr': 'TriggerSFWeight_2l_d/TriggerSFWeight_2l',
+    'samples': mc_emb
+}
+
+# # In WpWmJJ_EWK events, partons [0] and [1] are always the decay products of the first W
+# aliases['lhe_mW1'] = {
+#     'expr': 'TMath::Sqrt(2. * LHEPart_pt[0] * LHEPart_pt[1] * (TMath::CosH(LHEPart_eta[0] - LHEPart_eta[1]) - TMath::Cos(LHEPart_phi[0] - LHEPart_phi[1])))',
+#     'samples': ['WWewk']
+# }
+
+# # and [2] [3] are the second W
+# aliases['lhe_mW2'] = {
+#     'expr': 'TMath::Sqrt(2. * LHEPart_pt[2] * LHEPart_pt[3] * (TMath::CosH(LHEPart_eta[2] - LHEPart_eta[3]) - TMath::Cos(LHEPart_phi[2] - LHEPart_phi[3])))',
+#     'samples': ['WWewk']
+# }
+
+### BDT on-the-fly
+aliases['BDT_WHSS_v9'] = {
+    'linesToAdd' : ['.L %s/WH_chargeAsymmetry/UL/macros/BDT_WHSS_v9.C+' % configurations],
+    'class': 'BDT_WHSS_v9',
+    'args' : ('BDTG_6', '%s/WH_chargeAsymmetry/UL/data/BDT/2016HIPM/WHSS/weights/TMVAClassification_BDTG_6.weights.xml' % configurations),
+}
+
+########################
+### Charge misid SFs ###
+########################
+
+aliases['ttHMVA_SF_flip_2l'] = {
+    'linesToAdd': ['.L %s/WH_chargeAsymmetry/UL/macros/flipper.C+' % configurations],
+    'class': 'flipper',
+    'args' : ('UL_2016HIPM', 2, 'Total_SF', 'false'),
+    'samples': ['DY','WW', 'top']
+}
+
+aliases['ttHMVA_SF_err_flip_2l'] = {
+    'linesToAdd': ['.L %s/WH_chargeAsymmetry/UL/macros/flipper.C+' % configurations],
+    'class': 'flipper',
+    'args' : ('UL_2016HIPM', 2, 'Total_SF_err', 'false'),
+    'samples': ['DY','WW','top']
+}
+
+##################################
+### Charge misid probabilities ###
+##################################
+
+aliases['ttHMVA_eff_flip_2l'] = {
+    'linesToAdd': ['.L %s/WH_chargeAsymmetry/UL/macros/flipper_eff.C+' % configurations],
+    'class': 'flipper_eff',
+    'args' : ('UL_2016HIPM', 2, 'Total_SF', 'false'),
+    'samples': ['DY']
+}
+
+aliases['ttHMVA_eff_err_flip_2l'] = {
+    'linesToAdd': ['.L %s/WH_chargeAsymmetry/UL/macros/flipper_eff.C+' % configurations],
+    'class': 'flipper_eff',
+    'args' : ('UL_2016HIPM', 2, 'Total_SF_err', 'false'),
+    'samples': ['DY']
+}
+
+
+# Fix METFilter_DATA definition: Flag_ecalBadCalibFilter is removed since it is not needed in 2016
+aliases['METFilter_DATA_fix'] = {
+    'expr' : 'Flag_goodVertices*Flag_globalSuperTightHalo2016Filter*Flag_HBHENoiseFilter*Flag_HBHENoiseIsoFilter*Flag_EcalDeadCellTriggerPrimitiveFilter*Flag_BadPFMuonFilter*Flag_BadPFMuonDzFilter*Flag_eeBadScFilter',
+    'samples': ['DATA','Fake']
+}
---
+++ Configurations/WH_chargeAsymmetry/UL/2016HIPM_v9/WHSS_Mu82_EleUL90/variables.py
@@ -0,0 +1,302 @@
+# variables
+
+#variables = {}
+
+# 0 = not fold (default), 1 = fold underflowbin, 2 = fold overflow bin, 3 = fold underflow and overflow
+    
+variables['events'] = {
+    'name'  : '1',      
+    'range' : (1,0,2),  
+    'xaxis' : 'events', 
+    'fold'  : 3
+}
+
+# 1 jet binning
+variables['mlljj20_whss_1j_bin'] = {
+    'name'  : 'mlljj20_whss',
+    'range' : ([60.,70.,80.,90.,100.,110.,120.,130.,140.,150.,160.,170.,180.,190.,200.,250.,300.],),
+    'xaxis' : 'mlljj20_whss [GeV]',
+    'fold'  : 3
+}
+
+# 2 jets binning
+variables['mlljj20_whss_2j_bin'] = {
+    'name'  : 'mlljj20_whss',
+    'range' : ([60.,120.,130.,140.,150.,160.,170.,180.,190.,200.,250.,300.],),
+    'xaxis' : 'mlljj20_whss [GeV]',
+    'fold'  : 3
+}
+
+# BDT
+variables['BDTG6'] = {
+    'name'  : 'BDT_WHSS_v9',     
+    'range' : (40,-1,1),   
+    'xaxis' : 'BDT discriminant',
+    'fold'  : 3
+}
+
+variables['BDTG6_more'] = {
+    'name'  : 'BDT_WHSS_v9',     
+    'range' : (1000,-1,1),   
+    'xaxis' : 'BDT discriminant',
+    'fold'  : 3
+}
+
+# BDT binnings
+##############
+variables['BDTG6_binning_0_0'] = {
+    'name'  : 'BDT_WHSS_v9',
+    'range' : ([-1.0, -0.8, -0.6, -0.4, -0.2, 0.0, 1.0],),
+    'xaxis' : 'BDT discriminant',
+    'fold'  : 3
+}
+
+variables['BDTG6_binning_0_1'] = {
+    'name'  : 'BDT_WHSS_v9',
+    'range' : ([-1.0, -0.8, -0.6, -0.4, -0.2, 0.0, 0.1, 1.0],),
+    'xaxis' : 'BDT discriminant',
+    'fold'  : 3
+}
+
+variables['BDTG6_binning_0_2'] = {
+    'name'  : 'BDT_WHSS_v9',
+    'range' : ([-1.0, -0.8, -0.6, -0.4, -0.2, 0.0, 0.1, 0.2, 1.0],),
+    'xaxis' : 'BDT discriminant',
+    'fold'  : 3
+}
+
+variables['BDTG6_binning_0_3'] = {
+    'name'  : 'BDT_WHSS_v9',
+    'range' : ([-1.0, -0.8, -0.6, -0.4, -0.2, 0.0, 0.1, 0.2, 0.3, 1.0],),
+    'xaxis' : 'BDT discriminant',
+    'fold'  : 3
+}
+
+variables['BDTG6_binning_0_4'] = {
+    'name'  : 'BDT_WHSS_v9',
+    'range' : ([-1.0, -0.8, -0.6, -0.4, -0.2, 0.0, 0.1, 0.2, 0.3, 0.4, 1.0],),
+    'xaxis' : 'BDT discriminant',
+    'fold'  : 3
+}
+
+variables['BDTG6_binning_0_5'] = {
+    'name'  : 'BDT_WHSS_v9',
+    'range' : ([-1.0, -0.8, -0.6, -0.4, -0.2, 0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 1.0],),
+    'xaxis' : 'BDT discriminant',
+    'fold'  : 3
+}
+
+variables['BDTG6_binning_0_5'] = {
+    'name'  : 'BDT_WHSS_v9',
+    'range' : ([-1.0, -0.8, -0.6, -0.4, -0.2, 0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 1.0],),
+    'xaxis' : 'BDT discriminant',
+    'fold'  : 3
+}
+
+variables['BDTG6_binning_0_6'] = {
+    'name'  : 'BDT_WHSS_v9',
+    'range' : ([-1.0, -0.8, -0.6, -0.4, -0.2, 0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 1.0],),
+    'xaxis' : 'BDT discriminant',
+    'fold'  : 3
+}
+
+variables['BDTG6_binning_0_7'] = {
+    'name'  : 'BDT_WHSS_v9',
+    'range' : ([-1.0, -0.8, -0.6, -0.4, -0.2, 0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 1.0],),
+    'xaxis' : 'BDT discriminant',
+    'fold'  : 3
+}
+
+variables['BDTG6_binning_0_75'] = {
+    'name'  : 'BDT_WHSS_v9',
+    'range' : ([-1.0, -0.8, -0.6, -0.4, -0.2, 0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.75, 1.0],),
+    'xaxis' : 'BDT discriminant',
+    'fold'  : 3
+}
+
+
+# Control variables
+###################
+
+variables['puppimet'] = {
+    'name'  : 'PuppiMET_pt',    
+    'range' : (20,0,200),
+    'xaxis' : 'PUPPI met [GeV]',
+    'fold'  : 3
+}
+
+variables['mll'] = {
+    'name'  : 'mll',
+    'range' : (20, 40,120),
+    'xaxis' : 'm_{ll} [GeV]',
+    'fold'  : 0
+}
+
+variables['ptll'] = {
+    'name'  : 'ptll',     
+    'range' : (40, 0,200),   
+    'xaxis' : 'p_{T}^{ll} [GeV]',
+    'fold'  : 0
+}
+
+variables['pt1'] = {
+    'name'  : 'Lepton_pt[0]',     
+    'range' : (20,0,100),   
+    'xaxis' : 'p_{T} 1st lep',
+    'fold'  : 3                         
+}
+
+variables['pt2'] = {
+    'name'  : 'Lepton_pt[1]',     
+    'range' : (20,0,100),   
+    'xaxis' : 'p_{T} 2nd lep',
+    'fold'  : 3                         
+}
+
+variables['mlljj20_whss_bin2'] = {
+    'name'  : 'mlljj20_whss',
+    'range' : ([60.,100.,110.,120.,130.,140.,150.,160.,170.,180.,190.,200.,250.,300.],),
+    'xaxis' : 'mlljj20_whss [GeV]',
+    'fold'  : 3
+}
+
+variables['detall']  = {  
+    'name'  : 'abs(Lepton_eta[0] - Lepton_eta[1])',
+    'range' : (20,0.,10.),
+    'xaxis' : 'detall',
+    'fold'  : 3
+}
+
+variables['mth']  = {  
+    'name'  : 'mth',
+    'range' : (40,0,200),
+    'xaxis' : 'm_{T}^{H} [GeV]',
+    'fold'  : 0
+}
+
+variables['dphill']  = {  
+    'name'  : 'abs(dphill)',     
+    'range' : (20,0,3.14),   
+    'xaxis' : '#Delta#phi_{ll}',
+    'fold'  : 3
+}
+
+variables['eta1']  = {
+    'name'  : 'Lepton_eta[0]',     
+    'range' : (40,-3,3),   
+    'xaxis' : '#eta 1st lep',
+    'fold'  : 3                         
+}
+
+variables['eta2']  = {
+    'name'  : 'Lepton_eta[1]',     
+    'range' : (40,-3,3),   
+    'xaxis' : '#eta 2nd lep',
+    'fold'  : 3                         
+}
+
+variables['jetpt1']  = {
+    'name'  : 'CleanJet_pt[0]*(CleanJet_pt[0]>30)',     
+    'range' : (40,0,200),   
+    'xaxis' : 'p_{T} 1st jet',
+    'fold'  : 2   
+}
+
+variables['jetpt2'] = {
+    'name'  : 'CleanJet_pt[1]*(CleanJet_pt[1]>30)',     
+    'range' : (40,0,200),   
+    'xaxis' : 'p_{T} 2nd jet',
+    'fold'  : 0
+}
+
+variables['mjj'] = {
+    'name'  : 'mjj*(CleanJet_pt[1]>30)',
+    'range' : (50,0,400),
+    'xaxis' : 'm_{jj} [GeV]',
+    'fold'  : 3
+}
+
+variables['mtw1'] = {
+    'name'  : 'mtw1',
+    'range' : (40,0,200),
+    'xaxis' : 'm_{T}^{W_{1}} [GeV]',
+    'fold'  : 3
+}
+
+variables['mtw2'] = {
+    'name'  : 'mtw2',
+    'range' : (40,0,200),
+    'xaxis' : 'm_{T}^{W_{2}} [GeV]',
+    'fold'  : 3
+}
+
+variables['dphilljet'] = {
+    'name'  : 'dphilljet',
+    'range' : (20,0,3.2),
+    'xaxis' : 'dphilljet',
+    'fold'  : 3
+}
+
+variables['dphilljetjet'] = {
+    'name'  : 'dphilljetjet*(CleanJet_pt[1]>30)',
+    'range' : (20,0,3.2),
+    'xaxis' : 'dphilljetjet',
+    'fold'  : 3
+}
+
+variables['dphilmet'] = {
+    'name'  : 'dphilmet',
+    'range' : (20,0,3.2),
+    'xaxis' : 'dphilmet',
+    'fold'  : 3
+}
+
+variables['dphillmet'] = {
+    'name'  : 'dphillmet',
+    'range' : (20,0,3.2),
+    'xaxis' : 'dphillmet',
+    'fold'  : 3
+}
+
+variables['dphilep1jet1'] = {
+    'name'  : 'dphilep1jet1',
+    'range' : (20,0,3.2),
+    'xaxis' : 'dphilep1jet1',
+    'fold'  : 3
+}
+
+variables['dphilep1jet2'] = {
+    'name'  : 'dphilep1jet2*(CleanJet_pt[1]>30)',
+    'range' : (20,0,3.2),
+    'xaxis' : 'dphilep1jet2',
+    'fold'  : 3
+}
+
+variables['dphilep2jet1'] = {
+    'name'  : 'dphilep2jet1',
+    'range' : (20,0,3.2),
+    'xaxis' : 'dphilep2jet1',
+    'fold'  : 3
+}
+
+variables['dphilep2jet2'] = {
+    'name'  : 'dphilep2jet2*(CleanJet_pt[1]>30)',
+    'range' : (20,0,3.2),
+    'xaxis' : 'dphilep1jet2',
+    'fold'  : 3
+}
+
+variables['dphijjmet'] = {
+    'name'  : 'dphijjmet',
+    'range' : (20,0,3.2),
+    'xaxis' : 'dphijjmet',
+    'fold'  : 3
+}
+
+variables['ht'] = {
+    'name'  : 'ht',
+    'range' : (40,0,200),
+    'xaxis' : 'ht [GeV]',
+    'fold'  : 3
+}
+
