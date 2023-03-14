17c190631256e004044a0548cfbe9563ec593857
ntrevisa <nicolo.trevisani@cern.ch>
2023-03-14 18:13:27 +0100

    DY CR 2016HIPM UL

--- Configurations/ControlRegions/DY/2016HIPM_v9/samples.py
+++ Configurations/ControlRegions/DY/2016HIPM_v9/samples.py
@@ -32,22 +32,16 @@ except NameError:
 ################# SKIMS ########################
 ################################################
 
-# MC:   /eos/cms/store/group/phys_higgs/cmshww/amassiro/HWWNano/Summer20UL16_106x_nAODv9_HIPM_Full2016v9/MCl1loose2016v9__MCCorr2016v9__l2tightOR2016v9/
+# MC:   /eos/cms/store/group/phys_higgs/cmshww/amassiro/HWWNano/Summer20UL16_106x_nAODv9_HIPM_Full2016v9/MCl1loose2016v9__MCCorr2016v9NoJERInHorn__l2tightOR2016v9/
 # DATA: /eos/cms/store/group/phys_higgs/cmshww/amassiro/HWWNano/Run2016_UL2016_nAODv9_HIPM_Full2016v9/DATAl1loose2016v9__l2loose__l2tightOR2016v9/
 
 mcProduction = 'Summer20UL16_106x_nAODv9_HIPM_Full2016v9'
 
-dataReco = 'Run2016_UL2016_nAODv9_HIPM_Full2016v9'
+dataReco     = 'Run2016_UL2016_nAODv9_HIPM_Full2016v9'
 
-# embedReco = 'Embedding2016_102X_nAODv7_Full2016v7'
+mcSteps      = 'MCl1loose2016v9__MCCorr2016v9NoJERInHorn__l2tightOR2016v9'
 
-mcSteps = 'MCl1loose2016v9__MCCorr2016v9__l2tightOR2016v9'
-
-# fakeSteps = 'DATAl1loose2016v7__l2loose__fakeW'
-
-dataSteps = 'DATAl1loose2016v9__l2loose__l2tightOR2016v9'
-
-# embedSteps = 'DATAl1loose2016v7__l2loose__l2tightOR2016v7__Embedding'
+dataSteps    = 'DATAl1loose2016v9__l2loose__l2tightOR2016v9'
 
 ##############################################
 ###### Tree base directory for the site ######
@@ -66,9 +60,7 @@ def makeMCDirectory(var=''):
         return os.path.join(treeBaseDir, mcProduction, mcSteps.format(var=''))
 
 mcDirectory = makeMCDirectory()
-# fakeDirectory = os.path.join(treeBaseDir, dataReco, fakeSteps)
 dataDirectory = os.path.join(treeBaseDir, dataReco, dataSteps)
-# embedDirectory = os.path.join(treeBaseDir, embedReco, embedSteps)
 
 ################################################
 ############ DATA DECLARATION ##################
@@ -101,8 +93,6 @@ DataTrig = {
 mcCommonWeightNoMatch = 'XSWeight*SFweight*METFilter_MC'
 mcCommonWeight = 'XSWeight*SFweight*METFilter_MC*PromptGenLepMatch2l'
 
-#mcCommonWeight = 'XSWeight*LepWPCut*(HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL || HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL || HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_DZ || HLT_Mu12_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ || HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL || HLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL || HLT_IsoTkMu24 || HLT_IsoMu24 || HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_DZ || HLT_Ele27_WPTight_Gsf || HLT_Ele25_eta2p1_WPTight_Gsf)*METFilter_MC'
-
 ###########################################
 #############  BACKGROUNDS  ###############
 ###########################################
@@ -120,133 +110,6 @@ samples['DY'] = {
     'weight': mcCommonWeight + '*( !(Sum$(PhotonGen_isPrompt==1 && PhotonGen_pt>15 && abs(PhotonGen_eta)<2.6) > 0))',
     'FilesPerJob': 2,
 }
-# addSampleWeight(samples,'DY','DYJetsToLL_M-50',    'DY_NLO_pTllrw')
-# addSampleWeight(samples,'DY','DYJetsToLL_M-10to50','DY_LO_pTllrw')
-
-
-##### Top #######
-
-files = nanoGetSampleFiles(mcDirectory, 'TTTo2L2Nu') + \
-        nanoGetSampleFiles(mcDirectory, 'ST_s-channel') + \
-        nanoGetSampleFiles(mcDirectory, 'ST_t-channel_top') + \
-        nanoGetSampleFiles(mcDirectory, 'ST_t-channel_antitop') + \
-        nanoGetSampleFiles(mcDirectory, 'ST_tW_antitop') + \
-        nanoGetSampleFiles(mcDirectory, 'ST_tW_top')
-
-samples['top'] = {
-    'name': files,
-    'weight': mcCommonWeight+embed_tautauveto,
-    'FilesPerJob': 1,
-}
-
-addSampleWeight(samples,'top','TTTo2L2Nu','Top_pTrw')
-
-###### WW ########
-
-samples['WW'] = {
-    'name': nanoGetSampleFiles(mcDirectory, 'WWTo2L2Nu'),
-    'weight': mcCommonWeight+embed_tautauveto + '*nllW', # temporary - nllW module not run on PS and UE variation samples
-    #'weight': mcCommonWeight + '*nllWOTF', # temporary
-    'FilesPerJob': 1
-}
-
-# Do we have WWewk samples??
-# samples['WWewk'] = {
-#     'name': nanoGetSampleFiles(mcDirectory, 'WpWmJJ_EWK_noTop'),
-#     'weight': mcCommonWeight+embed_tautauveto + '*(Sum$(abs(GenPart_pdgId)==6 || GenPart_pdgId==25)==0)*(lhe_mW1[0] > 60. && lhe_mW1[0] < 100. && lhe_mW2[0] > 60. && lhe_mW2[0] < 100.)', # Filter tops and Higgs, limit w mass
-#     'FilesPerJob': 4
-# }
-
-files = nanoGetSampleFiles(mcDirectory, 'GluGluToWWToENEN') + \
-        nanoGetSampleFiles(mcDirectory, 'GluGluToWWToENMN') + \
-        nanoGetSampleFiles(mcDirectory, 'GluGluToWWToENTN') + \
-        nanoGetSampleFiles(mcDirectory, 'GluGluToWWToMNEN') + \
-        nanoGetSampleFiles(mcDirectory, 'GluGluToWWToMNMN') + \
-        nanoGetSampleFiles(mcDirectory, 'GluGluToWWToMNTN') + \
-        nanoGetSampleFiles(mcDirectory, 'GluGluToWWToTNEN') + \
-        nanoGetSampleFiles(mcDirectory, 'GluGluToWWToTNMN') + \
-        nanoGetSampleFiles(mcDirectory, 'GluGluToWWToTNTN')
-
-samples['ggWW'] = {
-    'name': files,
-    'weight': mcCommonWeight+embed_tautauveto + '*1.53/1.4', # updating k-factor <-- do we still need the k-factor?
-    'FilesPerJob': 4
-}
-
-######## Vg ########
-
-files = nanoGetSampleFiles(mcDirectory, 'WGToLNuG') + \
-        nanoGetSampleFiles(mcDirectory, 'ZGToLLG')
-
-samples['Vg'] = {
-    'name': files,
-    'weight': mcCommonWeightNoMatch+embed_tautauveto + '*(!(Gen_ZGstar_mass > 0))',
-    'FilesPerJob': 4
-}
-
-######## VgS ########
-
-files = nanoGetSampleFiles(mcDirectory, 'WGToLNuG') + \
-        nanoGetSampleFiles(mcDirectory, 'ZGToLLG') + \
-        nanoGetSampleFiles(mcDirectory, 'WZTo3LNu_mllmin4p0') # WZTo3LNu_mllmin01_ext1
-
-samples['VgS'] = {
-    'name': files,
-    'weight': mcCommonWeight+embed_tautauveto + ' * (gstarLow * 0.94 + gstarHigh * 1.14)',
-    'FilesPerJob': 4,
-    'subsamples': {
-      'L': 'gstarLow',
-      'H': 'gstarHigh'
-    }
-}
-addSampleWeight(samples, 'VgS', 'WGToLNuG', '(Gen_ZGstar_mass > 0 && Gen_ZGstar_mass <= 4.0)')
-addSampleWeight(samples, 'VgS', 'ZGToLLG', '(Gen_ZGstar_mass > 0)')
-addSampleWeight(samples, 'VgS', 'WZTo3LNu_mllmin4p0', '(Gen_ZGstar_mass > 4.0)')
-
-# ############ VZ ############
-
-files = nanoGetSampleFiles(mcDirectory, 'ZZTo2L2Nu') + \
-        nanoGetSampleFiles(mcDirectory, 'ZZTo2Q2L_mllmin4p0') + \
-        nanoGetSampleFiles(mcDirectory, 'WZTo2Q2L_mllmin4p0') # 'WZTo2L2Q')
-
-samples['VZ'] = {
-    'name': files,
-    'weight': mcCommonWeight+embed_tautauveto + '*1.11',
-    'FilesPerJob': 4
-}
-
-########## VVV #########
-
-files = nanoGetSampleFiles(mcDirectory, 'ZZZ') + \
-        nanoGetSampleFiles(mcDirectory, 'WZZ') + \
-        nanoGetSampleFiles(mcDirectory, 'WWZ') + \
-        nanoGetSampleFiles(mcDirectory, 'WWW')
-#+ nanoGetSampleFiles(mcDirectory, 'WWG'), #should this be included? or is it already taken into account in the WW sample?
-
-samples['VVV'] = {
-    'name': files,
-    'weight': mcCommonWeight,
-    'FilesPerJob': 4
-}
-
-# ###########################################
-# ################## FAKE ###################
-# ###########################################
-
-# samples['Fake'] = {
-#   'name': [],
-#   'weight': 'METFilter_DATA*fakeW',
-#   'weights': [],
-#   'isData': ['all'],
-#   'FilesPerJob': 50
-# }
-
-# for _, sd in DataRun:
-#   for pd in DataSets:
-#     files = nanoGetSampleFiles(fakeDirectory, pd + '_' + sd)
-
-#     samples['Fake']['name'].extend(files)
-#     samples['Fake']['weights'].extend([DataTrig[pd]] * len(files))
 
 ###########################################
 ################## DATA ###################
@@ -262,9 +125,9 @@ samples['DATA'] = {
 
 for _, sd in DataRun:
   for pd in DataSets:
-    # files = nanoGetSampleFiles(dataDirectory, pd + '_' + sd)
+
     tag = pd + '_' + sd
-    if 'DoubleEG' in pd and 'Run2016B-ver2' in sd:  # Run2016B-ver2_HIPM_UL2016-v2
+    if 'DoubleEG' in pd and 'Run2016B-ver2' in sd:
         print("sd      = {}".format(sd))
         print("pd      = {}".format(pd))
         print("Old tag = {}".format(tag))
