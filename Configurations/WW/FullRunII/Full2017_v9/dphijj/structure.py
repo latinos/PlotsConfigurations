8e859fdb0136f19b216b3a3124c7845cf4f4a7ce
Susan Dittmer <Susan.Dittmer@cern.ch>
2023-05-30 11:17:43 +0200

    Updating WW dphijj to UL

---
+++ Configurations/WW/FullRunII/Full2017_v9/dphijj/structure.py
@@ -0,0 +1,22 @@
+# structure configuration for datacard
+# keys here must match keys in samples.py    
+structure ={}
+
+# keys here must match keys in samples.py 
+for iproc in samples.keys():
+    structure[iproc] = {
+        'isSignal' : 1 if 'WW_B' in iproc else 0,
+        'isData'   : 1 if iproc == 'DATA' else 0,
+    }
+
+structure['Vg']['removeFromCuts'] = ['ww2l2v_13TeV_sr_2j_B11']
+structure['WW_B10']['removeFromCuts'] = ['ww2l2v_13TeV_sr_2j_B6']
+structure['WW_B3']['removeFromCuts'] = ['ww2l2v_13TeV_sr_2j_B9']
+structure['WW_B5']['removeFromCuts'] = ['ww2l2v_13TeV_sr_2j_B1']
+structure['WW_B8']['removeFromCuts'] = ['ww2l2v_13TeV_sr_2j_B6']
+
+for nuis in nuisances.itervalues():
+  if 'cutspost' in nuis:
+    nuis['cuts'] = nuis['cutspost'](nuis, cuts)
+
+    print nuis
---
+++ Configurations/WW/FullRunII/Full2018_v9/dphijj/TheoUnc/configuration.py
@@ -0,0 +1,27 @@
+# Configuration file to produce initial root files -- has both merged and binned ggH samples
+
+treeName = 'Events'
+
+tag = 'WW2018_v9_dphijj_TheoUnc'
+
+# used by mkShape to define output directory for root files
+outputDir = 'rootFile'
+
+# file with TTree aliases
+aliasesFile = 'aliases.py'
+
+# file with list of variables
+variablesFile = 'variables.py'
+
+# file with list of cuts
+cutsFile = 'cuts.py' 
+
+# file with list of samples
+samplesFile = 'samples.py' 
+
+# luminosity to normalize to (in 1/fb)
+# https://github.com/latinos/LatinoAnalysis/blob/UL_production/NanoGardener/python/data/TrigMaker_cfg.py#L868 (874)
+lumi =  59.83
+
+# nuisances file for mkDatacards and for mkShape
+nuisancesFile = 'nuisances.py'
