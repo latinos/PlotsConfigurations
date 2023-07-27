df305c5694373a3703c49f0e60ac677cec75f432
ntrevisa <nicolo.trevisani@cern.ch>
2023-07-27 09:08:41 +0200

    Update cfg for BDT WHSS 2017

--- Configurations/WH_chargeAsymmetry/UL/Full2017_v9/BDTconfig_WHSS/README.md
+++ Configurations/WH_chargeAsymmetry/UL/Full2017_v9/BDTconfig_WHSS/README.md
@@ -11,7 +11,8 @@ We consider as signals the WH samples:
 And as backgrounds:
 - DY
 - WZ
-- WJets
+- WJets (fakes)
+- Top (fakes)
 
 The instructions to run the trainings follow.
 
@@ -20,15 +21,19 @@ The instructions to run the trainings follow.
 
     python ClassificationBDT_WHSS.py _WHSS_btagVariables_zveto_orthogonalsamples
 
+Using TTbar semileptonic as source of fakes:
+
+    condor_submit do_WHSS_training.jds
+
 ### Plot training results
 
     root -l -b -q 'plotAll.C("./","TMVA_WHSS","plots_BDT_WHSS","dataset_WHSS")'
-
     root -l -b -q 'plotAll.C("./","TMVA_WHSS_btagVariables_zveto_orthogonalsamples","plots_BDT_WHSS_2017_v9_btagVariables_zveto_orthogonalsamples","dataset_WHSS_btagVariables_zveto_orthogonalsamples")'
+    root -l -b -q 'plotAll.C("./","TMVA_WHSS_TTToSemiLeptonic","plots_BDT_WHSS_TTToSemiLeptonic","dataset_WHSS_TTToSemiLeptonic")'
 
 ### Files description
 
-In this directory, few files are used to prepare the configuration.
+In this directory, a few files are used to prepare the configuration.
 
 - `config_WHSS_cfg.py`:
     + List of variables for the training
--- Configurations/WH_chargeAsymmetry/UL/Full2017_v9/BDTconfig_WHSS/aliases.py
+++ Configurations/WH_chargeAsymmetry/UL/Full2017_v9/BDTconfig_WHSS/aliases.py
@@ -15,28 +15,29 @@ configurations = os.path.dirname(configurations) # Configurations
 mc     = [skey for skey in samples if skey not in ('Fake', 'DATA', 'Dyemb')]
 mc_emb = [skey for skey in samples if skey not in ('Fake', 'DATA')]
 
-# LepCut2l__ele_mvaFall17V2Iso_WP90_tthmva_70__mu_cut_Tight_HWWW_tthmva_80
-eleWP = 'mvaFall17V2Iso_WP90_tthmva_70'
-muWP  = 'cut_Tight_HWWW_tthmva_80'
+# LepCut2l__ele_mvaFall17V2Iso_WP90__mu_cut_Tight_HWWW
+eleWP = 'mvaFall17V2Iso_WP90'
+muWP  = 'cut_Tight_HWWW'
 
 aliases['LepWPCut'] = {
     'expr': 'LepCut2l__ele_mvaFall17V2Iso_WP90__mu_cut_Tight_HWWW*\
-             ( (abs(Lepton_pdgId[0])==11 || Muon_mvaTTH[Lepton_muonIdx[0]]>0.82) && (abs(Lepton_pdgId[1])==11 || Muon_mvaTTH[Lepton_muonIdx[1]]>0.82) \
-            && (abs(Lepton_pdgId[0])==13 || Lepton_mvaTTH_UL[0]>0.90)            && (abs(Lepton_pdgId[1])==13 || Lepton_mvaTTH_UL[1]>0.90) )',
+     ( ((abs(Lepton_pdgId[0])==13 && Muon_mvaTTH[Lepton_muonIdx[0]]>0.82) || (abs(Lepton_pdgId[0])==11 && Lepton_mvaTTH_UL[0]>0.90)) \
+    && ((abs(Lepton_pdgId[1])==13 && Muon_mvaTTH[Lepton_muonIdx[1]]>0.82) || (abs(Lepton_pdgId[1])==11 && Lepton_mvaTTH_UL[1]>0.90)) )',
     'samples': mc_emb + ['DATA']
 }
 
+# Standard leptons SFs
 aliases['LepWPSF'] = {
     'expr': 'LepSF2l__ele_'+eleWP+'__mu_'+muWP,
     'samples': mc_emb
 }
 
-# Fake leptons transfer factor
-aliases['fakeW'] = {
-    'linesToAdd' : ['.L %s/WH_chargeAsymmetry/UL/macros/fake_rate_reader.C+' % configurations],
-    'class'      : 'fake_rate_reader',
-    'args'       : ('2017', '90', '82', 0.90, 0.82, 'nominal', 2, 'std'),
-    'samples'    : ['Fake']
+# ttHMVA SFs
+aliases['LepWPttHMVASF'] = {
+    'linesToAdd' : ['.L %s/WH_chargeAsymmetry/UL/macros/ttHMVASF.C+' % configurations],
+    'class'      : 'ttHMVASF',
+    'args'       : ("2018", 2, "all", "nominal"),
+    'samples'    : mc_emb
 }
 
 # No jet with pt > 30 GeV
@@ -44,96 +45,96 @@ aliases['zeroJet'] = {
     'expr': 'Alt$(CleanJet_pt[0], 0) < 30.'
 }
 
-aliases['oneJet'] = {
-    'expr': 'Alt$(CleanJet_pt[0], 0) > 30.'
-}
+# aliases['oneJet'] = {
+#     'expr': 'Alt$(CleanJet_pt[0], 0) > 30. && Alt$(CleanJet_pt[1], 0) < 30.'
+# }
 
-aliases['multiJet'] = {
-    'expr': 'Alt$(CleanJet_pt[1], 0) > 30.'
-}
+# aliases['multiJet'] = {
+#     'expr': 'Alt$(CleanJet_pt[1], 0) > 30.'
+# }
 
-####################################################################################
-# b tagging WPs: https://twiki.cern.ch/twiki/bin/view/CMS/BtagRecommendation106XUL18
-####################################################################################
+# ####################################################################################
+# # b tagging WPs: https://twiki.cern.ch/twiki/bin/view/CMS/BtagRecommendation106XUL18
+# ####################################################################################
 
-# DeepB = DeepCSV
-bWP_loose_deepB  = '0.1208'
-bWP_medium_deepB = '0.4168' 
-bWP_tight_deepB  = '0.7665'
+# # DeepB = DeepCSV
+# bWP_loose_deepB  = '0.1355'
+# bWP_medium_deepB = '0.4506' 
+# bWP_tight_deepB  = '0.7738'
 
-# DeepFlavB = DeepJet
-bWP_loose_deepFlavB  = '0.0490'
-bWP_medium_deepFlavB = '0.2783'
-bWP_tight_deepFlavB  = '0.7100'
+# # DeepFlavB = DeepJet
+# bWP_loose_deepFlavB  = '0.0532'
+# bWP_medium_deepFlavB = '0.3040'
+# bWP_tight_deepFlavB  = '0.7476'
 
-# Actual algo and WP definition. BE CONSISTENT!!
-bAlgo = 'DeepFlavB'         # ['DeepB',        'DeepFlavB'         ]
-bWP   = bWP_loose_deepFlavB # [bWP_loose_deepB, bWP_loose_deepFlavB]
-bSF   = 'deepjet'           # ['deepcsv',      'deepjet'           ]
+# # Actual algo and WP definition. BE CONSISTENT!!
+# bAlgo = 'DeepB'         # ['DeepB',        'DeepFlavB'         ]
+# bWP   = bWP_medium_deep # [bWP_loose_deepB, bWP_loose_deepFlavB]
+# bSF   = 'deepcsv'       # ['deepcsv',      'deepjet'           ]
 
-# b veto
-aliases['bVeto'] = {
-    'expr': 'Sum$(CleanJet_pt > 20. && abs(CleanJet_eta) < 2.5 && Jet_btag{}[CleanJet_jetIdx] > {}) == 0'.format(bAlgo, bWP)
-}
+# # b veto
+# aliases['bVeto'] = {
+#     'expr': 'Sum$(CleanJet_pt > 20. && abs(CleanJet_eta) < 2.5 && Jet_btag{}[CleanJet_jetIdx] > {}) == 0'.format(bAlgo, bWP)
+# }
 
-aliases['bVetoSF'] = {
-    'expr': 'TMath::Exp(Sum$(TMath::Log((CleanJet_pt>20 && abs(CleanJet_eta)<2.5)*Jet_btagSF_{}_shape[CleanJet_jetIdx]+1*(CleanJet_pt<20 || abs(CleanJet_eta)>2.5))))'.format(bSF),
-    'samples': mc
-}
+# aliases['bVetoSF'] = {
+#     'expr': 'TMath::Exp(Sum$(TMath::Log((CleanJet_pt>20 && abs(CleanJet_eta)<2.5)*Jet_btagSF_{}_shape[CleanJet_jetIdx]+1*(CleanJet_pt<20 || abs(CleanJet_eta)>2.5))))'.format(bSF),
+#     'samples': mc
+# }
 
-# At least one b-tagged jet
-aliases['bReq'] = {
-    'expr': 'Sum$(CleanJet_pt > 30. && abs(CleanJet_eta) < 2.5 && Jet_btag{}[CleanJet_jetIdx] > {}) >= 1'.format(bAlgo, bWP)
-}
+# # At least one b-tagged jet
+# aliases['bReq'] = {
+#     'expr': 'Sum$(CleanJet_pt > 30. && abs(CleanJet_eta) < 2.5 && Jet_btag{}[CleanJet_jetIdx] > {}) >= 1'.format(bAlgo, bWP)
+# }
 
-aliases['bReqSF'] = {
-    'expr': 'TMath::Exp(Sum$(TMath::Log((CleanJet_pt>30 && abs(CleanJet_eta)<2.5)*Jet_btagSF_{}_shape[CleanJet_jetIdx]+1*(CleanJet_pt<30 || abs(CleanJet_eta)>2.5))))'.format(bSF),
-    'samples': mc
-}
+# aliases['bReqSF'] = {
+#     'expr': 'TMath::Exp(Sum$(TMath::Log((CleanJet_pt>30 && abs(CleanJet_eta)<2.5)*Jet_btagSF_{}_shape[CleanJet_jetIdx]+1*(CleanJet_pt<30 || abs(CleanJet_eta)>2.5))))'.format(bSF),
+#     'samples': mc
+# }
 
-# Top control region
-aliases['topcr'] = {
-    'expr': 'mtw2>30 && mll>50 && ((zeroJet && !bVeto) || bReq)'
-}
+# # Top control region
+# aliases['topcr'] = {
+#     'expr': 'mtw2>30 && mll>50 && ((zeroJet && !bVeto) || bReq)'
+# }
 
-# WW control region
-aliases['wwcr'] = {
-    'expr': 'mth>60 && mtw2>30 && mll>100 && bVeto'
-}
+# # WW control region
+# aliases['wwcr'] = {
+#     'expr': 'mth>60 && mtw2>30 && mll>100 && bVeto'
+# }
 
-# Overall b tag SF
-aliases['btagSF'] = {
-    'expr': '(bVeto || (topcr && zeroJet))*bVetoSF + (topcr && !zeroJet)*bReqSF',
-    'samples': mc
-}
+# # Overall b tag SF
+# aliases['btagSF'] = {
+#     'expr': '(bVeto || (topcr && zeroJet))*bVetoSF + (topcr && !zeroJet)*bReqSF',
+#     'samples': mc
+# }
 
-for shift in ['jes', 'lf', 'hf', 'lfstats1', 'lfstats2', 'hfstats1', 'hfstats2', 'cferr1', 'cferr2']:
-    for targ in ['bVeto', 'bReq']:
-        alias = aliases['%sSF%sup' % (targ, shift)] = copy.deepcopy(aliases['%sSF' % targ])
-        alias['expr'] = alias['expr'].replace('btagSF_{}_shape'.format(bSF), 'btagSF_{}_shape_up_{}'.format(bSF, shift))
+# for shift in ['jes', 'lf', 'hf', 'lfstats1', 'lfstats2', 'hfstats1', 'hfstats2', 'cferr1', 'cferr2']:
+#     for targ in ['bVeto', 'bReq']:
+#         alias = aliases['%sSF%sup' % (targ, shift)] = copy.deepcopy(aliases['%sSF' % targ])
+#         alias['expr'] = alias['expr'].replace('btagSF_{}_shape'.format(bSF), 'btagSF_{}_shape_up_{}'.format(bSF, shift))
 
-        alias = aliases['%sSF%sdown' % (targ, shift)] = copy.deepcopy(aliases['%sSF' % targ])
-        alias['expr'] = alias['expr'].replace('btagSF_{}_shape'.format(bSF), 'btagSF_{}_shape_down_{}'.format(bSF, shift))
+#         alias = aliases['%sSF%sdown' % (targ, shift)] = copy.deepcopy(aliases['%sSF' % targ])
+#         alias['expr'] = alias['expr'].replace('btagSF_{}_shape'.format(bSF), 'btagSF_{}_shape_down_{}'.format(bSF, shift))
 
-    aliases['btagSF%sup' % shift] = {
-        'expr': aliases['btagSF']['expr'].replace('SF', 'SF' + shift + 'up'),
-        'samples': mc
-    }
+#     aliases['btagSF%sup' % shift] = {
+#         'expr': aliases['btagSF']['expr'].replace('SF', 'SF' + shift + 'up'),
+#         'samples': mc
+#     }
 
-    aliases['btagSF%sdown' % shift] = {
-        'expr': aliases['btagSF']['expr'].replace('SF', 'SF' + shift + 'down'),
-        'samples': mc
-    }
+#     aliases['btagSF%sdown' % shift] = {
+#         'expr': aliases['btagSF']['expr'].replace('SF', 'SF' + shift + 'down'),
+#         'samples': mc
+#     }
 
-####################################################################################
-# End of b tagging pippone
-####################################################################################
+# ####################################################################################
+# # End of b tagging pippone
+# ####################################################################################
 
 
-aliases['gstarLow'] = {
-    'expr': 'Gen_ZGstar_mass > 0 && Gen_ZGstar_mass < 4',
-    'samples': 'WgS'
-}
+# aliases['gstarLow'] = {
+#     'expr': 'Gen_ZGstar_mass > 0 && Gen_ZGstar_mass < 4',
+#     'samples': 'WgS'
+# }
 
 aliases['gstarHigh'] = {
     'expr': 'Gen_ZGstar_mass < 0 || Gen_ZGstar_mass > 4',
@@ -147,18 +148,18 @@ aliases['PromptGenLepMatch2l'] = {
     'samples': mc
 }
 
-# PostProcessing did not create (anti)topGenPt for ST samples with _ext1
-lastcopy = (1 << 13)
+# # PostProcessing did not create (anti)topGenPt for ST samples with _ext1
+# lastcopy = (1 << 13)
 
-aliases['isTTbar'] = {
-    'expr': 'Sum$(TMath::Abs(GenPart_pdgId) == 6 && TMath::Odd(GenPart_statusFlags / %d)) == 2' % lastcopy,
-    'samples': ['top']
-}
+# aliases['isTTbar'] = {
+#     'expr': 'Sum$(TMath::Abs(GenPart_pdgId) == 6 && TMath::Odd(GenPart_statusFlags / %d)) == 2' % lastcopy,
+#     'samples': ['top']
+# }
 
-aliases['isSingleTop'] = {
-    'expr': 'Sum$(TMath::Abs(GenPart_pdgId) == 6 && TMath::Odd(GenPart_statusFlags / %d)) == 1' % lastcopy,
-    'samples': ['top']
-}
+# aliases['isSingleTop'] = {
+#     'expr': 'Sum$(TMath::Abs(GenPart_pdgId) == 6 && TMath::Odd(GenPart_statusFlags / %d)) == 1' % lastcopy,
+#     'samples': ['top']
+# }
 
 aliases['Top_pTrw'] = {
     'expr': 'isTTbar * (topGenPt * antitopGenPt > 0.) * (TMath::Sqrt((0.103*TMath::Exp(-0.0118*topGenPt) - 0.000134*topGenPt + 0.973) * (0.103*TMath::Exp(-0.0118*antitopGenPt) - 0.000134*antitopGenPt + 0.973))) + (topGenPt * antitopGenPt <= 0.)',
@@ -171,7 +172,7 @@ aliases['Top_pTrw'] = {
 
 # data/MC scale factors
 aliases['SFweight'] = {
-    'expr': ' * '.join(['SFweight2l', 'LepWPCut', 'LepWPSF','Jet_PUIDSF_loose', 'btagSF']),
+    'expr': ' * '.join(['SFweight2l', 'LepWPCut', 'LepWPSF','Jet_PUIDSF_loose', 'LepWPttHMVASF']),
     'samples': mc
 }
 
