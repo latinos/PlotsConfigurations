3637a1f765e1df1890cf3b071f3172a8a849af16
Michelle Hawksworth <mhawks2@uic.edu>
2023-08-15 16:39:31 +0200

    AZH plotting scripts update

--- Configurations/AToZH_Full/scripts/plot_contour_exclusion.py
+++ Configurations/AToZH_Full/scripts/plot_contour_exclusion.py
@@ -6,105 +6,94 @@ import re
 
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.tri import Traingulation, LinearTriInterpolator
-import mplhep as hep
+from matplotlib.tri import Triangulation, LinearTriInterpolator
+#import mplhep as hep
 
-from plot_utils import CMSSW_BASE
+#from plot_utils import CMSSW_BASE
 
-plt.style.use(hep.style.CMS)
+#plt.style.use(hep.style.CMS)
 
 
 class MassPlaneContourPlotter:
 
-    def __init__(self, year, run_name, channel, regions):
-	self.year = year
-	self.run_name = run_name
-	self.thdmc_brs = self.load_thdmc()
-	self.mass_points = self.load_mass_points()
+    def __init__(self, year, path_to_limits):#, channel, regions):
+        self.year = year
+        self.path_to_limits = path_to_limits
+        self.thdmc_brs = self.load_thdmc()
+        self.mass_points = self.load_mass_points()
 
     def load_thdmc(self):
-	with open("/afs/cern.ch/work/s/srudrabh/AZH/TheoryComputations/CMSSW_10_2_22/src/cp3_llbb/Calculators42HDM/AToZH_xsc_br_results.json", 'r') as f:
-	    return json.load(f)
-
+        with open(f"AToZH_xsc_br_results.json", 'r') as f:
+            return json.load(f)
 
     def load_limit(self, mA, mH):
-	pct = "50.0%"
-        fname = f"AsymptoticLimits_AZH_MA-{mA}_MH-{mH}_2DEllipses.log"
-	with open(os.path.join(CMSSW_BASE(), f"src/PlotsConfigurations/Configurations/AtoZH_Full/{self.year}_v9"), 'r') as f:
-	     limit_line = [x for x in f if pct in x][0]
-
-	limit_value = float(re.findall(r"\d.\d+", line.split("<")[1])[0])
-
+        with open(os.path.join(self.path_to_limits, "CL.log"), 'r') as f:
+            limit_vals = []
+            for line in f:
+                if f"{mA} {mH}" in line:
+                    limit_vals = np.array(line.split(" ")[2:],float)
+            return limit_vals[2]
 
         return limit_value
 
     def load_mass_points(self):
-	with open(os.path.join(CMSSW_BASE(), "src/PlotsConfigurations/Configurations/AtoZH_Full/scripts/signals.txt"), 'r') as f:
-	    mass_points = np.array([(MA, MH) for MA, MH in mass_points if MA < 1201 and MH < 1201])
-	    return mass_points
-
+        with open("signals.txt", 'r') as f:
+            mass_points = np.array([(int(re.findall(r"\d+", x)[0]), int(re.findall(r"\d+", x)[1])) for x in f])
+        return mass_points
 
     def compute_theory_value(self, mA, mH):
-	try:
-	   br_z_ll = 0.066
-	   br_h_tt = self.thdmc_brs[f"{mA},{mH}"]["HtottBR"]
-	   br_a_zh = self.thdmc_brs[f"{mA},{mH}"]["AtoZHBR"]
-	   xsec_a = self.thdmc_brs[f"{mA},{mH}"]["xsec_ggH"]
-	except KeyError:
-	   return 1
-	return xsec_a*br_a_zh*br_h_tt*br_z_ll
+        try:
+            br_z_ll = 0.066
+            br_h_tt = self.thdmc_brs[f"{mA},{mH}"]["HtottBR"]
+            br_a_zh = self.thdmc_brs[f"{mA},{mH}"]["AtoZHBR"]
+            xsec_a = self.thdmc_brs[f"{mA},{mH}"]["xsec_ggH"]
+        except KeyError:
+            return 1
+        return xsec_a*br_a_zh*br_h_tt*br_z_ll
 
     def plot_contour(self):
+        fig, ax = plt.subplots(figsize=(11, 8))
+        expected_values = np.array(
+            [self.load_limit(MA, MH) for MA, MH in zip(self.mass_points[:, 0], self.mass_points[:, 1])]
+        )
 
-	MA_MH = np.array(
-		[[MA, MH] for MA in range (500, 1200, 2) for MH in range(350 + 50 * int(MA / 900), MA - int(MA / 5), 2)]
-      )
-
-       fig, ax = plt.subplots(figsize=(11, 8))
-       expected_values = np.array(
-	       [self.load_limit(MA, MH) for MA, MH in zip(self.mass_points[:, 0], self.mass_points[:, 1])]
-	    )
-
-       triObj = Triangulation(self.mass_points[:, 0], self.mass_points[:, 1])
-       f_interpolate = LinearTriInterpolator(triObj, expected_values)
-       expected_values_interpolated = f_interpolate(MA_MH[:, 0], MA_MH[:, 1])
+        triObj = Triangulation(self.mass_points[:, 0], self.mass_points[:, 1])
+        f_interpolate = LinearTriInterpolator(triObj, expected_values)
+        expected_values_interpolated = f_interpolate(self.mass_points[:, 0], self.mass_points[:, 1])
 
 
-       theory_values = np.array(
-	       [self.compute_theory_value(MA, MH) for MA, MH in zip(MA_MH[:, 0], MA_MH[:, 1])]
-	     )
-       theory_limit_ratio = theory_values / expected_values_interpolated
-       countour = ax.tricontourf(MA_MH[:, 0], MA_MH[:, 1], theory_limit_ratio, levels=10, cmap="viridis")
-       ax.tricontour(MA_MH[:, 0], MA_MH[:, 1], theory_limit_ratio, levels=[0,1], colors="red")
-       ax.scatter(self.mass_points[:, 0], self.mass_points[:, 1], color="orange")
+        theory_values = np.array(
+            [self.compute_theory_value(MA, MH) for MA, MH in zip(self.mass_points[:, 0], self.mass_points[:, 1])]
+        )
+        theory_limit_ratio = theory_values / expected_values_interpolated
+        contour = ax.tricontourf(self.mass_points[:, 0], self.mass_points[:, 1], theory_limit_ratio, levels=10, cmap="viridis")
+        ax.tricontour(self.mass_points[:, 0], self.mass_points[:, 1], theory_limit_ratio, levels=[0,1], colors="red")
+        ax.scatter(self.mass_points[:, 0], self.mass_points[:, 1], color="orange", s=12)
 
-       fig.colorbar(contour, ax=ax, label=r"$\sigma_{Theory}/\sigma_{Expected}$")
-       ax.set_xlabel(r"m$_{A}$ [GeV]")
-       ax.set_ylabel(r"m$_{H}$ [GeV]")
-       ax.set_xlim(500, 1200)
-       ax.set_ylim(350, 1000)
+        fig.colorbar(contour, ax=ax, label=r"$\sigma_{Theory}/\sigma_{Expected}$")
+        ax.set_xlabel(r"m$_{A}$ [GeV]")
+        ax.set_ylabel(r"m$_{H}$ [GeV]")
+        ax.set_xlim(500, 2100)
+        ax.set_ylim(350, 2000)
 
-       os.makedirs("plot_output/contours", exist_ok=True)
-       fname = "mass_plane_contour_tanb1"
-       plt.savefig(f"plot_output/contours/{fname}.png")
-       plt.savefig(f"plot_output/contours/{fname}.pdf")
-       plt.close()
+        os.makedirs(os.path.join(self.path_to_limits, "plot_output/contours"), exist_ok=True)
+        fname = "mass_plane_contour_tanb1"
+        plt.savefig(os.path.join(self.path_to_limits, f"plot_output/contours/{fname}.png"))
+        plt.savefig(os.path.join(self.path_to_limits, f"plot_output/contours/{fname}.pdf"))
+        plt.close()
 
-if __name__ == "__main__"
-       parser = argparse.ArgumentParser()
-       parser.add_argument("run_name", type=str, help="Name of the directory containing the inputs.")
-       parser.add_argument("--year", type=str, help="all/UL16/UL17/UL18/combined")
-       args = parser.parse_args()
-
-
-       if args.year in ["UL16", "UL17", "UL18", "ULCombined"]:
-           YEARS = [args.year]
-       if args.year == "all":
-           YEARS = ["UL16", "UL17", "UL18", "ULCombined"]
-
-       for year in YEARS:
-           plotter = MassPlaneContourPlotter(year=year, run_name=args.run_name, channel=args.channel, regions=args.regions)
-           plotter.plot_contour()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--path_to_limits", type=str, help="Name of the directory containing the inputs.")
+    parser.add_argument("--year", type=str, help="all/UL16/UL17/UL18/combined")
+    args = parser.parse_args()
 
 
+    if args.year in ["UL16", "UL17", "UL18", "ULCombined"]:
+        YEARS = [args.year]
+    if args.year == "all":
+        YEARS = ["UL16", "UL17", "UL18", "ULCombined"]
 
+    for year in YEARS:
+        plotter = MassPlaneContourPlotter(year=year, path_to_limits=args.path_to_limits)
+        plotter.plot_contour()
