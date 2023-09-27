ffa582eda1b912c812d0d0d17dc122dc38169867
ntrevisa <nicolo.trevisani@cern.ch>
2023-09-27 16:00:36 +0200

    2016noHIPM WH3l impacts

--- Configurations/WH_chargeAsymmetry/UL/2016noHIPM_v9/WH3l_Mu82_EleUL90/do_optimize_cards.sh
+++ Configurations/WH_chargeAsymmetry/UL/2016noHIPM_v9/WH3l_Mu82_EleUL90/do_optimize_cards.sh
@@ -7,15 +7,17 @@ then
 else
   echo "We got some argument(s)"
   echo "==========================="
-  echo "Number of arguments.: $#"
-  echo "List of arguments...: $@"
-  echo "Arg #1: Variable    : $1"
+  echo "Number of arguments. : $#"
+  echo "List of arguments... : $@"
+  echo "Arg #1: Variable     : $1"
+  echo "Arg #2: Per-bin error: $2"
   echo "==========================="
   VAR=$1
+  ERR=$2
 fi
 
 # Example:
-# python Rebin.py datacards wh3l_13TeV_sssf_minus_pt2ge20 BDT_WH3l_OSSF_v9_more datacard_opt 0.25
+# python Rebin.py datacards wh3l_13TeV_sssf_minus_pt2ge20 BDT_WH3l_OSSF_v9_more datacard_opt 0.10
 
 ### Datacards with enhanced signal scaling
 ##########################################
@@ -23,23 +25,23 @@ fi
 ### SSSF
 
 # minus
-python Rebin.py datacards wh3l_13TeV_sssf_minus_pt2ge20 ${VAR/OSSF/SSSF} datacards_opt 0.25
-python Rebin.py datacards wh3l_13TeV_sssf_minus_pt2lt20 ${VAR/OSSF/SSSF} datacards_opt 0.25
+python Rebin.py datacards wh3l_13TeV_sssf_minus_pt2ge20 ${VAR/OSSF/SSSF} datacards_opt ${ERR}
+python Rebin.py datacards wh3l_13TeV_sssf_minus_pt2lt20 ${VAR/OSSF/SSSF} datacards_opt ${ERR}
 
 # plus
-python Rebin.py datacards wh3l_13TeV_sssf_plus_pt2ge20 ${VAR/OSSF/SSSF} datacards_opt 0.25
-python Rebin.py datacards wh3l_13TeV_sssf_plus_pt2lt20 ${VAR/OSSF/SSSF} datacards_opt 0.25
+python Rebin.py datacards wh3l_13TeV_sssf_plus_pt2ge20 ${VAR/OSSF/SSSF} datacards_opt ${ERR}
+python Rebin.py datacards wh3l_13TeV_sssf_plus_pt2lt20 ${VAR/OSSF/SSSF} datacards_opt ${ERR}
 
 
 ### OSSF
 
 # minus
-python Rebin.py datacards wh3l_13TeV_ossf_minus_pt2ge20 ${VAR} datacards_opt 0.25
-python Rebin.py datacards wh3l_13TeV_ossf_minus_pt2lt20 ${VAR} datacards_opt 0.25
+python Rebin.py datacards wh3l_13TeV_ossf_minus_pt2ge20 ${VAR} datacards_opt ${ERR}
+python Rebin.py datacards wh3l_13TeV_ossf_minus_pt2lt20 ${VAR} datacards_opt ${ERR}
 
 # plus
-python Rebin.py datacards wh3l_13TeV_ossf_plus_pt2ge20 ${VAR} datacards_opt 0.25
-python Rebin.py datacards wh3l_13TeV_ossf_plus_pt2lt20 ${VAR} datacards_opt 0.25
+python Rebin.py datacards wh3l_13TeV_ossf_plus_pt2ge20 ${VAR} datacards_opt ${ERR}
+python Rebin.py datacards wh3l_13TeV_ossf_plus_pt2lt20 ${VAR} datacards_opt ${ERR}
 
 
 ### Datacards with original signal scaling
@@ -48,20 +50,20 @@ python Rebin.py datacards wh3l_13TeV_ossf_plus_pt2lt20 ${VAR} datacards_opt 0.25
 ### SSSF
 
 # minus
-python Rebin.py datacards_original_signal_scale wh3l_13TeV_sssf_minus_pt2ge20 ${VAR/OSSF/SSSF} datacards_original_signal_scale_opt 0.25
-python Rebin.py datacards_original_signal_scale wh3l_13TeV_sssf_minus_pt2lt20 ${VAR/OSSF/SSSF} datacards_original_signal_scale_opt 0.25
+python Rebin.py datacards_original_signal_scale wh3l_13TeV_sssf_minus_pt2ge20 ${VAR/OSSF/SSSF} datacards_original_signal_scale_opt ${ERR}
+python Rebin.py datacards_original_signal_scale wh3l_13TeV_sssf_minus_pt2lt20 ${VAR/OSSF/SSSF} datacards_original_signal_scale_opt ${ERR}
 
 # plus
-python Rebin.py datacards_original_signal_scale wh3l_13TeV_sssf_plus_pt2ge20 ${VAR/OSSF/SSSF} datacards_original_signal_scale_opt 0.25
-python Rebin.py datacards_original_signal_scale wh3l_13TeV_sssf_plus_pt2lt20 ${VAR/OSSF/SSSF} datacards_original_signal_scale_opt 0.25
+python Rebin.py datacards_original_signal_scale wh3l_13TeV_sssf_plus_pt2ge20 ${VAR/OSSF/SSSF} datacards_original_signal_scale_opt ${ERR}
+python Rebin.py datacards_original_signal_scale wh3l_13TeV_sssf_plus_pt2lt20 ${VAR/OSSF/SSSF} datacards_original_signal_scale_opt ${ERR}
 
 
 ### OSSF
 
 # minus
-python Rebin.py datacards_original_signal_scale wh3l_13TeV_ossf_minus_pt2ge20 ${VAR} datacards_original_signal_scale_opt 0.25
-python Rebin.py datacards_original_signal_scale wh3l_13TeV_ossf_minus_pt2lt20 ${VAR} datacards_original_signal_scale_opt 0.25
+python Rebin.py datacards_original_signal_scale wh3l_13TeV_ossf_minus_pt2ge20 ${VAR} datacards_original_signal_scale_opt ${ERR}
+python Rebin.py datacards_original_signal_scale wh3l_13TeV_ossf_minus_pt2lt20 ${VAR} datacards_original_signal_scale_opt ${ERR}
 
 # plus
-python Rebin.py datacards_original_signal_scale wh3l_13TeV_ossf_plus_pt2ge20 ${VAR} datacards_original_signal_scale_opt 0.25
-python Rebin.py datacards_original_signal_scale wh3l_13TeV_ossf_plus_pt2lt20 ${VAR} datacards_original_signal_scale_opt 0.25
+python Rebin.py datacards_original_signal_scale wh3l_13TeV_ossf_plus_pt2ge20 ${VAR} datacards_original_signal_scale_opt ${ERR}
+python Rebin.py datacards_original_signal_scale wh3l_13TeV_ossf_plus_pt2lt20 ${VAR} datacards_original_signal_scale_opt ${ERR}
