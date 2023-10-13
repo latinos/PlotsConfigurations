7711ae8b85d71013fc7e94f35554fd969bb22c87
Michelle Hawksworth <mhawks2@uic.edu>
2023-10-13 18:56:36 +0200

    AZH 2017 separate ellipse variables for each SR

--- Configurations/AToZH_Full/2017_v9/variables_fit.py
+++ Configurations/AToZH_Full/2017_v9/variables_fit.py
@@ -14,7 +14,6 @@ variables['events']     = { 'name': '1',
                             'xaxis' : 'events', 
                             'fold' : 3
                         }
-
 variables['ellipse_mA_900_mH_400']          = { 'name'  : 'ellipse_mA_900_mH_400',
                                                 'range' : (7,-0.5,6.5),
                                                 'xaxis' : 'elliptical bin in mA-mH, pTZ',
@@ -45,10 +44,42 @@ variables['ellipse_mA_800_mH_600']          = { 'name'  : 'ellipse_mA_800_mH_600
                                                 'fold'  : 0
 }
 
+### onebjet ellipse variables ###
+
+variables['ellipse_onebjet_mA_900_mH_400']          = { 'name'  : 'ellipse_onebjet_mA_900_mH_400',
+                                                'range' : (7,-0.5,6.5),
+                                                'xaxis' : 'elliptical bin in mA-mH, pTZ',
+                                                'cuts'  : ['bveto_1j_SR'],
+						'samples' : bkg+['AZH_900_400'],
+                                               'fold'  : 0
+                                            }
+
+variables['ellipse_onebjet_mA_500_mH_350']          = { 'name'  : 'ellipse_onebjet_mA_500_mH_350',
+                                                'range' : (7,-0.5,6.5),
+                                                'xaxis' : 'elliptical bin in mA-mH, pTZ',
+                                                'cuts'  : ['bveto_1j_SR'],
+						'samples' : bkg+['AZH_500_350'],
+                                                'fold'  : 0
+                                            }
+variables['ellipse_onebjet_mA_1000_mH_600']          = { 'name'  : 'ellipse_onebjet_mA_1000_mH_600',
+                                                'range' : (7,-0.5,6.5),
+                                                'xaxis' : 'elliptical bin in mA-mH, pTZ',
+                                                'cuts'  : ['bveto_1j_SR'],
+						'samples' : bkg+['AZH_1000_600'],
+                                                'fold'  : 0
+                                           }
+variables['ellipse_onebjet_mA_800_mH_600']          = { 'name'  : 'ellipse_onebjet_mA_800_mH_600',
+                                                'range' : (7,-0.5,6.5),
+                                                'xaxis' : 'elliptical bin in mA-mH, pTZ',
+                                                'cuts'  : ['bveto_1j_SR'],
+						'samples' : bkg+['AZH_800_600'],
+                                                'fold'  : 0
+}
+
 variables['ptz']        = { 'name' : 'ZH3l_pTZ',
                             'range' : (6,0,1000),
                             'xaxis' : 'ptz',
-			    'cuts' : ['breq_SR'],
+			    'cuts' : ['breq_SR','bveto_1j_SR'],
                             'fold' : 3 
                         }
 
@@ -61,5 +92,11 @@ variables['DeltaM']  = { 'name' : 'AZH_mA_minus_mH_patch',
 			    'fold'  : 3
                          }
 
+variables['DeltaM_onebjet']  = { 'name' : 'AZH_mA_minus_mH_onebjet',
+                            'range' : (6,0,1000),
+			    'xaxis' : 'm_{A}-m_{H}',
+			    'cuts' : ['bveto_1j_SR'],
+			    'fold'  : 3
+                         }
 
 
