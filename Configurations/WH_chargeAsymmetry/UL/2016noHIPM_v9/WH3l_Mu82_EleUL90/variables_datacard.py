15709afe914cd285915a27f4d25314eda1098696
ntrevisa <nicolo.trevisani@cern.ch>
2024-01-11 11:37:45 +0100

    variables for datacards 2016noHIPM WH3l

---
+++ Configurations/WH_chargeAsymmetry/UL/2016noHIPM_v9/WH3l_Mu82_EleUL90/variables_datacard.py
@@ -0,0 +1,49 @@
+# variables
+
+#variables = {}
+    
+#'fold' : # 0 = not fold (default), 1 = fold underflowbin, 2 = fold overflow bin, 3 = fold underflow and overflow
+
+variables['events'] = {
+    'name'  : '1',
+    'range' : (1,0,2),
+    'xaxis' : 'events',
+    'fold'  : 3
+}
+
+# New training considering Top and Z+jets as Fakes
+variables['BDT_WH3l_OSSF_new_v9'] = { 
+    'name'     : 'BDT_WH3l_OSSF_new_v9',
+    'range'    : (40,-1.,1.),
+    'doWeight' : 1,
+    'binX'     : 1,
+    'binY'     : 40,
+    'xaxis'    : 'MVA discriminant',
+    'yaxis'    : 'Events',
+    'fold'     : 3,
+}
+
+variables['BDT_WH3l_SSSF_new_v9'] = { 
+    'name'     : 'BDT_WH3l_SSSF_new_v9',
+    'range'    : (40,-1.,1.),
+    'doWeight' : 1,
+    'binX'     : 1,
+    'binY'     : 40,
+    'xaxis'    : 'MVA discriminant',
+    'yaxis'    : 'Events',
+    'fold'     : 3,
+}
+
+variables['BDT_WH3l_OSSF_new_v9_100_bins'] = { 
+    'name': 'BDT_WH3l_OSSF_new_v9',
+    'range' : (100,-1.,1.),
+    'xaxis' : 'MVA discriminant',
+    'fold' : 3,
+}
+
+variables['BDT_WH3l_SSSF_new_v9_100_bins'] = { 
+    'name': 'BDT_WH3l_SSSF_new_v9',
+    'range' : (100,-1.,1.),
+    'xaxis' : 'MVA discriminant',
+    'fold' : 3,
+}
