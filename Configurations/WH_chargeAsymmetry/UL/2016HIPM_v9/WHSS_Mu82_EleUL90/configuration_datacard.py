a3f42f19681c97d3c7f237c1378d43f06d1847bd
ntrevisa <nicolo.trevisani@cern.ch>
2024-01-11 11:38:55 +0100

    configuration for datacards 2016HIPM WHSS

---
+++ Configurations/WH_chargeAsymmetry/UL/2016HIPM_v9/WHSS_Mu82_EleUL90/configuration_datacard.py
@@ -0,0 +1,41 @@
+# Configuration file for charge asymmetry WHSS analysis using the UL 2018 dataset
+
+treeName = 'Events'
+
+tag = 'WHSS_2016HIPM_v9_chargeAsymmetry_Mu82_EleUL90'
+
+# used by mkShape to define output directory for root files
+outputDir = 'rootFile'
+
+# file with TTree aliases
+aliasesFile = 'aliases.py'
+
+# file with list of variables
+variablesFile = 'variables_datacard.py'
+
+# file with list of cuts
+cutsFile = 'cuts.py' 
+
+# file with list of samples
+samplesFile = 'samples.py' 
+
+# file with list of samples
+plotFile = 'plot.py' 
+
+# luminosity to normalize to (in 1/fb)
+# https://github.com/latinos/LatinoAnalysis/blob/UL_production/NanoGardener/python/data/TrigMaker_cfg.py#L27 (#95 #165)
+# 16.802739097 + 1.063261220 + 1.655228036 = 19.521228353 
+lumi = 19.52
+
+# used by mkPlot to define output directory for plots
+# different from "outputDir" to do things more tidy
+outputDirPlots = 'plots_' + tag
+
+# used by mkDatacards to define output directory for datacards
+outputDirDatacard = 'datacards_DYflip'
+
+# structure file for datacard
+structureFile = 'structure_DYflip.py'
+
+# nuisances file for mkDatacards and for mkShape
+nuisancesFile = 'nuisances.py'
