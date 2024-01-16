0e8c3002ed770df0f0b27b0ea2faa70f1bd3b116
sahithi <srudra4@uic.edu>
2024-01-16 16:44:13 +0100

    AZH modifications to doFit scripts and added comb script

--- Configurations/AToZH_Full/scripts/doFit_condor_2016_HIPM.sh
+++ Configurations/AToZH_Full/scripts/doFit_condor_2016_HIPM.sh
@@ -10,23 +10,23 @@ cd /afs/cern.ch/work/s/srudrabh/AZH/postprocessing/combine/CMSSW_11_3_4/src/
 export SCRAM_ARCH=slc7_amd64_gcc700
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 eval `scramv1 runtime -sh`
-cd /afs/cern.ch/work/s/srudrabh/AZH/postprocessing/CMSSW_10_6_4/src/PlotsConfigurations/Configurations/AToZH_Full/2017_v9/
+cd /afs/cern.ch/work/s/srudrabh/AZH/postprocessing/CMSSW_10_6_4/src/PlotsConfigurations/Configurations/AToZH_Full/2016HIPM_v9/
 
 
-configFile='configuration_forDatacards.py'
+#configFile='configuration_forDatacards.py'
 
 ## scrape tag from configuration file and
 ## create parent directory to hold per-mass-point combine output
-tag=$(cat $configFile | awk '/tag =/ {print $3}' | tr -d \')
-datacardDir="datacards_condor_${tag}"
-mkdir higgsCombine_$tag
+#tag=$(cat $configFile | awk '/tag =/ {print $3}' | tr -d \')
+datacardDir="datacards"
+mkdir higgsCombine_2016HIPM_v9
 
 echo -e "\n\ndatacard directory: $datacardDir"
 echo -e "\nrunning the fit..."
 
 
 line=$((${1}+1))
-mass_point=$(sed "${line}q;d" ../scripts/signals_test.txt)
+mass_point=$(sed "${line}q;d" ../scripts/signals.txt)
 
 
 #------------------------------------------------------------------------------
@@ -37,30 +37,30 @@ echo "          next mass point = ($mA, $mH)"
 echo "--------------------------------------------------"
 
 
-xsec_mA_mH=$(cat ../scripts/total_xsec_results.json | jq ".[\"${mA},${mH}\"]")
+xsec_mA_mH=$(cat ../scripts/total_xsec_results.json | /usr/bin/jq ".[\"${mA},${mH}\"]")
 echo "xsec_mA_${mA}_mH_${mH} = ${xsec_mA_mH}"
 
 ### make workspaces
 breq_path="../breq_SR/ellipse_mA_${mA}_mH_${mH}/datacard.txt"
 bveto_1j_path="../bveto_1j_SR/ellipse_onebjet_mA_${mA}_mH_${mH}/datacard.txt"
 bveto_4j_path="../bveto_4j/events/datacard.txt"
-combinedPath="/afs/cern.ch/work/s/srudrabh/AZH/postprocessing/CMSSW_10_6_4/src/PlotsConfigurations/Configurations/AToZH_Full/2017_v9/${datacardDir}/mA_${mA}_mH_${mH}/combined_SR/"
+combinedPath="/afs/cern.ch/work/s/srudrabh/AZH/postprocessing/CMSSW_10_6_4/src/PlotsConfigurations/Configurations/AToZH_Full/2016HIPM_v9/${datacardDir}/mA_${mA}_mH_${mH}/combined_SR/"
 mkdir $combinedPath
 cd $combinedPath
-combineCards.py breq_SR=$breq_path bveto_1j_SR=$bveto_1j_path bveto_4j_path=$bveto_4j_path > datacard_test.txt
+combineCards.py breq_SR=$breq_path bveto_1j_SR=$bveto_1j_path bveto_4j_path=$bveto_4j_path > datacard.txt
 
-echo -e "\n{doFit.sh} >> text2workspace.py ${combinedPath}/datacard_test.txt -m 125 -o $combinedPath/workspace_test.root"
-text2workspace.py datacard_test.txt -m 125 -o workspace_test.root
+echo -e "\n{doFit.sh} >> text2workspace.py ${combinedPath}/datacard.txt -m 125 -o $combinedPath/workspace.root"
+text2workspace.py datacard.txt -m 125 -o workspace.root
 
 
 ### run AsymptoticLimits
-cd /afs/cern.ch/work/s/srudrabh/AZH/postprocessing/CMSSW_10_6_4/src/PlotsConfigurations/Configurations/AToZH_Full/2017_v9/higgsCombine_${tag}/
+cd /afs/cern.ch/work/s/srudrabh/AZH/postprocessing/CMSSW_10_6_4/src/PlotsConfigurations/Configurations/AToZH_Full/2016HIPM_v9/higgsCombine_2016HIPM_v9/
 fitTag="_AZH_mA${mA}_mH${mH}_SR"
 echo -e "\n\n{doFit.sh} >> combine -M AsymptoticLimits -m 125 --run blind -d ${combinedPath}/workspace.root -n $fitTag"
-combine -M AsymptoticLimits -m 125 --run blind -d ${combinedPath}/workspace_test.root -n $fitTag > CL_${mass_point}_test.log
-cat CL_${mass_point}_test.log
+combine -M AsymptoticLimits -m 125 --run blind -d ${combinedPath}/workspace.root -n $fitTag > CL_${mass_point}.log
+cat CL_${mass_point}.log
 echo -e "\n\n{doFit.sh} >> combine -M Significance -t -1 --expectSignal=${xsec_mA_mH} ${combinedPath}/workspace.root -n $fitTag"
-combine -M Significance -t -1 --expectSignal=${xsec_mA_mH} ${combinedPath}/workspace_test.root -n $fitTag > ExpSig_${mass_point}_test.log
-cat ExpSig_${mass_point}_test.log
+combine -M Significance -t -1 --expectSignal=${xsec_mA_mH} ${combinedPath}/workspace.root -n $fitTag > ExpSig_${mass_point}.log
+cat ExpSig_${mass_point}.log
 
 echo -e "\n\ndone :)"
---
+++ Configurations/AToZH_Full/scripts/doFit_condor_comb.sh
@@ -0,0 +1,70 @@
+#! /bin/sh
+#------------------------------------------------------------------------
+# usage: ./doFit.sh <year>  (year = 2018,2017,2016HIPM,2016noHIPM)
+#
+# NOTE: doFit.sh will use the given year's configuration.py and
+#       samples_all.py, so make sure they are up-to-date.
+#------------------------------------------------------------------------
+
+cd /afs/cern.ch/work/s/srudrabh/AZH/postprocessing/combine/CMSSW_11_3_4/src/ 
+export SCRAM_ARCH=slc7_amd64_gcc700
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+eval `scramv1 runtime -sh`
+cd /afs/cern.ch/work/s/srudrabh/AZH/postprocessing/CMSSW_10_6_4/src/PlotsConfigurations/Configurations/AToZH_Full/
+
+
+## scrape tag from configuration file and
+## create parent directory to hold per-mass-point combine output
+datacardDir2017="2017_v9/datacards_condor_AZH_2017_v9_btagSFpatch"
+datacardDir2018="2018_v9/datacards_condor_AZH_2018_v9_Full"
+datacardDir2016HIPM="2016HIPM_v9/datacards"
+datacardDir2016noHIPM="2016noHIPM_v9/datacards"
+mkdir /afs/cern.ch/work/s/srudrabh/AZH/postprocessing/CMSSW_10_6_4/src/PlotsConfigurations/Configurations/AToZH_Full/datacards_test
+datacardDircomb="datacards_test"
+mkdir higgsCombine_comb
+
+echo -e "\n\ndatacard directory: $datacardDir2017, $datacardDir2018, $datacardDir2016HIPM, $datacardDir2016noHIPM"
+echo -e "\nrunning the fit..."
+
+
+line=$((${1}+1))
+mass_point=$(sed "${line}q;d" /afs/cern.ch/work/s/srudrabh/AZH/postprocessing/CMSSW_10_6_4/src/PlotsConfigurations/Configurations/AToZH_Full/scripts/signals_test.txt)
+
+
+#------------------------------------------------------------------------------
+mA=$(echo ${mass_point} | awk -F 'MA-' '{print $2}' | awk -F '_MH-' '{print $1}')
+mH=$(echo ${mass_point} | awk -F '_MH-' '{print $2}' | awk -F "')" '{print $1}')
+echo -e "\n\n--------------------------------------------------"
+echo "          next mass point = ($mA, $mH)"
+echo "--------------------------------------------------"
+
+
+xsec_mA_mH=$(cat /afs/cern.ch/work/s/srudrabh/AZH/postprocessing/CMSSW_10_6_4/src/PlotsConfigurations/Configurations/AToZH_Full/scripts/total_xsec_results.json | jq ".[\"${mA},${mH}\"]")
+echo "xsec_mA_${mA}_mH_${mH} = ${xsec_mA_mH}"
+
+### make workspaces
+combinedPath2017="${datacardDir2017}/mA_${mA}_mH_${mH}/combined_SR/datacard_test.txt"
+combinedPath2018="${datacardDir2018}/mA_${mA}_mH_${mH}/combined_SR/datacard_test.txt"
+combinedPath2016HIPM="${datacardDir2016HIPM}/mA_${mA}_mH_${mH}/combined_SR/datacard_test.txt"
+combinedPath2016noHIPM="${datacardDir2016noHIPM}/mA_${mA}_mH_${mH}/combined_SR/datacard_test.txt"
+combinedPath="${datacardDircomb}/mA_${mA}_mH_${mH}/"
+
+mkdir $combinedPath
+#cd $combinedPath
+combineCards.py combined2017=$combinedPath2017 combined2018=$combinedPath2018 combined2016HIPM=$combinedPath2016HIPM combined2016noHIPM=$combinedPath2016noHIPM > $combinedPath/datacard.txt
+
+echo -e "\n{doFit.sh} >> text2workspace.py ${combinedPath}/datacard.txt -m 125 -o $combinedPath/workspace.root"
+text2workspace.py $combinedPath/datacard.txt -m 125 -o ${combinedPath}/workspace.root
+
+
+### run AsymptoticLimits
+cd higgsCombine_comb/
+fitTag="_AZH_mA${mA}_mH${mH}_SR"
+echo -e "\n\n{doFit.sh} >> combine -M AsymptoticLimits -m 125 --run blind -d ${combinedPath}/workspace.root -n $fitTag"
+combine -M AsymptoticLimits -m 125 --run blind -d ../${combinedPath}/workspace.root -n $fitTag > CL_${mass_point}.log
+cat CL_${mass_point}.log
+echo -e "\n\n{doFit.sh} >> combine -M Significance -t -1 --expectSignal=${xsec_mA_mH} ${combinedPath}/workspace.root -n $fitTag"
+combine -M Significance -t -1 --expectSignal=${xsec_mA_mH} ../${combinedPath}/workspace.root -n $fitTag > ExpSig_${mass_point}.log
+cat ExpSig_${mass_point}.log
+
+echo -e "\n\ndone :)"
