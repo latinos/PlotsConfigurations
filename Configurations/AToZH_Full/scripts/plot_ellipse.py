5c1daaf0afd5a7433a42f3186d6da5d9e5b02cb2
sahithi <srudra4@uic.edu>
2024-03-27 00:30:11 +0100

    AZH script to plot the 2D distribution with ellipses overlaid

--- Configurations/AToZH_Full/scripts/plot_ellipse.py
+++ Configurations/AToZH_Full/scripts/plot_ellipse.py
@@ -1,59 +1,47 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.patches as mpatches
-import ROOT 
+import ROOT
 from array import array
+import matplotlib.patches as mpatches
+
+_file0 = ROOT.TFile.Open("../2018_v9/rootFiles_AZH_2018_v9_Full/plots_AZH_2018_v9_Full.root")
+h_input = _file0.Get("breq_SR/ptzVSdeltam/histo_DATA")
 
 
-fig, ax = plt.subplots(figsize=(11, 8))
-
-ellipse_1 = mpatches.Ellipse(xy=(93.77776,161.33923689584662), width=71.4033550304184, height =147.93717045091483, angle=145.9668747907025, ec='r', fc="None")
-ellipse_2 = mpatches.Ellipse(xy=(93.77776,161.33923689584662), width=110.10273722366176, height =228.11655554322616, angle=145.9668747907025, ec='r', fc="None")
-ellipse_3 = mpatches.Ellipse(xy=(93.77776,161.33923689584662), width=145.79778650243574, height =302.0714080450197, angle=145.9668747907025, ec='r', fc="None")
-ellipse_4 = mpatches.Ellipse(xy=(93.77776,161.33923689584662), width=180.6478383786047, height =374.27555114770297, angle=145.9668747907025, ec='r', fc="None")
-ellipse_5 = mpatches.Ellipse(xy=(93.77776,161.33923689584662), width=215.2826467834335, height =446.033741674425, angle=145.9668747907025, ec='r', fc="None")
-ellipse_6 = mpatches.Ellipse(xy=(93.77776,161.33923689584662), width=249.926805724941, height =517.8113051275012, angle=145.9668747907025, ec='r', fc="None")
+xbinedges = array('d', [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, 210, 220, 230, 240, 250, 260, 270, 280, 290, 300, 350, 400, 450, 500, 550, 600, 700, 800, 900, 1000])
+ybinedges = array('d', [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, 210, 220, 230, 240, 250, 260, 270, 280, 290, 300, 350, 400, 450, 500, 550, 600, 700, 800, 900, 1000])
 
 
-file0 = ROOT.TFile.Open("../2018_v9/rootFiles_AZH_2018_v9_Full/plots_AZH_2018_v9_Full.root")
-#pairVec = [(900, 400), (900, 370), (700, 400), (700, 370), (700, 350),
-#	               (500, 400), (500, 350), (1200, 850), (1200, 1000), (1000, 600), (800, 600)]
-h_input = file0.Get("breq_SR/ptzVSdeltam/histo_AZH_500_350")
-
-xbinedges = array('d', [0,10,20,30,40,50,60,70,80,90,100,110,120,130,140,150,160,170,180,190,200,210,220,230,240,250,260,270,280,290,300,350,400,450,500,550,600,700,800,900,1000])
-ybinedges = array('d', [0,10,20,30,40,50,60,70,80,90,100,110,120,130,140,150,160,170,180,190,200,210,220,230,240,250,260,270,280,290,300,350,400,450,500,550,600,700,800,900,1000])
-h_output = ROOT.TH2D("h_output", "PtZ vs DeltaM", 40, xbinedges, 40, ybinedges)
-
-hist_array = np.zeros((h_output.GetNbinsX(), h_output.GetNbinsY()))
-
-for ibinx in xrange(1, 41):
-    for ibiny in xrange(1, 41):
-        h_output.SetBinContent(ibinx, ibiny, h_input.GetBinContent(ibinx + 40 * (ibiny - 1)))
-
-for i in range(1, h_output.GetNbinsX() + 1):
-    for j in range(1, h_output.GetNbinsY() + 1):
-        hist_array[i - 1, j - 1] = h_output.GetBinContent(i, j)
-
-xbins = [h_output.GetXaxis().GetBinLowEdge(i) for i in range(1, h_output.GetNbinsX() + 2)]
-ybins = [h_output.GetYaxis().GetBinLowEdge(i) for i in range(1, h_output.GetNbinsX() + 2)]
+h_output = ROOT.TH2D("h_output", "PtZ vs DeltaM", len(xbinedges)-1, xbinedges, len(ybinedges)-1, ybinedges)
 
 
-im = ax.imshow(hist_array, extent=[xbins[0], xbins[-1], ybins[0], ybins[-1]], origin='lower', cmap='viridis')
+for ibinx in range(1, len(xbinedges)):
+        for ibiny in range(1, len(ybinedges)):
+	            h_output.SetBinContent(ibinx, ibiny, h_input.GetBinContent(ibinx + (len(xbinedges)-1) * (ibiny - 1)))
 
-ax.add_patch(ellipse_1)
-ax.add_patch(ellipse_2)
-ax.add_patch(ellipse_3)
-ax.add_patch(ellipse_4)
-ax.add_patch(ellipse_5)
-ax.add_patch(ellipse_6)
-
-cbar = plt.colorbar(im, ax=ax)
+plt.figure(figsize=(8, 6))
+ROOT.gStyle.SetPalette(55)
+plt.pcolormesh(xbinedges, ybinedges, np.array([[h_output.GetBinContent(ix, iy) for ix in range(1, h_output.GetNbinsX()+1)] for iy in range(1, h_output.GetNbinsY()+1)]), cmap='viridis')
+plt.xlabel("PtZ")
+plt.ylabel("DeltaM")
+plt.title("PtZ vs DeltaM")
 
 
-ax.set_xlabel(r"$p_{T,Z}$")
-ax.set_ylabel(r"$\Delta m$")
-#ax.set_xlim(0,400)
-#ax.set_ylim(0,1200)
-plt.title("2018 mA500_mH350 Ellipses, Fakes")
-plt.savefig("/eos/user/s/srudrabh/AZH/mA500_mH350_Fakes_ellipses_2018.png")
+
+ellipse_1 = mpatches.Ellipse(xy=(93.77776,161.33923689584662), width=71.4033550304184, height=147.93717045091483, angle=145.9668747907025, ec='r', fc="None")
+ellipse_2 = mpatches.Ellipse(xy=(93.77776,161.33923689584662), width=110.10273722366176, height=228.11655554322616, angle=145.9668747907025, ec='r', fc="None")
+ellipse_3 = mpatches.Ellipse(xy=(93.77776,161.33923689584662), width=145.79778650243574, height=302.0714080450197, angle=145.9668747907025, ec='r', fc="None")
+ellipse_4 = mpatches.Ellipse(xy=(93.77776,161.33923689584662), width=180.6478383786047, height=374.27555114770297, angle=145.9668747907025, ec='r', fc="None")
+ellipse_5 = mpatches.Ellipse(xy=(93.77776,161.33923689584662), width=215.2826467834335, height=446.033741674425, angle=145.9668747907025, ec='r', fc="None")
+ellipse_6 = mpatches.Ellipse(xy=(93.77776,161.33923689584662), width=249.926805724941, height=517.8113051275012, angle=145.9668747907025, ec='r', fc="None")
+
+plt.gca().add_patch(ellipse_1)
+plt.gca().add_patch(ellipse_2)
+plt.gca().add_patch(ellipse_3)
+plt.gca().add_patch(ellipse_4)
+plt.gca().add_patch(ellipse_5)
+plt.gca().add_patch(ellipse_6)
+#plt.xlim(0, 500)
+#plt.ylim(0, 800)
+plt.colorbar(label='Bin Content')
 plt.show()
