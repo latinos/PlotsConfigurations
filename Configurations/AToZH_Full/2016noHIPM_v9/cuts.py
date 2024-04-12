2b247085661f34a709037d4af79b1b2623307699
sahithi <srudra4@uic.edu>
2024-04-12 06:14:35 +0200

    AZH bug fix

--- Configurations/AToZH_Full/2016noHIPM_v9/cuts.py
+++ Configurations/AToZH_Full/2016noHIPM_v9/cuts.py
@@ -19,7 +19,7 @@ cuts['MET'] = 'PuppiMET_pt > 40 \
 cuts['jet_cut_3j'] = ' Alt$( CleanJet_pt[0], 0) > 30 \
                     && Alt$( CleanJet_pt[1], 0) > 30 \
                     && Alt$( CleanJet_pt[2], 0) > 30 \
-		    && Alt$( CleanJet_pt[3], 0) < 30 \
+                    && Alt$( CleanJet_pt[3], 0) < 30 \
                     && PuppiMET_pt > 40 \
                     && WH3l_ZVeto < 25 \
                     && bReq \
@@ -53,11 +53,11 @@ cuts['bveto_4j'] = ' Alt$( CleanJet_pt[0], 0) > 30 \
 cuts['bveto_3j'] = ' Alt$( CleanJet_pt[0], 0) > 30 \
                && Alt$( CleanJet_pt[1], 0) > 30 \
                && Alt$( CleanJet_pt[2], 0) > 30 \
-	       && Alt$( CleanJet_pt[3], 0) < 30 \
+               && Alt$( CleanJet_pt[3], 0) < 30 \
                && PuppiMET_pt > 40 \
                && WH3l_ZVeto < 25 \
                && bVeto \
-	       '
+               '
 
 
 cuts['bveto_1j_SR'] = ' Alt$( CleanJet_pt[0], 0) > 30 \
