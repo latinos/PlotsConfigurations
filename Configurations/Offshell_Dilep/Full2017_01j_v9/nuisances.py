e1e6cd72ac48eb7605908ec435483b5976dcf266
Taylor Carnahan <71470193+tbcarnahan@users.noreply.github.com>
2024-05-28 15:34:44 -0500

    QCDscale_ggH

--- Configurations/Offshell_Dilep/Full2017_01j_v9/nuisances.py
+++ Configurations/Offshell_Dilep/Full2017_01j_v9/nuisances.py
@@ -542,7 +542,7 @@ nuisances['QCDscale_VBF']  = {
 }
 
 nuisances['QCDscale_ggH']  = {
-               'name'  : 'QCDscale_VBF',
+               'name'  : 'QCDscale_ggH',
                 #'kind'  : 'weight_envelope', ##TC
                 'type'  : 'lnN', ##TC
                 'samples'  : {
