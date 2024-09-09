6a7b021950881cb566b1c089f485096d71d5dafa
Taylor Carnahan <71470193+tbcarnahan@users.noreply.github.com>
2024-09-09 14:10:18 -0500

    Range change + symmetrization

--- Configurations/Offshell_Dilep/Full2018_1j_v9/nuisances.py
+++ Configurations/Offshell_Dilep/Full2018_1j_v9/nuisances.py
@@ -507,7 +507,7 @@ nuisances['pdf_WW']  = {
 }
 
 # PDF eigenvariations for WW and top
-for i in range(1,101):
+for i in range(1,33): ##
   # LHEPdfWeight are PDF4LHC variations, while nominal is NNPDF.
   # LHEPdfWeight[i] reweights from NNPDF nominal to PDF4LHC member i
   # LHEPdfWeight[0] in particular reweights from NNPDF nominal to PDF4LHC nominal
@@ -533,6 +533,7 @@ for i in range(1,101):
     'samples'  : {
       'top'   : pdf_variations,
     },
+      'symmetrize' : True,
   }
 
   # nuisances['pdf_ggH_eigen'+str(i)]  = {
@@ -556,6 +557,7 @@ for i in range(1,101):
       'qqH_sonly_on'   : pdf_variations, 
       'qqH_sonly_off'   : pdf_variations,
     },
+      'symmetrize' : True,
   }
   nuisances['pdf_VBF_bonly_eigen'+str(i)]  = {
     'name'  : 'CMS_hww_pdf_VBF_sonly_eigen'+str(i),
@@ -566,6 +568,7 @@ for i in range(1,101):
       'qqH_bonly_on'   : pdf_variations,  
       'qqH_bonly_off'   : pdf_variations,
     },
+      'symmetrize' : True,
   }
   nuisances['pdf_VBF_sand_eigen'+str(i)]  = {
     'name'  : 'CMS_hww_pdf_VBF_sand_eigen'+str(i),
@@ -576,6 +579,7 @@ for i in range(1,101):
       'qqH_sand_on'   : pdf_variations,  
       'qqH_sand_off'   : pdf_variations,
     },
+      'symmetrize' : True,
   }
 
 
