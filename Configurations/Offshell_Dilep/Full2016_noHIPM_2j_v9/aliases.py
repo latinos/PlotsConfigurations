f5fa452e30494147698d107c899e9c4c3abb7b12
Pedro J. Fernández Manteca <pedro.fernandez.manteca@gmail.com>
2025-01-30 13:21:19 +0100

    Jan 30 update

--- Configurations/Offshell_Dilep/Full2016_noHIPM_2j_v9/aliases.py
+++ Configurations/Offshell_Dilep/Full2016_noHIPM_2j_v9/aliases.py
@@ -83,7 +83,7 @@ aliases['LepWPCut'] = {
 }
 
 aliases['ttHMVAULSF'] = {
-    'linesToAdd' : ['.L %s/WH_chargeAsymmetry/UL/macros/ttHMVASF.C+' % configurations],
+    'linesToAdd' : ['.L %s/WW/FullRunII/ttHMVASF.C+' % configurations],
     'class'      : 'ttHMVASF',
     'args'       : ("2016noHIPM", 2, "all","nominal"),
     'samples'    : mc
@@ -168,11 +168,6 @@ aliases['nCleanGenJet'] = {
     'samples': mc
 }
 
-#aliases['fiducial'] = {
-#    'linesToAdd': ['.L %s/WW/FullRunII/fiducial.cc+' % configurations],
-#    'class': 'FiducialRegion',
-#    'samples': mc
-#}
 
 ##### DY Z pT reweighting
 ##### TEMP this also needs updating
