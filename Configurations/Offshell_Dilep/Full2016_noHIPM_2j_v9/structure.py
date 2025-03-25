e7736cbae9afde8beff82f2bb4c0a0ac6b6a9594
Pedro J. Fernández Manteca <pedro.fernandez.manteca@gmail.com>
2025-03-25 15:18:07 +0100

    update March 25th

--- Configurations/Offshell_Dilep/Full2016_noHIPM_2j_v9/structure.py
+++ Configurations/Offshell_Dilep/Full2016_noHIPM_2j_v9/structure.py
@@ -1,39 +1,12 @@
 # structure configuration for datacard
 # keys here must match keys in samples.py    
-#structure ={}
-
-
-scale_factors = {"Vg" : 3.9035767061 / 3.8063594225,
-"qqH_bonly_off" : 27.7433156162 / 28.2590589168,
-"VVV" : 28.2907251665 / 29.5304570527,
-"top" : 54038.9194473 / 53104.1320988, ## INCLUDES RATE PARAM,
-"Higgs" : 44.549770773 / 45.9865981303,
-"qqH_bonly_on" : 0.00276089968126 / 0.0028127842788,
-"WW" : 960.392081125 / 979.345541808,  ## INCLUDES RATE PARAM,
-"qqH_sand_on" : 14.670112283 / 14.7811660015,
-"ggH_bonly_on" : 0.343154478776 / 0.351274435229,
-"ZZ" : 2.37336338422 / 2.54473982269,
-"DY" : 138.813827264 / 141.70471594,
-"ggH_bonly_off" : 61.935859296 / 62.8639780522,
-"WZ" : 95.5187839122 / 104.520400309,
-"qqH_sonly_on" : 14.6680199401 / 14.7790623693,
-"qqH_sand_off" : 24.4376256872 / 24.9199964557,
-"ggH_sand_on" : 4.39610772285 / 4.49782612178,
-"ggH_sonly_on" : 4.47430047849 / 4.53456981711,
-"ggH_sonly_off" : 7.1573030726 / 7.25539232023,
-"qqH_sonly_off" : 2.61896848289 / 2.64707459257,
-"ggH_sand_off" : 55.0583494601 / 55.8954029555,
-"Fake_em" : 1,
-"Fake_me" : 1,
-"DATA" : 1
-}
+structure ={}
 
 # keys here must match keys in samples.py 
 for iproc in samples.keys():
     structure[iproc] = {
-        'isSignal' : 1 if ('Offshell' in iproc or 'qqH_sonly_off' in iproc or 'qqH_bonly_off'  in iproc or 'qqH_sonly_on'  in iproc) else 0,
+        'isSignal' : 1 if ('Offshell' in iproc or 'qqH_sonly_off' in iproc or 'qqH_sand_off'  in iproc or 'ggH_sonly_off'  in iproc or 'ggH_sand_off'  in iproc) else 0, ##TC 2024 have sand which we don't count as signal, so we need no double counting...and qqH not independent samples!
         'isData'   : 1 if iproc == 'DATA' else 0,
-        'scaleSampleForDatacard' : scale_factors[iproc],
     }
 
 for nuis in nuisances.itervalues():
