e9356f93f5bec51ebfd7b42bae9101803fba16e8
Susan Dittmer <Susan.Dittmer@cern.ch>
2025-07-11 21:22:10 +0200

    WW diff updating wwpt

--- Configurations/WW/FullRunII/Tools/wwpt.cc
+++ Configurations/WW/FullRunII/Tools/wwpt.cc
@@ -23,12 +23,16 @@ public:
 protected:
   void bindTree_(multidraw::FunctionLibrary&) override;
 
-   FloatArrayReader* DressedLepton_pt{};
-   FloatArrayReader* DressedLepton_eta{};
-   FloatArrayReader* DressedLepton_phi{};
-   FloatArrayReader* DressedLepton_mass{};
-   FloatValueReader* GenMET_pt;
-   FloatValueReader* GenMET_phi;
+  UIntValueReader* nLeptonGen;
+  BoolArrayReader* LeptonGen_isPrompt;
+  IntArrayReader* DressedLepton_pdgId;
+  FloatArrayReader* DressedLepton_pt;
+  FloatArrayReader* DressedLepton_eta;
+  FloatArrayReader* DressedLepton_phi;
+  FloatArrayReader* DressedLepton_mass;
+  FloatValueReader* GenMET_pt;
+  FloatValueReader* GenMET_phi;
+  IntArrayReader* LeptonGen_MotherPID;
 };
 
 WWPT::WWPT() :
@@ -39,18 +43,42 @@ WWPT::WWPT() :
 double
 WWPT::evaluate(unsigned)
 {
+  unsigned nL{*nLeptonGen->Get()};
+  if (nL < 2)
+    return -999.;
+
+  std::vector<unsigned> iPromptL{};
+  iPromptL.reserve(nL);
+
+  for (unsigned iL{0}; iL != nL; ++iL) {
+    if (!LeptonGen_isPrompt->At(iL))
+      continue;
+
+    unsigned tauId{static_cast<unsigned>(std::abs(LeptonGen_MotherPID->At(iL)))};
+    if (tauId == 15)
+      continue; 
+ 
+    unsigned absId{static_cast<unsigned>(std::abs(DressedLepton_pdgId->At(iL)))};
+    if (absId != 11 && absId != 13)
+      continue;
+
+    iPromptL.push_back(iL);
+  }
+
+  if (iPromptL.size() < 2)
+    return -999.; // false
 
   ROOT::Math::PtEtaPhiMVector pl0(
-    DressedLepton_pt->At(0),
-    DressedLepton_eta->At(0),
-    DressedLepton_phi->At(0),
-    DressedLepton_mass->At(0)
+    DressedLepton_pt->At(iPromptL[0]),
+    DressedLepton_eta->At(iPromptL[0]),
+    DressedLepton_phi->At(iPromptL[0]),
+    DressedLepton_mass->At(iPromptL[0])
   );
   ROOT::Math::PtEtaPhiMVector pl1(
-    DressedLepton_pt->At(1),
-    DressedLepton_eta->At(1),
-    DressedLepton_phi->At(1),
-    DressedLepton_mass->At(1)
+    DressedLepton_pt->At(iPromptL[1]),
+    DressedLepton_eta->At(iPromptL[1]),
+    DressedLepton_phi->At(iPromptL[1]),
+    DressedLepton_mass->At(iPromptL[1])
   );
 
   double genMet{*GenMET_pt->Get()};
@@ -70,10 +98,15 @@ WWPT::evaluate(unsigned)
 void
 WWPT::bindTree_(multidraw::FunctionLibrary& _library)
 {
+  _library.bindBranch(nLeptonGen, "nLeptonGen");
+  _library.bindBranch(LeptonGen_isPrompt, "LeptonGen_isPrompt");
+  _library.bindBranch(DressedLepton_pdgId, "DressedLepton_pdgId");
   _library.bindBranch(DressedLepton_pt, "DressedLepton_pt");
   _library.bindBranch(DressedLepton_eta, "DressedLepton_eta");
   _library.bindBranch(DressedLepton_phi, "DressedLepton_phi");
   _library.bindBranch(DressedLepton_mass, "DressedLepton_mass");
   _library.bindBranch(GenMET_pt, "GenMET_pt");
   _library.bindBranch(GenMET_phi, "GenMET_phi");
+  _library.bindBranch(LeptonGen_MotherPID, "LeptonGen_MotherPID");
+
 }
